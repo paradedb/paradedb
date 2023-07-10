a1fc82eaeba9d4b0acd68f883cdab0638b8deb3d
Mauricio Araujo <mauricio.araujo@proton.me>
2023-07-10 15:57:44 -0600

    Fix mypy errors

--- core/sdk/pipeline.py
+++ core/sdk/pipeline.py
@@ -160,7 +160,7 @@ class Pipeline:
 
         progress_bar.close()
 
-    def pipe_real_time(self):
+    def pipe_real_time(self) -> None:
         source_conn = self.source.parse_connection_string()
         sink_conn = self.sink.dict()
         index = self.target.index_name
@@ -181,5 +181,5 @@ class Pipeline:
         )
         start_worker()
 
-    def teardown(self):
+    def teardown(self) -> None:
         self.extractor.teardown()

6ba84afa8e35176c2814869c0b4496509f0b02b0
Ming Ying <ming.ying.nyc@gmail.com>
2023-07-10 15:02:05 -0700

    fix: Don't run sentence_transformer test in GH action

--- core/transform/tests/test_sentence_transformers.py
+++ core/transform/tests/test_sentence_transformers.py
@@ -1,35 +1,35 @@
 import pytest
 import numpy as np
-from unittest.mock import patch, Mock, MagicMock
+
+from unittest.mock import patch
 from typing import List
-import importlib
+from core.transform.sentence_transformers import SentenceTransformerEmbedding
 
 
-@patch("sentence_transformers.SentenceTransformer", autospec=True)
-def test_sentence_transformer_embedding_initialization(mocked_transformer):
+def test_sentence_transformer_embedding_initialization():
     model = "all-MiniLM-L6-v2"
-    SentenceTransformerEmbedding = importlib.import_module(
-        "core.transform.sentence_transformers"
-    ).SentenceTransformerEmbedding
-    sentence_transformers_embedding = SentenceTransformerEmbedding(model)
-    assert sentence_transformers_embedding.model is not None
+
+    with patch(
+        "core.transform.sentence_transformers.SentenceTransformer"
+    ) as MockedTransformer:
+        sentence_transformers_embedding = SentenceTransformerEmbedding(model)
+
+        assert sentence_transformers_embedding.model is not None
 
 
-@patch.dict("sys.modules", {"core.transform.sentence_transformers": MagicMock()})
 def test_sentence_transformer_create_embeddings():
-    from core.transform.sentence_transformers import SentenceTransformerEmbedding
-
     model_name = "all-MiniLM-L6-v2"
     documents = ["doc"]
     mock_embeddings = np.array([[0.1, 0.2, 0.3]])
 
-    mock_instance = MagicMock()
-    mock_instance.create_embeddings.return_value = mock_embeddings.tolist()
+    with patch(
+        "core.transform.sentence_transformers.SentenceTransformer"
+    ) as MockedTransformer:
+        mock_instance = MockedTransformer.return_value
+        mock_instance.encode.return_value = mock_embeddings
 
-    SentenceTransformerEmbedding.return_value = mock_instance
+        sentence_transformer_embedding = SentenceTransformerEmbedding(model_name)
+        result = sentence_transformer_embedding.create_embeddings(documents)
 
-    embedding = SentenceTransformerEmbedding(model_name)
-    result = embedding.create_embeddings(documents)
-
-    mock_instance.create_embeddings.assert_called_once_with(documents)
-    assert np.array_equal(result, mock_embeddings.tolist())
+        mock_instance.encode.assert_called_once_with(documents)
+        assert result == mock_embeddings.tolist()

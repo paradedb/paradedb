5b3c7e929d5f6883b0f3868e24b87c12b30ff5e1
Mauricio Araujo <mauricio.araujo@proton.me>
2023-07-10 13:42:20 -0600

    Add configuration layer

--- realtime/app.py
+++ realtime/app.py
@@ -1,7 +1,9 @@
 import numpy
+import os
 import requests
 import json
 from asyncio import get_event_loop, ensure_future
+from config import SourceConfig, KafkaConfig, SinkConfig
 from confluent_kafka import Producer
 from confluent_kafka.serialization import SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
@@ -10,21 +12,18 @@ from core.transform import embedding as tf
 from faust import App, Worker
 from typing import Callable
 
-app = App("realtime", broker="kafka://localhost:9094", value_serializer="raw")
-kafka_bootstrap_servers = "localhost:9094"
-kafka_connect_host = "http://localhost:8083"
-schema_registry_host = "http://localhost:8081"
+kafka_config = KafkaConfig()
+sink_config = SinkConfig()
+app = App(
+    "realtime",
+    broker=f"kafka://{kafka_config.bootstrap_servers}",
+    value_serializer="raw",
+)
 
-# TODO: find a better way to set this. Since we create
-# the connector in kafka connect, it needs to use the internal
-# hostname of the database container.
-docker_db_host = "postgres"
-docker_sink_host = "http://elastic:9200"
-docker_schema_registry_host = "http://schema-registry:8081"
 
 def create_source_connector(conn: dict, schema_name: str, relation: str):
     try:
-        url = f"{kafka_connect_host}/connectors"
+        url = f"{kafka_config.connect_server}/connectors"
         r = requests.post(
             url,
             json={
@@ -33,7 +32,7 @@ def create_source_connector(conn: dict, schema_name: str, relation: str):
                     "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
                     "plugin.name": "pgoutput",
                     "value.converter": "org.apache.kafka.connect.json.JsonConverter",  # TODO: support avro
-                    "database.hostname": f"{docker_db_host}",
+                    "database.hostname": f'{conn["host"]}',
                     "database.port": f'{conn["port"]}',
                     "database.user": f'{conn["user"]}',
                     "database.password": f'{conn["password"]}',
@@ -54,36 +53,43 @@ def create_source_connector(conn: dict, schema_name: str, relation: str):
 
 def register_sink_value_schema(index: str) -> int:
     schema_str = """
+    {
+    "name": "embedding",
+    "type": "record",
+    "fields": [
         {
-            "name": "embedding",
-            "type": "record",
-            "fields": [
-                {
-                    "name": "doc",
-                    "type": {
-                        "type": "array",
-                        "items": "float"
-                    }
-                }
-            ]
+        "name": "doc",
+        "type": {
+            "type": "array",
+            "items": "float"
         }
+        },
+        {
+        "name": "metadata",
+        "type": {
+            "type": "array",
+            "items": "string"
+        },
+        "default": []
+        }
+    ]
+    }
     """
     avro_schema = Schema(schema_str, "AVRO")
-    sr = SchemaRegistryClient({"url": schema_registry_host})
+    sr = SchemaRegistryClient({"url": kafka_config.schema_registry_external_server})
     schema_id = sr.register_schema(f"{index}-value", avro_schema)
     return schema_id
 
 
-def return_schema(
-    schema_registry_client: SchemaRegistryClient, schema_id: int
-) -> str:
-    # The result is cached so subsequent attempts will not require an additional round-trip to the Schema Registry.
+def return_schema(schema_registry_client: SchemaRegistryClient, schema_id: int) -> str:
+    # The result is cached so subsequent attempts will not
+    # require an additional round-trip to the Schema Registry.
     return schema_registry_client.get_schema(schema_id).schema_str
 
 
 def create_sink_connector(conn: dict, index: str):
     try:
-        url = f"{kafka_connect_host}/connectors"
+        url = f"{kafka_config.connect_server}/connectors"
         r = requests.post(
             url,
             json={
@@ -94,8 +100,8 @@ def create_sink_connector(conn: dict, index: str):
                     "key.ignore": "true",
                     "name": "sink-connector",
                     "value.converter": "io.confluent.connect.avro.AvroConverter",
-                    "value.converter.schema.registry.url": f"{docker_schema_registry_host}",
-                    "connection.url": f"{docker_sink_host}",
+                    "value.converter.schema.registry.url": f"{kafka_config.schema_registry_internal_server}",
+                    "connection.url": f"{sink_config.server}",
                     "connection.username": f'{conn["user"]}',
                     "connection.password": f'{conn["password"]}',
                 },
@@ -115,12 +121,13 @@ def register_agents(
     metadata_fn: Callable,
 ):
     source_topic = app.topic(topic, value_serializer="raw")
-    sr_client = SchemaRegistryClient({"url": schema_registry_host})
+    sr_client = SchemaRegistryClient(
+        {"url": kafka_config.schema_registry_external_server}
+    )
     schema_str = return_schema(sr_client, schema_id)
     avro_serializer = AvroSerializer(sr_client, schema_str)
-    producer_conf = {'bootstrap.servers': kafka_bootstrap_servers}
+    producer_conf = {"bootstrap.servers": kafka_config.bootstrap_servers}
     producer = Producer(producer_conf)
-
 
     @app.agent(source_topic)
     async def process_records(records):
@@ -132,19 +139,19 @@ def register_agents(
                 if payload["__deleted"] == "true":
                     print("record was deleted, removing embedding...")
                 else:
-                    # TODO: Detect when update or new record
+                    # TODO: Make distinction when update or new record
                     payload.pop("__deleted")
                     document = transform_fn(*payload)
                     metadata = metadata_fn(*payload)
                     embedding = embedding_fn(document)
 
-                    print(schema_str)
-                    message = {
-                        'doc': embedding.tolist()
-                    }
-                    print(embedding)
-                    producer.produce(topic=index,
-                    value=avro_serializer(message, SerializationContext(topic, MessageField.VALUE)))
+                    message = {"doc": embedding.tolist(), "metadata": metadata}
+                    producer.produce(
+                        topic=index,
+                        value=avro_serializer(
+                            message, SerializationContext(topic, MessageField.VALUE)
+                        ),
+                    )
 
 
 def start_worker():

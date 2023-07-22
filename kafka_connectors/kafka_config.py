6b900763b918b7292a13ca3658dd9dad2acf80ef
Mauricio Araujo <mauricio.araujo@proton.me>
2023-07-22 14:52:05 -0600

    Remove http sink references

--- kafka_connectors/kafka_config.py
+++ kafka_connectors/kafka_config.py
@@ -7,9 +7,6 @@ schema_registry_host = os.environ.get("SCHEMA_REGISTRY_HOST")
 schema_registry_port = os.environ.get("SCHEMA_REGISTRY_PORT")
 connect_host = os.environ.get("KAFKA_CONNECT_HOST")
 connect_port = os.environ.get("KAFKA_CONNECT_PORT")
-http_sink_host = os.environ.get("HTTP_SINK_HOST")
-http_sink_port = os.environ.get("HTTP_SINK_PORT")
-http_sink_topic = os.environ.get("HTTP_SINK_TOPIC")
 
 if not (
     kafka_host
@@ -52,13 +49,3 @@ class KafkaConfig(Config):
         host = self.get_property("KAFKA_CONNECT_HOST")
         port = self.get_property("KAFKA_CONNECT_PORT")
         return f"http://{host}:{port}"
-
-    @property
-    def http_sink_server(self) -> str:
-        host = self.get_property("HTTP_SINK_HOST")
-        port = self.get_property("HTTP_SINK_PORT")
-        return f"http://{host}:{port}"
-
-    @property
-    def http_sink_topic(self) -> str:
-        return self.get_property("HTTP_SINK_TOPIC")

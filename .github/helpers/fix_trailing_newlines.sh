aa4a63961a5b216c9e71e62763340a4ea10b573d
Philippe Noël <philippemnoel@gmail.com>
2023-07-22 20:55:55 -0700

    mega anthills PR

---
+++ .github/helpers/fix_trailing_newlines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [[ -f "$1" ]]; then
+  echo -n "$1"
+  if (diff /dev/null "$1" || true) | tail -1 | grep -q '^\\ No newline'; then
+    echo >> "$1"
+    echo "...fixed"
+  else
+    echo ""
+  fi
+fi

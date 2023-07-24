b537baec9aaabc578b3a5728cc3bb37383642326
Ming Ying <ming.ying.nyc@gmail.com>
2023-07-24 15:35:27 -0700

    fix: Expose enums like WeaviateVectorizer to the user

--- retake/__init__.py
+++ retake/__init__.py
@@ -2,7 +2,12 @@ from core.sdk.source import Source
 from core.sdk.transform import Transform
 from core.sdk.embedding import Embedding
 from core.sdk.sink import Sink
-from core.sdk.target import Target
+from core.sdk.target import (
+    Target,
+    ElasticSimilarity,
+    QdrantSimilarity,
+    WeaviateVectorizer,
+)
 from core.sdk.pipeline import Pipeline
 from core.sdk.realtime import RealtimeServer
 
@@ -14,4 +19,7 @@ __all__ = [
     "Target",
     "Pipeline",
     "RealtimeServer",
+    "ElasticSimilarity",
+    "QdrantSimilarity",
+    "WeaviateVectorizer",
 ]

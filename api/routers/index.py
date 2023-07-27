b09b9caccc7357c1f75b93f6be683d0d757c431d
Ming Ying <ming.ying.nyc@gmail.com>
2023-07-27 12:50:02 -0700

    fix: Bring back old add_source endpoint to not break things

--- api/routers/index.py
+++ api/routers/index.py
@@ -82,8 +82,50 @@ async def create_index(payload: IndexCreatePayload) -> JSONResponse:
         )
 
 
-@router.post(f"/{tag}/realtime/link", tags=[tag])
+@router.post(f"/{tag}/add_source", tags=[tag])
 async def add_source(payload: AddSourcePayload) -> JSONResponse:
+    # Number of rows to extract at once
+    BATCH_SIZE = 500
+
+    try:
+        index = client.get_index(payload.index_name)
+
+        extractor = PostgresExtractor(
+            host=payload.source_host,
+            port=payload.source_port,
+            user=payload.source_user,
+            password=payload.source_password,
+        )
+
+        if payload.source_neural_columns:
+            index.register_neural_search_fields(payload.source_neural_columns)
+
+        for chunk in extractor.extract_all(
+            relation=payload.source_relation,
+            columns=payload.source_columns,
+            primary_key=payload.source_primary_key,
+            chunk_size=BATCH_SIZE,
+        ):
+            rows = chunk.get("rows")
+            primary_keys = chunk.get("primary_keys")
+
+            if rows and primary_keys:
+                print("upserting", rows)
+                index.upsert(documents=rows, ids=primary_keys)
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=f"Source {payload.source_relation} linked to index {payload.index_name} successfully",
+        )
+    except Exception as e:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=str(e),
+        )
+
+
+@router.post(f"/{tag}/realtime/link", tags=[tag])
+async def realtime_link(payload: AddSourcePayload) -> JSONResponse:
     try:
         index = client.get_index(payload.index_name)
 

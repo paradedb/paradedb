f36346bf2310581f51b7d98bdfb16a4a728272ae
Ming Ying <ming.ying.nyc@gmail.com>
2023-07-30 15:05:02 -0700

    wip: Define field types as Enum

--- core/search/index.py
+++ core/search/index.py
@@ -6,7 +6,7 @@ from loguru import logger
 from opensearchpy import OpenSearch, helpers
 from typing import Dict, List, Optional, Any, Union, cast
 
-from core.search.index_mappings import IndexMappings
+from core.search.index_mappings import IndexMappings, FieldType
 from core.search.index_settings import IndexSettings
 from core.search.model_group import ModelGroup
 from core.search.model import Model
@@ -73,7 +73,7 @@ class Index:
         knn_vector_properties = []
 
         for prop, prop_data in properties.items():
-            if prop_data.get("type") == "knn_vector":
+            if prop_data.get("type") == FieldType.KNN_VECTOR.value:
                 knn_vector_properties.append(prop)
 
         return knn_vector_properties
@@ -199,10 +199,14 @@ class Index:
             self.mappings.upsert(
                 properties={
                     f"{field}{reserved_embedding_field_name_ending}": {
-                        "type": "knn_vector",
+                        "type": FieldType.KNN_VECTOR.value,
                         "dimension": default_model_dimensions,
                         "method": {"name": "hnsw", "engine": "lucene"},
                     }
                     for field in fields
                 }
             )
+
+    def set_field_types(self, field_types: Dict[str, str]) -> None:
+        properties = {key: {"type": value} for key, value in field_types.items()}
+        self.mappings.upsert(properties=properties)
--- core/search/index_mappings.py
+++ core/search/index_mappings.py
@@ -1,6 +1,54 @@
+from enum import Enum
 from opensearchpy import OpenSearch
 from typing import Dict, Any
 
+class FieldType(Enum):
+    # Core Data Types
+    TEXT = "text"
+    KEYWORD = "keyword"
+    INTEGER = "integer"
+    LONG = "long"
+    SHORT = "short"
+    BYTE = "byte"
+    DOUBLE = "double"
+    FLOAT = "float"
+    HALF_FLOAT = "half_float"
+    SCALED_FLOAT = "scaled_float"
+    DATE = "date"
+    BOOLEAN = "boolean"
+    BINARY = "binary"
+
+    # Complex Data Types
+    OBJECT = "object"
+    NESTED = "nested"
+
+    # Geo Data Types
+    GEO_POINT = "geo_point"
+    GEO_SHAPE = "geo_shape"
+
+    # Specialized Data Types
+    IP = "ip"
+    COMPLETION = "completion"
+    TOKEN_COUNT = "token_count"
+    CUSTOM = "custom"
+    SEARCH_AS_YOU_TYPE = "search_as_you_type"
+    CONSTANT_KEYWORD = "constant_keyword"
+    DENSE_VECTOR = "dense_vector"
+    SPARSE_VECTOR = "sparse_vector"
+    KNN_VECTOR = "knn_vector"
+    RANK_FEATURE = "rank_feature"
+    RANK_FEATURES = "rank_features"
+    PERCOLATOR = "percolator"
+
+    # Range Data Types
+    INTEGER_RANGE = "integer_range"
+    FLOAT_RANGE = "float_range"
+    LONG_RANGE = "long_range"
+    DATE_RANGE = "date_range"
+    DOUBLE_RANGE = "double_range"
+
+    # Join Data Types
+    JOIN = "join"
 
 class IndexMappings:
     def __init__(self, name: str, client: OpenSearch):

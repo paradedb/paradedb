0fcbc7316ca6b47d3a6294e18a22745f0834ed8c
Philippe Noël <philippemnoel@gmail.com>
2023-07-30 14:31:47 -0700

    clarify tihngs

--- tests/conftest.py
+++ tests/conftest.py
@@ -98,12 +98,12 @@ def opensearch_service_and_fastapi_client(docker_ip, docker_services):
     """Ensure that OpenSearch & FastAPI services are up and responsive."""
 
     print("\nSpinning up OpenSearch service...")
-    port = docker_services.port_for("core", 9200)
-    url = f"https://{docker_ip}:{port}"
+    os_port = docker_services.port_for("core", 9200)
+    os_url = f"https://{docker_ip}:{os_port}"
 
     print(f"Waiting for OpenSearch service at {url} to be responsive...")
     docker_services.wait_until_responsive(
-        timeout=90.0, pause=1, check=lambda: is_opensearch_responsive(url)
+        timeout=90.0, pause=1, check=lambda: is_opensearch_responsive(os_url)
     )
     # We need to wait another 60-90 seconds for it to initialize, after the
     # Docker container is responsive
@@ -113,11 +113,11 @@ def opensearch_service_and_fastapi_client(docker_ip, docker_services):
     print("\nSpinning up FastAPI service...")
     test_api_key = "retake-test-key"
 
-    port = docker_services.port_for("api", 8000)
-    url = f"http://{docker_ip}:{port}"
-    ping_url = f"{url}/ping"
+    fastapi_port = docker_services.port_for("api", 8000)
+    fastapi_url = f"http://{docker_ip}:{fastapi_port}"
+    ping_url = f"{fastapi_url}/ping"
 
-    print(f"Waiting for FastAPI service at {url} to be responsive...")
+    print(f"Waiting for FastAPI service at {fastapi_url} to be responsive...")
     docker_services.wait_until_responsive(
         timeout=90.0,
         pause=1,
@@ -125,4 +125,4 @@ def opensearch_service_and_fastapi_client(docker_ip, docker_services):
     )
     print("FastAPI service is responsive!")
 
-    return Client(api_key=test_api_key, url=url)
+    return Client(api_key=test_api_key, url=fastapi_url)

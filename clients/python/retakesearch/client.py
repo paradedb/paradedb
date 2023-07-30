a76a215b6df8ac60622aeab2b7395fffe5bf890c
Ming Ying <ming.ying.nyc@gmail.com>
2023-07-30 14:08:10 -0700

    feat: Introduce delete index to python client

--- clients/python/retakesearch/client.py
+++ clients/python/retakesearch/client.py
@@ -21,7 +21,7 @@ class Client:
             if response.status_code == 200:
                 return Index(index_name=index_name, api_key=self.api_key, url=self.url)
             else:
-                return None
+                raise Exception(response.text)
 
     def create_index(self, index_name: str) -> Optional[Index]:
         with httpx.Client(timeout=None) as http:
@@ -36,4 +36,17 @@ class Client:
             if response.status_code == 200:
                 return Index(index_name=index_name, api_key=self.api_key, url=self.url)
             else:
-                return None
+                raise Exception(response.text)
+
+    def delete_index(self, index_name: str) -> None:
+        with httpx.Client(timeout=None) as http:
+            response = http.post(
+                f"{self.url}/index/delete",
+                headers={
+                    "Authorization": f"Bearer {self.api_key}",
+                    "Content-Type": "application/json",
+                },
+                json={"index_name": index_name},
+            )
+            if not response.status_code == 200:
+                raise Exception(response.text)

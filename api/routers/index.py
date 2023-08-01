f3a6f50178b1a534f22b4e6406f8d972f0f03d19
Mauricio Araujo <mauricio.araujo@proton.me>
2023-08-01 17:03:20 -0600

    refactor: use pgsync for real time sync (#143)
    Co-authored-by: Ming Ying <ming.ying.nyc@gmail.com>

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, BackgroundTasks, status
+import requests
+from fastapi import APIRouter, status
 from loguru import logger
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 from typing import List, Dict, Any, Union
 
-from api.config.kafka import KafkaConfig
 from api.config.opensearch import OpenSearchConfig
+from api.config.pgsync import PgSyncConfig
 
-from core.extract.postgres import PostgresExtractor, ConnectionError
-from core.kafka.consumer import KafkaConsumer
+from core.extract.postgres import PostgresExtractor
 from core.search.client import Client
 from core.search.index_mappings import FieldType
 
@@ -16,7 +16,8 @@ tag = "index"
 
 router = APIRouter()
 opensearch_config = OpenSearchConfig()
-kafka_consumer = KafkaConsumer()
+pgsync_config = PgSyncConfig()
+
 client = Client(
     host=opensearch_config.host,
     port=opensearch_config.port,
@@ -67,6 +68,11 @@ class CreateFieldPayload(BaseModel):
     index_name: str
     field_name: str
     field_type: str
+
+
+class SyncPayload(BaseModel):
+    source: AddSourcePayload
+    pgsync_schema: Dict[str, Any]
 
 
 @router.get("/index/{index_name}", tags=[tag])
@@ -175,91 +181,35 @@ async def add_source(payload: AddSourcePayload) -> JSONResponse:
 
 
 @router.post(f"/{tag}/realtime/link", tags=[tag])
-async def realtime_link(payload: AddSourcePayload) -> JSONResponse:
+async def realtime_link(payload: SyncPayload) -> JSONResponse:
     try:
-        extractor = PostgresExtractor(
-            host=payload.source_host,
-            port=payload.source_port,
-            user=payload.source_user,
-            password=payload.source_password,
-            dbname=payload.source_dbname,
-            schema_name=payload.source_schema_name,
-        )
-        logger.info("Successfully setup extractor")
+        # Make sure all values are strings
+        source = {}
+        for k, v in payload.source.dict().items():
+            if isinstance(v, str):
+                source[k] = v
+            else:
+                source[k] = str(v)
 
-        # Validate that relation, columns, and primary key are valid
-        extractor.validate(
-            payload.source_relation, payload.source_columns, payload.source_primary_key
-        )
-
-        kafka_config = KafkaConfig()
-        extractor.extract_real_time(
-            kafka_config.connect_server,
-            kafka_config.schema_registry_server,
-            payload.source_relation,
-            payload.source_primary_key,
-            payload.source_columns,
-        )
-        logger.info("Created connector. Waiting for it to be ready...")
-
-        if extractor.is_connector_ready(
-            kafka_config.connect_server, payload.source_relation
-        ):
-            logger.info("Connector ready!")
+        body = {"source": source, "schema": [payload.pgsync_schema]}
+        logger.info(pgsync_config.url)
+        res = requests.post(f"{pgsync_config.url}/sync", json=body)
+        logger.info(res.content)
+        if res.status_code == status.HTTP_200_OK:
             return JSONResponse(
                 status_code=status.HTTP_200_OK,
-                content=f"{payload.source_relation} linked successfully",
+                content="Real time sync started successfully",
             )
         else:
             return JSONResponse(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                content="Failed to link data. Connector not created successfully. Check the Kafka Connect logs for more information",
+                content=f"Could not start real time sync: {res.reason}",
             )
-    except ConnectionError as e:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=f"Could not connect to database: {e}",
-        )
     except Exception as e:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
-            content=f"Failed to link data: {e}",
+            content=str(e),
         )
-
-
-@router.post(f"/{tag}/realtime/start", tags=[tag])
-async def realtime_start(
-    payload: AddSourcePayload, bg_tasks: BackgroundTasks
-) -> JSONResponse:
-    logger.info("Starting consumer coroutine...")
-
-    # Create index
-    index = None
-    try:
-        index = client.get_index(payload.source_relation)
-    except Exception:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content=f"Index {payload.source_relation} not found",
-        )
-
-    # Follow topic naming convention used by Debezium
-    topic = f"{payload.source_relation}.{payload.source_schema_name}.{payload.source_relation}"
-
-    kafka_consumer.initialize()
-
-    # Start the background process to begin consuming Kafka events
-    if not kafka_consumer.is_consuming:
-        logger.info("Starting consume_records background task")
-        bg_tasks.add_task(kafka_consumer.consume_records, index.upsert)
-
-    # Subscribe to the new table
-    kafka_consumer.add_topic(topic, payload.source_primary_key)
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=f"Processing started on index {payload.source_relation}",
-    )
 
 
 @router.post(f"/{tag}/search", tags=[tag])

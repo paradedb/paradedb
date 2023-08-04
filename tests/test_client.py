71791da80ff48ab880f4206be639516491b42632
Ming Ying <ming.ying.nyc@gmail.com>
2023-08-03 18:05:20 -0700

    feat: Added a test for all Client and Index functions

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from clients.python.retakesearch import Table
 from clients.python.retakesearch.search import Search
 
@@ -16,9 +18,6 @@ def test_add_source(client, test_index_name, database):
     # Ensure the index exists
     assert test_index_name in client.list_indices()
 
-    # Ensure the index has documents
-    assert client.describe_index(test_index_name)["count"] > 0
-
 
 def test_vectorize(client, test_index_name):
     index = client.get_index(test_index_name)
@@ -28,8 +27,13 @@ def test_vectorize(client, test_index_name):
         client.describe_index(test_index_name)
         .get("vectorized_fields", dict())
         .get("name")
-        is not None
+        .get("type")
+        == "knn_vector"
     )
+
+
+def test_describe_index(client, test_index_name):
+    assert client.describe_index(test_index_name)["count"] > 0
 
 
 def test_search_all(client, test_index_name):
@@ -54,3 +58,47 @@ def test_neural_search(client, test_index_name):
     query = Search().with_neural("New York City", ["name"])
     response = index.search(query)
     assert len(response["hits"]["hits"]) > 0
+
+
+def test_upsert(client, test_index_name):
+    index = client.get_index(test_index_name)
+
+    index.upsert(
+        documents=[
+            {
+                "name": "Retakeville",
+            },
+            {"name": "Vectorvilla"},
+        ],
+        ids=["retakeville-id", "vectorvilla-id"],
+    )
+
+    # Temporary: Allow time for upsert to complete
+    sleep(2)
+
+    query = Search().query("match", name="Retakeville")
+    response = index.search(query)
+    assert len(response["hits"]["hits"]) > 0
+
+    query = Search().query("match", name="Vectorvilla")
+    response = index.search(query)
+    assert len(response["hits"]["hits"]) > 0
+
+
+def test_create_field(client, test_index_name):
+    index = client.get_index(test_index_name)
+
+    index.create_field("population", "float")
+
+    assert (
+        client.describe_index(test_index_name)
+        .get("fields")
+        .get("population")
+        .get("type")
+        == "float"
+    )
+
+
+def test_delete_index(client, test_index_name):
+    client.delete_index(test_index_name)
+    assert test_index_name not in client.list_indices()

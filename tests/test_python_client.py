896436f0f46bdc09bf9d53230f073da041a23a72
Ming Ying <ming.ying.nyc@gmail.com>
2023-08-03 17:43:33 -0700

    wip: Writing more tests

--- tests/test_python_client.py
+++ tests/test_python_client.py
@@ -2,7 +2,7 @@ from clients.python.retakesearch import Table
 from clients.python.retakesearch.search import Search
 
 
-def test_python_client(retake_client, test_index_name, database):
+def test_add_source(client, test_index_name, database):
     table = Table(
         name="city",
         columns=["city_name"],
@@ -10,13 +10,47 @@ def test_python_client(retake_client, test_index_name, database):
     )
 
     # Create an index for our vectors in OpenSearch, and sync the database table to it
-    index = retake_client.create_index(test_index_name)
+    index = client.create_index(test_index_name)
     index.add_source(database, table)
 
-    # Vectorize the field
+    # Ensure the index exists
+    assert test_index_name in client.list_indices()
+
+    # Ensure the index has documents
+    assert client.describe_index(test_index_name)["count"] > 0
+
+
+def test_vectorize(client, test_index_name):
+    index = client.get_index(test_index_name)
     index.vectorize(["name"])
 
-    # Test search
-    neural_search_query = Search().with_neural("New York City", ["name"])
-    response = index.search(neural_search_query)
+    assert (
+        client.describe_index(test_index_name)
+        .get("vectorized_fields", dict())
+        .get("name")
+        is not None
+    )
+
+
+def test_search_all(client, test_index_name):
+    index = client.get_index(test_index_name)
+
+    query = Search().query("match_all")
+    response = index.search(query)
+    assert len(response["hits"]["hits"]) > 0
+
+
+def test_semantic_search(client, test_index_name):
+    index = client.get_index(test_index_name)
+
+    query = Search().with_semantic("New York City", ["name"])
+    response = index.search(query)
+    assert len(response["hits"]["hits"]) > 0
+
+
+def test_neural_search(client, test_index_name):
+    index = client.get_index(test_index_name)
+
+    query = Search().with_neural("New York City", ["name"])
+    response = index.search(query)
     assert len(response["hits"]["hits"]) > 0

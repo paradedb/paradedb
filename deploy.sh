37232ed299b5f280e76c546ff2470b9d76d60783
Mauricio Araujo <mauricio.araujo@proton.me>
2023-08-04 19:49:06 -0600

    Fix shellcheck SC1091

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,8 @@ sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# shellcheck source=/dev/null
 echo \
   "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   \"$(. /etc/os-release && echo "$VERSION_CODENAME")\" stable" | \

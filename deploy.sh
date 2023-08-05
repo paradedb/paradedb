5d10f9511c4de558b83f4c37ee50d9d765c28e8a
Mauricio Araujo <mauricio.araujo@proton.me>
2023-08-04 19:38:40 -0600

    Lint deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 if [ -z "$1" ]; then
     echo "Domain not provided."
@@ -10,7 +10,7 @@ fi
 
 DOMAIN=$1
 
-if [ ! -z "$2" ]; then
+if [ -n "$2" ]; then
     echo "Using 'dev' branch as default."
     GIT_BRANCH="dev"
 else
@@ -37,8 +37,8 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  \"$(. /etc/os-release && echo "$VERSION_CODENAME")\" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin

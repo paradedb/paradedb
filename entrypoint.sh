0a10b23d83bbe7c1118cfdf709eafa22d6a82144
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2023-08-31 14:32:14 -0700

    feat: Install `pg_ivm`, `pg_graphql`, `pgtap` (#47)
    Co-authored-by: Ming Ying <ming.ying.nyc@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,23 @@ set -Eeuo pipefail
 # Start PostgreSQL
 docker-entrypoint.sh postgres &
 
-# Give Postgres time to start
+# Give PostgreSQL time to start
 sleep 10
 
 # Wait for the PostgreSQL server to start
 until pg_isready -h localhost -p 5432 -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
-  echo "Postgres is unavailable - sleeping"
+  echo "PostgreSQL is unavailable - sleeping..."
   sleep 1
 done
 
-echo "Postgres is up - installing extension"
+echo "PostgreSQL is up - installing extensions..."
 
-# Load the compiled extension
+# Preinstall some extensions for the user
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pg_bm25;"
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pg_ivm;"
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pg_graphql;"
 
 # Wait for the PostgreSQL server to stop
 wait $!
 
-echo "PostgreSQL server has stopped"
+echo "PostgreSQL server has stopped - exiting..."

b5398a0c7781575a2855553e06cdf136818665cb
Suriya Kandaswamy <suriya@getretake.com>
2023-09-08 17:52:27 -0400

    benchmarking script for paradedb - needs some cleanup

--- benchmark/benchmark-paradedb.sh
+++ benchmark/benchmark-paradedb.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# 1. Install and run docker container for paradedb:
+source "get_data.sh"
+
+# 1. Install and run docker container for paradedb in detached mode
+echo "Spinning up paradedb server..."
 docker run \
- -e POSTGRES_USER=suriya \
- -e POSTGRES_PASSWORD=password \
+ -d \
+ -e POSTGRES_USER=myuser \
+ -e POSTGRES_PASSWORD=mypassword \
  -e POSTGRES_DB=mydatabase \
  -p 5432:5432 \
- paradedb/paradedb:latest
+ paradedb/paradedb:latest 
+
+# Wait for docker container to spin up
+echo "Waiting for server to spin up..."
+sleep 5
 
 # 2. Load data into database mydatabase via load_data.sql
+echo "Loading data into database..."
+load_data localhost 5432 mydatabase myuser mypassword
+
+TABLE_NAME=wikipedia_articles
+INDEX_NAME=search_index
 
 # 3. Run and time indexing
-# SELECT index_bm25('wikipedia_articles', 'bench_index', '{title, body}');
+# CREATE INDEX search_index ON wikipedia_articles USING bm25 ((wikipedia_articles.*));
+echo "Time indexing..."
+time db_query localhost 5432 mydatabase myuser mypassword "CREATE INDEX $INDEX_NAME ON $TABLE_NAME USING bm25 (($TABLE_NAME.*));"
 
 # 4. Run and time search
-# SELECT search_bm25('america', 'bench_index', 10);
+# SELECT * FROM wikipedia_articles WHERE wikipedia_articles @@@ 'america' LIMIT 10
+echo "Time search query..."
+time db_query localhost 5432 mydatabase myuser mypassword "SELECT * FROM $TABLE_NAME WHERE $TABLE_NAME @@@ 'america' LIMIT 10" >> search_output.txt;
 
 # 5. Destroy db?
--- benchmark/load_data.sql
+++ benchmark/load_data.sql
@@ -1,9 +1,12 @@
+-- This script is inteneded to be run after the temp_json table has been filled.
+
 BEGIN;
 
 CREATE TABLE wikipedia_articles ( url TEXT, title TEXT, body TEXT );
 
-CREATE TEMPORARY TABLE temp_json ( j JSONB ) ON COMMIT DROP;
-COPY temp_json FROM '/Users/suriya-retake/Documents/paradedb/benchmark/wiki-articles-1000.json' CSV QUOTE E'\x01' DELIMITER E'\x02';
+-- This is executed directly from the command line with the -c option
+-- CREATE TEMPORARY TABLE temp_json ( j JSONB ) ON COMMIT DROP;
+-- COPY temp_json FROM STDIN CSV QUOTE E'\x01' DELIMITER E'\x02';
 
 INSERT INTO wikipedia_articles ("url", "title", "body")
 
@@ -11,5 +14,7 @@ SELECT values->>'url' AS url,
        values->>'title' AS title,
        values->>'body' AS body
 FROM   (SELECT j AS values from temp_json) A;
+
+DROP TABLE temp_json;
 
 COMMIT;

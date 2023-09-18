402a64faddd238237d9e35aba5fd6e4cbd5f8e45
Neil Hansen <neil.hansen.31@gmail.com>
2023-09-18 10:21:02 -0700

    Add separate tables and indices

--- pg_bm25/test/sql/search_config.sql
+++ pg_bm25/test/sql/search_config.sql
@@ -1,12 +1,12 @@
 -- Basic seach query
-SELECT id, description, rating, category FROM products WHERE products @@@ 'category:electronics';
+SELECT id, description, rating, category FROM search_config WHERE search_config @@@ 'category:electronics';
 -- With trailing delimiter
-SELECT id, description, rating, category FROM products WHERE products @@@ 'category:electronics:::';
+SELECT id, description, rating, category FROM search_config WHERE search_config @@@ 'category:electronics:::';
 -- With limit
-SELECT id, description, rating, category FROM products WHERE products @@@ 'category:electronics:::limit=2';
+SELECT id, description, rating, category FROM search_config WHERE search_config @@@ 'category:electronics:::limit=2';
 -- With limit and trailing &
-SELECT id, description, rating, category FROM products WHERE products @@@ 'category:electronics:::limit=2&';
+SELECT id, description, rating, category FROM search_config WHERE search_config @@@ 'category:electronics:::limit=2&';
 -- With limit and offset
-SELECT id, description, rating, category FROM products WHERE products @@@ 'category:electronics:::limit=2&offset=1';
+SELECT id, description, rating, category FROM search_config WHERE search_config @@@ 'category:electronics:::limit=2&offset=1';
 
 

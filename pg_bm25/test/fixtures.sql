c6c3d9664c1b3dad0fb506c4a1a4554ee09a411d
Ming Ying <ming.ying.nyc@gmail.com>
2023-09-25 16:42:32 -0400

    Fix pg_search tests

--- pg_bm25/test/fixtures.sql
+++ pg_bm25/test/fixtures.sql
@@ -4,6 +4,6 @@ CREATE TABLE index_config AS SELECT * FROM paradedb.mock_items;
 CREATE TABLE search_config AS SELECT * FROM paradedb.mock_items;
 CREATE TABLE bm25_search AS SELECT * FROM paradedb.mock_items;
 
-CREATE INDEX idxmockitems ON paradedb.mock_items USING bm25((mock_items.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');;
+CREATE INDEX idxmockitems ON paradedb.mock_items USING bm25((mock_items.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
 CREATE INDEX idxsearchconfig ON search_config USING bm25 ((search_config.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
 CREATE INDEX idxbm25search ON bm25_search USING bm25 ((bm25_search.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');

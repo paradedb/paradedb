c79cdbfaf1fe3b7333d5041a0439a2b50133e694
Ming Ying <ming.ying.nyc@gmail.com>
2023-09-25 16:47:24 -0400

    Add tokenizer config test

--- pg_bm25/test/fixtures.sql
+++ pg_bm25/test/fixtures.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pg_bm25;
 
 CREATE TABLE index_config AS SELECT * FROM paradedb.mock_items;
 CREATE TABLE search_config AS SELECT * FROM paradedb.mock_items;
+CREATE TABLE tokenizer_config AS SELECT * FROM paradedb.mock_items;
 CREATE TABLE bm25_search AS SELECT * FROM paradedb.mock_items;
 
 CREATE INDEX idxmockitems ON paradedb.mock_items USING bm25((mock_items.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');

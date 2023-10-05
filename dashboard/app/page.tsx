1565b56af6a706e44d7675ffabc7b31339a7226b
Ming Ying <ming.ying.nyc@gmail.com>
2023-10-05 13:09:47 -0400

    wip

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,68 +1,50 @@
-// pages/index.js
-'use client';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { useState, useEffect } from 'react';
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Index() {
-    const { user, error, isLoading } = useUser();
-    const [isNewUser, setisNewUser] = useState(false);
-    const [creds, setCreds] = useState(null);
+const Index = async () => {
+  const { user } = await getSession();
 
-    useEffect(() => {
-        const fetchUserCreds = async () => {
-            try {
-                const response = await fetch("/api/cloud", { method: 'GET' });
-                if (!response.ok && response.status == 404) {
-                    setisNewUser(true);
-                } else if (!response.ok) {
-                    throw new Error('Server error');
-                }
+  const response = await fetch(`/api/cloud`, { method: "GET" });
+  let creds = response.ok ? await response.json() : null;
+  const isNewUser = !creds;
 
-                const data = await response.json();
-                setCreds({ host: data["host"], port: data["port"], user: data["user"], password: data["password"] });
-            } catch (error) {
-                console.error("There was a problem with the fetch operation:", error);
-            }
-        };
-
-        fetchUserCreds();
-    }, []); // Empty dependency array means this useEffect runs once when the component mounts
-
-    const signupFreePlan = async () => {
-        try {
-            const response = await fetch("/api/cloud", { method: 'POST' });
-            if (!response.ok) {
-                throw new Error('Server Error');
-            }
-            setisNewUser(false);
-        } catch (error) {
-            console.error("There was a problem with the fetch operation:", error);
-        }
-
+  const signupFreePlan = async () => {
+    try {
+      const response = await fetch("/api/cloud", { method: "POST" });
+      if (!response.ok) {
+        throw new Error("Server Error");
+      }
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
     }
+  };
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
+  return (
+    <div className={"container"}>
+      <div className={"userInfo"}>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+      {isNewUser ? (
+        <div className={"buttonContainer"}>
+          <button className={"freePlanButton"} onClick={signupFreePlan}>
+            Sign up for free plan
+          </button>
+        </div>
+      ) : (
+        <div className={"buttonContainer"}>
+          {creds ? (
+            <p>
+              Your connection string is <br />
+              host={creds.host} port={creds.port} user={creds.user} password=
+              {creds.password}
+            </p>
+          ) : (
+            <p>User already exists</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-    if (user) {
-        return (
-            <div className={"container"}>
-                <div className={"userInfo"}>
-                    Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-                </div>
-                {
-                    isNewUser ?
-                        <div className={"buttonContainer"}>
-                            <button className={"freePlanButton"} onClick={signupFreePlan}>Sign up for free plan</button>
-                        </div> :
-                        <div className={"buttonContainer"}>
-                            {creds != null ? <p>Your connection string is <br></br>host={creds.host} port={creds.port} user={creds.user} password={creds.password}</p> : <p>User already exists</p>}
-                        </div>
-                }
-
-            </div>
-        );
-    }
-
-    return <a href="/api/auth/login">Login</a>;
-}
+export default withPageAuthRequired(Index, { returnTo: "/" });

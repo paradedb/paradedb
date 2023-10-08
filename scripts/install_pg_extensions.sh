384ca7b4d486397d7a9749a8dcbbaf1e5a049203
Philippe Noël <philippemnoel@gmail.com>
2023-10-08 09:25:36 -0400

    consolidate install_pg_extension command to avoid duplication

--- scripts/install_pg_extensions.sh
+++ scripts/install_pg_extensions.sh
@@ -33,19 +33,22 @@ install_pg_extension() {
   tar -xvf "/tmp/$PG_EXTENSION_NAME.tar.gz" --strip-components=1 -C "/tmp/$PG_EXTENSION_NAME-$SANITIZED_VERSION"
   cd "/tmp/$PG_EXTENSION_NAME-$SANITIZED_VERSION"
 
+  # Set OPTFLAGS to an empty string if it's not already set
+  OPTFLAGS=${OPTFLAGS:-""}
+
   # Build and package as a .deb
   if [ "$PG_EXTENSION_NAME" == "pgvector" ]; then
-    # Disable -march=native to avoid "illegal instruction" errors on macOS arm64
-    make OPTFLAGS="" "-j$(nproc)"
+    # Disable -march=native to avoid "illegal instruction" errors on macOS arm64 by
+    # setting OPTFLAGS to an empty string
+    OPTFLAGS=""
   elif [ "$PG_EXTENSION_NAME" == "postgis" ]; then
     ./autogen.sh
     ./configure
-    make "-j$(nproc)"
   elif [ "$PG_EXTENSION_NAME" == "pgrouting" ]; then
     mkdir build && cd build
     cmake ..
-    make "-j$(nproc)"
   fi
+  make OPTFLAGS="$OPTFLAGS" "-j$(nproc)"
   checkinstall -D --nodoc --install=no --fstrans=no --backup=no --pakdir=/tmp
 }
 

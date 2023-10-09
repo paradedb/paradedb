89d7ff8d3bfa7448a48da2c32388ce22a35d9548
Philippe Noël <philippemnoel@gmail.com>
2023-10-08 23:51:51 -0400

    basic version of storing builds of extensions in S3, need to test

--- scripts/install_pg_extensions.sh
+++ scripts/install_pg_extensions.sh
@@ -15,10 +15,41 @@ sanitize_version() {
 }
 
 
-# Function to compile & package a single PostgreSQL extension as a .deb
-# Example:
-# install_pg_extension "pg_cron" "1.0.0" "https://github.com/citusdata/pg_cron/archive/refs/tags/v1.0.0.tar.gz"
-install_pg_extension() {
+
+
+check_pgx_package_exists() {
+  local s3_bucket_url="https://pgx-get.s3.amazonaws.com"
+  local deb_file="$1"
+
+  # Check if the .deb file exists by sending a HEAD request
+  response_code=$(curl -s -o /dev/null -w "%{http_code}" -I "$s3_bucket_url/$deb_file")
+
+  if [ "$response_code" == "200" ]; then
+    return 0  # File exists
+  else
+    return 1  # File doesn't exist
+  fi
+}
+
+download_pgx_package() {
+  local s3_bucket_url="https://pgx-get.s3.amazonaws.com"
+  local deb_file="$1"
+
+  # Download the .deb file from S3
+  curl -O "$s3_bucket_url/$deb_file"
+  echo ".deb file downloaded from S3."
+}
+
+upload_pgx_package() {
+  local s3_bucket_url="https://pgx-get.s3.amazonaws.com"
+  local deb_file="$1"
+
+  # Upload the .deb file to S3 using a PUT request
+  curl -X PUT --upload-file "$deb_file" "$s3_bucket_url/$deb_file"
+  echo ".deb file created and uploaded to S3."
+}
+
+compile_pgx_package() {
   local PG_EXTENSION_NAME=$1
   local PG_EXTENSION_VERSION=$2
   local PG_EXTENSION_URL=$3
@@ -50,6 +81,33 @@ install_pg_extension() {
   fi
   make OPTFLAGS="$OPTFLAGS" "-j$(nproc)"
   checkinstall -D --nodoc --install=no --fstrans=no --backup=no --pakdir=/tmp
+}
+
+
+
+
+
+
+
+
+# Function to compile & package a single PostgreSQL extension as a .deb
+# Example:
+# install_pg_extension "pg_cron" "1.0.0" "https://github.com/citusdata/pg_cron/archive/refs/tags/v1.0.0.tar.gz"
+install_pg_extension() {
+  local PG_EXTENSION_NAME=$1
+  local PG_EXTENSION_VERSION=$2
+  local PG_EXTENSION_URL=$3
+
+  # If the extension package already exists in S3, we simply retrieve it. Otherwise we compile and upload it
+  Check if the extension is already compiled and stored in S3
+  if check_pgx_package_exists "$PG_EXTENSION_NAME-$PG_EXTENSION_VERSION.deb"; then
+    echo "Extension package $PG_EXTENSION_NAME-$PG_EXTENSION_VERSION already exists in S3, downloading pre-compiled package..."
+    download_pgx_package "$PG_EXTENSION_NAME-$PG_EXTENSION_VERSION.deb"
+  else
+    echo "Extension package $PG_EXTENSION_NAME-$PG_EXTENSION_VERSION does not exist in S3, compiling and uploading..."
+    compile_pgx_package "$PG_EXTENSION_NAME" "$PG_EXTENSION_VERSION" "$PG_EXTENSION_URL"
+    upload_pgx_package "$PG_EXTENSION_NAME-$PG_EXTENSION_VERSION.deb"
+  fi
 }
 
 

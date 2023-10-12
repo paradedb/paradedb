d271d5d108e223b5dd750a81a42535efadde058f
Philippe Noël <philippemnoel@gmail.com>
2023-10-11 21:59:19 -0400

    added all hooks, commenting out the ones we need to fix issues for for now

--- .github/workflows/helpers/fix_trailing_newline.sh
+++ .github/workflows/helpers/fix_trailing_newline.sh
@@ -4,11 +4,11 @@
 set -Eeuo pipefail
 
 if [[ -f "$1" ]]; then
-    echo -n "$1"
-    if (diff /dev/null "$1" || true) | tail -1 | grep -q '^\\ No newline'; then
-        echo >> "$1"
-        echo "...fixed"
-    else
-        echo ""
-    fi
+  echo -n "$1"
+  if (diff /dev/null "$1" || true) | tail -1 | grep -q '^\\ No newline'; then
+    echo >> "$1"
+    echo "...fixed"
+  else
+    echo ""
+  fi
 fi

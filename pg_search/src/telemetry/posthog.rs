2326a8114eb08950970e1662d5625e4349b047df
Philippe Noël <philippemnoel@gmail.com>
2023-10-13 17:28:15 -0400

    this works as well for cargo pgrx run, need to test w/ dockerfile but first let's decide on logging

--- pg_search/src/telemetry/posthog.rs
+++ pg_search/src/telemetry/posthog.rs
@@ -1,55 +1,99 @@
 use pgrx::*;
+use serde::Deserialize;
 use serde_json::json;
-use std::env;
-use std::fs;
+use std::fmt;
 
-pub unsafe fn init() {
-    // Check if telemetry was already handled at the ParadeDB level
-    let telemetry_handled = fs::read_to_string("/tmp/telemetry").map_or_else(
-        |_| "false".to_string(),
-        |content| content.trim().to_string(),
-    );
+#[derive(Deserialize, Debug)]
+struct Config {
+    telemetry_handled: Option<String>, // Option because it's from a file, not env
+    telemetry: String,
+    posthog_api_key: String,
+    posthog_host: String,
+    commit_sha: String,
+}
 
-    if telemetry_handled == "true" {
-        return;
-    }
+enum EnvError {
+    PosthogApiKey,
+    PosthogHost,
+    CommitSha,
+    ParsingError,
+}
 
-    // Check if telemetry is enabled
-    let telemetry = env::var("TELEMETRY").unwrap_or_else(|_| "false".to_string());
-    if telemetry != "true" {
-        info!("Telemetry for pg_search disabled!");
-        return;
-    }
-
-    // Retrieve necessary environment variables or exit early if they're not set
-    let api_key = match env::var("POSTHOG_API_KEY") {
-        Ok(key) => key,
-        Err(_) => return,
-    };
-
-    let posthog_host = match env::var("POSTHOG_HOST") {
-        Ok(host) => host,
-        Err(_) => return,
-    };
-
-    let commit_sha = env::var("COMMIT_SHA").unwrap_or_else(|_| "".to_string());
-
-    // Construct the endpoint and event data
-    let endpoint = format!("{}/capture", posthog_host);
-    let data = json!({
-        "api_key": api_key,
-        "event": "pg_search Deployment",
-        "distinct_id": uuid::Uuid::new_v4().to_string(),
-        "properties": {
-            "commit_sha": commit_sha
+impl fmt::Display for EnvError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            EnvError::PosthogApiKey => write!(f, "Failed to retrieve POSTHOG_API_KEY from environment variables, not sending pg_search telemetry!"),
+            EnvError::PosthogHost => write!(f, "Failed to retrieve POSTHOG_HOST from environment variables, not sending pg_search telemetry!"),
+            EnvError::CommitSha => write!(f, "Failed to retrieve COMMIT_SHA from environment variables, sending pg_search telemetry without commit_sha!"),
+            EnvError::ParsingError => write!(f, "Telemetry for pg_search disabled!"),
         }
-    });
+    }
+}
 
-    // Send the event
-    let client = reqwest::blocking::Client::new();
-    let _response = client
-        .post(endpoint)
-        .header("Content-Type", "application/json")
-        .body(data.to_string())
-        .send();
+impl Config {
+    fn from_env() -> Result<Self, EnvError> {
+        let telemetry_handled = std::fs::read_to_string("/tmp/telemetry")
+            .map(|content| content.trim().to_string())
+            .ok();
+
+        match envy::from_env::<Config>() {
+            Ok(config) => Ok(Config {
+                telemetry_handled,
+                ..config
+            }),
+            Err(envy::Error::MissingValue(field)) => match field {
+                "posthog_api_key" => Err(EnvError::PosthogApiKey),
+                "posthog_host" => Err(EnvError::PosthogHost),
+                "commit_sha" => Err(EnvError::CommitSha),
+                _ => Err(EnvError::ParsingError),
+            },
+            Err(_) => Err(EnvError::ParsingError),
+        }
+    }
+}
+
+pub fn init() {
+    let config = Config::from_env();
+
+    match config {
+        Ok(config) => {
+            if config.telemetry_handled.as_deref() == Some("true") || config.telemetry != "true" {
+                return;
+            }
+
+            let endpoint = format!("{}/capture", config.posthog_host);
+            let data = json!({
+                "api_key": config.posthog_api_key,
+                "event": "pg_search Deployment",
+                "distinct_id": uuid::Uuid::new_v4().to_string(),
+                "properties": {
+                    "commit_sha": config.commit_sha
+                }
+            });
+
+            let client = reqwest::blocking::Client::new();
+            let response = client
+                .post(endpoint)
+                .header("Content-Type", "application/json")
+                .body(data.to_string())
+                .send();
+
+            match response {
+                Ok(res) if res.status().is_success() => {
+                    let _body = res
+                        .text()
+                        .unwrap_or_else(|_| String::from("Failed to read response body"));
+                }
+                Ok(res) => {
+                    info!("Failed to send event. Status: {}", res.status());
+                }
+                Err(e) => {
+                    info!("Error sending request: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            info!("{}", e);
+        }
+    }
 }

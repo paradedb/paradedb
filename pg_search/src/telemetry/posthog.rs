ccdea128af5f228e89aaaea9ea4a4d8ead0b4261
Philippe Noël <philippemnoel@gmail.com>
2023-10-13 14:24:10 -0400

    tested entrypoint, bm25, search, need to test dockerfiles one last time

--- pg_search/src/telemetry/posthog.rs
+++ pg_search/src/telemetry/posthog.rs
@@ -4,18 +4,19 @@ use std::env;
 use std::fs;
 
 pub unsafe fn init() {
-    let telemetry = env::var("TELEMETRY").unwrap_or_else(|_| String::from("false"));
+    // Read whether telemetry was already handled at the ParadeDB level
+    let telemetry_handled = match fs::read_to_string("/tmp/telemetry") {
+        Ok(content) => content.trim().to_string(),
+        Err(_) => String::from("false"),
+    };
 
-    if telemetry == "true" {
-        // Read whether telemetry was already handled at the ParadeDB level
-        let telemetry_handled = match fs::read_to_string("/tmp/telemetry") {
-            Ok(content) => content.trim().to_string(),
-            Err(_) => String::from("false"),
-        };
+    // If telemetry was not already handled at the ParadeDB level, this is a standalone deployment
+    // of pg_search, and we should send telemetry
+    if telemetry_handled != "true" {
 
-        // If telemetry was not already handled at the ParadeDB level, this is a standalone deployment
-        // of pg_search, and we should send telemetry
-        if telemetry_handled != "true" {
+        // We only send telemetry in the standalone deployment if TELEMETRY is set to true
+        let telemetry = env::var("TELEMETRY").unwrap_or_else(|_| String::from("false"));
+        if telemetry == "true" {
             if let Ok(api_key) = env::var("POSTHOG_API_KEY") {
                 if let Ok(posthog_host) = env::var("POSTHOG_HOST") {
                     if let Ok(commit_sha) = env::var("COMMIT_SHA") {
@@ -43,11 +44,9 @@ pub unsafe fn init() {
                         // Check if the request was successful
                         match response {
                             Ok(res) if res.status().is_success() => {
-                                info!("Event sent successfully!");
-                                let body = res.text().unwrap_or_else(|_| {
+                                let _body = res.text().unwrap_or_else(|_| {
                                     String::from("Failed to read response body")
                                 });
-                                info!("Response body: {}", body);
                             }
                             Ok(res) => {
                                 info!("Failed to send event. Status: {}", res.status());
@@ -65,8 +64,8 @@ pub unsafe fn init() {
             } else {
                 info!("Failed to retrieve POSTHOG_API_KEY from environment variables, not sending pg_search telemetry!");
             }
+        } else {
+            info!("Telemetry for pg_search disabled!");
         }
-    } else {
-        info!("Telemetry for pg_search disabled!");
     }
 }

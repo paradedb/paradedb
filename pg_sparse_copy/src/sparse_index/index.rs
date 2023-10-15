825032afec3ec2fe9239dfa8d024b4f1238bacc2
Ming Ying <ming.ying.nyc@gmail.com>
2023-10-15 17:18:37 -0400

    Checkpoint 2

--- pg_sparse_copy/src/sparse_index/index.rs
+++ pg_sparse_copy/src/sparse_index/index.rs
@@ -24,61 +24,19 @@ pub struct TantivyScanState {
 }
 
 pub struct SparseIndex {
-    pub fields: HashMap<String, Field>,
-    pub underlying_index: Index,
+    pub name: String
 }
 
 impl SparseIndex {
     pub fn new(name: String, table_name: String, options: PgBox<ParadeOptions>) -> Self {
-        let dir = Self::get_data_directory(&name);
-        let path = Path::new(&dir);
-        if path.exists() {
-            remove_dir_all(path).expect("failed to remove paradedb directory");
-        }
-
-        create_dir_all(path).expect("failed to create paradedb directory");
-
-        let result = Self::build_index_schema(&table_name, options);
-        let (schema, fields) = match result {
-            Ok((s, f)) => (s, f),
-            Err(e) => {
-                panic!("{}", e);
-            }
-        };
-        let settings = IndexSettings {
-            docstore_compress_dedicated_thread: false, // Must run on single thread, or pgrx will panic
-            ..Default::default()
-        };
-
-        let mut underlying_index = Index::builder()
-            .schema(schema.clone())
-            .settings(settings.clone())
-            .create_in_dir(dir)
-            .expect("failed to create index");
-
         Self {
-            fields,
-            underlying_index,
+            name: name
         }
     }
 
     pub fn from_index_name(name: String) -> Self {
-        let dir = Self::get_data_directory(&name);
-
-        let underlying_index = Index::open_in_dir(dir).expect("failed to open index");
-        let schema = underlying_index.schema();
-
-        let fields = schema
-            .fields()
-            .map(|field| {
-                let (field, entry) = field;
-                (entry.name().to_string(), field)
-            })
-            .collect();
-
         Self {
-            fields,
-            underlying_index,
+            name: name
         }
     }
 
@@ -87,82 +45,20 @@ impl SparseIndex {
         writer: &mut SingleSegmentIndexWriter,
         heap_tid: ItemPointerData,
         builder: JsonBuilder,
-    ) {
-        let mut doc: Document = Document::new();
-
-        for (col_name, value) in builder.values {
-            let field_option = self.fields.get(col_name.trim_matches('"'));
-            if let Some(field) = field_option {
-                value.add_to_tantivy_doc(&mut doc, field);
-            }
-        }
-
-        let field_option = self.fields.get("heap_tid");
-        doc.add_u64(*field_option.unwrap(), item_pointer_to_u64(heap_tid));
-        writer.add_document(doc).expect("failed to add document");
-    }
+    ) {}
 
     pub fn bulk_delete(
         &self,
         stats_binding: *mut IndexBulkDeleteResult,
         callback: IndexBulkDeleteCallback,
         callback_state: *mut ::std::os::raw::c_void,
-    ) {
-        let mut index_writer = self.underlying_index.writer(50_000_000).unwrap(); // Adjust the size as necessary
-
-        let schema = self.underlying_index.schema();
-        let heap_tid_field = schema
-            .get_field("heap_tid")
-            .expect("Field 'heap_tid' not found in schema");
-
-        let searcher = self
-            .underlying_index
-            .reader()
-            .expect("Failed to acquire index reader")
-            .searcher();
-
-        for segment_reader in searcher.segment_readers() {
-            let store_reader = segment_reader
-                .get_store_reader(CACHE_NUM_BLOCKS)
-                .expect("Failed to get store reader");
-
-            for doc_id in 0..segment_reader.num_docs() {
-                if let Ok(stored_fields) = store_reader.get(doc_id) {
-                    if let Some(Value::I64(heap_tid_val)) = stored_fields.get_first(heap_tid_field)
-                    {
-                        if let Some(actual_callback) = callback {
-                            let should_delete = unsafe {
-                                actual_callback(
-                                    *heap_tid_val as *mut ItemPointerData,
-                                    callback_state,
-                                )
-                            };
-
-                            if should_delete {
-                                let term_to_delete =
-                                    Term::from_field_i64(heap_tid_field, *heap_tid_val);
-                                index_writer.delete_term(term_to_delete);
-                                unsafe {
-                                    (*stats_binding).tuples_removed += 1.0;
-                                }
-                            } else {
-                                unsafe {
-                                    (*stats_binding).num_index_tuples += 1.0;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        index_writer.commit().unwrap();
-    }
+    ) {}
 
     pub fn scan(&self) -> TantivyScanState {
-        let schema = self.underlying_index.schema();
-        let reader = self
-            .underlying_index
+        let schema_builder = Schema::builder();
+        let schema = schema_builder.build();
+        let underlying_index = Index::create_in_ram(schema.clone());
+        let reader = underlying_index
             .reader_builder()
             .reload_policy(tantivy::ReloadPolicy::Manual)
             .try_into()
@@ -171,7 +67,7 @@ impl SparseIndex {
         let searcher = reader.searcher();
 
         let query_parser = QueryParser::for_index(
-            &self.underlying_index,
+            &underlying_index,
             schema.fields().map(|(field, _)| field).collect::<Vec<_>>(),
         );
         let empty_query = query_parser.parse_query("").unwrap();
@@ -186,7 +82,9 @@ impl SparseIndex {
     }
 
     pub fn copy_tantivy_index(&self) -> tantivy::Index {
-        self.underlying_index.clone()
+        let schema_builder = Schema::builder();
+        let schema = schema_builder.build();
+        Index::create_in_ram(schema.clone())
     }
 
     fn get_data_directory(name: &str) -> String {

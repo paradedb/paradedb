0a12cdd8cfb49a722c24b4b7fe270ec9b7302af5
Ming Ying <ming.ying.nyc@gmail.com>
2023-10-15 17:42:22 -0400

    checkpoint 6

--- pg_sparse_copy/src/index_access/options.rs
+++ pg_sparse_copy/src/index_access/options.rs
@@ -31,101 +31,15 @@ static mut RELOPT_KIND_PDB: pg_sys::relopt_kind = 0;
 pub struct ParadeOptions {
     // varlena header (needed bc postgres treats this as bytea)
     vl_len_: i32,
-    text_fields_offset: i32,
-    numeric_fields_offset: i32,
-    boolean_fields_offset: i32,
-    json_fields_offset: i32,
 }
 
-#[pg_guard]
-extern "C" fn validate_text_fields(value: *const std::os::raw::c_char) {
-    let json_str = cstr_to_rust_str(value);
-
-    if json_str.is_empty() {
-        return;
-    }
-
-    let _options: HashMap<String, ParadeTextOptions> =
-        from_str(&json_str).expect("failed to validate text_fields");
-}
-
-#[pg_guard]
-extern "C" fn validate_numeric_fields(value: *const std::os::raw::c_char) {
-    let json_str = cstr_to_rust_str(value);
-
-    if json_str.is_empty() {
-        return;
-    }
-
-    let _options: HashMap<String, ParadeNumericOptions> =
-        from_str(&json_str).expect("failed to validate numeric_fields");
-}
-
-#[pg_guard]
-extern "C" fn validate_boolean_fields(value: *const std::os::raw::c_char) {
-    let json_str = cstr_to_rust_str(value);
-
-    if json_str.is_empty() {
-        return;
-    }
-
-    let _options: HashMap<String, ParadeBooleanOptions> =
-        from_str(&json_str).expect("failed to validate boolean_fields");
-}
-
-#[pg_guard]
-extern "C" fn validate_json_fields(value: *const std::os::raw::c_char) {
-    let json_str = cstr_to_rust_str(value);
-
-    if json_str.is_empty() {
-        return;
-    }
-
-    let _options: HashMap<String, ParadeJsonOptions> =
-        from_str(&json_str).expect("failed to validate boolean_fields");
-}
-
-#[inline]
-fn cstr_to_rust_str(value: *const std::os::raw::c_char) -> String {
-    if value.is_null() {
-        return "".to_string();
-    }
-
-    unsafe { CStr::from_ptr(value) }
-        .to_str()
-        .expect("failed to parse fields as utf-8")
-        .to_string()
-}
-
-// For now, we support changing the tokenizer between default, raw, and en_stem
-const NUM_REL_OPTS: usize = 4;
+const NUM_REL_OPTS: usize = 0;
 #[pg_guard]
 pub unsafe extern "C" fn amoptions(
     reloptions: pg_sys::Datum,
     validate: bool,
 ) -> *mut pg_sys::bytea {
-    let options: [pg_sys::relopt_parse_elt; NUM_REL_OPTS] = [
-        pg_sys::relopt_parse_elt {
-            optname: "text_fields".as_pg_cstr(),
-            opttype: pg_sys::relopt_type_RELOPT_TYPE_STRING,
-            offset: offset_of!(ParadeOptions, text_fields_offset) as i32,
-        },
-        pg_sys::relopt_parse_elt {
-            optname: "numeric_fields".as_pg_cstr(),
-            opttype: pg_sys::relopt_type_RELOPT_TYPE_STRING,
-            offset: offset_of!(ParadeOptions, numeric_fields_offset) as i32,
-        },
-        pg_sys::relopt_parse_elt {
-            optname: "boolean_fields".as_pg_cstr(),
-            opttype: pg_sys::relopt_type_RELOPT_TYPE_STRING,
-            offset: offset_of!(ParadeOptions, boolean_fields_offset) as i32,
-        },
-        pg_sys::relopt_parse_elt {
-            optname: "json_fields".as_pg_cstr(),
-            opttype: pg_sys::relopt_type_RELOPT_TYPE_STRING,
-            offset: offset_of!(ParadeOptions, json_fields_offset) as i32,
-        },
-    ];
+    let options: [pg_sys::relopt_parse_elt; NUM_REL_OPTS] = [];
     build_relopts(reloptions, validate, options)
 }
 
@@ -180,62 +94,7 @@ unsafe fn build_relopts(
     rdopts as *mut pg_sys::bytea
 }
 
-impl ParadeOptions {
-    pub fn get_text_fields(&self) -> HashMap<String, ParadeTextOptions> {
-        let fields = self.get_str(self.text_fields_offset, "".to_string());
-
-        if fields.is_empty() {
-            return HashMap::new();
-        }
-
-        from_str::<HashMap<String, ParadeTextOptions>>(&fields).expect("failed to get text_fields")
-    }
-
-    pub fn get_numeric_fields(&self) -> HashMap<String, ParadeNumericOptions> {
-        let fields = self.get_str(self.numeric_fields_offset, "".to_string());
-
-        if fields.is_empty() {
-            return HashMap::new();
-        }
-
-        from_str::<HashMap<String, ParadeNumericOptions>>(&fields)
-            .expect("failed to parse numeric_fields")
-    }
-
-    pub fn get_boolean_fields(&self) -> HashMap<String, ParadeBooleanOptions> {
-        let fields = self.get_str(self.boolean_fields_offset, "".to_string());
-
-        if fields.is_empty() {
-            return HashMap::new();
-        }
-
-        from_str::<HashMap<String, ParadeBooleanOptions>>(&fields)
-            .expect("failed to parse boolean_fields")
-    }
-
-    pub fn get_json_fields(&self) -> HashMap<String, ParadeJsonOptions> {
-        let fields = self.get_str(self.json_fields_offset, "".to_string());
-
-        if fields.is_empty() {
-            return HashMap::new();
-        }
-
-        from_str::<HashMap<String, ParadeJsonOptions>>(&fields)
-            .expect("failed to parse json_fields")
-    }
-
-    fn get_str(&self, offset: i32, default: String) -> String {
-        if offset == 0 {
-            default
-        } else {
-            let opts = self as *const _ as void_ptr as usize;
-            let value =
-                unsafe { CStr::from_ptr((opts + offset as usize) as *const std::os::raw::c_char) };
-
-            value.to_str().unwrap().to_owned()
-        }
-    }
-}
+impl ParadeOptions {}
 
 // it adds the tokenizer option to the list of relation options so we can parse it in amoptions
 pub unsafe fn init() {}

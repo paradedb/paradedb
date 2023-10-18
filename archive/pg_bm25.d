12650fdb8ea02615b53f6db18139e410e9d86e5f
Philippe Noël <philippemnoel@gmail.com>
2023-10-18 15:41:48 -0400

    use absolute paths because what is this mess

---
+++ archive/pg_bm25.d
@@ -0,0 +1,37 @@
+/Users/phil/Downloads/paradedb/target/release/deps/libpg_bm25.dylib: pg_bm25/src/lib.rs pg_bm25/src/api/mod.rs pg_bm25/src/api/aggregation.rs pg_bm25/src/api/index.rs pg_bm25/src/api/search.rs pg_bm25/src/index_access/mod.rs pg_bm25/src/index_access/build.rs pg_bm25/src/index_access/cost.rs pg_bm25/src/index_access/delete.rs pg_bm25/src/index_access/insert.rs pg_bm25/src/index_access/options.rs pg_bm25/src/index_access/scan.rs pg_bm25/src/index_access/vacuum.rs pg_bm25/src/index_access/validate.rs pg_bm25/src/index_access/utils.rs pg_bm25/src/json/mod.rs pg_bm25/src/json/builder.rs pg_bm25/src/json/json_string.rs pg_bm25/src/manager/mod.rs pg_bm25/src/operator/mod.rs pg_bm25/src/parade_index/mod.rs pg_bm25/src/parade_index/fields.rs pg_bm25/src/parade_index/index.rs pg_bm25/src/tokenizers/mod.rs pg_bm25/src/tokenizers/cjk.rs pg_bm25/src/tokenizers/code.rs /Users/phil/Downloads/paradedb/pg_bm25/pg_bm25.control pg_bm25/src/../sql/_bootstrap.sql pg_bm25/src/../sql/_bootstrap_quickstart.sql
+
+/Users/phil/Downloads/paradedb/target/release/deps/pg_bm25.d: pg_bm25/src/lib.rs pg_bm25/src/api/mod.rs pg_bm25/src/api/aggregation.rs pg_bm25/src/api/index.rs pg_bm25/src/api/search.rs pg_bm25/src/index_access/mod.rs pg_bm25/src/index_access/build.rs pg_bm25/src/index_access/cost.rs pg_bm25/src/index_access/delete.rs pg_bm25/src/index_access/insert.rs pg_bm25/src/index_access/options.rs pg_bm25/src/index_access/scan.rs pg_bm25/src/index_access/vacuum.rs pg_bm25/src/index_access/validate.rs pg_bm25/src/index_access/utils.rs pg_bm25/src/json/mod.rs pg_bm25/src/json/builder.rs pg_bm25/src/json/json_string.rs pg_bm25/src/manager/mod.rs pg_bm25/src/operator/mod.rs pg_bm25/src/parade_index/mod.rs pg_bm25/src/parade_index/fields.rs pg_bm25/src/parade_index/index.rs pg_bm25/src/tokenizers/mod.rs pg_bm25/src/tokenizers/cjk.rs pg_bm25/src/tokenizers/code.rs /Users/phil/Downloads/paradedb/pg_bm25/pg_bm25.control pg_bm25/src/../sql/_bootstrap.sql pg_bm25/src/../sql/_bootstrap_quickstart.sql
+
+pg_bm25/src/lib.rs:
+pg_bm25/src/api/mod.rs:
+pg_bm25/src/api/aggregation.rs:
+pg_bm25/src/api/index.rs:
+pg_bm25/src/api/search.rs:
+pg_bm25/src/index_access/mod.rs:
+pg_bm25/src/index_access/build.rs:
+pg_bm25/src/index_access/cost.rs:
+pg_bm25/src/index_access/delete.rs:
+pg_bm25/src/index_access/insert.rs:
+pg_bm25/src/index_access/options.rs:
+pg_bm25/src/index_access/scan.rs:
+pg_bm25/src/index_access/vacuum.rs:
+pg_bm25/src/index_access/validate.rs:
+pg_bm25/src/index_access/utils.rs:
+pg_bm25/src/json/mod.rs:
+pg_bm25/src/json/builder.rs:
+pg_bm25/src/json/json_string.rs:
+pg_bm25/src/manager/mod.rs:
+pg_bm25/src/operator/mod.rs:
+pg_bm25/src/parade_index/mod.rs:
+pg_bm25/src/parade_index/fields.rs:
+pg_bm25/src/parade_index/index.rs:
+pg_bm25/src/tokenizers/mod.rs:
+pg_bm25/src/tokenizers/cjk.rs:
+pg_bm25/src/tokenizers/code.rs:
+/Users/phil/Downloads/paradedb/pg_bm25/pg_bm25.control:
+pg_bm25/src/../sql/_bootstrap.sql:
+pg_bm25/src/../sql/_bootstrap_quickstart.sql:
+
+# env-dep:CARGO_CRATE_NAME=pg_bm25
+# env-dep:CARGO_MANIFEST_DIR=/Users/phil/Downloads/paradedb/pg_bm25
+# env-dep:CARGO_PKG_VERSION=0.0.0

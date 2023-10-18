948aaa99723868634236bbc6a02eb484513fdef6
Mauricio Araujo <mauricio.araujo@proton.me>
2023-10-18 13:08:10 -0600

    More linting, update shebang checker to consider sh

--- .github/workflows/helpers/check_shebang.sh
+++ .github/workflows/helpers/check_shebang.sh
@@ -8,6 +8,9 @@ do
   if [[ "$(head -c 11 "$file")" = "#!/bin/bash" ]]
   then
     echo "[#!/bin/bash -> Present] $file"
+  elif [[ "$(head -c 11 "$file")" = "#!/bin/sh" ]]
+  then
+    echo "[#!/bin/sh -> Present] $file"
   else
     echo "[#!/bin/bash -> NOT FOUND] $file" && exit 1
   fi

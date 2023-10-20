d74a617239d225c4c134e55c68169433b4a7c000
Philippe Noël <philippemnoel@gmail.com>
2023-10-20 15:35:28 -0400

    nit

--- scripts/build_and_deploy_third_party_pg_extensions.sh
+++ scripts/build_and_deploy_third_party_pg_extensions.sh
@@ -85,11 +85,10 @@ build_and_publish_pg_extension() {
 
     # Upload the .deb file to the newly created GitHub release
     asset_name="$PG_EXTENSION_NAME-v$SANITIZED_PG_EXTENSION_VERSION-pg$PG_MAJOR_VERSION-$ARCH-linux-gnu.deb"
-    deb_file_path="/tmp/*.deb"
     curl -X POST "${upload_url}?name=${asset_name}" \
       -H "Authorization: token $GITHUB_TOKEN" \
       -H "Content-Type: application/vnd.DEBIAN.binary-package" \
-      --data-binary "@${deb_file_path}"
+      --data-binary "@/tmp/*.deb"
   fi
 }
 

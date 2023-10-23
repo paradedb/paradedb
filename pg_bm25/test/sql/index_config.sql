af4b49d16444480e9510bc2f396604636d60dcf1
Neil Hansen <neil.hansen.31@gmail.com>
2023-10-22 21:17:34 -0700

    Add tests

--- pg_bm25/test/sql/index_config.sql
+++ pg_bm25/test/sql/index_config.sql
@@ -8,12 +8,12 @@ SELECT * from paradedb.schema_bm25('idxindexconfig');
 DROP INDEX idxindexconfig;
 
 -- Text field with options
-CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (text_fields='{"description": {"fast": true, "tokenizer": "en_stem", "record": "freq", "normalizer": "raw"}}');
+CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (text_fields='{"description": {"fast": true, "tokenizer": { "type": "en_stem" }, "record": "freq", "normalizer": "raw"}}');
 SELECT * from paradedb.schema_bm25('idxindexconfig');
 DROP INDEX idxindexconfig;
 
 -- Multiple text fields
-CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (text_fields='{"description": {"fast": true, "tokenizer": "en_stem", "record": "freq", "normalizer": "raw"}, "category": {}}');
+CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (text_fields='{"description": {"fast": true, "tokenizer": { "type": "en_stem" }, "record": "freq", "normalizer": "raw"}, "category": {}}');
 SELECT * from paradedb.schema_bm25('idxindexconfig');
 DROP INDEX idxindexconfig;
 
@@ -43,7 +43,7 @@ SELECT * from paradedb.schema_bm25('idxindexconfig');
 DROP INDEX idxindexconfig;
 
 -- Json field with options
-CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (json_fields='{"metadata": {"fast": true, "expand_dots": false, "tokenizer": "raw", "normalizer": "raw"}}');
+CREATE INDEX idxindexconfig ON index_config USING bm25 ((index_config.*)) WITH (json_fields='{"metadata": {"fast": true, "expand_dots": false, "tokenizer": { "type": "raw" }, "normalizer": "raw"}}');
 SELECT * from paradedb.schema_bm25('idxindexconfig');
 DROP INDEX idxindexconfig;
 
--- pg_bm25/test/sql/tokenizer_config.sql
+++ pg_bm25/test/sql/tokenizer_config.sql
@@ -4,6 +4,18 @@ SELECT * FROM tokenizer_config WHERE tokenizer_config @@@ 'description:earbud';
 DROP INDEX idxtokenizerconfig;
 
 -- en_stem
-CREATE INDEX idxtokenizerconfig ON tokenizer_config USING bm25 ((tokenizer_config.*)) WITH (text_fields='{"description": {"tokenizer": "en_stem"}}');
+CREATE INDEX idxtokenizerconfig ON tokenizer_config USING bm25 ((tokenizer_config.*)) WITH (text_fields='{"description": {"tokenizer": { "type": "en_stem" }}}');
 SELECT * FROM tokenizer_config WHERE tokenizer_config @@@ 'description:earbud';
 DROP INDEX idxtokenizerconfig;
+
+-- ngram
+CREATE INDEX idxtokenizerconfig ON tokenizer_config USING bm25 ((tokenizer_config.*)) WITH (text_fields='{"description": {"tokenizer": {"type": "ngram", "min_gram": 3, "max_gram": 8, "prefix_only": false}}}');
+SELECT * FROM tokenizer_config WHERE tokenizer_config @@@ 'description:boa';
+DROP INDEX idxtokenizerconfig;
+
+-- chinese_compatible
+CREATE INDEX idxtokenizerconfig  ON tokenizer_config  USING bm25 ((tokenizer_config.*))  WITH (text_fields='{"description": {"tokenizer": {"type": "chinese_compatible"}, "record": "position"}}');
+INSERT INTO tokenizer_config (description, rating, category) VALUES ('电脑', 4, 'Electronics');
+SELECT * FROM tokenizer_config WHERE tokenizer_config @@@ '电脑';
+DROP INDEX idxtokenizerconfig;
+

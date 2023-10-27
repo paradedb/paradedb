e315611b80586d5519c2380b33ff752b94fcf89d
Ming Ying <ming.ying.nyc@gmail.com>
2023-10-27 11:45:17 -0400

    Fix cost estimate function

--- pg_sparse/src/index_access/cost.rs
+++ pg_sparse/src/index_access/cost.rs
@@ -1,62 +1,57 @@
 use pgrx::*;
+use std::ptr::null_mut;
 
-// TODO: Rework cost function to be HNSW index specific
-// https://github.com/neondatabase/pg_embedding/blob/5d48508aeaeb86b9e9c630468ada7a124b905795/embedding.c#L394C5-L394C5
+use crate::sparse_index::index::from_index_name;
+
 #[allow(clippy::too_many_arguments)]
 #[pg_guard(immutable, parallel_safe)]
 pub unsafe extern "C" fn amcostestimate(
-    _root: *mut pg_sys::PlannerInfo,
+    root: *mut pg_sys::PlannerInfo,
     path: *mut pg_sys::IndexPath,
-    _loop_count: f64,
+    loop_count: f64,
     index_startup_cost: *mut pg_sys::Cost,
     index_total_cost: *mut pg_sys::Cost,
     index_selectivity: *mut pg_sys::Selectivity,
     index_correlation: *mut f64,
     index_pages: *mut f64,
 ) {
-    info!("cost");
-    let path = path.as_ref().expect("path argument is NULL");
-    let indexinfo = path.indexinfo.as_ref().expect("indexinfo in path is NULL");
-    let index_relation = unsafe {
-        PgRelation::with_lock(
-            indexinfo.indexoid,
-            pg_sys::AccessShareLock as pg_sys::LOCKMODE,
-        )
-    };
-    let heap_relation = index_relation
-        .heap_relation()
-        .expect("failed to get heap relation for index");
+    let pathref = path.as_ref().expect("path argument is NULL");
 
-    *index_correlation = 1.0;
-    *index_startup_cost = 0.0;
-    *index_pages = 0.0;
-    *index_total_cost = 0.0;
-    *index_selectivity = 1.0;
-
-    #[cfg(any(feature = "pg10", feature = "pg11"))]
-    let index_clauses = PgList::<pg_sys::RestrictInfo>::from_pg(path.indexclauses);
-
-    #[cfg(any(feature = "pg12", feature = "pg13", feature = "pg14", feature = "pg15"))]
-    let index_clauses = PgList::<pg_sys::IndexClause>::from_pg(path.indexclauses);
-
-    for clause in index_clauses.iter_ptr() {
-        #[cfg(any(feature = "pg10", feature = "pg11"))]
-        let ri = clause.as_ref().expect("restrict info is NULL");
-
-        #[cfg(any(feature = "pg12", feature = "pg13", feature = "pg14", feature = "pg15"))]
-        let ri = clause
+    if pathref.indexorderbys == null_mut() {
+        *index_startup_cost = f64::MAX;
+        *index_total_cost = f64::MAX;
+        *index_selectivity = 0.0;
+        *index_correlation = 0.0;
+        *index_pages = 0.0;
+    } else {
+        let indexinfo = pathref
+            .indexinfo
             .as_ref()
-            .unwrap()
-            .rinfo
-            .as_ref()
-            .expect("restrict info in index clause is NULL");
+            .expect("indexinfo in path is NULL");
+        let index_relation = unsafe {
+            PgRelation::with_lock(
+                indexinfo.indexoid,
+                pg_sys::AccessShareLock as pg_sys::LOCKMODE,
+            )
+        };
+        let heap_relation = index_relation
+            .heap_relation()
+            .expect("failed to get heap relation for index");
+        let mut sparse_index = from_index_name(index_relation.name());
+        let meta = sparse_index.get_hnsw_metadata();
+        let ef_search = meta.ef_search as f64;
 
-        if ri.norm_selec > 0f64 {
-            *index_selectivity = ri.norm_selec.min(*index_selectivity);
-        }
+        let mut generic_costs = pg_sys::GenericCosts::default();
+        pg_sys::genericcostestimate(root, path, loop_count, &mut generic_costs);
+
+        *index_startup_cost = ef_search * pg_sys::random_page_cost;
+        *index_total_cost = *index_startup_cost;
+        *index_selectivity = if (*indexinfo.rel).rows != 0.0 {
+            ef_search / (*indexinfo.rel).rows
+        } else {
+            generic_costs.indexSelectivity
+        };
+        *index_correlation = generic_costs.indexCorrelation;
+        *index_pages = ef_search;
     }
-
-    let reltuples = heap_relation.reltuples().unwrap_or(1f32) as f64;
-    *index_total_cost += *index_selectivity * reltuples * pg_sys::cpu_index_tuple_cost;
-    *index_total_cost -= pg_sys::random_page_cost;
 }

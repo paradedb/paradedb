8f35acc1c3da29e8236f8113fc0ce977e4fa82c4
Ming Ying <ming.ying.nyc@gmail.com>
2023-10-28 12:54:38 -0400

    Modify quickstart table

--- pg_sparse/sql/_bootstrap_quickstart.sql
+++ pg_sparse/sql/_bootstrap_quickstart.sql
@@ -6,55 +6,52 @@ CREATE TABLE mock_items (
     rating INTEGER CHECK (
         rating BETWEEN 1
         AND 5
-    ),
-    category VARCHAR(255),
-    in_stock BOOLEAN,
-    metadata JSONB
+    )
 );
 
-INSERT INTO mock_items (description, rating, category, in_stock, metadata)
+INSERT INTO mock_items (description, rating)
 VALUES
-    ('Ergonomic metal keyboard', 4, 'Electronics', true, '{"color": "Silver", "location": "United States"}'::JSONB),
-    ('Plastic Keyboard', 4, 'Electronics', false, '{"color": "Black", "location": "Canada"}'::JSONB),
-    ('Sleek running shoes', 5, 'Footwear', true, '{"color": "Blue", "location": "China"}'::JSONB),
-    ('White jogging shoes', 3, 'Footwear', false, '{"color": "White", "location": "United States"}'::JSONB),
-    ('Generic shoes', 4, 'Footwear', true, '{"color": "Brown", "location": "Canada"}'::JSONB),
-    ('Compact digital camera', 5, 'Photography', false, '{"color": "Black", "location": "China"}'::JSONB),
-    ('Hardcover book on history', 2, 'Books', true, '{"color": "Brown", "location": "United States"}'::JSONB),
-    ('Organic green tea', 3, 'Groceries', true, '{"color": "Green", "location": "Canada"}'::JSONB),
-    ('Modern wall clock', 4, 'Home Decor', false, '{"color": "Silver", "location": "China"}'::JSONB),
-    ('Colorful kids toy', 1, 'Toys', true, '{"color": "Multicolor", "location": "United States"}'::JSONB),
-    ('Soft cotton shirt', 5, 'Apparel', true, '{"color": "Blue", "location": "Canada"}'::JSONB),
-    ('Innovative wireless earbuds', 5, 'Electronics', true, '{"color": "Black", "location": "China"}'::JSONB),
-    ('Sturdy hiking boots', 4, 'Footwear', true, '{"color": "Brown", "location": "United States"}'::JSONB),
-    ('Elegant glass table', 3, 'Furniture', true, '{"color": "Clear", "location": "Canada"}'::JSONB),
-    ('Refreshing face wash', 2, 'Beauty', false, '{"color": "White", "location": "China"}'::JSONB),
-    ('High-resolution DSLR', 4, 'Photography', true, '{"color": "Black", "location": "United States"}'::JSONB),
-    ('Paperback romantic novel', 3, 'Books', true, '{"color": "Multicolor", "location": "Canada"}'::JSONB),
-    ('Freshly ground coffee beans', 5, 'Groceries', true, '{"color": "Brown", "location": "China"}'::JSONB),
-    ('Artistic ceramic vase', 4, 'Home Decor', false, '{"color": "Multicolor", "location": "United States"}'::JSONB),
-    ('Interactive board game', 3, 'Toys', true, '{"color": "Multicolor", "location": "Canada"}'::JSONB),
-    ('Slim-fit denim jeans', 5, 'Apparel', false, '{"color": "Blue", "location": "China"}'::JSONB),
-    ('Fast charging power bank', 4, 'Electronics', true, '{"color": "Black", "location": "United States"}'::JSONB),
-    ('Comfortable slippers', 3, 'Footwear', true, '{"color": "Brown", "location": "Canada"}'::JSONB),
-    ('Classic leather sofa', 5, 'Furniture', false, '{"color": "Brown", "location": "China"}'::JSONB),
-    ('Anti-aging serum', 4, 'Beauty', true, '{"color": "White", "location": "United States"}'::JSONB),
-    ('Portable tripod stand', 4, 'Photography', true, '{"color": "Black", "location": "Canada"}'::JSONB),
-    ('Mystery detective novel', 2, 'Books', false, '{"color": "Multicolor", "location": "China"}'::JSONB),
-    ('Organic breakfast cereal', 5, 'Groceries', true, '{"color": "Brown", "location": "United States"}'::JSONB),
-    ('Designer wall paintings', 5, 'Home Decor', true, '{"color": "Multicolor", "location": "Canada"}'::JSONB),
-    ('Robot building kit', 4, 'Toys', true, '{"color": "Multicolor", "location": "China"}'::JSONB),
-    ('Sporty tank top', 4, 'Apparel', true, '{"color": "Blue", "location": "United States"}'::JSONB),
-    ('Bluetooth-enabled speaker', 3, 'Electronics', true, '{"color": "Black", "location": "Canada"}'::JSONB),
-    ('Winter woolen socks', 5, 'Footwear', false, '{"color": "Gray", "location": "China"}'::JSONB),
-    ('Rustic bookshelf', 4, 'Furniture', true, '{"color": "Brown", "location": "United States"}'::JSONB),
-    ('Moisturizing lip balm', 4, 'Beauty', true, '{"color": "Pink", "location": "Canada"}'::JSONB),
-    ('Lightweight camera bag', 5, 'Photography', false, '{"color": "Black", "location": "China"}'::JSONB),
-    ('Historical fiction book', 3, 'Books', true, '{"color": "Multicolor", "location": "United States"}'::JSONB),
-    ('Pure honey jar', 4, 'Groceries', true, '{"color": "Yellow", "location": "Canada"}'::JSONB),
-    ('Handcrafted wooden frame', 5, 'Home Decor', false, '{"color": "Brown", "location": "China"}'::JSONB),
-    ('Plush teddy bear', 4, 'Toys', true, '{"color": "Brown", "location": "United States"}'::JSONB),
-    ('Warm woolen sweater', 3, 'Apparel', false, '{"color": "Red", "location": "Canada"}'::JSONB);
+    ('Ergonomic metal keyboard', 4),
+    ('Plastic Keyboard', 4),
+    ('Sleek running shoes', 5),
+    ('White jogging shoes', 3),
+    ('Generic shoes', 4),
+    ('Compact digital camera', 5),
+    ('Hardcover book on history', 2),
+    ('Organic green tea', 3),
+    ('Modern wall clock', 4),
+    ('Colorful kids toy', 1),
+    ('Soft cotton shirt', 5),
+    ('Innovative wireless earbuds', 5),
+    ('Sturdy hiking boots', 4),
+    ('Elegant glass table', 3),
+    ('Refreshing face wash', 2),
+    ('High-resolution DSLR', 4),
+    ('Paperback romantic novel', 3),
+    ('Freshly ground coffee beans', 5),
+    ('Artistic ceramic vase', 4),
+    ('Interactive board game', 3),
+    ('Slim-fit denim jeans', 5),
+    ('Fast charging power bank', 4),
+    ('Comfortable slippers', 3),
+    ('Classic leather sofa', 5),
+    ('Anti-aging serum', 4),
+    ('Portable tripod stand', 4),
+    ('Mystery detective novel', 2),
+    ('Organic breakfast cereal', 5),
+    ('Designer wall paintings', 5),
+    ('Robot building kit', 4),
+    ('Sporty tank top', 4),
+    ('Bluetooth-enabled speaker', 3),
+    ('Winter woolen socks', 5),
+    ('Rustic bookshelf', 4),
+    ('Moisturizing lip balm', 4),
+    ('Lightweight camera bag', 5),
+    ('Historical fiction book', 3),
+    ('Pure honey jar', 4),
+    ('Handcrafted wooden frame', 5),
+    ('Plush teddy bear', 4),
+    ('Warm woolen sweater', 3);
 
 ALTER TABLE mock_items DROP COLUMN IF EXISTS sparse_embedding;
 ALTER TABLE mock_items ADD COLUMN sparse_embedding sparse;
@@ -68,7 +65,7 @@ UPDATE mock_items m
 SET sparse_embedding = (
     SELECT '[' || string_agg(
         CASE 
-            WHEN random() < 0.5 THEN '0'
+            WHEN random() < 0.3 THEN '0'
             ELSE trunc(random()*10)::text
         END, ',') || ']'
     FROM generate_series(1,10)

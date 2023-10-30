66370d52cfec087c12e2ec1dfe367ea04ad70de8
Neil Hansen <neil.hansen.31@gmail.com>
2023-10-30 12:46:36 -0700

    remove comments

--- pg_bm25/src/index_access/delete.rs
+++ pg_bm25/src/index_access/delete.rs
@@ -16,13 +16,6 @@ pub extern "C" fn ambulkdelete(
     let index_name = &index_relation.name().to_string();
     let parade_index = get_parade_index(index_name.into());
 
-    // let mut stats_binding = stats;
-
-    // if stats_binding.is_null() {
-    //     stats_binding =
-    //         unsafe { pg_sys::palloc0(std::mem::size_of::<pg_sys::IndexBulkDeleteResult>()).cast() };
-    // }
-
     if stats.is_null() {
         stats = unsafe {
             PgBox::from_pg(
@@ -30,14 +23,6 @@ pub extern "C" fn ambulkdelete(
             )
         };
     }
-
-    // let index_rel: pg_sys::Relation = unsafe { (*info).index };
-    // let index_relation = unsafe { PgRelation::from_pg(index_rel) };
-    // let index_name = index_relation.name().to_string();
-
-    // let parade_index = get_parade_index(index_name);
-    // parade_index.bulk_delete(stats_binding, callback, callback_state);
-    // stats_binding
 
     stats = parade_index.bulk_delete(stats, callback, callback_state);
     stats.into_pg()

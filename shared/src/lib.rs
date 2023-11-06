6adcbb11ce1f1ad25624c0277773424ec25153ae
Neil Hansen <neil.hansen.31@gmail.com>
2023-11-06 13:18:29 -0800

    typo

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -2,6 +2,6 @@ pub mod logs;
 pub mod telemetry;
 
 // We need to re-export the dependencies below, because they're used by our public macros.
-// This lets consumers of the macros use time without needing to also install these dependencies.
+// This lets consumers of the macros use them without needing to also install these dependencies.
 pub use pgrx;
 pub use serde_json;

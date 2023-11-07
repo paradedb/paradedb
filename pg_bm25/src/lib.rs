729ff20c0c9548fafc895f9c1527f37916765c21
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2023-11-06 16:25:46 -0800

    feat: Introduce the plog! macro to log to internal table (#496)

--- pg_bm25/src/lib.rs
+++ pg_bm25/src/lib.rs
@@ -1,4 +1,5 @@
 use pgrx::*;
+use shared::logs::ParadeLogsGlobal;
 use shared::telemetry;
 
 mod api;
@@ -9,18 +10,23 @@ mod operator;
 mod parade_index;
 mod tokenizers;
 
+// This is a flag that can be set by the user in a session to enable logs.
+// You need to initialize this in every extension that uses `plog!`.
+static PARADE_LOGS_GLOBAL: ParadeLogsGlobal = ParadeLogsGlobal::new("pg_bm25");
+
 pgrx::pg_module_magic!();
 
 extension_sql_file!("../sql/_bootstrap.sql", bootstrap);
 extension_sql_file!("../sql/_bootstrap_quickstart.sql");
 
-// initializes option parsing and telemetry
+// Initializes option parsing and telemetry
 #[allow(clippy::missing_safety_doc)]
 #[allow(non_snake_case)]
 #[pg_guard]
 pub unsafe extern "C" fn _PG_init() {
     index_access::options::init();
     telemetry::posthog::init("pg_bm25 Deployment");
+    PARADE_LOGS_GLOBAL.init();
 }
 
 /// This module is required by `cargo pgrx test` invocations.
@@ -34,5 +40,14 @@ pub mod pg_test {
     pub fn postgresql_conf_options() -> Vec<&'static str> {
         // return any postgresql.conf settings that are required for your tests
         vec![]
+    }
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pgrx::pg_schema]
+mod tests {
+    #[pgrx::pg_test]
+    fn test_parade_logs() {
+        shared::test_plog!("pg_bm25");
     }
 }
--- pg_search/src/lib.rs
+++ pg_search/src/lib.rs
@@ -1,4 +1,5 @@
 use pgrx::prelude::*;
+use shared::logs::ParadeLogsGlobal;
 use shared::telemetry;
 
 mod api;
@@ -7,12 +8,17 @@ pgrx::pg_module_magic!();
 
 extension_sql_file!("../sql/_bootstrap_quickstart.sql");
 
+// This is a flag that can be set by the user in a session to enable logs.
+// You need to initialize this in every extension that uses `plog!`.
+static PARADE_LOGS_GLOBAL: ParadeLogsGlobal = ParadeLogsGlobal::new("pg_search");
+
 // initializes telemetry
 #[allow(clippy::missing_safety_doc)]
 #[allow(non_snake_case)]
 #[pg_guard]
 pub unsafe extern "C" fn _PG_init() {
     telemetry::posthog::init("pg_search Deployment");
+    PARADE_LOGS_GLOBAL.init();
 }
 
 /// This module is required by `cargo pgrx test` invocations.
@@ -26,5 +32,14 @@ pub mod pg_test {
     pub fn postgresql_conf_options() -> Vec<&'static str> {
         // return any postgresql.conf settings that are required for your tests
         vec![]
+    }
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pgrx::pg_schema]
+mod tests {
+    #[pgrx::pg_test]
+    fn test_parade_logs() {
+        shared::test_plog!("pg_search");
     }
 }

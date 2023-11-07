d0eff0cd7253083c6b0d5a0d650d457007ca07ed
Neil Hansen <neil.hansen.31@gmail.com>
2023-11-07 13:53:43 -0800

    Finish tests

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod logs;
 pub mod telemetry;
+pub mod testing;
 
 // We need to re-export the dependencies below, because they're used by our public macros.
 // This lets consumers of the macros use them without needing to also install these dependencies.

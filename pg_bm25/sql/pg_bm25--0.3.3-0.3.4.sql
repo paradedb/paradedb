b2adf6c66d53d38763e44b8bd599e5f897c2e049
Ming Ying <ming.ying.nyc@gmail.com>
2023-11-13 11:35:58 -0500

    wip

---
+++ pg_bm25/sql/pg_bm25--0.3.3-0.3.4.sql
@@ -0,0 +1,4 @@
+\echo Use "ALTER EXTENSION pg_bm25 UPDATE TO '0.3.4'" to load this file. \quit
+
+
+
--- pg_bm25/sql/pg_bm25--0.3.6--0.3.7.sql
+++ pg_bm25/sql/pg_bm25--0.3.6--0.3.7.sql
@@ -1,2 +1,1 @@
--- Complain if this script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION pg_bm25 UPDATE TO '0.3.7'" to load this file. \quit

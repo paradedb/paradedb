54b7e026a1d9772831f6bcb3f72ba9346641a849
Suriya Kandaswamy <suriya@getretake.com>
2023-11-17 12:13:24 -0500

    dummy functions for table am to permit table creation

---
+++ pg_columnar/src/am_funcs.rs
@@ -0,0 +1,185 @@
+use pgrx::pg_sys::*;
+use core::ffi::c_int;
+use core::ffi::c_char;
+use core::ffi::c_void;
+use std::ptr;
+
+pub unsafe extern "C" fn memam_slot_callbacks(rel: Relation) -> *const TupleTableSlotOps {
+	return ptr::null();
+}
+
+pub unsafe extern "C" fn memam_scan_begin(rel: Relation, snapshot: Snapshot, nkeys: c_int, key: *mut ScanKeyData, pscan: ParallelTableScanDesc, flags: uint32) -> TableScanDesc {
+	return ptr::null_mut::<TableScanDescData>();
+}
+
+pub unsafe extern "C" fn memam_scan_end(scan: TableScanDesc) {
+
+}
+
+pub unsafe extern "C" fn memam_scan_rescan(scan: TableScanDesc, key: *mut ScanKeyData, set_params: bool, allow_strat: bool, allow_sync: bool, allow_pagemode: bool) {
+
+}
+
+pub unsafe extern "C" fn memam_scan_getnextslot(scan: TableScanDesc, direction: ScanDirection, slot: *mut TupleTableSlot) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_scan_set_tidrange(scan: TableScanDesc, mintid: ItemPointer, maxtid: ItemPointer) {
+
+}
+
+pub unsafe extern "C" fn memam_scan_getnextslot_tidrange(scan: TableScanDesc, direction: ScanDirection, slot: *mut TupleTableSlot) -> bool {
+	return false
+}
+
+pub unsafe extern "C" fn memam_parallelscan_estimate(rel: Relation) -> Size {
+	return table_block_parallelscan_estimate(rel);
+}
+
+pub unsafe extern "C" fn memam_parallelscan_initialize(rel: Relation, pscan: ParallelTableScanDesc) -> Size {
+	return table_block_parallelscan_initialize(rel, pscan);
+}
+
+pub unsafe extern "C" fn memam_parallelscan_reinitialize(rel: Relation, pscan: ParallelTableScanDesc) {
+	return table_block_parallelscan_reinitialize(rel, pscan);
+}
+
+pub unsafe extern "C" fn memam_index_fetch_begin(rel: Relation) -> *mut IndexFetchTableData {
+	return ptr::null_mut::<IndexFetchTableData>();
+}
+
+pub unsafe extern "C" fn memam_index_fetch_reset(data: *mut IndexFetchTableData) {
+
+}
+
+pub unsafe extern "C" fn memam_index_fetch_end(data: *mut IndexFetchTableData) {
+
+}
+
+pub unsafe extern "C" fn memam_index_fetch_tuple(scan: *mut IndexFetchTableData, tid: ItemPointer, snapshot: Snapshot, slot: *mut TupleTableSlot, call_again: *mut bool, all_dead: *mut bool) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_tuple_fetch_row_version(rel: Relation, tid: ItemPointer, snapshot: Snapshot, slot: *mut TupleTableSlot) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_tuple_tid_valid(scan: TableScanDesc, tid: ItemPointer) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_tuple_get_latest_tid(scan: TableScanDesc, tid: ItemPointer) {
+
+}
+
+pub unsafe extern "C" fn memam_tuple_satisfies_snapshot(rel: Relation, slot: *mut TupleTableSlot, snapshot: Snapshot) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_index_delete_tuples(rel: Relation, delstate: *mut TM_IndexDeleteOp) -> TransactionId {
+	return 0;
+}
+
+pub unsafe extern "C" fn memam_tuple_insert(rel: Relation, slot: *mut TupleTableSlot, cid: CommandId, options: c_int, bistate: *mut BulkInsertStateData) {
+
+}
+
+pub unsafe extern "C" fn memam_tuple_insert_speculative(rel: Relation, slot: *mut TupleTableSlot, cid: CommandId, options: c_int, bistate: *mut BulkInsertStateData, specToken: uint32) {
+
+}
+
+pub unsafe extern "C" fn memam_tuple_complete_speculative(rel: Relation, slot: *mut TupleTableSlot, specToken: uint32, succeeded: bool) {
+
+}
+
+pub unsafe extern "C" fn memam_multi_insert(rel: Relation, slots: *mut *mut TupleTableSlot, nslots: c_int, cid: CommandId, options: c_int, bistate: *mut BulkInsertStateData) {
+
+}
+
+pub unsafe extern "C" fn memam_tuple_delete(rel: Relation, tid: ItemPointer, cid: CommandId, snapshot: Snapshot, crosscheck: Snapshot, wait: bool, tmfd: *mut TM_FailureData, changingPart: bool) -> TM_Result {
+	return 0;
+}
+
+pub unsafe extern "C" fn memam_tuple_update(rel: Relation, otid: ItemPointer, slot: *mut TupleTableSlot, cid: CommandId, snapshot: Snapshot, crosscheck: Snapshot, wait: bool, tmfd: *mut TM_FailureData, lockmode: *mut LockTupleMode, update_indexes: *mut bool) -> TM_Result {
+	return 0;
+}
+
+pub unsafe extern "C" fn memam_tuple_lock(rel: Relation, tid: ItemPointer, snapshot: Snapshot, slot: *mut TupleTableSlot, cid: CommandId, mode: LockTupleMode, wait_policy: LockWaitPolicy, flags: uint8, tmfd: *mut TM_FailureData) -> TM_Result {
+	return 0;
+}
+
+pub unsafe extern "C" fn memam_finish_bulk_insert(rel: Relation, options: c_int) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_set_new_filenode(rel: Relation, newrnode: *const RelFileNode, persistence: c_char, freezeXid: *mut TransactionId, minmulti: *mut MultiXactId) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_nontransactional_truncate(rel: Relation) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_copy_data(rel: Relation, newrnode: *const RelFileNode) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_copy_for_cluster(NewTable: Relation, OldTable: Relation, OldIndex: Relation, use_sort: bool, OldestXmin: TransactionId, xid_cutoff: *mut TransactionId, multi_cutoff: *mut MultiXactId, num_tuples: *mut f64, tups_vacuumed: *mut f64, tups_recently_dead: *mut f64) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_vacuum(rel: Relation, params: *mut VacuumParams, bstrategy: BufferAccessStrategy) {
+
+}
+
+pub unsafe extern "C" fn memam_scan_analyze_next_block(scan: TableScanDesc, blockno: BlockNumber, bstrategy: BufferAccessStrategy) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_scan_analyze_next_tuple(scan: TableScanDesc, OldestXmin: TransactionId, liverows: *mut f64, deadrows: *mut f64, slot: *mut TupleTableSlot) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_index_build_range_scan(table_rel: Relation, index_rel: Relation, index_info: *mut IndexInfo, allow_sync: bool, anyvisible: bool, progress: bool, start_blockno: BlockNumber, numblocks: BlockNumber, callback: IndexBuildCallback, callback_state: *mut c_void, scan: TableScanDesc) -> f64 {
+	return 0.0;
+}
+
+pub unsafe extern "C" fn memam_index_validate_scan(table_rel: Relation, index_rel: Relation, index_info: *mut IndexInfo, snapshot: Snapshot, state: *mut ValidateIndexState) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_size(rel: Relation, forkNumber: ForkNumber) -> uint64 {
+	return 0;
+}
+
+pub unsafe extern "C" fn memam_relation_needs_toast_table(rel: Relation) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_relation_toast_am(rel: Relation) -> Oid {
+	return Oid::INVALID;
+}
+
+pub unsafe extern "C" fn memam_relation_fetch_toast_slice(toastrel: Relation, valueid: Oid, attrsize: int32, sliceoffset: int32, slicelength: int32, result: *mut varlena) {
+
+}
+
+pub unsafe extern "C" fn memam_relation_estimate_size(rel: Relation, attr_widths: *mut int32, pages: *mut BlockNumber, tuples: *mut f64, allvisfrac: *mut f64) {
+
+}
+
+pub unsafe extern "C" fn memam_scan_bitmap_next_block(scan: TableScanDesc, tbmres: *mut TBMIterateResult) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_scan_bitmap_next_tuple(scan: TableScanDesc, tbmres: *mut TBMIterateResult, slot: *mut TupleTableSlot) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_scan_sample_next_block(scan: TableScanDesc, scanstate: *mut SampleScanState) -> bool {
+	return false;
+}
+
+pub unsafe extern "C" fn memam_scan_sample_next_tuple(scan: TableScanDesc, scanstate: *mut SampleScanState, slot: *mut TupleTableSlot) -> bool {
+	return false;
+}
--- pg_columnar/src/lib.rs
+++ pg_columnar/src/lib.rs
@@ -2,6 +2,9 @@ use pgrx::prelude::*;
 use shared::logs::ParadeLogsGlobal;
 use shared::telemetry;
 
+mod am_funcs;
+use am_funcs::*;
+
 // This is a flag that can be set by the user in a session to enable logs.
 // You need to initialize this in every extension that uses `plog!`.
 static PARADE_LOGS_GLOBAL: ParadeLogsGlobal = ParadeLogsGlobal::new("pg_columnar");
@@ -24,51 +27,55 @@ extern "C" fn mem_tableam_handler(
         unsafe { PgBox::<pg_sys::TableAmRoutine>::alloc_node(pg_sys::NodeTag::T_TableAmRoutine) };
 
     amroutine.type_ = pg_sys::NodeTag::T_TableAmRoutine;
-    amroutine.slot_callbacks = None;
-    amroutine.scan_begin = None;
-    amroutine.scan_end = None;
-    amroutine.scan_rescan = None;
-    amroutine.scan_getnextslot = None;
-    amroutine.scan_set_tidrange = None;
-    amroutine.scan_getnextslot_tidrange = None;
-    amroutine.parallelscan_estimate = None;
-    amroutine.parallelscan_initialize = None;
-    amroutine.parallelscan_reinitialize = None;
-    amroutine.index_fetch_begin = None;
-    amroutine.index_fetch_reset = None;
-    amroutine.index_fetch_end = None;
-    amroutine.index_fetch_tuple = None;
-    amroutine.tuple_fetch_row_version = None;
-    amroutine.tuple_tid_valid = None;
-    amroutine.tuple_get_latest_tid = None;
-    amroutine.tuple_satisfies_snapshot = None;
-    amroutine.index_delete_tuples = None;
-    amroutine.tuple_insert = None;
-    amroutine.tuple_insert_speculative = None;
-    amroutine.tuple_complete_speculative = None;
-    amroutine.multi_insert = None;
-    amroutine.tuple_delete = None;
-    amroutine.tuple_update = None;
-    amroutine.tuple_lock = None;
-    amroutine.finish_bulk_insert = None;
-    amroutine.relation_set_new_filenode = None;
-    amroutine.relation_nontransactional_truncate = None;
-    amroutine.relation_copy_data = None;
-    amroutine.relation_copy_for_cluster = None;
-    amroutine.relation_vacuum = None;
-    amroutine.scan_analyze_next_block = None;
-    amroutine.scan_analyze_next_tuple = None;
-    amroutine.index_build_range_scan = None;
-    amroutine.index_validate_scan = None;
-    amroutine.relation_size = None;
-    amroutine.relation_needs_toast_table = None;
-    amroutine.relation_toast_am = None;
-    amroutine.relation_fetch_toast_slice = None;
-    amroutine.relation_estimate_size = None;
-    amroutine.scan_bitmap_next_block = None;
-    amroutine.scan_bitmap_next_tuple = None;
-    amroutine.scan_sample_next_block = None;
-    amroutine.scan_sample_next_tuple = None;
+
+    amroutine.slot_callbacks = Some(memam_slot_callbacks);
+
+    amroutine.scan_begin = Some(memam_scan_begin);
+    amroutine.scan_end = Some(memam_scan_end);
+    amroutine.scan_rescan = Some(memam_scan_rescan);
+    amroutine.scan_getnextslot = Some(memam_scan_getnextslot);
+    amroutine.scan_set_tidrange = Some(memam_scan_set_tidrange);
+    amroutine.scan_getnextslot_tidrange = Some(memam_scan_getnextslot_tidrange);
+
+    amroutine.parallelscan_estimate = Some(memam_parallelscan_estimate);
+    amroutine.parallelscan_initialize = Some(memam_parallelscan_initialize);
+    amroutine.parallelscan_reinitialize = Some(memam_parallelscan_reinitialize);
+
+    amroutine.index_fetch_begin = Some(memam_index_fetch_begin);
+    amroutine.index_fetch_reset = Some(memam_index_fetch_reset);
+    amroutine.index_fetch_end = Some(memam_index_fetch_end);
+    amroutine.index_fetch_tuple = Some(memam_index_fetch_tuple);
+    amroutine.tuple_fetch_row_version = Some(memam_tuple_fetch_row_version);
+    amroutine.tuple_tid_valid = Some(memam_tuple_tid_valid);
+    amroutine.tuple_get_latest_tid = Some(memam_tuple_get_latest_tid);
+    amroutine.tuple_satisfies_snapshot = Some(memam_tuple_satisfies_snapshot);
+    amroutine.index_delete_tuples = Some(memam_index_delete_tuples);
+    amroutine.tuple_insert = Some(memam_tuple_insert);
+    amroutine.tuple_insert_speculative = Some(memam_tuple_insert_speculative);
+    amroutine.tuple_complete_speculative = Some(memam_tuple_complete_speculative);
+    amroutine.multi_insert = Some(memam_multi_insert);
+    amroutine.tuple_delete = Some(memam_tuple_delete);
+    amroutine.tuple_update = Some(memam_tuple_update);
+    amroutine.tuple_lock = Some(memam_tuple_lock);
+    amroutine.finish_bulk_insert = Some(memam_finish_bulk_insert);
+    amroutine.relation_set_new_filenode = Some(memam_relation_set_new_filenode);
+    amroutine.relation_nontransactional_truncate = Some(memam_relation_nontransactional_truncate);
+    amroutine.relation_copy_data = Some(memam_relation_copy_data);
+    amroutine.relation_copy_for_cluster = Some(memam_relation_copy_for_cluster);
+    amroutine.relation_vacuum = Some(memam_relation_vacuum);
+    amroutine.scan_analyze_next_block = Some(memam_scan_analyze_next_block);
+    amroutine.scan_analyze_next_tuple = Some(memam_scan_analyze_next_tuple);
+    amroutine.index_build_range_scan = Some(memam_index_build_range_scan);
+    amroutine.index_validate_scan = Some(memam_index_validate_scan);
+    amroutine.relation_size = Some(memam_relation_size);
+    amroutine.relation_needs_toast_table = Some(memam_relation_needs_toast_table);
+    amroutine.relation_toast_am = Some(memam_relation_toast_am);
+    amroutine.relation_fetch_toast_slice = Some(memam_relation_fetch_toast_slice);
+    amroutine.relation_estimate_size = Some(memam_relation_estimate_size);
+    amroutine.scan_bitmap_next_block = Some(memam_scan_bitmap_next_block);
+    amroutine.scan_bitmap_next_tuple = Some(memam_scan_bitmap_next_tuple);
+    amroutine.scan_sample_next_block = Some(memam_scan_sample_next_block);
+    amroutine.scan_sample_next_tuple = Some(memam_scan_sample_next_tuple);
 
     amroutine.into_pg_boxed().as_ptr()
 }

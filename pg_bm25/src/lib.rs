01e7403a65516cd3979c1f4518ef8d16899c1418
Philippe Noël <philippemnoel@gmail.com>
2023-11-30 12:02:15 -0500

    fix path

--- pg_bm25/src/lib.rs
+++ pg_bm25/src/lib.rs
@@ -18,8 +18,8 @@ pgrx::pg_module_magic!();
 
 extension_sql_file!("../sql/_bootstrap.sql");
 
-pub const SETUP_SQL: &str = include_str!("index_setup.sql");
-pub const QUERY_SQL: &str = include_str!("search_query.sql");
+pub const SETUP_SQL: &str = include_str!("../sql/index_setup.sql");
+pub const QUERY_SQL: &str = include_str!("../sql/search_query.sql");
 
 // Initializes option parsing and telemetry
 #[allow(clippy::missing_safety_doc)]

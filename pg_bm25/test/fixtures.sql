525fd1158a8d1988a3f4abeca26996c720974653
Ming Ying <ming.ying.nyc@gmail.com>
2023-11-30 15:39:09 -0500

    Add regression test

--- pg_bm25/test/fixtures.sql
+++ pg_bm25/test/fixtures.sql
@@ -7,8 +7,10 @@ CREATE TABLE search_config AS SELECT * FROM paradedb.bm25_test_table;
 CREATE TABLE tokenizer_config AS SELECT * FROM paradedb.bm25_test_table;
 CREATE TABLE bm25_search AS SELECT * FROM paradedb.bm25_test_table;
 CREATE TABLE aggregations AS SELECT * FROM paradedb.bm25_test_table;
+CREATE TABLE dump_bm25 AS SELECT * FROM paradedb.bm25_test_table;
 
 CREATE INDEX idxmockitems ON paradedb.bm25_test_table USING bm25((bm25_test_table.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
 CREATE INDEX idxsearchconfig ON search_config USING bm25 ((search_config.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
 CREATE INDEX idxbm25search ON bm25_search USING bm25 ((bm25_search.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
 CREATE INDEX idxaggregations ON bm25_search USING bm25 ((bm25_search.*)) WITH (text_fields='{"description": {"fast": true}, "category": {"fast": true}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
+CREATE INDEX idxdumpbm25 ON dump_bm25 USING bm25 ((dump_bm25.*)) WITH (text_fields='{"description": {}, "category": {}}', numeric_fields='{"rating": {}}', boolean_fields='{"in_stock": {}}', json_fields='{"metadata": {}}');
---
+++ pg_bm25/test/sql/dump_bm25.sql
@@ -0,0 +1,1 @@
+SELECT * FROM paradedb.dump_bm25('idxdumpbm25') ORDER BY heap_tid;

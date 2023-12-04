0b69d8222bc7064a5065c3578637eaf05f3bf2f7
Baasit <baasisek01@gmail.com>
2023-12-04 23:12:01 +0100

    test: unit tests for pg_search (#573)
    Co-authored-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- pg_search/src/api.rs
+++ pg_search/src/api.rs
@@ -22,9 +22,81 @@ pub fn weighted_mean(a: f64, b: f64, weights: Vec<f64>) -> f64 {
     );
 
     assert!(
-        (weight_a + weight_b - 1.0).abs() < std::f64::EPSILON,
+        (weight_a + weight_b - 1.0).abs() < f64::EPSILON,
         "Weights must add up to 1"
     );
 
     a * weight_a + b * weight_b
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pgrx::pg_schema]
+mod tests {
+    use pgrx::*;
+    use shared::testing::SETUP_SQL;
+
+    use super::{minmax_norm, weighted_mean};
+
+    #[pg_test]
+    fn test_minmax_norm() {
+        let value = 60.0;
+        let min = 20.0;
+        let max = 30.0;
+        assert_eq!(minmax_norm(value, min, max), (value - min) / (max - min));
+    }
+
+    #[pg_test]
+    fn test_weighted_mean() {
+        let result = weighted_mean(3.0, 7.0, vec![0.4, 0.6]);
+        assert!((result - 6.0).abs() > f64::EPSILON);
+    }
+
+    #[pg_test]
+    fn test_weighted_mean_spi() {
+        Spi::run(SETUP_SQL).expect("failed to create index and table");
+
+        let add_ratings = r#"
+            ALTER TABLE one_republic_songs ADD COLUMN rating vector(7);
+
+            WITH NumberedRows AS (
+                SELECT ctid,
+                    ROW_NUMBER() OVER () as row_num
+                FROM one_republic_songs
+            )
+            UPDATE one_republic_songs m
+            SET rating = ('[' ||
+                ((n.row_num + 1) % 5 + 1)::integer || ',' ||
+                ((n.row_num + 2) % 5 + 2)::integer || ',' ||
+                ((n.row_num + 2) % 5 + 3)::integer || ',' ||
+                ((n.row_num + 2) % 5 + 4)::integer || ',' ||
+                ((n.row_num + 2) % 5 + 5)::integer || ',' ||
+                ((n.row_num + 2) % 5 + 6)::integer || ',' ||
+                ((n.row_num + 3) % 5 + 7)::integer || ']')::vector
+            FROM NumberedRows n
+            WHERE m.ctid = n.ctid;
+            "#;
+        Spi::run(add_ratings).expect("failed to add ratings column to table");
+
+        // hnsw index
+        Spi::run("CREATE INDEX ON one_republic_songs USING hnsw (rating vector_l2_ops);")
+            .expect("failed to create hnsw index");
+
+        let query = r#"
+    SELECT
+        paradedb.weighted_mean(
+            paradedb.minmax_bm25(song_id, 'idx_one_republic', 'lyrics:im'),
+            1 - paradedb.minmax_norm(
+              '[1,2,3,4,5,6,7]' <-> rating,
+              MIN('[1,2,3,4,5,6,7]' <-> rating) OVER (),
+              MAX('[1,2,3,4,5,6,7]' <-> rating) OVER ()
+            ),
+            ARRAY[0.8,0.2]
+        ) as score_hybrid
+    FROM one_republic_songs
+    ORDER BY score_hybrid DESC;
+            "#;
+
+        let mean = Spi::get_one::<f64>(query).expect("failed to get weighted mean");
+        assert!(mean.unwrap() > f64::EPSILON);
+    }
 }

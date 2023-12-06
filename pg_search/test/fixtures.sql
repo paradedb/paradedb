eceee1f38bd6498646901508fec5841b0953f795
Philippe Noël <philippemnoel@gmail.com>
2023-12-06 17:14:34 -0500

    nit

--- pg_search/test/fixtures.sql
+++ pg_search/test/fixtures.sql
@@ -3,4 +3,4 @@ CREATE EXTENSION IF NOT EXISTS pg_search CASCADE;
 
 CALL paradedb.create_search_test_table();
 
-CREATE TABLE mock_items AS SELECT * FROM paradedb.search_test_table;
+CREATE TABLE IF NOT EXISTS mock_items AS SELECT * FROM paradedb.search_test_table;

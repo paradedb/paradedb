160e32fa33fd8d4696180f3ab967950f8f19e508
Philippe Noël <philippemnoel@gmail.com>
2023-12-06 12:19:54 -0500

    try this instad

--- pg_search/test/runtests.sh
+++ pg_search/test/runtests.sh
@@ -211,17 +211,17 @@ function run_tests() {
 
   # Execute tests using pg_regress
   echo "Running tests..."
-  echo "$BASEDIR"
   ${REGRESS} --use-existing --dbname=test_db --inputdir="${BASEDIR}/test/" "${TESTS[@]}"
-  if [ -f "$BASEDIR/regression.diffs" ]; then
+  exit_status=$?
+  if [ $exit_status -ne 0 ]; then
     echo "Some test(s) failed! Printing the diff between the expected and actual test results..."
     cat "$BASEDIR/regression.diffs"
-  fi
 
-  # Uncomment this to display test ERROR logs if you need to debug. Note that many of these errors are
-  # expected, since we are testing error handling/invalid cases in our regression tests.
-  echo "Displaying PostgreSQL ERROR logs from tests..."
-  # cat "$BASEDIR/test/test_logs.log"
+    # Uncomment this to display test ERROR logs if you need to debug. Note that many of these errors are
+    # expected, since we are testing error handling/invalid cases in our regression tests.
+    echo "Displaying PostgreSQL ERROR logs from tests..."
+    # cat "$BASEDIR/test/test_logs.log"
+  fi
 }
 
 # Loop over PostgreSQL versions

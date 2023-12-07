4318e7af79f35e31e888e31070062405dd5b3f2e
Neil Hansen <neil.hansen.31@gmail.com>
2023-12-07 13:09:10 -0800

    sql tests pass

--- pg_bm25/test/sql/index_config.sql
+++ pg_bm25/test/sql/index_config.sql
@@ -1,116 +1,129 @@
 -- Invalid create_bm25
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items'
+	index_name => 'index_config',
+	table_name => 'index_config'
 );
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
+	index_name => 'index_config',
+	table_name => 'index_config',
 	key_field => 'id'
 );
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
+	index_name => 'index_config',
+	table_name => 'index_config',
 	key_field => 'id',
 	invalid_field => '{}'		
 );
 
 -- Default text field
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
 	key_field => 'id',
-    text_fields => '{"description": {}}'
+	text_fields => '{"description": {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Text field with options
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    text_fields => '{"description": {"fast": true, "tokenizer": { "type": "en_stem" }, "record": "freq", "normalizer": "raw"}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	text_fields => '{"description": {"fast": true, "tokenizer": { "type": "en_stem" }, "record": "freq", "normalizer": "raw"}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Multiple text fields
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    text_fields => '{"description": {fast: true, tokenizer: { type: "en_stem" }, record: "freq", normalizer: "raw"}, category: {}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	text_fields => '{"description": {fast: true, tokenizer: { type: "en_stem" }, record: "freq", normalizer: "raw"}, category: {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Default numeric field
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    numeric_fields => '{"rating": {}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	numeric_fields => '{"rating": {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Numeric field with options
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    numeric_fields => '{"rating": {"fast": false}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	numeric_fields => '{"rating": {"fast": false}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Default boolean field
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    boolean_fields => '{"in_stock": {}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	boolean_fields => '{"in_stock": {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Boolean field with options
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    boolean_fields => '{"in_stock": {"fast": false}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	boolean_fields => '{"in_stock": {"fast": false}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Default Json field
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    json_fields => '{"metadata": {}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	json_fields => '{"metadata": {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Json field with options
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    json_fields => '{metadata: {fast: true, expand_dots: false, tokenizer: { type: "raw" }, normalizer: "raw"}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	json_fields => '{metadata: {fast: true, expand_dots: false, tokenizer: { type: "raw" }, normalizer: "raw"}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');
 
 -- Multiple fields
 CALL paradedb.create_bm25(
-	index_name => 'mock_items',
-	table_name => 'mock_items',
-    key_field => 'id',
-    text_fields => '{description: {}, category: {}}', numeric_fields='{rating: {}}', boolean_fields='{in_stock: {}}', json_fields='{metadata: {}}'
+	index_name => 'index_config',
+	table_name => 'index_config',
+	schema_name => 'paradedb',
+	key_field => 'id',
+	text_fields => '{description: {}, category: {}}',
+	numeric_fields => '{rating: {}}',
+	boolean_fields => '{in_stock: {}}',
+	json_fields => '{metadata: {}}'
 );
-SELECT * from paradedb.schema_bm25('mock_items');
-CALL paradedb.drop_bm25('mock_items');
+SELECT * from paradedb.schema_bm25('index_config_bm25_index');
+CALL paradedb.drop_bm25('index_config');

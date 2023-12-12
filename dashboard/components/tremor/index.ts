99021f5ea160e0208e529e8c94c4ef495164973e
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2023-12-12 11:22:10 -0500

    chore: Remove `dashboard/` from this repo (#631)

--- dashboard/components/tremor/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./card";
-export * from "./button";
-/*
-    Custom ParadeDB components that override the styling of Tremor's default
-    components. All components in this directory should be exported here.
-*/
-
-export * from "./button";
-export * from "./card";

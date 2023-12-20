81468d12ec7fe7e55eeeb574257f93b03f20fe5b
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2023-12-20 15:40:35 -0500

    feat: Finish ClickBench setup (#15)
    * fixy
    
    * rm
    
    * it works with inserts
    
    * clean up the script, make it robust

---
+++ pg_columnar/benchmarks/clickbench/README.md
@@ -0,0 +1,29 @@
+# ParadeDB
+
+ParadeDB is an alternative to ElasticSearch built on PostgreSQL.
+
+- [Homepage](https://paradedb.com)
+- [GitHub](https://github.com/paradedb/paradedb)
+
+## Running the benchmark
+
+### Local
+
+The benchmarks are configured to run locally via `cargo pgrx bench` within `pg_columnar`,
+
+### Official
+
+The benchmark has been configured for a `c6a.4xlarge` running Ubuntu 22.04 and can be run without attendance.
+
+```
+export FQDN=ec2-127-0-0-1.compute-1.amazonaws.com
+scp -i ~/.ssh/aws.pem *.sh *.sql ubuntu@$FQDN:~
+ssh -i ~/.ssh/aws.pem ubuntu@$FQDN ./benchmark.sh
+```
+
+
+List of things we can do to tune, based on postgresql-tuned and datafusion:
+
+- Check the datafusion one, and do the same ones onc we have them
+- Check the postgresql-tuned, and do the same one
+- Check the clickbench full README
---
+++ pg_columnar/src/bin/clickbench.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Set default to "hot" if no argument is provided
+    let benchmark_mode = if args.len() > 1 { &args[1] } else { "hot" };
+
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+
+    // Function to recursively find the pg_columnar directory
+    fn find_pg_columnar_dir(path: &Path) -> Option<PathBuf> {
+        if path.ends_with("pg_columnar") {
+            Some(path.to_path_buf())
+        } else if path.ends_with("rd") {
+            let pg_columnar_path = path.join("pg_columnar");
+            if pg_columnar_path.exists() {
+                Some(pg_columnar_path)
+            } else {
+                None
+            }
+        } else if let Some(parent) = path.parent() {
+            find_pg_columnar_dir(parent)
+        } else {
+            None
+        }
+    }
+
+    let pg_columnar_dir = find_pg_columnar_dir(&current_dir)
+        .expect("Failed to find pg_columnar directory");
+
+    let script_path = pg_columnar_dir.join("benchmarks/clickbench/benchmark.sh");
+
+    if env::current_dir().unwrap() != pg_columnar_dir {
+        env::set_current_dir(&pg_columnar_dir).expect("Failed to change directory");
+    }
+
+    Command::new("sh")
+        .arg(script_path)
+        .arg("-t")
+        .arg("pgrx")
+        .arg("-s")
+        .arg(benchmark_mode)
+        .status()
+        .expect("Failed to execute benchmark script");
+}

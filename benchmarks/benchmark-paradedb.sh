55531b9b7cd945408144fd96da20974f6ea20e48
Philippe Noël <philippemnoel@gmail.com>
2023-12-27 17:49:18 -0500

    rm hypopg

--- benchmarks/benchmark-paradedb.sh
+++ benchmarks/benchmark-paradedb.sh
@@ -94,7 +94,6 @@ if [ "$FLAG_TAG" == "local" ]; then
     --build-arg PGAUDIT_VERSION="1.7.0" \
     --build-arg POSTGIS_VERSION="3.4.1" \
     --build-arg PGROUTING_VERSION="3.6.1" \
-    --build-arg HYPOPG_VERSION="1.4.0" \
     --build-arg RUM_VERSION="1.3.13" \
     --build-arg PGSODIUM_VERSION="3.1.9" \
     --build-arg PGFINCORE_VERSION="1.3.1" \

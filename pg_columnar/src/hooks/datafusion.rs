87e61246bdf468ba8e406754a0017a1f1e62ce4a
Ming <ming.ying.nyc@gmail.com>
2023-12-31 15:16:50 -0500

    fix: Reset bulk insert state (#39)

--- pg_columnar/src/hooks/datafusion.rs
+++ pg_columnar/src/hooks/datafusion.rs
@@ -4,11 +4,14 @@ use datafusion::common::arrow::array::types::UInt64Type;
 use datafusion::common::arrow::array::RecordBatch;
 use pgrx::hooks::PgHooks;
 use pgrx::*;
+use std::ffi::CStr;
 
-use crate::hooks::utils::{planned_stmt_is_columnar, send_tuples_if_necessary};
+use crate::hooks::utils::{
+    copy_stmt_is_columnar, planned_stmt_is_columnar, send_tuples_if_necessary,
+};
 use crate::nodes::root::RootPlanNode;
 use crate::nodes::utils::DatafusionPlanTranslator;
-use crate::tableam::utils::CONTEXT;
+use crate::tableam::utils::{BulkInsertState, BULK_INSERT_STATE, CONTEXT};
 
 pub struct DatafusionHook;
 
@@ -61,5 +64,51 @@ impl PgHooks for DatafusionHook {
 
             HookResult::new(())
         }
+    }
+
+    fn process_utility_hook(
+        &mut self,
+        pstmt: PgBox<pg_sys::PlannedStmt>,
+        query_string: &CStr,
+        read_only_tree: Option<bool>,
+        context: pg_sys::ProcessUtilityContext,
+        params: PgBox<pg_sys::ParamListInfoData>,
+        query_env: PgBox<pg_sys::QueryEnvironment>,
+        dest: PgBox<pg_sys::DestReceiver>,
+        completion_tag: *mut pg_sys::QueryCompletion,
+        prev_hook: fn(
+            pstmt: PgBox<pg_sys::PlannedStmt>,
+            query_string: &CStr,
+            read_only_tree: Option<bool>,
+            context: pg_sys::ProcessUtilityContext,
+            params: PgBox<pg_sys::ParamListInfoData>,
+            query_env: PgBox<pg_sys::QueryEnvironment>,
+            dest: PgBox<pg_sys::DestReceiver>,
+            completion_tag: *mut pg_sys::QueryCompletion,
+        ) -> HookResult<()>,
+    ) -> HookResult<()> {
+        let plan = pstmt.utilityStmt;
+
+        if unsafe { (*plan).type_ } == pg_sys::NodeTag::T_CopyStmt {
+            let copy_stmt = plan as *mut pg_sys::CopyStmt;
+
+            if unsafe { copy_stmt_is_columnar(copy_stmt) } {
+                let mut bulk_insert_state = BULK_INSERT_STATE.lock().unwrap();
+                *bulk_insert_state = BulkInsertState::new();
+            }
+        }
+
+        prev_hook(
+            pstmt,
+            query_string,
+            read_only_tree,
+            context,
+            params,
+            query_env,
+            dest,
+            completion_tag,
+        );
+
+        HookResult::new(())
     }
 }

42e56ccf35148dc389e2a6150b4c629c67010933
Ming <ming.ying.nyc@gmail.com>
2024-01-04 13:57:56 -0500

    feat: Enable SIMD (#50)
    Co-authored-by: Philippe Noël <philippemnoel@gmail.com>

--- pg_columnar/src/datafusion/error.rs
+++
@@ -1,5 +0,0 @@
-use datafusion::common::DataFusionError;
-
-pub fn datafusion_err_to_string() -> impl Fn(DataFusionError) -> String {
-    move |dfe: DataFusionError| -> String { format!("{}", dfe) }
-}

603f9505c1f12a2c770388c204eae5ae290343fb
Ming <ming.ying.nyc@gmail.com>
2024-01-10 15:56:13 -0500

    feat: Make `init` a procedure (#81)

--- pg_columnar/benchmarks/clickbench/benchmark_hot.sql
+++ pg_columnar/benchmarks/clickbench/benchmark_hot.sql
@@ -4,8 +4,8 @@ CREATE EXTENSION pg_columnar;
 \echo Initialize ParadeDB context...
 CALL paradedb.init();
 
-\echo Creating persistent table...
-\i create_cold.sql
+\echo Creating temporary table...
+\i create_hot.sql
 
 \echo Loading data...
 \timing on
@@ -13,4 +13,6 @@ TRUNCATE hits;
 \copy hits FROM 'hits_100k_rows.csv' WITH (FORMAT CSV, QUOTE '"', ESCAPE '"');
 VACUUM FREEZE hits;
 
--- In benchmark_cold.sql, we run the queries via `./run.sh`
+\echo Running ClickBench queries...
+\timing on
+\i queries.sql
---
+++ pg_columnar/src/lib.rs
@@ -0,0 +1,38 @@
+#![allow(non_snake_case)]
+
+mod api;
+mod datafusion;
+mod hooks;
+mod nodes;
+mod tableam;
+
+use pgrx::*;
+use shared::logs::ParadeLogsGlobal;
+use shared::telemetry;
+
+use crate::hooks::ParadeHook;
+
+pgrx::pg_module_magic!();
+extension_sql_file!("../sql/_bootstrap.sql");
+
+// This is a flag that can be set by the user in a session to enable logs.
+// You need to initialize this in every extension that uses `plog!`.
+static PARADE_LOGS_GLOBAL: ParadeLogsGlobal = ParadeLogsGlobal::new("pg_columnar");
+static mut PARADE_HOOK: ParadeHook = ParadeHook;
+
+#[pg_guard]
+pub extern "C" fn _PG_init() {
+    telemetry::posthog::init("pg_columnar deployment");
+    PARADE_LOGS_GLOBAL.init();
+
+    unsafe { register_hook(&mut PARADE_HOOK) };
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {}
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        vec![]
+    }
+}

c02202466fb6d1f0bdabcc868476de8673490854
Neil Hansen <neil.hansen.31@gmail.com>
2024-01-15 18:43:21 -0800

    remove unsafe

--- pg_bm25/src/index_access/utils.rs
+++ pg_bm25/src/index_access/utils.rs
@@ -87,7 +87,7 @@ pub unsafe fn row_to_index_entries(
     values: *mut pg_sys::Datum,
     field_lookup: &HashMap<String, IndexKey>,
 ) -> Result<Vec<IndexEntry>, IndexError> {
-    let row = unsafe { std::slice::from_raw_parts(values, 1)[0] };
+    let row = std::slice::from_raw_parts(values, 1)[0];
     let td =
         pg_sys::pg_detoast_datum(row.cast_mut_ptr::<pg_sys::varlena>()) as pg_sys::HeapTupleHeader;
 

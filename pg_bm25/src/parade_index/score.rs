b5ec586436141e49a2e52a5da86bf4a21f289eff
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2024-01-18 09:11:08 -0800

    fix: Stabilize result order on tied BM25 scores (#696)

---
+++ pg_bm25/src/parade_index/score.rs
@@ -0,0 +1,29 @@
+use std::cmp::Ordering;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct ParadeIndexScore {
+    pub bm25: f32,
+    pub key: i64,
+}
+
+// We do these custom trait impls, because we want these to be sortable so:
+// - they're ordered descending by bm25 score.
+// - in case of a tie, they're ordered by ascending key.
+
+impl PartialEq for ParadeIndexScore {
+    fn eq(&self, other: &Self) -> bool {
+        self.bm25 == other.bm25 && self.key == other.key
+    }
+}
+
+impl PartialOrd for ParadeIndexScore {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.bm25 == other.bm25 {
+            other.key.partial_cmp(&self.key)
+        } else {
+            self.bm25.partial_cmp(&other.bm25)
+        }
+    }
+}

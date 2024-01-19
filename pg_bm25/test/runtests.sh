8edf21a838b88acd595bd9623226e1103b46973d
Philippe Noël <philippemnoel@gmail.com>
2024-01-19 12:57:20 -0500

    pg_regress test

--- pg_bm25/test/runtests.sh
+++ pg_bm25/test/runtests.sh
@@ -194,7 +194,7 @@ function run_tests() {
   else
     # Use cargo-pgx to install the extension for the specified version
     echo "Installing pg_bm25 extension onto the test database..."
-    cargo pgrx install --pg-config="$PG_BIN_PATH/pg_config" --profile dev > /dev/null
+    cargo pgrx install --features icu --pg-config="$PG_BIN_PATH/pg_config" --profile dev > /dev/null
   fi
 
   # Configure shared_preload_libraries to include pg_bm25

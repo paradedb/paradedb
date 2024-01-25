ecbf3c9da46423725c7976c5f162f294246a4ba8
Ming Ying <ming.ying.nyc@gmail.com>
2024-01-25 10:25:49 -0500

    Move init to datafusioncontext

--- pg_analytics/src/api/init.rs
+++ pg_analytics/src/api/init.rs
@@ -1,4 +1,3 @@
-use async_std::task;
 use deltalake::datafusion::execution::runtime_env::{RuntimeConfig, RuntimeEnv};
 use deltalake::datafusion::prelude::{SessionConfig, SessionContext};
 use pgrx::*;
@@ -18,35 +17,5 @@ extension_sql!(
 #[pg_guard]
 #[no_mangle]
 pub extern "C" fn init() {
-    init_impl().expect("Failed to initialize context");
-}
-
-#[inline]
-fn init_impl() -> Result<(), ParadeError> {
-    let session_config = SessionConfig::from_env()?.with_information_schema(true);
-
-    let rn_config = RuntimeConfig::new();
-    let runtime_env = RuntimeEnv::new(rn_config)?;
-
-    DatafusionContext::with_write_lock(|mut context_lock| {
-        let mut context = SessionContext::new_with_config_rt(session_config, Arc::new(runtime_env));
-
-        // Create schema directory if it doesn't exist
-        ParadeDirectory::create_delta_path()?;
-
-        // Register catalog list
-        context.register_catalog_list(Arc::new(ParadeCatalogList::try_new()?));
-
-        // Create and register catalog
-        let catalog = ParadeCatalog::try_new()?;
-        task::block_on(catalog.init())?;
-        context.register_catalog(PARADE_CATALOG, Arc::new(catalog));
-
-        // Set context
-        *context_lock = Some(context);
-
-        Ok::<(), ParadeError>(())
-    })?;
-
-    Ok(())
+    let _ = DatafusionContext::init().expect("Failed to initialize context");
 }

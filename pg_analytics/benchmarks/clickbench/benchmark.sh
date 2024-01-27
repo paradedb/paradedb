ecb99bb8a0adf0264f0b8e68e3963e54605aa6f3
Philippe Noël <philippemnoel@gmail.com>
2024-01-27 16:30:31 -0500

    Add basic structure for TPC-H, need to dbgen and load

--- pg_analytics/benchmarks/clickbench/benchmark.sh
+++ pg_analytics/benchmarks/clickbench/benchmark.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-
-# This script benchmarks the performance of pg_analytics using the TPC-H benchmark
+# This script benchmarks the performance of pg_analytics using the ClickBench benchmkark
 # suite. It is supported on both Ubuntu and macOS, for local development via `cargo` as
 # well as in CI testing via Docker.
 #
-# The local development versions runs the smaller dataset
-# TODO: Write description
+# The local development version runs a smaller subset of the hits dataset, hits_100k_rows.csv,
+# which is a randomly sampled 100,000 rows from the full ClickBench dataset, hits.csv. It is roughly
+# 0.1% (~0.075GB) of the full dataset of 100M rows (~75GB). Other dataset sizes available inckude:
+# - hits_500k_rows.csv.gz
+# - hits_1m_rows.csv.gz
+# The local development version is intended for quick iteration and is
+# designed to be run via `cargo clickbench`, instead of running this script directly.
+#
+# The CI version runs the full ClickBench dataset, hits.csv, which is roughly 100M rows
+# (~75GB). The CI version is intended for use in CI and official benchmarking, and is
+# designed to be run directly via `./benchmark.sh`.
 
 # Exit on subcommand errors
 set -Eeuo pipefail
@@ -76,35 +84,93 @@ cleanup() {
 # Register the cleanup function to run when the script exits
 trap cleanup EXIT
 
-# Download function to retrieve the dataset
-download_dataset() {
+# Download function to retrieve the dataset and verify its checksum
+download_and_verify() {
   local url=$1
-  local filename=$2
+  local checksum=$2
+  local filename=$3
 
-  # Check if the file already exists
+  # Check if the file already exists and verify its checksum
   if [ -e "$filename" ]; then
-    echo "Dataset '$filename' already exists, skipping download..."
-    return
+    if echo "$checksum  $filename" | md5sum -c --status; then
+      echo "Dataset '$filename' already exists and is verified, skipping download..."
+      return
+    else
+      echo "Checksum mismatch. Re-downloading '$filename'..."
+    fi
   fi
 
-  # Downloading the TPC-H folder
+  # Downloading the file
   echo "Downloading $filename dataset..."
-  wget --no-verbose --continue -O "$filename" "$url"
-  unzip "$filename"
+  wget --no-verbose --continue -O "$filename.gz" "$url"
+  gzip -d "$filename.gz"
+  chmod 666 "$filename"
 }
 
 echo ""
 echo "*********************************************************************************"
-echo "* Benchmarking pg_analytics version '$FLAG_TAG' against TPC-H"
+echo "* Benchmarking pg_analytics version '$FLAG_TAG' against ClickBench"
 echo "*********************************************************************************"
 echo ""
 
 if [ "$FLAG_TAG" == "pgrx" ]; then
-  # TODO
-  echo "TODO"
+  # For local benchmarking via pgrx, we download hits_100k_rows.csv, which is ~5M rows (~3.75GB)
+  download_and_verify "https://paradedb-benchmarks.s3.amazonaws.com/hits_100k_rows.csv.gz" "06b18e929bc94ea93706b782d8b1120e" "hits_100k_rows.csv"
+  echo ""
+
+  # Rust nightly is required for SIMD support, which is mandatory for benchmarking as
+  # it is a major performance boost
+  CURRENT_RUST_TOOLCHAIN=$(rustup show active-toolchain)
+  if [[ $CURRENT_RUST_TOOLCHAIN != *"nightly"* ]]; then
+    echo "Switching to Rust nightly toolchain for SIMD..."
+    rustup override unset
+    rustup update nightly
+    rustup default nightly
+
+    echo "Reinstalling cargo-pgrx on Rust nightly toolchain..."
+    cargo install --locked cargo-pgrx --version 0.11.2 --force
+  else
+    echo "Already on Rust nightly toolchain, skipping toolchain switch..."
+  fi
+
+  # Retrieve pg_config path
+  OS_NAME=$(uname)
+  case "$OS_NAME" in
+    Darwin)
+      # Check arch to set proper pg_config path
+      if [ "$(uname -m)" = "arm64" ]; then
+        PG_CONFIG_PATH="/opt/homebrew/opt/postgresql@16/bin/pg_config"
+      elif [ "$(uname -m)" = "x86_64" ]; then
+        PG_CONFIG_PATH="/usr/local/opt/postgresql@16/bin/pg_config"
+      else
+        echo "Unknown arch, exiting..."
+        exit 1
+      fi
+      ;;
+    Linux)
+      PG_CONFIG_PATH="/usr/lib/postgresql/16/bin/pg_config"
+      ;;
+  esac
+
+  # Build pg_analytics and start its pgrx PostgreSQL instance
+  echo ""
+  echo "Building pg_analytics in release mode with SIMD..."
+  cargo pgrx stop
+  cargo pgrx init "--pg16=$PG_CONFIG_PATH"
+  cargo pgrx install "--pg-config=$PG_CONFIG_PATH"
+  cargo pgrx start
+  echo ""
+
+  # Run the benchmarking
+  psql -h localhost -p 28816 -d pg_analytics -t < benchmark.sql
+  # For local benchmarking via pgrx, we don't print the disk usage or parse the results into
+  # the format expected by the ClickBench dashboard
 else
-  # TODO: Make the various dataset sizes compatible here
-  download_dataset "https://paradedb-benchmarks.s3.amazonaws.com/TPC-H_V3.0.1.zip" "TPC-H_V3.0.1.zip"
+  # For CI benchmarking via Docker, we have a few dataset options:
+  # - hits_5m.tsv.gz: 5M rows (~3.75GB)
+  download_and_verify "https://paradedb-benchmarks.s3.amazonaws.com/hits_5m_rows.tsv.gz" "0dd087f3b6c8262fb962bd262163d402" "hits.tsv"
+  # - hits.tsv.gz: 100M rows (~75GB) (full dataset)
+  # download_and_verify "https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz" "5ef60063da951e18ae3fa929c9f3aad4" "hits.tsv"
 
   # If the version tag is "local", we build the ParadeDB Docker image from source to test the current commit
   if [ "$FLAG_TAG" == "local" ]; then
@@ -112,7 +178,6 @@ else
     docker build -t paradedb/paradedb:"$FLAG_TAG" \
       -f "../../../docker/Dockerfile" \
       "../../../"
-    echo ""
   fi
 
   # Install and run Docker container for ParadeDB in detached mode
@@ -137,15 +202,7 @@ else
   echo "Loading dataset..."
   export PGPASSWORD='mypassword'
   psql -h localhost -U myuser -d mydatabase -p 5432 -t < create.sql
-
-  # TODO: Handle the data generation + loading, this here is broken
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY nation FROM 'TPC-H_V3.0.1/ref_data/1/nation.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY customer FROM 'TPC-H_V3.0.1/ref_data/1/customer.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY supplier FROM 'TPC-H_V3.0.1/ref_data/1/supplier.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY part FROM 'TPC-H_V3.0.1/ref_data/1/part.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY partsupp FROM 'TPC-H_V3.0.1/ref_data/1/partsupp.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY orders FROM 'TPC-H_V3.0.1/ref_data/1/orders.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
-  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c "CALL paradedb.init();" -c '\timing' -c "\\COPY lineitem FROM 'TPC-H_V3.0.1/ref_data/1/lineitem.tbl.1' WITH (FORMAT CSV, DELIMITER '|')"
+  psql -h localhost -U myuser -d mydatabase -p 5432 -t -c '\timing' -c "\\copy hits FROM 'hits.tsv'"
 
   echo ""
   echo "Running queries..."

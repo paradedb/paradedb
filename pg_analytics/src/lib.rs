5f85058cbcfc347939b3f4c4eb8a8f1c35489a0d
Philippe Noël <philippemnoel@gmail.com>
2024-02-12 16:09:45 -0500

    Add pg_analytics to shared_preload in tests

--- pg_analytics/src/lib.rs
+++ pg_analytics/src/lib.rs
@@ -36,11 +36,16 @@ pub extern "C" fn _PG_init() {
     };
 }
 
+/// This module is required by `cargo pgrx test` invocations.
+/// It must be visible at the root of your extension crate.
 #[cfg(test)]
 pub mod pg_test {
-    pub fn setup(_options: Vec<&str>) {}
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
 
     pub fn postgresql_conf_options() -> Vec<&'static str> {
-        vec![]
+        // return any postgresql.conf settings that are required for your tests
+        vec!["shared_preload_libraries='pg_analytics.so'"]
     }
 }

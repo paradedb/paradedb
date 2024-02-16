3f75088ee16e13437677d859864b8cb636e39b4c
Neil Hansen <neil.hansen.31@gmail.com>
2024-02-15 17:02:22 -0800

    query api complete

--- pg_bm25/src/api/index.rs
+++ pg_bm25/src/api/index.rs
@@ -2,7 +2,9 @@ use pgrx::{iter::TableIterator, *};
 use tantivy::schema::*;
 
 use crate::postgres::utils::get_search_index;
+use crate::query::SearchQueryInput;
 use crate::schema::ToString;
+use std::ops::Bound;
 
 #[allow(clippy::type_complexity)]
 #[pg_extern]
@@ -85,4 +87,197 @@ pub fn schema_bm25(
     }
 
     TableIterator::new(field_rows)
+}
+
+#[pg_extern]
+pub fn all() -> SearchQueryInput {
+    SearchQueryInput::All
+}
+
+#[pg_extern]
+pub fn boolean(
+    must: Option<Vec<SearchQueryInput>>,
+    should: Option<Vec<SearchQueryInput>>,
+    must_not: Option<Vec<SearchQueryInput>>,
+) -> SearchQueryInput {
+    SearchQueryInput::Boolean {
+        must: must.map(|v| v.into_iter().map(Box::new).collect()),
+        should: should.map(|v| v.into_iter().map(Box::new).collect()),
+        must_not: must_not.map(|v| v.into_iter().map(Box::new).collect()),
+    }
+}
+
+#[pg_extern]
+pub fn boost(query: Option<SearchQueryInput>, boost: Option<f32>) -> SearchQueryInput {
+    SearchQueryInput::Boost {
+        query: query.map(Box::new),
+        boost,
+    }
+}
+
+#[pg_extern]
+pub fn const_score(query: Option<SearchQueryInput>, score: Option<f32>) -> SearchQueryInput {
+    SearchQueryInput::ConstScore {
+        query: query.map(Box::new),
+        score,
+    }
+}
+
+#[pg_extern]
+pub fn disjunction_max(
+    disjuncts: Option<Vec<SearchQueryInput>>,
+    tie_breaker: Option<f32>,
+) -> SearchQueryInput {
+    SearchQueryInput::DisjunctionMax {
+        disjuncts: disjuncts.map(|v| v.into_iter().map(Box::new).collect()),
+        tie_breaker,
+    }
+}
+
+#[pg_extern]
+pub fn empty() -> SearchQueryInput {
+    SearchQueryInput::Empty
+}
+
+#[pg_extern]
+pub fn fast_field_range_weight(field: String, range: pgrx::Range<i32>) -> SearchQueryInput {
+    match range.into_inner() {
+        None => SearchQueryInput::FastFieldRangeWeight {
+            field,
+            lower_bound: Bound::Included(0),
+            upper_bound: Bound::Excluded(0),
+        },
+        Some((lower, upper)) => SearchQueryInput::FastFieldRangeWeight {
+            field,
+            lower_bound: match lower {
+                RangeBound::Infinite => Bound::Unbounded,
+                RangeBound::Inclusive(n) => Bound::Included(n as u64),
+                RangeBound::Exclusive(n) => Bound::Excluded(n as u64),
+            },
+            upper_bound: match upper {
+                RangeBound::Infinite => Bound::Unbounded,
+                RangeBound::Inclusive(n) => Bound::Included(n as u64),
+                RangeBound::Exclusive(n) => Bound::Excluded(n as u64),
+            },
+        },
+    }
+}
+
+#[pg_extern]
+pub fn fuzzy_term(
+    field: String,
+    text: String,
+    distance: i16,
+    tranposition_cost_one: bool,
+    prefix: bool,
+) -> SearchQueryInput {
+    SearchQueryInput::FuzzyTerm {
+        field,
+        text,
+        distance: distance as u8,
+        tranposition_cost_one,
+        prefix,
+    }
+}
+
+#[pg_extern]
+pub fn more_like_this(
+    min_doc_frequency: Option<i32>,
+    max_doc_frequency: Option<i32>,
+    min_term_frequency: Option<i32>,
+    max_query_terms: Option<i32>,
+    min_word_length: Option<i32>,
+    max_word_length: Option<i32>,
+    boost_factor: Option<f32>,
+    stop_words: Option<Vec<String>>,
+    fields: Json,
+) -> SearchQueryInput {
+    SearchQueryInput::MoreLikeThis {
+        min_doc_frequency: min_doc_frequency.map(|n| n as u64),
+        max_doc_frequency: max_doc_frequency.map(|n| n as u64),
+        min_term_frequency: min_term_frequency.map(|n| n as usize),
+        max_query_terms: max_query_terms.map(|n| n as usize),
+        min_word_length: min_word_length.map(|n| n as usize),
+        max_word_length: max_word_length.map(|n| n as usize),
+        boost_factor,
+        stop_words,
+        fields: fields.0.into(),
+    }
+}
+
+#[pg_extern]
+pub fn phrase_prefix(
+    field: String,
+    prefix: Option<String>,
+    phrases: Option<Array<String>>,
+    max_expansion: Option<i32>,
+) -> SearchQueryInput {
+    SearchQueryInput::PhrasePrefix {
+        field,
+        prefix,
+        phrases: phrases.map(|arr| arr.iter_deny_null().collect()),
+        max_expansion: max_expansion.map(|n| n as u32),
+    }
+}
+
+#[pg_extern]
+pub fn phrase(
+    field: String,
+    phrases: Option<Array<String>>,
+    slop: Option<i32>,
+) -> SearchQueryInput {
+    SearchQueryInput::Phrase {
+        field,
+        phrases: phrases.map(|arr| arr.iter_deny_null().collect()),
+        slop: slop.map(|n| n as u32),
+    }
+}
+
+#[pg_extern]
+pub fn range(field: String, range: Range<i32>) -> SearchQueryInput {
+    match range.into_inner() {
+        None => SearchQueryInput::FastFieldRangeWeight {
+            field,
+            lower_bound: Bound::Included(0),
+            upper_bound: Bound::Excluded(0),
+        },
+        Some((lower, upper)) => SearchQueryInput::FastFieldRangeWeight {
+            field,
+            lower_bound: match lower {
+                RangeBound::Infinite => Bound::Unbounded,
+                RangeBound::Inclusive(n) => Bound::Included(n as u64),
+                RangeBound::Exclusive(n) => Bound::Excluded(n as u64),
+            },
+            upper_bound: match upper {
+                RangeBound::Infinite => Bound::Unbounded,
+                RangeBound::Inclusive(n) => Bound::Included(n as u64),
+                RangeBound::Exclusive(n) => Bound::Excluded(n as u64),
+            },
+        },
+    }
+}
+
+#[pg_extern]
+pub fn regex(field: String, pattern: String) -> SearchQueryInput {
+    SearchQueryInput::Regex { field, pattern }
+}
+
+#[pg_extern]
+pub fn term(
+    field: String,
+    text: String,
+    freqs: Option<bool>,
+    position: Option<bool>,
+) -> SearchQueryInput {
+    SearchQueryInput::Term {
+        field,
+        text,
+        freqs,
+        position,
+    }
+}
+
+#[pg_extern]
+pub fn term_set(fields: Json) -> SearchQueryInput {
+    SearchQueryInput::TermSet { fields: fields.0 }
 }
--- pg_bm25/src/query/mod.rs
+++ pg_bm25/src/query/mod.rs
@@ -1,40 +1,116 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
+use pgrx::PostgresType;
+use serde::{Deserialize, Serialize};
 
-enum SearchQuery {
-    AllQuery,
-    BooleanQuery {
-        must: Option<Vec<Box<SearchQuery>>>,
-        should: Option<Vec<Box<SearchQuery>>>,
-        must_not: Option<Vec<Box<SearchQuery>>>,
+// enum SearchQuery {
+//     AllQuery,
+//     BooleanQuery {
+//         must: Option<Vec<Box<SearchQuery>>>,
+//         should: Option<Vec<Box<SearchQuery>>>,
+//         must_not: Option<Vec<Box<SearchQuery>>>,
+//     },
+//     BoostQuery {
+//         query: Option<Box<SearchQuery>>,
+//         boost: Option<f32>,
+//     },
+//     ConstScoreQuery {
+//         query: Option<Box<SearchQuery>>,
+//         score: Option<f32>,
+//     },
+//     DisjunctionMaxQuery {
+//         disjuncts: Option<Vec<Box<SearchQuery>>>,
+//         tie_breaker: Option<f32>,
+//     },
+//     EmptyQuery,
+//     FastFieldRangeWeight {
+//         field: tantivy::schema::Field,
+//         lower_bound: Option<std::ops::Bound<u64>>,
+//         upper_bound: Option<std::ops::Bound<u64>>,
+//     },
+//     FuzzyTermQuery {
+//         field: tantivy::schema::Field,
+//         text: String,
+//         distance: u8,
+//         tranposition_cost_one: bool,
+//         prefix: bool,
+//     },
+//     MoreLikeThisQuery {
+//         min_doc_frequency: Option<u64>,
+//         max_doc_frequency: Option<u64>,
+//         min_term_frequency: Option<usize>,
+//         max_query_terms: Option<usize>,
+//         min_word_length: Option<usize>,
+//         max_word_length: Option<usize>,
+//         boost_factor: Option<f32>,
+//         stop_words: Option<Vec<String>>,
+//         fields: Option<HashMap<tantivy::schema::Field, Vec<tantivy::schema::Value>>>,
+//     },
+//     PhrasePrefixQuery {
+//         field: tantivy::schema::Field,
+//         prefix: Option<String>,
+//         phrases: Option<Vec<String>>,
+//         max_expansion: Option<u32>,
+//     },
+//     PhraseQuery {
+//         field: tantivy::schema::Field,
+//         phrases: Option<Vec<String>>,
+//         slop: Option<u32>,
+//     },
+//     RangeQuery {
+//         field: tantivy::schema::Field,
+//         lower_bound: Option<std::ops::Bound<u64>>,
+//         upper_bound: Option<std::ops::Bound<u64>>,
+//         schema_type: tantivy::schema::Type,
+//     },
+//     RegexQuery {
+//         field: tantivy::schema::Field,
+//         pattern: String,
+//     },
+//     TermQuery {
+//         text: String,
+//         freqs: Option<bool>,
+//         position: Option<bool>,
+//     },
+//     TermSetQuery {
+//         fields: HashMap<tantivy::schema::Field, Vec<tantivy::schema::Value>>,
+//     },
+// }
+
+#[derive(PostgresType, Deserialize, Serialize)]
+pub enum SearchQueryInput {
+    All,
+    Boolean {
+        must: Option<Vec<Box<SearchQueryInput>>>,
+        should: Option<Vec<Box<SearchQueryInput>>>,
+        must_not: Option<Vec<Box<SearchQueryInput>>>,
     },
-    BoostQuery {
-        query: Option<Box<SearchQuery>>,
+    Boost {
+        query: Option<Box<SearchQueryInput>>,
         boost: Option<f32>,
     },
-    ConstScoreQuery {
-        query: Option<Box<SearchQuery>>,
+    ConstScore {
+        query: Option<Box<SearchQueryInput>>,
         score: Option<f32>,
     },
-    DisjunctionMaxQuery {
-        disjuncts: Option<Vec<Box<SearchQuery>>>,
+    DisjunctionMax {
+        disjuncts: Option<Vec<Box<SearchQueryInput>>>,
         tie_breaker: Option<f32>,
     },
-    EmptyQuery,
+    Empty,
     FastFieldRangeWeight {
-        field: tantivy::schema::Field,
-        lower_bound: Option<std::ops::Bound<u64>>,
-        upper_bound: Option<std::ops::Bound<u64>>,
+        field: String,
+        lower_bound: std::ops::Bound<u64>,
+        upper_bound: std::ops::Bound<u64>,
     },
-    FuzzyTermQuery {
-        field: tantivy::schema::Field,
+    FuzzyTerm {
+        field: String,
         text: String,
         distance: u8,
         tranposition_cost_one: bool,
         prefix: bool,
     },
-    MoreLikeThisQuery {
+    MoreLikeThis {
         min_doc_frequency: Option<u64>,
         max_doc_frequency: Option<u64>,
         min_term_frequency: Option<usize>,
@@ -43,40 +119,35 @@ enum SearchQuery {
         max_word_length: Option<usize>,
         boost_factor: Option<f32>,
         stop_words: Option<Vec<String>>,
-        fields: Option<HashMap<tantivy::schema::Field, Vec<tantivy::schema::Value>>>,
+        fields: serde_json::Value,
     },
-    PhrasePrefixQuery {
-        field: tantivy::schema::Field,
+    PhrasePrefix {
+        field: String,
         prefix: Option<String>,
         phrases: Option<Vec<String>>,
         max_expansion: Option<u32>,
     },
-    PhraseQuery {
-        field: tantivy::schema::Field,
+    Phrase {
+        field: String,
         phrases: Option<Vec<String>>,
         slop: Option<u32>,
     },
-    RangeQuery {
-        field: tantivy::schema::Field,
-        lower_bound: Option<std::ops::Bound<u64>>,
-        upper_bound: Option<std::ops::Bound<u64>>,
-        schema_type: tantivy::schema::Type,
+    Range {
+        field: String,
+        lower_bound: std::ops::Bound<u64>,
+        upper_bound: std::ops::Bound<u64>,
     },
-    RegexQuery {
-        field: tantivy::schema::Field,
+    Regex {
+        field: String,
         pattern: String,
     },
-    TermQuery {
+    Term {
+        field: String,
         text: String,
         freqs: Option<bool>,
         position: Option<bool>,
     },
-    TermSetQuery {
-        fields: HashMap<tantivy::schema::Field, Vec<tantivy::schema::Value>>,
+    TermSet {
+        fields: serde_json::Value,
     },
-}
-
-enum BooleanQueryParams {
-    Union(Vec<SearchQuery>),
-    Intersection(Vec<SearchQuery>),
 }

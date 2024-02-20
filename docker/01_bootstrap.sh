e61a825b424091c97c41611156582913fe58b102
Philippe Noël <philippemnoel@gmail.com>
2024-02-20 16:26:53 -0500

    nit

--- docker/01_bootstrap.sh
+++ docker/01_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script is executed at container initialization to configure
-# it for ParadeDB workloads
+# This script is executed at the initialization of the ParadeDB container
+# to configure it with required extensions and Postgres settings
 
 echo "Configuring PostgreSQL search_path..."
 

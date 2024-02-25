a279836706d5e8b95fa6647191feafdb13f6af4f
Ming <ming.ying.nyc@gmail.com>
2024-02-25 14:37:59 -0500

    chore: Move BM25 benchmarks to pg_bm25 (#907)
    Co-authored-by: Philippe Noël <philippemnoel@gmail.com>

--- pg_bm25/benchmarks/benchmark-pg_bm25.sh
+++ pg_bm25/benchmarks/benchmark-pg_bm25.sh
@@ -35,7 +35,7 @@ do
 done
 
 PORT=5431
-OUTPUT_CSV=out/benchmark_paradedb.csv
+OUTPUT_CSV=out/benchmark_pg_bm25.csv
 
 # Ensure the "out" directory exists
 mkdir -p out/
@@ -78,8 +78,8 @@ if [ "$FLAG_TAG" == "local" ]; then
     --build-arg POSTGRESQL_PASSWORD=mypassword \
     --build-arg POSTGRESQL_DATABASE=mydatabase \
     --build-arg POSTGRESQL_POSTGRES_PASSWORD=postgres \
-    --file "../docker/Dockerfile" \
-    "../"
+    --file "../../docker/Dockerfile" \
+    "../../"
   echo ""
 fi
 

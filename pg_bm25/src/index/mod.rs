6bf322e04c62b8ef0d45e5374e88071adc70672d
Neil Hansen <neil.hansen.31@gmail.com>
2024-03-03 20:29:14 -1000

    add current search public functions

--- pg_bm25/src/index/mod.rs
+++ pg_bm25/src/index/mod.rs
@@ -6,9 +6,10 @@ pub mod state;
 use once_cell::sync::Lazy;
 use pgrx::pg_sys::Alias;
 pub use search::*;
+use tantivy::DocAddress;
 
-use crate::schema::{SearchConfig, SearchField};
-use derive_more::{AsRef, From};
+use crate::schema::{SearchConfig, SearchField, SearchFieldId};
+use derive_more::{AsRef, Display, From};
 use shared::postgres::transaction::{Transaction, TransactionError};
 use std::{
     collections::HashMap,
@@ -16,16 +17,17 @@ use std::{
 };
 use thiserror::Error;
 
-static CURRENT_SEARCH_CONFIG: Lazy<Arc<CurrentSearchConfig>> = Lazy::new(|| {
-    Arc::new(CurrentSearchConfig {
-        map: Arc::new(Mutex::new(HashMap::new())),
+static CURRENT_SEARCH: Lazy<Arc<CurrentSearch>> = Lazy::new(|| {
+    Arc::new(CurrentSearch {
+        doc_map: Arc::new(Mutex::new(HashMap::new())),
+        config_map: Arc::new(Mutex::new(HashMap::new())),
     })
 });
 
-const TRANSACTION_CALLBACK_CACHE_ID: &str = "parade_current_search_config";
+const TRANSACTION_CALLBACK_CACHE_ID: &str = "parade_current_search";
 const DEFAULT_ALIAS: &str = "";
 
-#[derive(AsRef, Eq, PartialEq, Hash, From)]
+#[derive(Debug, Display, AsRef, Eq, PartialEq, Hash, From)]
 #[as_ref(forward)]
 struct SearchAlias(String);
 
@@ -35,84 +37,190 @@ impl From<&str> for SearchAlias {
     }
 }
 
-struct CurrentSearchConfig {
-    map: Arc<Mutex<HashMap<SearchAlias, SearchConfig>>>,
+struct CurrentSearch {
+    /// A map from key_field value to document address.
+    doc_map: Arc<Mutex<HashMap<SearchAlias, HashMap<i64, DocAddress>>>>,
+    /// A map from query alias to search configuration.
+    config_map: Arc<Mutex<HashMap<SearchAlias, SearchConfig>>>,
 }
 
-impl CurrentSearchConfig {
+impl CurrentSearch {
     fn register_callback(&self) -> Result<(), TransactionError> {
-        let cloned_map = self.map.clone();
+        // Commit and abort are mutually exclusive. One of the two is guaranteed
+        // to be called on any transaction. We'll use that opportunity to clean
+        // up the cache.
+        let cloned_map = self.config_map.clone();
         Transaction::call_once_on_commit(TRANSACTION_CALLBACK_CACHE_ID, move || {
             cloned_map
                 .lock()
-                .map_err(CurrentSearchConfigError::from)
+                .map_err(CurrentSearchError::from)
                 .expect("could not lock search config lookup in commit callback")
                 .drain();
         })?;
-        let cloned_map = self.map.clone();
+        let cloned_map = self.config_map.clone();
         Transaction::call_once_on_abort(TRANSACTION_CALLBACK_CACHE_ID, move || {
             cloned_map
                 .lock()
-                .map_err(CurrentSearchConfigError::from)
+                .map_err(CurrentSearchError::from)
                 .expect("could not lock search config lookup in commit callback")
                 .drain();
         })?;
         Ok(())
     }
 
-    fn set_default(&mut self, config: SearchConfig) -> Result<(), CurrentSearchConfigError> {
-        match self
-            .map
+    fn get_doc_default(&self, key: i64) -> Result<DocAddress, CurrentSearchError> {
+        self.doc_map
             .lock()
-            .map_err(CurrentSearchConfigError::from)?
-            .insert(DEFAULT_ALIAS.into(), config)
-        {
-            None => Ok(()),
-            Some(_) => Err(CurrentSearchConfigError::AliasRequired),
+            .map_err(CurrentSearchError::from)?
+            .get(&DEFAULT_ALIAS.into())
+            .ok_or(CurrentSearchError::NoQuery)?
+            .get(&key)
+            .cloned()
+            .ok_or(CurrentSearchError::DocLookup(key))
+    }
+
+    fn get_doc_alias(
+        &self,
+        key: i64,
+        alias: SearchAlias,
+    ) -> Result<DocAddress, CurrentSearchError> {
+        self.doc_map
+            .lock()
+            .map_err(CurrentSearchError::from)?
+            .get(&alias)
+            .ok_or(CurrentSearchError::AliasLookup(alias))?
+            .get(&key)
+            .cloned()
+            .ok_or(CurrentSearchError::DocLookup(key))
+    }
+
+    pub fn get_doc(
+        &self,
+        key: i64,
+        alias: Option<SearchAlias>,
+    ) -> Result<DocAddress, CurrentSearchError> {
+        if let Some(alias) = alias {
+            self.get_doc_alias(key, alias)
+        } else {
+            self.get_doc_default(key)
         }
     }
 
-    fn get_default(&self) -> Result<SearchConfig, CurrentSearchConfigError> {
-        self.map
+    fn set_doc_default(&mut self, key: i64, address: DocAddress) -> Result<(), CurrentSearchError> {
+        self.doc_map
             .lock()
-            .map_err(CurrentSearchConfigError::from)?
-            .get(&DEFAULT_ALIAS.into())
-            .cloned()
-            .ok_or(CurrentSearchConfigError::NoQuery)
+            .map_err(CurrentSearchError::from)?
+            .entry(DEFAULT_ALIAS.into())
+            .or_insert_with(|| HashMap::new())
+            .insert(key, address);
+        Ok(())
     }
 
-    fn set_alias(
+    fn set_doc_alias(
         &mut self,
+        key: i64,
+        address: DocAddress,
         SearchAlias(alias): SearchAlias,
-        config: SearchConfig,
-    ) -> Result<(), CurrentSearchConfigError> {
+    ) -> Result<(), CurrentSearchError> {
         if alias == DEFAULT_ALIAS {
-            Err(CurrentSearchConfigError::EmptyAlias)
+            Err(CurrentSearchError::EmptyAlias)
         } else {
-            self.map
+            self.doc_map
                 .lock()
-                .map_err(CurrentSearchConfigError::from)?
-                .insert(SearchAlias(alias.clone()), config)
-                .ok_or(CurrentSearchConfigError::DuplicateAlias(alias))?;
+                .map_err(CurrentSearchError::from)?
+                .entry(alias.into())
+                .or_insert_with(|| HashMap::new())
+                .insert(key, address);
             Ok(())
         }
     }
 
-    fn get_alias(
-        &self,
-        SearchAlias(alias): SearchAlias,
-    ) -> Result<SearchConfig, CurrentSearchConfigError> {
-        self.map
+    pub fn set_doc(
+        &mut self,
+        key: i64,
+        address: DocAddress,
+        alias: Option<SearchAlias>,
+    ) -> Result<(), CurrentSearchError> {
+        if let Some(alias) = alias {
+            self.set_doc_alias(key, address, alias)
+        } else {
+            self.set_doc_default(key, address)
+        }
+    }
+
+    fn get_config_default(&self) -> Result<SearchConfig, CurrentSearchError> {
+        self.config_map
             .lock()
-            .map_err(CurrentSearchConfigError::from)?
-            .get(&alias.clone().into())
+            .map_err(CurrentSearchError::from)?
+            .get(&DEFAULT_ALIAS.into())
             .cloned()
-            .ok_or(CurrentSearchConfigError::AliasLookup(alias))
+            .ok_or(CurrentSearchError::NoQuery)
+    }
+
+    fn get_config_alias(&self, alias: SearchAlias) -> Result<SearchConfig, CurrentSearchError> {
+        self.config_map
+            .lock()
+            .map_err(CurrentSearchError::from)?
+            .get(&alias)
+            .cloned()
+            .ok_or(CurrentSearchError::AliasLookup(alias))
+    }
+
+    pub fn get_config(
+        &self,
+        alias: Option<SearchAlias>,
+    ) -> Result<SearchConfig, CurrentSearchError> {
+        if let Some(alias) = alias {
+            self.get_config_alias(alias)
+        } else {
+            self.get_config_default()
+        }
+    }
+
+    fn set_config_default(&mut self, config: SearchConfig) -> Result<(), CurrentSearchError> {
+        match self
+            .config_map
+            .lock()
+            .map_err(CurrentSearchError::from)?
+            .insert(DEFAULT_ALIAS.into(), config)
+        {
+            None => Ok(()),
+            Some(_) => Err(CurrentSearchError::AliasRequired),
+        }
+    }
+
+    fn set_config_alias(
+        &mut self,
+        config: SearchConfig,
+        SearchAlias(alias): SearchAlias,
+    ) -> Result<(), CurrentSearchError> {
+        if alias == DEFAULT_ALIAS {
+            Err(CurrentSearchError::EmptyAlias)
+        } else {
+            self.config_map
+                .lock()
+                .map_err(CurrentSearchError::from)?
+                .insert(SearchAlias(alias.clone()), config)
+                .ok_or(CurrentSearchError::DuplicateAlias(alias))?;
+            Ok(())
+        }
+    }
+
+    pub fn set_config(
+        &mut self,
+        config: SearchConfig,
+        alias: Option<SearchAlias>,
+    ) -> Result<(), CurrentSearchError> {
+        if let Some(alias) = alias {
+            self.set_config_alias(config, alias)
+        } else {
+            self.set_config_default(config)
+        }
     }
 }
 
 #[derive(Debug, Error)]
-enum CurrentSearchConfigError {
+enum CurrentSearchError {
     #[error("to use multiple pg_search queries, pass a query alias with the 'as' parameter")]
     AliasRequired,
     #[error("no pg_search query in current transaction")]
@@ -121,14 +229,16 @@ enum CurrentSearchConfigError {
     EmptyAlias,
     #[error("a pg_search alias must be unique, found duplicate: '{0}'")]
     DuplicateAlias(String),
+    #[error("error looking up result data for document with id: '{0}'")]
+    DocLookup(i64),
     #[error("no query found with alias: '{0}'")]
-    AliasLookup(String),
+    AliasLookup(SearchAlias),
     #[error("could not lock the current search config lookup: {0}")]
     Lock(String),
 }
 
-impl<T> From<PoisonError<T>> for CurrentSearchConfigError {
+impl<T> From<PoisonError<T>> for CurrentSearchError {
     fn from(err: PoisonError<T>) -> Self {
-        CurrentSearchConfigError::Lock(format!("{err}"))
+        CurrentSearchError::Lock(format!("{err}"))
     }
 }

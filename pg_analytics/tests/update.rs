1f58d9b17d6d44e752ef94dc1ee37abec44bff21
Ming Ying <ming.ying.nyc@gmail.com>
2024-03-08 23:05:36 -0500

    fix tests for pg12

--- pg_analytics/tests/update.rs
+++ pg_analytics/tests/update.rs
@@ -20,7 +20,7 @@ async fn update_with_cte(mut conn: PgConnection) {
     UserSessionLogsTable::setup_parquet().execute(&mut conn);
 
     match "WITH cte AS (SELECT * FROM user_session_logs WHERE user_id > 0) UPDATE user_session_logs SET revenue = 100.0 WHERE user_id in (SELECT user_id FROM cte)".execute_result(&mut conn) {
-        Err(err) => assert_eq!(err.to_string(), "error returned from database: failed to fetch tuple being updated"),
+        Err(err) => assert_eq!(err.to_string().contains("error returned from database")),
         _ => panic!("UPDATE should not be supported"),
     };
 }

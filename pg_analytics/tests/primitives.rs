88bd17bdc11e31df776401604c586b079f4e1b0c
Ming <ming.ying.nyc@gmail.com>
2024-03-12 10:31:41 -0400

    docs: Document and test supported types + backup solutions (#977)

--- pg_analytics/tests/primitives.rs
+++ pg_analytics/tests/primitives.rs
@@ -9,81 +9,153 @@ use time::{macros::format_description, Date, PrimitiveDateTime, Time};
 
 #[rstest]
 fn text_type(mut conn: PgConnection) {
-    "CREATE TABLE test_text (a text) USING parquet".execute(&mut conn);
-    "INSERT INTO test_text VALUES ('hello world')".execute(&mut conn);
-    let row: (String,) = "SELECT * FROM test_text".fetch_one(&mut conn);
+    "CREATE TABLE t (a text) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES ('hello world')".execute(&mut conn);
+    let row: (String,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, "hello world".to_string());
+}
+
+#[rstest]
+fn text_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a text[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY['hello', 'world'])".execute(&mut conn);
+    let row: (Vec<String>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec!["hello", "world"]);
 }
 
 #[rstest]
 fn varchar_type(mut conn: PgConnection) {
-    "CREATE TABLE test_varchar (a varchar) USING parquet".execute(&mut conn);
-    "INSERT INTO test_varchar VALUES ('hello world')".execute(&mut conn);
-    let row: (String,) = "SELECT * FROM test_varchar".fetch_one(&mut conn);
+    "CREATE TABLE t (a varchar) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES ('hello world')".execute(&mut conn);
+    let row: (String,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, "hello world".to_string());
 }
 
 #[rstest]
+fn varchar_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a varchar[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY['hello', 'world'])".execute(&mut conn);
+    let row: (Vec<String>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec!["hello", "world"]);
+}
+
+#[rstest]
 fn char_type(mut conn: PgConnection) {
-    "CREATE TABLE test_char (a char) USING parquet".execute(&mut conn);
-    "INSERT INTO test_char VALUES ('h')".execute(&mut conn);
-    let row: (String,) = "SELECT * FROM test_char".fetch_one(&mut conn);
+    "CREATE TABLE t (a char) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES ('h')".execute(&mut conn);
+    let row: (String,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, "h".to_string());
 }
 
 #[rstest]
+fn char_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a char[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY['h', 'i'])".execute(&mut conn);
+    let row: (Vec<String>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec!["h", "i"]);
+}
+
+#[rstest]
 fn smallint_type(mut conn: PgConnection) {
-    "CREATE TABLE test_smallint (a smallint) USING parquet".execute(&mut conn);
-    "INSERT INTO test_smallint VALUES (1)".execute(&mut conn);
-    let row: (i16,) = "SELECT * FROM test_smallint".fetch_one(&mut conn);
+    "CREATE TABLE t (a smallint) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1)".execute(&mut conn);
+    let row: (i16,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, 1);
+}
+
+#[rstest]
+fn smallint_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a smallint[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[1, 2])".execute(&mut conn);
+    let row: (Vec<i16>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![1, 2]);
 }
 
 #[rstest]
 fn integer_type(mut conn: PgConnection) {
-    "CREATE TABLE test_integer (a integer) USING parquet".execute(&mut conn);
-    "INSERT INTO test_integer VALUES (1)".execute(&mut conn);
-    let row: (i32,) = "SELECT * FROM test_integer".fetch_one(&mut conn);
+    "CREATE TABLE t (a integer) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1)".execute(&mut conn);
+    let row: (i32,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, 1);
+}
+
+#[rstest]
+fn integer_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a integer[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[1, 2])".execute(&mut conn);
+    let row: (Vec<i32>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![1, 2]);
 }
 
 #[rstest]
 fn bigint_type(mut conn: PgConnection) {
-    "CREATE TABLE test_bigint (a bigint) USING parquet".execute(&mut conn);
-    "INSERT INTO test_bigint VALUES (1)".execute(&mut conn);
-    let row: (i64,) = "SELECT * FROM test_bigint".fetch_one(&mut conn);
+    "CREATE TABLE t (a bigint) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1)".execute(&mut conn);
+    let row: (i64,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, 1);
 }
 
 #[rstest]
+fn bigint_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a bigint[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[1, 2])".execute(&mut conn);
+    let row: (Vec<i64>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![1, 2]);
+}
+
+#[rstest]
 fn real_type(mut conn: PgConnection) {
-    "CREATE TABLE test_real (a real) USING parquet".execute(&mut conn);
-    "INSERT INTO test_real VALUES (1.0)".execute(&mut conn);
-    let row: (f32,) = "SELECT * FROM test_real".fetch_one(&mut conn);
+    "CREATE TABLE t (a real) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1.0)".execute(&mut conn);
+    let row: (f32,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, 1.0);
+}
+
+#[rstest]
+fn real_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a real[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[1.0, 2.0])".execute(&mut conn);
+    let row: (Vec<f32>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![1.0, 2.0]);
 }
 
 #[rstest]
 fn double_type(mut conn: PgConnection) {
-    "CREATE TABLE test_double (a double precision) USING parquet".execute(&mut conn);
-    "INSERT INTO test_double VALUES (1.0)".execute(&mut conn);
-    let row: (f64,) = "SELECT * FROM test_double".fetch_one(&mut conn);
+    "CREATE TABLE t (a double precision) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1.0)".execute(&mut conn);
+    let row: (f64,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, 1.0);
 }
 
 #[rstest]
+fn double_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a double precision[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[1.0, 2.0])".execute(&mut conn);
+    let row: (Vec<f64>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![1.0, 2.0]);
+}
+
+#[rstest]
 fn bool_type(mut conn: PgConnection) {
-    "CREATE TABLE test_bool (a bool) USING parquet".execute(&mut conn);
-    "INSERT INTO test_bool VALUES (true)".execute(&mut conn);
-    let row: (bool,) = "SELECT * FROM test_bool".fetch_one(&mut conn);
+    "CREATE TABLE t (a bool) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (true)".execute(&mut conn);
+    let row: (bool,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, true);
 }
 
 #[rstest]
+fn bool_array_type(mut conn: PgConnection) {
+    "CREATE TABLE t (a bool[]) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (ARRAY[true, false])".execute(&mut conn);
+    let row: (Vec<bool>,) = "SELECT * FROM t".fetch_one(&mut conn);
+    assert_eq!(row.0, vec![true, false]);
+}
+
+#[rstest]
 fn numeric_type(mut conn: PgConnection) {
-    "CREATE TABLE test_numeric (a numeric(5, 2)) USING parquet".execute(&mut conn);
-    "INSERT INTO test_numeric VALUES (1.01)".execute(&mut conn);
-    let row: (BigDecimal,) = "SELECT * FROM test_numeric".fetch_one(&mut conn);
+    "CREATE TABLE t (a numeric(5, 2)) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES (1.01)".execute(&mut conn);
+    let row: (BigDecimal,) = "SELECT * FROM t".fetch_one(&mut conn);
     assert_eq!(row.0, BigDecimal::from_str("1.01").unwrap());
 }
 
@@ -110,9 +182,9 @@ fn timestamp_type(mut conn: PgConnection) {
 
 #[rstest]
 fn date_type(mut conn: PgConnection) {
-    "CREATE TABLE test_date (a date) USING parquet".execute(&mut conn);
-    "INSERT INTO test_date VALUES ('2024-01-29')".execute(&mut conn);
-    let row: (Date,) = "SELECT * FROM test_date".fetch_one(&mut conn);
+    "CREATE TABLE t (a date) USING parquet".execute(&mut conn);
+    "INSERT INTO t VALUES ('2024-01-29')".execute(&mut conn);
+    let row: (Date,) = "SELECT * FROM t".fetch_one(&mut conn);
     let fd = format_description!("[year]-[month]-[day]");
     assert_eq!(row.0, Date::parse("2024-01-29", fd).unwrap());
 }

3f3a653935378538b16ae95b60f36b654455025b
Ming Ying <ming.ying.nyc@gmail.com>
2024-03-22 12:41:06 -0400

    use bin/pgrx_embed

--- pg_analytics/src/types/array.rs
+++ pg_analytics/src/types/array.rs
@@ -320,7 +320,7 @@ where
     where
         T: FromDatum,
         A: ArrowPrimitiveType<Native = T>,
-        Vec<Option<T>>: FromDatum
+        Vec<Option<T>>: FromDatum,
     {
         let iter = self.into_array::<Column<T>>()?;
 

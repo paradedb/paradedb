c492507d0d06f31c577db6376321cd4421812fbb
Ming <ming.ying.nyc@gmail.com>
2024-03-23 10:56:50 -0400

    fix: `paradedb.help` already installed won't cause extension install to fail (#1003)

--- shared/src/github.rs
+++ shared/src/github.rs
@@ -3,7 +3,19 @@ use url::{form_urlencoded, Url};
 
 const BASE_GITHUB_URL: &str = "https://github.com";
 
-#[pg_extern]
+#[pg_extern(sql = r#"
+    DO $$
+    BEGIN
+    IF NOT EXISTS (SELECT FROM pg_proc p
+        JOIN pg_namespace n ON p.pronamespace = n.oid
+        WHERE n.nspname = 'paradedb' AND p.proname = 'help') THEN
+            CREATE OR REPLACE FUNCTION paradedb.help(subject TEXT, body TEXT) RETURNS TEXT
+            STRICT
+            LANGUAGE c 
+            AS '@MODULE_PATHNAME@', '@FUNCTION_NAME@';
+        END IF;
+    END $$;
+"#)]
 pub fn help(subject: &str, body: &str) -> String {
     let mut url = Url::parse(BASE_GITHUB_URL).expect("Failed to parse Github URL");
     url.set_path("/orgs/paradedb/discussions/new");

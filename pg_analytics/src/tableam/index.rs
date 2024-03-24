1488d1c2a17f18724837dfa00ef322d542927b00
Ming Ying <ming.ying.nyc@gmail.com>
2024-03-24 12:34:45 -0400

    index scan works but pk not being respected

--- pg_analytics/src/tableam/index.rs
+++ pg_analytics/src/tableam/index.rs
@@ -1,20 +1,27 @@
 use async_std::task;
 use core::ffi::{c_int, c_void};
-use deltalake::datafusion::common::arrow::array::RecordBatch;
+use deltalake::arrow::datatypes::Int64Type;
+use deltalake::datafusion::common::arrow::array::{AsArray, RecordBatch};
+use deltalake::datafusion::common::ScalarValue;
+use deltalake::datafusion::common::DataFusionError;
+use deltalake::datafusion::logical_expr::expr::Expr;
+use deltalake::datafusion::logical_expr::{col, LogicalPlanBuilder};
+use deltalake::datafusion::sql::TableReference;
 use pgrx::*;
+use shared::postgres::tid::{RowNumber, TIDError};
 use std::any::type_name;
 use std::sync::Arc;
+use thiserror::Error;
 
+use crate::datafusion::session::Session;
 use crate::datafusion::stream::Stream;
-use crate::datafusion::table::DatafusionTable;
-use crate::errors::{NotFound, ParadeError};
+use crate::datafusion::table::{DatafusionTable, RESERVED_TID_FIELD};
+use crate::errors::ParadeError;
+use crate::types::datatype::DataTypeError;
 use crate::types::datum::GetDatum;
 
 struct IndexScanDesc {
     rs_base: pg_sys::IndexFetchTableData,
-    curr_batch: Option<Arc<RecordBatch>>,
-    curr_batch_idx: usize,
-    completed: bool,
 }
 
 #[pg_guard]
@@ -25,21 +32,19 @@ pub extern "C" fn deltalake_index_fetch_begin(
         PgMemoryContexts::CurrentMemoryContext.switch_to(|_context| {
             let mut scan = PgBox::<IndexScanDesc>::alloc0();
             scan.rs_base.rel = rel;
-            scan.curr_batch = None;
-            scan.curr_batch_idx = 0;
-            scan.completed = false;
-
             scan.into_pg() as *mut pg_sys::IndexFetchTableData
         })
     }
 }
 
 #[pg_guard]
-pub extern "C" fn deltalake_index_fetch_reset(_data: *mut pg_sys::IndexFetchTableData) {}
+pub extern "C" fn deltalake_index_fetch_reset(_data: *mut pg_sys::IndexFetchTableData) {
+    info!("fetch reset");
+}
 
 #[pg_guard]
 pub extern "C" fn deltalake_index_fetch_end(_data: *mut pg_sys::IndexFetchTableData) {
-    info!("fetch end");
+    info!("scan done");
 }
 
 #[pg_guard]
@@ -52,13 +57,15 @@ pub extern "C" fn deltalake_index_fetch_tuple(
     all_dead: *mut bool,
 ) -> bool {
     unsafe {
-        // *call_again = false;
+        *call_again = false;
 
-        // if !all_dead.is_null() {
-        //     *all_dead = false;
-        // }
+        if !all_dead.is_null() {
+            *all_dead = false;
+        }
 
-        task::block_on(index_fetch_tuple_impl(scan, slot, tid)).expect("Failed to get next slot")
+        task::block_on(index_fetch_tuple_impl(scan, slot, tid)).unwrap_or_else(|err| {
+            panic!("{}", err);
+        })
     }
 }
 
@@ -67,73 +74,72 @@ async unsafe fn index_fetch_tuple_impl(
     scan: *mut pg_sys::IndexFetchTableData,
     slot: *mut pg_sys::TupleTableSlot,
     tid: pg_sys::ItemPointer,
-) -> Result<bool, ParadeError> {
-    info!("fetch tuple");
+) -> Result<bool, IndexScanError> {
     let dscan = scan as *mut IndexScanDesc;
-    if (*dscan).completed {
-        return Ok(false);
-    }
 
     if let Some(clear) = (*slot)
         .tts_ops
         .as_ref()
-        .ok_or(NotFound::Value(
-            type_name::<pg_sys::TupleTableSlotOps>().to_string(),
-        ))?
+        .ok_or(IndexScanError::NoTupleTableSlotOps)?
         .clear
     {
         clear(slot);
     }
 
     let pg_relation = unsafe { PgRelation::from_pg((*dscan).rs_base.rel) };
-    let schema_name = pg_relation.namespace();
-    let table_path = pg_relation.table_path()?;
+    let table_name = pg_relation.name().to_string();
+    let schema_name = pg_relation.namespace().to_string();
+    let catalog_name = Session::catalog_name()?;
+    let RowNumber(row_number) = RowNumber::try_from(*tid)?;
 
-    if (*dscan).curr_batch.is_none()
-        || (*dscan).curr_batch_idx
-            >= (*dscan)
-                .curr_batch
-                .as_ref()
-                .ok_or(NotFound::Value(type_name::<RecordBatch>().to_string()))?
-                .num_rows()
-    {
-        (*dscan).curr_batch_idx = 0;
 
-        (*dscan).curr_batch = match Stream::get_next_batch(schema_name, &table_path).await? {
-            Some(batch) => Some(Arc::new(batch)),
-            None => {
-                (*dscan).completed = true;
-                return Ok(false);
-            }
-        };
-    }
+    let dataframe = Session::with_session_context(|context| {
+        Box::pin(async move {
+            let arrow_schema = pg_relation.arrow_schema()?;
+            let column_names = arrow_schema.fields().iter()
+                .map(|field| field.name().as_str())
+                .filter(|&name| name != RESERVED_TID_FIELD)
+                .collect::<Vec<&str>>();
+        
+            let reference = TableReference::full(catalog_name, schema_name, table_name);
+            let table = context.table(reference).await?;
+            Ok(table
+                .filter(col(RESERVED_TID_FIELD).eq(Expr::Literal(ScalarValue::from(row_number))))?
+                .select_columns(&column_names)?)
+        })
+    })?;
 
-    let current_batch = (*dscan)
-        .curr_batch
-        .as_ref()
-        .ok_or(NotFound::Value(type_name::<RecordBatch>().to_string()))?;
-
-    for col_index in 0..current_batch.num_columns() {
-        let column = current_batch.column(col_index);
-
-        unsafe {
-            let tts_value = (*slot).tts_values.add(col_index);
-            let tts_isnull = (*slot).tts_isnull.add(col_index);
-
-            if let Some(datum) = column.get_datum((*dscan).curr_batch_idx)? {
-                *tts_value = datum;
-            } else {
-                *tts_isnull = true;
-            }
+    match dataframe.collect().await?.as_slice() {
+        [] => {
+            Ok(false)
         }
+        [batch] => {
+            if batch.num_rows() > 1 {
+                return Err(IndexScanError::DuplicateRowNumber(row_number));
+            }
+
+            for col_index in 0..batch.num_columns() {
+                let column = batch.column(col_index);
+
+                unsafe {
+                    let tts_value = (*slot).tts_values.add(col_index);
+                    let tts_isnull = (*slot).tts_isnull.add(col_index);
+
+                    if let Some(datum) = column.get_datum(0)? {
+                        *tts_value = datum;
+                    } else {
+                        *tts_isnull = true;
+                    }
+                }
+            }
+
+            (*slot).tts_tid = *tid;
+            pg_sys::ExecStoreVirtualTuple(slot);
+
+            Ok(false)
+        }
+        _ => Err(IndexScanError::DuplicateBatch(row_number)),
     }
-
-    (*slot).tts_tid = *tid;
-    pg_sys::ExecStoreVirtualTuple(slot);
-
-    (*dscan).curr_batch_idx += 1;
-
-    Ok(true)
 }
 
 #[pg_guard]
@@ -171,4 +177,28 @@ pub extern "C" fn deltalake_index_validate_scan(
     _state: *mut pg_sys::ValidateIndexState,
 ) {
     info!("validate scan");
+}
+
+#[derive(Error, Debug)]
+pub enum IndexScanError {
+    #[error(transparent)]
+    DataFusion(#[from] DataFusionError),
+
+    #[error(transparent)]
+    DataType(#[from] DataTypeError),
+
+    #[error(transparent)]
+    ParadeError(#[from] ParadeError),
+
+    #[error(transparent)]
+    TIDError(#[from] TIDError),
+
+    #[error("More than one row with row number {0} was found")]
+    DuplicateRowNumber(i64),
+
+    #[error("More than one batch with row number {0} was found")]
+    DuplicateBatch(i64),
+
+    #[error("TupleTableSlotOps not found")]
+    NoTupleTableSlotOps,
 }

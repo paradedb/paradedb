8153281279cb1f0b19cefe1837ffcb23e83a66cd
Ming Ying <ming.ying.nyc@gmail.com>
2024-04-11 19:44:51 -0400

    add drop test

--- pg_analytics/tests/drop.rs
+++ pg_analytics/tests/drop.rs
@@ -62,3 +62,17 @@ fn drop_heap_and_parquet(mut conn: PgConnection) {
         Err(err) => assert!(err.to_string().contains("does not exist")),
     };
 }
+
+#[rstest]
+fn drop_without_directory(mut conn: PgConnection) {
+    "CREATE TABLE t (a int, b text) USING parquet".execute(&mut conn);
+
+    let table_path = default_database_path(&mut conn);
+    std::fs::remove_dir_all(&table_path).unwrap();
+
+    "DROP TABLE t".execute(&mut conn);
+    match "SELECT * FROM t".fetch_result::<()>(&mut conn) {
+        Ok(_) => panic!("relation 't' should not exist after drop"),
+        Err(err) => assert!(err.to_string().contains("does not exist")),
+    };
+}

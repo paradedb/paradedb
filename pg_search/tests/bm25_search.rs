359424a40826d0f889ee6f1e32380f49b14d86b4
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2024-04-24 11:32:11 -0700

    fix: hardcoded column name in pg_search minmax function (#1082)

--- pg_search/tests/bm25_search.rs
+++ pg_search/tests/bm25_search.rs
@@ -305,6 +305,50 @@ fn highlight(mut conn: PgConnection) {
 }
 
 #[rstest]
+fn hybrid_with_complex_key_field_name(mut conn: PgConnection) {
+    // Create a test table.
+    "CALL paradedb.create_bm25_test_table(table_name => 'bm25_search', schema_name => 'paradedb');"
+        .execute(&mut conn);
+
+    // Add the custom key field column.
+    "ALTER TABLE paradedb.bm25_search ADD COLUMN custom_key_field SERIAL".execute(&mut conn);
+
+    // The test table will normally be created here, but it'll be skipped because we did it above.
+    SimpleProductsTable::setup_with_key_field("custom_key_field").execute(&mut conn);
+
+    r#"
+    CREATE EXTENSION vector;
+    ALTER TABLE paradedb.bm25_search ADD COLUMN embedding vector(3);
+
+    UPDATE paradedb.bm25_search m
+    SET embedding = ('[' ||
+    ((m.id + 1) % 10 + 1)::integer || ',' ||
+    ((m.id + 2) % 10 + 1)::integer || ',' ||
+    ((m.id + 3) % 10 + 1)::integer || ']')::vector;
+
+    CREATE INDEX on paradedb.bm25_search
+    USING hnsw (embedding vector_l2_ops)"#
+        .execute(&mut conn);
+
+    // Test with query object.
+    let columns: SimpleProductsTableVec = r#"
+    SELECT m.*, s.rank_hybrid
+    FROM paradedb.bm25_search m
+    LEFT JOIN (
+        SELECT * FROM bm25_search.rank_hybrid(
+            bm25_query => paradedb.parse('description:keyboard OR category:electronics'),
+            similarity_query => '''[1,2,3]'' <-> embedding',
+            bm25_weight => 0.9,
+            similarity_weight => 0.1
+        )
+    ) s ON m.custom_key_field = s.custom_key_field
+    LIMIT 5"#
+        .fetch_collect(&mut conn);
+
+    assert_eq!(columns.id, vec![2, 1, 29, 39, 9]);
+}
+
+#[rstest]
 fn alias(mut conn: PgConnection) {
     SimpleProductsTable::setup().execute(&mut conn);
 

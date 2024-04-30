a03b1d4b9365d307fe070757b4364d2c9d01d2be
Ming Ying <ming.ying.nyc@gmail.com>
2024-04-30 13:43:43 -0400

    introduce basefdw trait

---
+++ pg_lakehouse/src/object_store.rs
@@ -0,0 +1,51 @@
+use object_store::aws::{AmazonS3, AmazonS3Builder};
+use supabase_wrappers::prelude::require_option;
+use thiserror::Error;
+
+use super::options::*;
+
+impl TryFrom<ServerOptions> for AmazonS3 {
+    type Error = ObjectStoreError;
+
+    fn try_from(options: ServerOptions) -> Result<Self, Self::Error> {
+        let ServerOptions(options) = options;
+
+        let url = require_option(AmazonServerOption::Url.as_str(), &options)?;
+        let region = require_option(AmazonServerOption::Region.as_str(), &options)?;
+
+        let mut builder = AmazonS3Builder::new().with_url(url).with_region(region);
+
+        if let Some(access_key_id) = options.get(AmazonServerOption::AccessKeyId.as_str()) {
+            builder = builder.clone().with_access_key_id(access_key_id.as_str());
+        }
+
+        if let Some(secret_access_key) = options.get(AmazonServerOption::SecretAccessKey.as_str()) {
+            builder = builder.with_secret_access_key(secret_access_key.as_str());
+        }
+
+        if let Some(session_token) = options.get(AmazonServerOption::SessionToken.as_str()) {
+            builder = builder.with_token(session_token.as_str());
+        }
+
+        if let Some(endpoint) = options.get(AmazonServerOption::Endpoint.as_str()) {
+            builder = builder.with_endpoint(endpoint.as_str());
+        }
+
+        if let Some(allow_http) = options.get(AmazonServerOption::AllowHttp.as_str()) {
+            if allow_http == "true" {
+                builder = builder.with_allow_http(true);
+            }
+        }
+
+        Ok(builder.build()?)
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum ObjectStoreError {
+    #[error(transparent)]
+    Option(#[from] supabase_wrappers::options::OptionsError),
+
+    #[error(transparent)]
+    ObjectStore(#[from] object_store::Error),
+}

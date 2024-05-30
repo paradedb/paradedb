d4eef44c9587cdc4942de6914e7753c222f5b036
Ming Ying <ming.ying.nyc@gmail.com>
2024-05-30 14:52:22 -0400

    analyze tests

--- pg_lakehouse/tests/explain.rs
+++ pg_lakehouse/tests/explain.rs
@@ -74,3 +74,76 @@ async fn test_explain_federated(#[future(awt)] s3: S3, mut conn: PgConnection) -
 
     Ok(())
 }
+
+#[rstest]
+async fn test_explain_analyze_fdw(#[future(awt)] s3: S3, mut conn: PgConnection) -> Result<()> {
+    NycTripsTable::setup().execute(&mut conn);
+    let rows: Vec<NycTripsTable> = "SELECT * FROM nyc_trips".fetch(&mut conn);
+    s3.client.create_bucket().bucket(S3_BUCKET).send().await?;
+    s3.create_bucket(S3_BUCKET).await?;
+    s3.put_rows(S3_BUCKET, S3_KEY, &rows).await?;
+
+    NycTripsTable::setup_s3_listing_fdw(&s3.url.clone(), &format!("s3://{S3_BUCKET}/{S3_KEY}"))
+        .execute(&mut conn);
+
+    let explain: Vec<(String,)> =
+        "EXPLAIN ANALYZE SELECT COUNT(*) FROM trips WHERE tip_amount <> 0".fetch(&mut conn);
+
+    assert!(explain[0].0.contains("AggregateExec"));
+    assert!(explain[1].0.contains("CoalescePartitionsExec"));
+    assert!(explain[2].0.contains("AggregateExec"));
+    assert!(explain[3].0.contains("RepartitionExec"));
+    assert!(explain[4].0.contains("ProjectionExec"));
+    assert!(explain[5].0.contains("CoalesceBatchesExec"));
+    assert!(explain[6].0.contains("FilterExec"));
+    assert!(explain[7].0.contains("ParquetExec"));
+
+    Ok(())
+}
+
+#[rstest]
+async fn test_explain_analyze_heap(mut conn: PgConnection) -> Result<()> {
+    NycTripsTable::setup().execute(&mut conn);
+
+    let explain: Vec<(String,)> =
+        "EXPLAIN ANALYZE SELECT COUNT(*) FROM nyc_trips WHERE tip_amount <> 0".fetch(&mut conn);
+
+    assert!(explain[0].0.contains("Aggregate"));
+    assert!(explain[1].0.contains("Seq Scan on nyc_trips"));
+    assert!(explain[2].0.contains("Filter"));
+    assert!(explain[3].0.contains("Rows Removed by Filter"));
+    assert!(explain[4].0.contains("Planning Time"));
+    assert!(explain[5].0.contains("Execution Time"));
+
+    Ok(())
+}
+
+#[rstest]
+async fn test_explain_analyze_federated(
+    #[future(awt)] s3: S3,
+    mut conn: PgConnection,
+) -> Result<()> {
+    NycTripsTable::setup().execute(&mut conn);
+    let rows: Vec<NycTripsTable> = "SELECT * FROM nyc_trips".fetch(&mut conn);
+    s3.client.create_bucket().bucket(S3_BUCKET).send().await?;
+    s3.create_bucket(S3_BUCKET).await?;
+    s3.put_rows(S3_BUCKET, S3_KEY, &rows).await?;
+
+    NycTripsTable::setup_s3_listing_fdw(&s3.url.clone(), &format!("s3://{S3_BUCKET}/{S3_KEY}"))
+        .execute(&mut conn);
+
+    let explain: Vec<(String,)> =
+        "EXPLAIN ANALYZE SELECT COUNT(*) FROM trips LEFT JOIN nyc_trips ON trips.\"VendorID\" = nyc_trips.\"VendorID\"".fetch(&mut conn);
+
+    assert!(explain[0].0.contains("Aggregate"));
+    assert!(explain[1].0.contains("Hash Right Join"));
+    assert!(explain[2]
+        .0
+        .contains("Hash Cond: (nyc_trips.\"VendorID\" = trips.\"VendorID\")"));
+    assert!(explain[3].0.contains("Seq Scan on nyc_trips"));
+    assert!(explain[4].0.contains("Hash"));
+    assert!(explain[5].0.contains("Buckets"));
+    assert!(explain[6].0.contains("Foreign Scan on trips"));
+
+    Ok(())
+}

d2f1ea9c0d1b6b162dfaf938eeca2f6a5dd51e44
Ming Ying <ming.ying.nyc@gmail.com>
2024-06-06 14:12:37 -0400

    Tests wip

--- pg_lakehouse/tests/interrupt.rs
+++ pg_lakehouse/tests/interrupt.rs
@@ -3,10 +3,12 @@ mod fixtures;
 use anyhow::Result;
 use fixtures::*;
 use rstest::*;
+use shared::fixtures::arrow::primitive_setup_fdw_local_file_listing;
+use shared::fixtures::tempfile::TempDir;
 use sqlx::PgConnection;
 
 #[rstest]
-async fn test_sigint(database: Db) -> Result<()> {
+async fn test_sigint(database: Db, tempdir: TempDir) -> Result<()> {
     let mut first_conn = database.connection().await;
     let mut second_conn = database.connection().await;
 
@@ -14,4 +16,18 @@ async fn test_sigint(database: Db) -> Result<()> {
         .execute(&mut first_conn)
         .await
         .expect("could not create extension pg_lakehouse");
+
+    let stored_batch = primitive_record_batch()?;
+    let parquet_path = tempdir.path().join("test_arrow_types.parquet");
+    let parquet_file = File::create(&parquet_path)?;
+
+    let mut writer = ArrowWriter::try_new(parquet_file, stored_batch.schema(), None).unwrap();
+    writer.write(&stored_batch)?;
+    writer.close()?;
+
+    primitive_setup_fdw_local_file_listing(parquet_path.as_path().to_str().unwrap(), "parquet")
+        .execute(&mut first_conn);
+
+    let retrieved_batch = "SELECT pg_sleep(60), * FROM primitive"
+        .fetch_recordbatch(&mut first_conn, &stored_batch.schema());
 }

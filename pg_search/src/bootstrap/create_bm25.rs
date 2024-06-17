4beaec2612f755c17f125366981c2f10e07199b3
Neil Hansen <neil.hansen.31@gmail.com>
2024-06-17 13:41:17 -0600

    fix bad changes

--- pg_search/src/bootstrap/create_bm25.rs
+++ pg_search/src/bootstrap/create_bm25.rs
@@ -251,7 +251,9 @@ CREATE OR REPLACE PROCEDURE paradedb.drop_bm25(
 )
 LANGUAGE c AS 'MODULE_PATHNAME', '@FUNCTION_NAME@';
 ")]
-fn drop_bm25(index_name: &str, schema_name: &str) -> Result<()> {
+fn drop_bm25(index_name: &str, schema_name: Option<&str>) -> Result<()> {
+    let schema_name = schema_name.unwrap_or("current_schema()");
+
     Spi::run(&format!(
         r#"
         DO $$

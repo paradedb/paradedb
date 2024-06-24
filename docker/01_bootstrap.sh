46a3ddac9d26d2f3f98f92a0065077bf47820b98
Philippe Noël <philippemnoel@gmail.com>
2024-06-24 18:27:33 -0400

    Remove pg_cron

--- docker/01_bootstrap.sh
+++ docker/01_bootstrap.sh
@@ -34,15 +34,6 @@ PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "GR
 
 echo "Installing PostgreSQL extensions..."
 
-# This setting is required by pg_cron to CREATE EXTENSION properly. It can only be installed in one database, so we install it in
-# the user database. Creating the `pg_cron` extension requires a restart of the PostgreSQL server, so we can't do it here. A restart
-# is already part of the launch process of the Bitnami PostgreSQL container post this point, so the extension can be created by the
-# user after the container has been launched.
-#
-# For simplicity, and because we don't expect most users to use pg_cron, we don't force a restart here and we don't pre-create the
-# extension, leaving it to the user to do it if they want to use it.
-echo "cron.database_name = '$POSTGRESQL_DATABASE'" >> "/opt/bitnami/postgresql/conf/postgresql.conf"
-
 # Pre-install all required PostgreSQL extensions to the user database via the `postgres` superuser
 PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_search CASCADE;"
 PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_lakehouse CASCADE;"

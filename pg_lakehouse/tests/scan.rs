241c9d504d1bb9c60f74887dd8ad6df12e5abb11
Ming Ying <ming.ying.nyc@gmail.com>
2024-06-24 21:25:30 -0400

    almost all tests passing

--- pg_lakehouse/tests/scan.rs
+++ pg_lakehouse/tests/scan.rs
@@ -55,60 +55,7 @@ async fn test_trip_count(#[future(awt)] s3: S3, mut conn: PgConnection) -> Resul
     .execute(&mut conn);
 
     let count: (i64,) = "SELECT COUNT(*) FROM trips".fetch_one(&mut conn);
-
     assert_eq!(count.0, 100);
-
-    Ok(())
-}
-
-#[rstest]
-async fn test_trip_connect_success(#[future(awt)] s3: S3, mut conn: PgConnection) -> Result<()> {
-    NycTripsTable::setup().execute(&mut conn);
-    let rows: Vec<NycTripsTable> = "SELECT * FROM nyc_trips".fetch(&mut conn);
-    s3.client
-        .create_bucket()
-        .bucket(S3_TRIPS_BUCKET)
-        .send()
-        .await?;
-    s3.create_bucket(S3_TRIPS_BUCKET).await?;
-    s3.put_rows(S3_TRIPS_BUCKET, S3_TRIPS_KEY, &rows).await?;
-
-    NycTripsTable::setup_s3_listing_fdw(
-        &s3.url.clone(),
-        &format!("s3://{S3_TRIPS_BUCKET}/{S3_TRIPS_KEY}"),
-    )
-    .execute(&mut conn);
-    "CALL connect_table('trips')".execute_result(&mut conn)?;
-    "CALL connect_table('public.trips')".execute_result(&mut conn)?;
-
-    Ok(())
-}
-
-#[rstest]
-async fn test_trip_connect_failure(#[future(awt)] s3: S3, mut conn: PgConnection) -> Result<()> {
-    NycTripsTable::setup().execute(&mut conn);
-    let rows: Vec<NycTripsTable> = "SELECT * FROM nyc_trips".fetch(&mut conn);
-    s3.client
-        .create_bucket()
-        .bucket(S3_TRIPS_BUCKET)
-        .send()
-        .await?;
-    s3.create_bucket(S3_TRIPS_BUCKET).await?;
-    s3.put_rows(S3_TRIPS_BUCKET, S3_TRIPS_KEY, &rows).await?;
-
-    NycTripsTable::setup_s3_listing_fdw(
-        &s3.url.clone(),
-        &format!("s3://{S3_TRIPS_BUCKET}/invalid_file.parquet"),
-    )
-    .execute(&mut conn);
-    match "CALL connect_table('trips')".execute_result(&mut conn) {
-        Ok(_) => panic!("connect_table should have errored on an invalid Parquet path"),
-        Err(err) => {
-            assert!(err
-                .to_string()
-                .contains("Object at location invalid_file.parquet not found"))
-        }
-    }
 
     Ok(())
 }
@@ -184,41 +131,6 @@ async fn test_arrow_types_s3_delta(
 }
 
 #[rstest]
-async fn test_s3_delta_connect_success(
-    #[future(awt)] s3: S3,
-    mut conn: PgConnection,
-    tempdir: TempDir,
-) -> Result<()> {
-    let s3_bucket = "test-arrow-types-s3-delta";
-    let s3_path = "test_arrow_types";
-    let s3_endpoint = s3.url.clone();
-    let s3_object_path = format!("s3://{s3_bucket}/{s3_path}");
-    let temp_path = tempdir.path();
-
-    let batch = delta_primitive_record_batch()?;
-
-    let delta_schema = deltalake::kernel::Schema::try_from(batch.schema().as_ref())?;
-    let mut table = CreateBuilder::new()
-        .with_location(temp_path.to_string_lossy())
-        .with_columns(delta_schema.fields().to_vec())
-        .await?;
-    let mut writer = RecordBatchWriter::for_table(&table)?;
-    writer.write(batch.clone()).await?;
-    writer.flush_and_commit(&mut table).await?;
-
-    s3.create_bucket(s3_bucket).await?;
-    s3.put_directory(s3_bucket, s3_path, temp_path).await?;
-
-    primitive_setup_fdw_s3_delta(&s3_endpoint, &s3_object_path, "delta_primitive")
-        .execute(&mut conn);
-
-    "CALL connect_table('delta_primitive')".execute_result(&mut conn)?;
-    "CALL connect_table('public.delta_primitive')".execute_result(&mut conn)?;
-
-    Ok(())
-}
-
-#[rstest]
 async fn test_arrow_types_local_file_listing(
     mut conn: PgConnection,
     tempdir: TempDir,
@@ -273,60 +185,6 @@ async fn test_arrow_types_local_file_delta(mut conn: PgConnection, tempdir: Temp
             batch.column_by_name(field.name()),
             retrieved_batch.column_by_name(field.name())
         )
-    }
-
-    Ok(())
-}
-
-#[rstest]
-async fn test_local_file_delta_connect_success(
-    mut conn: PgConnection,
-    tempdir: TempDir,
-) -> Result<()> {
-    let temp_path = tempdir.path();
-    let batch = delta_primitive_record_batch()?;
-    let delta_schema = deltalake::kernel::Schema::try_from(batch.schema().as_ref())?;
-    let mut table = CreateBuilder::new()
-        .with_location(temp_path.to_string_lossy().as_ref())
-        .with_columns(delta_schema.fields().to_vec())
-        .await?;
-    let mut writer = RecordBatchWriter::for_table(&table)?;
-    writer.write(batch.clone()).await?;
-    writer.flush_and_commit(&mut table).await?;
-
-    primitive_setup_fdw_local_file_delta(&temp_path.to_string_lossy(), "delta_primitive")
-        .execute(&mut conn);
-
-    "CALL connect_table('delta_primitive')".execute_result(&mut conn)?;
-    "CALL connect_table('public.delta_primitive')".execute_result(&mut conn)?;
-
-    Ok(())
-}
-
-#[rstest]
-async fn test_local_file_delta_connect_failure(
-    mut conn: PgConnection,
-    tempdir: TempDir,
-) -> Result<()> {
-    let temp_path = tempdir.path();
-    let batch = delta_primitive_record_batch()?;
-    let delta_schema = deltalake::kernel::Schema::try_from(batch.schema().as_ref())?;
-    let mut table = CreateBuilder::new()
-        .with_location(temp_path.to_string_lossy().as_ref())
-        .with_columns(delta_schema.fields().to_vec())
-        .await?;
-    let mut writer = RecordBatchWriter::for_table(&table)?;
-    writer.write(batch.clone()).await?;
-    writer.flush_and_commit(&mut table).await?;
-
-    primitive_setup_fdw_local_file_delta("/var/folders/invalid", "delta_primitive")
-        .execute(&mut conn);
-
-    match "CALL connect_table('delta_primitive')".execute_result(&mut conn) {
-        Ok(_) => panic!("connect_table should have errored on an invalid path"),
-        Err(err) => {
-            assert!(err.to_string().contains("Generic LocalFileSystem error"))
-        }
     }
 
     Ok(())

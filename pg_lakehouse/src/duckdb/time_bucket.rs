a38b257791ba5bda67543f0023c9131dff2af42f
Devan <devandbenz@gmail.com>
2024-08-01 08:40:08 -0500

    feat: 1414: add support for time_bucket()
    Signed-off-by: Devan <devandbenz@gmail.com>

--- pg_lakehouse/src/duckdb/time_bucket.rs
+++ pg_lakehouse/src/duckdb/time_bucket.rs
@@ -1,18 +1,64 @@
-//time_bucket(bucket_width, date[, offset])
-// Description	Truncate date by the specified interval bucket_width. Buckets are offset by offset interval.
-// Example	time_bucket(INTERVAL '2 months', DATE '1992-04-20', INTERVAL '1 month')
-// Result	1992-04-01
-// time_bucket(bucket_width, date[, origin])
-// Description	Truncate date by the specified interval bucket_width. Buckets are aligned relative to origin date. origin defaults to 2000-01-03 for buckets that don't include a month or year interval, and to 2000-01-01 for month and year buckets.
-// Example	time_bucket(INTERVAL '2 weeks', DATE '1992-04-20', DATE '1992-04-01')
-// Result	1992-04-15
-//
+use pgrx::iter::TableIterator;
 use pgrx::*;
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
+
+#[derive(Serialize, Deserialize)]
+pub enum TimeBucketInput {
+    Date(Date),
+    Timestamp(Timestamp),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum TimeBucketOffset {
+    Interval(Interval),
+    Date(Date),
+}
+
+impl Display for TimeBucketInput {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TimeBucketInput::Date(input) => {
+                write!(f, "{}::DATE", input.to_string())
+            }
+            TimeBucketInput::Timestamp(input) => {
+                write!(f, "{}", input.to_string())
+            }
+        }
+    }
+}
+
+impl Display for TimeBucketOffset {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TimeBucketOffset::Date(input) => {
+                write!(f, "DATE {}", input.to_string())
+            }
+            TimeBucketOffset::Interval(input) => {
+                write!(f, "INTERVAL {}", input.to_string())
+            }
+        }
+    }
+}
+
+fn create_time_bucket(
+    bucket_width: Interval,
+    input: TimeBucketInput,
+    offset: Option<TimeBucketOffset>,
+) -> String {
+    if let Some(bucket_offset) = offset {
+        format!("INTERVAL {}, {}, {}", bucket_width, input, bucket_offset)
+    } else {
+        format!("INTERVAL {}, {}", bucket_width, input)
+    }
+}
 
 #[pg_extern]
-pub fn time_bucket(param: &str) -> String {
-    log!("Logging out to psql maybe?");
-    log!("{}", param);
+pub fn time_bucket(
+    bucket_width: Interval,
+    input: Date,
+) -> TableIterator<'static, (name!(time_bucket, Date),)> {
+    let bucket_query = create_time_bucket(bucket_width, TimeBucketInput::Date(input), None);
 
-    "Ok!".to_string()
+    TableIterator::once((bucket_query.parse().unwrap(),))
 }

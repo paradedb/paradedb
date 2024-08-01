46f58fe390370bd79a9247c52267352eab38da3d
Philippe Noël <philippemnoel@gmail.com>
2024-08-01 15:29:12 -0700

    Add to bootstrap

--- docker/01_bootstrap.sh
+++ docker/01_bootstrap.sh
@@ -26,44 +26,31 @@ for DB in template_paradedb "$POSTGRES_DB"; do
 EOSQL
 done
 
+# Add the `paradedb` schema to both template_database and $POSTGRES_DB
+for DB in template_paradedb "$POSTGRES_DB"; do
+  echo "Adding 'paradedb' schema to $DB"
+  ALTER DATABASE "$DB" SET search_path TO "$user",public,paradedb;
+EOSQL
+done
 
+# Add ParadeDB extensions to shared_preload_libraries
+PG_CONF="/var/lib/postgresql/data/postgresql.conf"
+LIBRARIES_TO_ADD="pg_search,pg_lakehouse"
+if [ -f "$PG_CONF" ]; then
+  if grep -q "^shared_preload_libraries" "$PG_CONF"; then
+    # If the line exists, append new libraries to it
+    sed -i "s/^shared_preload_libraries = '\(.*\)'/shared_preload_libraries = '\1,$LIBRARIES_TO_ADD'/" "$PG_CONF"
+  else
+    # If the line doesn't exist, add it
+    echo "shared_preload_libraries = '$LIBRARIES_TO_ADD'" >> "$PG_CONF"
+  fi
+  echo "Added $LIBRARIES_TO_ADD to shared_preload_libraries in postgresql.conf"
+else
+  echo "Error: postgresql.conf not found at $PG_CONF"
+  exit 1
+fi
 
-
-
-# If no user is set, the default user will be the `postgres` superuser, so we
-# set the superuser password to default to the user password in that case
-SUPERUSER_PASSWORD=${POSTGRESQL_POSTGRES_PASSWORD:-$POSTGRESQL_PASSWORD}
-
-echo "ParadeDB bootstrap started..."
-echo "Configuring PostgreSQL search path..."
-
-# Add the `paradedb` schema to the user database, and default to public (by listing it first)
-PGPASSWORD=$POSTGRESQL_PASSWORD psql -U "$POSTGRESQL_USERNAME" -d "$POSTGRESQL_DATABASE" -c "ALTER DATABASE $POSTGRESQL_DATABASE SET search_path TO public,paradedb;"
-
-# Add the `paradedb` schema to the template1 database, to have it inherited by all new databases
-# created post-initialization, and default to public (by listing it first)
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "ALTER DATABASE template1 SET search_path TO public,paradedb;"
-
-echo "Configuring PostgreSQL permissions..."
-
-# Grant pg_read_all_settings role to the user (necessary for general database introspection)
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "GRANT pg_read_all_settings TO \"$POSTGRESQL_USERNAME\";"
-
-echo "Installing PostgreSQL extensions..."
-
-# Pre-install all required PostgreSQL extensions to the user database via the `postgres` superuser
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_search CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_lakehouse CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS pg_ivm CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS vector CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;"
-
-# Pre-install all required PostgreSQL extensions to the template1 database, to have them inherited by all new
-# databases created post-initialization, via the `postgres` user
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS pg_search CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS pg_lakehouse CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS pg_ivm CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS vector CASCADE;"
-PGPASSWORD=$SUPERUSER_PASSWORD psql -U postgres -d template1 -c "CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;"
+echo "Restarting PostgreSQL to apply changes..."
+pg_ctl restart
 
 echo "ParadeDB bootstrap completed!"

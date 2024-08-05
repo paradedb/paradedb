fe387d279998dfa81505ec202afa8c03b214b7da
Philippe Noël <philippemnoel@gmail.com>
2024-08-05 16:24:22 -0700

    Invert

--- docker/update_pg_search.sh
+++ docker/update_pg_search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Upgrades ParadeDB pg_lakehouse to the specified version. Assumes the new version of the
+# Upgrades ParadeDB pg_search to the specified version. Assumes the new version of the
 # extension is already downloaded and installed on the system.
 
 # Exit on subcommand errors
@@ -11,13 +11,13 @@ export PGUSER="$POSTGRES_USER"
 
 PARADEDB_VERSION="${PARADEDB_VERSION%%+*}"
 
-# Update ParadeDB pg_lakehouse into both template_database and $POSTGRES_DB
+# Update ParadeDB pg_search into both template_database and $POSTGRES_DB
 for DB in template_paradedb "$POSTGRES_DB" "${@}"; do
-  echo "Updating ParadeDB pg_lakehouse '$DB' to $PARADEDB_VERSION"
+  echo "Updating ParadeDB pg_search '$DB' to $PARADEDB_VERSION"
   psql -d "$DB" -c "
     -- Upgrade ParadeDB extensions
-    -- pg_lakehouse
-    CREATE EXTENSION IF NOT EXISTS pg_lakehouse VERSION '$PARADEDB_VERSION';
-    ALTER EXTENSION pg_lakehouse UPDATE TO '$PARADEDB_VERSION';
+    -- pg_search
+    CREATE EXTENSION IF NOT EXISTS pg_search VERSION '$PARADEDB_VERSION';
+    ALTER EXTENSION pg_search UPDATE TO '$PARADEDB_VERSION';
   "
 done

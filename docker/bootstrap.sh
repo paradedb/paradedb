8b41f25e4e6345925246b0850a45f2d6335e7d34
Philippe Noël <philippemnoel@gmail.com>
2024-08-05 13:59:30 -0700

    Move shared_preload to main dockerfile

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -9,32 +9,6 @@ set -Eeuo pipefail
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-# Add extensions to shared_preload_libraries
-PG_CONF="/var/lib/postgresql/data/postgresql.conf"
-LIBRARIES_TO_ADD="pg_search,pg_lakehouse,pg_cron"
-if [ -f "$PG_CONF" ]; then
-  if grep -q "^shared_preload_libraries" "$PG_CONF"; then
-    # If the line exists, append new libraries to it
-    sed -i "s/^shared_preload_libraries = '\(.*\)'/shared_preload_libraries = '\1,$LIBRARIES_TO_ADD'/" "$PG_CONF"
-  else
-    # If the line doesn't exist, add it
-    echo "shared_preload_libraries = '$LIBRARIES_TO_ADD'" >> "$PG_CONF"
-  fi
-  echo "Added $LIBRARIES_TO_ADD to shared_preload_libraries in postgresql.conf"
-else
-  echo "Error: postgresql.conf not found at $PG_CONF"
-  exit 1
-fi
-
-# This setting is required by pg_cron to CREATE EXTENSION properly. It can only be installed in one database,
-# so we install it in the user database. Creating the `pg_cron` extension requires a restart of the PostgreSQL server.
-echo "cron.database_name = '$POSTGRES_DB'" >> "$PG_CONF"
-
-# Sleep for 10 seconds to allow the server to restart
-echo "Restarting PostgreSQL to apply changes..."
-pg_ctl restart
-sleep 10
-
 # Create the 'template_paradedb' template db
 "${psql[@]}" <<- 'EOSQL'
 CREATE DATABASE template_paradedb IS_TEMPLATE true;
@@ -43,7 +17,6 @@ EOSQL
 # Load ParadeDB extensions into both template_database and $POSTGRES_DB
 for DB in template_paradedb "$POSTGRES_DB"; do
   echo "Loading ParadeDB extensions into $DB"
-  echo "${psql[@]} --dbname=$DB"  # Debugging statement to check the command
   "${psql[@]}" --dbname="$DB" <<-'EOSQL'
     CREATE EXTENSION IF NOT EXISTS pg_search;
     CREATE EXTENSION IF NOT EXISTS pg_lakehouse;

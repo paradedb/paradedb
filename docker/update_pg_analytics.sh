52a3ac8638438b20beae591cd46118f9937c1bd3
Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
2024-08-06 15:12:33 -0700

    Update update_pg_analytics.sh
    Signed-off-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>

--- docker/update_pg_analytics.sh
+++ docker/update_pg_analytics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Upgrades ParadeDB pg_lakehouse to the specified version. Assumes the new version of the
+# Upgrades ParadeDB pg_analytics to the specified version. Assumes the new version of the
 # extension is already downloaded and installed on the system.
 
 # Exit on subcommand errors
@@ -11,13 +11,13 @@ export PGUSER="$POSTGRES_USER"
 
 PARADEDB_VERSION="${PARADEDB_VERSION%%+*}"
 
-# Update ParadeDB pg_lakehouse into both template_database and $POSTGRES_DB
+# Update ParadeDB pg_analytics into both template_database and $POSTGRES_DB
 for DB in template_paradedb "$POSTGRES_DB" "${@}"; do
-  echo "Updating ParadeDB pg_lakehouse '$DB' to $PARADEDB_VERSION"
+  echo "Updating ParadeDB pg_analytics '$DB' to $PARADEDB_VERSION"
   psql -d "$DB" -c "
     -- Upgrade ParadeDB extensions
-    -- pg_lakehouse
-    CREATE EXTENSION IF NOT EXISTS pg_lakehouse VERSION '$PARADEDB_VERSION';
-    ALTER EXTENSION pg_lakehouse UPDATE TO '$PARADEDB_VERSION';
+    -- pg_analytics
+    CREATE EXTENSION IF NOT EXISTS pg_analytics VERSION '$PARADEDB_VERSION';
+    ALTER EXTENSION pg_analytics UPDATE TO '$PARADEDB_VERSION';
   "
 done

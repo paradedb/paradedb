e62c700b7ffa86871363f1ef29126a60cf1a92f3
Philippe Noël <philippemnoel@gmail.com>
2024-08-28 18:24:08 -0400

    Try this

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -9,19 +9,19 @@ set -Eeuo pipefail
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-# Create the `template_paradedb` template db
-psql -d postgres -c "CREATE DATABASE template_paradedb IS_TEMPLATE true;"
+# # Create the `template_paradedb` template db
+# psql -d postgres -c "CREATE DATABASE template_paradedb IS_TEMPLATE true;"
 
 # The `pg_cron` extension can only be installed in the `postgres` database, as per
 # our configuration in our Dockerfile. Therefore, we install it separately here.
 psql -d postgres -c "CREATE EXTENSION IF NOT EXISTS pg_cron;"
 
 # Load ParadeDB and third-party extensions into both template_database and $POSTGRES_DB
-for DB in template_paradedb "$POSTGRES_DB"; do
+# for DB in template_paradedb "$POSTGRES_DB"; do
+for DB in "$POSTGRES_DB"; do
   echo "Loading ParadeDB extensions into $DB"
   psql -d "$DB" <<-'EOSQL'
     CREATE EXTENSION IF NOT EXISTS pg_search;
-    sleep 5
     CREATE EXTENSION IF NOT EXISTS pg_analytics;
     CREATE EXTENSION IF NOT EXISTS pg_ivm;
     CREATE EXTENSION IF NOT EXISTS vector;
@@ -30,7 +30,8 @@ EOSQL
 done
 
 # Add the `paradedb` schema to both template_database and $POSTGRES_DB
-for DB in template_paradedb "$POSTGRES_DB"; do
+# for DB in template_paradedb "$POSTGRES_DB"; do
+for DB in "$POSTGRES_DB"; do
   echo "Adding 'paradedb' search_path to $DB"
   psql -d "$DB" -c "ALTER DATABASE \"$DB\" SET search_path TO public,paradedb;"
 done

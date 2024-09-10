fdaf5feb25f1b58735a67be9f43074a47d768455
MikeRotchip <p3tr.kada@gmail.com>
2024-09-10 19:36:02 +0200

    spaces

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -27,12 +27,12 @@ for DB in template_paradedb "$POSTGRES_DB"; do
     CREATE EXTENSION IF NOT EXISTS vectorscale;
 
     CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		-- Reconnect to update pg_setting.resetval
-		-- See https://github.com/postgis/docker-postgis/issues/288
-		\c
-		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    -- Reconnect to update pg_setting.resetval
+    -- See https://github.com/postgis/docker-postgis/issues/288
+    \c
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
 done
 

d10c3937861047a20d504dd7c706e2baeffd404f
Weijun-H <huangweijun1001@gmail.com>
2024-09-28 13:08:00 +0800

    chore: test

--- shared/src/telemetry/bgworker.rs
+++ shared/src/telemetry/bgworker.rs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+// tesst filles
 use super::{TelemetryConfigStore, TermPoll};
 use crate::telemetry::controller::{TelemetryController, TelemetrySender};
 use crate::telemetry::postgres::PostgresDirectoryStore;

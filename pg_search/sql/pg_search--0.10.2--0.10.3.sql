db64ecb681661734d1c0552ab1797551d6f58dd2
Ming Ying <ming.ying.nyc@gmail.com>
2024-09-30 17:41:15 -0400

    upgrade 0.10.3

---
+++ pg_search/sql/pg_search--0.10.2--0.10.3.sql
@@ -0,0 +1,7 @@
+CREATE TYPE TestTable AS ENUM (
+	'Items',
+	'Orders',
+	'Parts'
+);
+DROP FUNCTION IF EXISTS paradedb.create_bm25_test_table(table_name pg_catalog."varchar", schema_name pg_catalog."varchar");
+CREATE OR REPLACE PROCEDURE paradedb.create_bm25_test_table(table_name pg_catalog."varchar" DEFAULT 'bm25_test_table', schema_name pg_catalog."varchar" DEFAULT 'paradedb', table_type paradedb.testtable DEFAULT 'Items') AS 'MODULE_PATHNAME', 'create_bm25_test_table_wrapper' LANGUAGE c;

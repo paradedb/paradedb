3d4759fa28f43a1e25a353dbd23bcf786188c1d7
Eric B. Ridge <eebbrr@paradedb.com>
2024-10-08 13:34:29 -0400

    abstract away Integer/String/Value across the various postgres versions that are different

--- pg_search/src/api/mod.rs
+++ pg_search/src/api/mod.rs
@@ -16,7 +16,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use pgrx::{is_a, pg_sys};
-use std::ffi::c_void;
+use std::ffi::{c_void, CStr};
 
 pub mod config;
 pub mod index;
@@ -39,4 +39,44 @@ macro_rules! nodecast {
     ($type_:ident, $kind:ident, $node:expr) => {
         $crate::api::node::<pg_sys::$type_>($node.cast(), pg_sys::NodeTag::$kind)
     };
+}
+
+pub trait AsInt {
+    unsafe fn as_int(&self) -> Option<i32>;
+}
+
+pub trait AsCStr {
+    unsafe fn as_c_str(&self) -> Option<&CStr>;
+}
+
+#[cfg(any(feature = "pg13", feature = "pg14"))]
+impl AsInt for *mut pg_sys::Node {
+    unsafe fn as_int(&self) -> Option<i32> {
+        let node = nodecast!(Value, T_Integer, *self)?;
+        Some((*node).val.ival)
+    }
+}
+
+#[cfg(not(any(feature = "pg13", feature = "pg14")))]
+impl AsInt for *mut pg_sys::Node {
+    unsafe fn as_int(&self) -> Option<i32> {
+        let node = nodecast!(Integer, T_Integer, *self)?;
+        Some((*node).ival)
+    }
+}
+
+#[cfg(any(feature = "pg13", feature = "pg14"))]
+impl AsCStr for *mut pg_sys::Node {
+    unsafe fn as_c_str(&self) -> Option<&CStr> {
+        let node = nodecast!(Value, T_String, *self)?;
+        Some(CStr::from_ptr((*node).val.str_))
+    }
+}
+
+#[cfg(not(any(feature = "pg13", feature = "pg14")))]
+impl AsCStr for *mut pg_sys::Node {
+    unsafe fn as_c_str(&self) -> Option<&CStr> {
+        let node = nodecast!(String, T_String, *self)?;
+        Some(CStr::from_ptr((*node).sval))
+    }
 }

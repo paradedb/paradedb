243f9f03d89b4bc8e7e41b78f09d950c85a3de45
Eric B. Ridge <eebbrr@paradedb.com>
2024-10-09 18:14:25 -0400

    copyright headers

--- pg_search/src/postgres/customscan/pdbscan/scan_state.rs
+++ pg_search/src/postgres/customscan/pdbscan/scan_state.rs
@@ -1,3 +1,20 @@
+// Copyright (c) 2023-2024 Retake, Inc.
+//
+// This file is part of ParadeDB - Postgres for Search and Analytics
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 use crate::index::reader::{SearchIndexReader, SearchResults};
 use crate::postgres::customscan::builders::custom_state::CustomScanStateWrapper;
 use crate::postgres::customscan::pdbscan::exec_methods::ExecState;

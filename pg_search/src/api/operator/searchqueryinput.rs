50e2bd54884cfc35e0fa8592010999bfdcaca88c
Neil Hansen <neil.hansen.31@gmail.com>
2024-10-24 18:29:56 -0700

    revert searchqueryinput

--- pg_search/src/api/operator/searchqueryinput.rs
+++ pg_search/src/api/operator/searchqueryinput.rs
@@ -50,11 +50,10 @@ pub fn search_with_query_input(
                 _ => panic!("the SeachQueryInput must be wrapped in a WithIndex variant"),
             }
         };
-        let indexrel = unsafe {
-            PgRelation::with_lock(index_oid, pg_sys::AccessShareLock as pg_sys::LOCKMODE)
-        };
-        let search_index =
-            open_search_index(&indexrel).expect("should be able to open search index");
+        let search_index = open_search_index(unsafe {
+            &PgRelation::with_lock(index_oid, pg_sys::AccessShareLock as pg_sys::LOCKMODE)
+        })
+        .expect("should be able to open search index");
 
         let scan_state = search_index.get_reader().unwrap();
         let top_docs = scan_state.search_via_channel(

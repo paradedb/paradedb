f55b7c73ac16d1bd4170afb486286caf626a0946
Neil Hansen <neil.hansen.31@gmail.com>
2024-11-11 12:53:45 -0800

    upgrade script

--- pg_search/sql/pg_search--0.12.0--0.12.1.sql
+++ pg_search/sql/pg_search--0.12.0--0.12.1.sql
@@ -9,3 +9,6 @@ CREATE  FUNCTION "term"(
 IMMUTABLE STRICT PARALLEL SAFE 
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'term_anyenum_wrapper';
+
+DROP PROCEDURE IF EXISTS paradedb.create_bm25(index_name text, table_name text, key_field text, schema_name text, text_fields jsonb, numeric_fields jsonb, boolean_fields jsonb, json_fields jsonb, range_fields jsonb, datetime_fields jsonb, predicates text);
+CREATE OR REPLACE PROCEDURE paradedb.create_bm25(index_name text DEFAULT '', table_name text DEFAULT '', key_field text DEFAULT '', schema_name text DEFAULT 'current_schema', text_fields jsonb DEFAULT '{}', numeric_fields jsonb DEFAULT '{}', boolean_fields jsonb DEFAULT '{}', json_fields jsonb DEFAULT '{}', range_fields jsonb DEFAULT '{}', datetime_fields jsonb DEFAULT '{}', predicates text DEFAULT '', fields jsonb DEFAULT '{}') AS 'MODULE_PATHNAME', 'create_bm25_jsonb_wrapper' LANGUAGE c;

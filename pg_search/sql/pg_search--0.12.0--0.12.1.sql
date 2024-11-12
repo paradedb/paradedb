4ab4d71b45d3b2c57a314ad72fcc39e9f154f41f
Ming <ming.ying.nyc@gmail.com>
2024-11-12 11:58:06 -0500

    feat: Support indexing enums (#1924)

--- pg_search/sql/pg_search--0.12.0--0.12.1.sql
+++ pg_search/sql/pg_search--0.12.0--0.12.1.sql
@@ -1,1 +1,11 @@
 ALTER TYPE TestTable ADD VALUE 'Customers';
+
+-- pg_search/src/api/index.rs:580
+-- pg_search::api::index::term
+CREATE  FUNCTION "term"(
+	"field" FieldName, /* pg_search::api::index::FieldName */
+	"value" anyenum /* pg_search::schema::anyenum::AnyEnum */
+) RETURNS SearchQueryInput /* pg_search::query::SearchQueryInput */
+IMMUTABLE STRICT PARALLEL SAFE 
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'term_anyenum_wrapper';

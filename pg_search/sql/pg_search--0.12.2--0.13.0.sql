0abc611a42e46a4f126596dbf7f15f2cc8c51767
Ming Ying <ming.ying.nyc@gmail.com>
2024-11-22 11:04:11 -0500

    fix upgrade script

--- pg_search/sql/pg_search--0.12.2--0.13.0.sql
+++ pg_search/sql/pg_search--0.12.2--0.13.0.sql
@@ -1,10 +1,6 @@
 DROP PROCEDURE IF EXISTS paradedb.create_bm25(index_name text, table_name text, key_field text, schema_name text, text_fields jsonb, numeric_fields jsonb, boolean_fields jsonb, json_fields jsonb, range_fields jsonb, datetime_fields jsonb, predicates text);
 DROP PROCEDURE IF EXISTS paradedb.drop_bm25(index_name text, schema_name text);
-/* </end connected objects> */
-/* <begin connected objects> */
--- pg_search/src/api/config.rs:84
--- pg_search::api::config::format_create_index
-CREATE OR REPLACE FUNCTION paradedb.format_create_index(
+CREATE OR REPLACE FUNCTION paradedb.format_create_bm25(
     index_name text DEFAULT '',
     table_name text DEFAULT '',
     key_field text DEFAULT '',
@@ -18,7 +14,7 @@ CREATE OR REPLACE FUNCTION paradedb.format_create_index(
     predicates text DEFAULT ''
 )
 RETURNS text
-LANGUAGE c AS 'MODULE_PATHNAME', 'format_create_index_wrapper';
+LANGUAGE c AS 'MODULE_PATHNAME', 'format_create_bm25_wrapper';
 
 DROP FUNCTION IF EXISTS boost(boost pg_catalog.float4, query searchqueryinput);
 CREATE OR REPLACE FUNCTION boost(factor pg_catalog.float4, query searchqueryinput) RETURNS searchqueryinput AS 'MODULE_PATHNAME', 'boost_wrapper' IMMUTABLE LANGUAGE c PARALLEL SAFE STRICT;

a7457fbb6cba099be9132abb131ab81a38b1d865
Weijun-H <huangweijun1001@gmail.com>
2024-11-27 21:55:11 +0800

    chore: Clean up

--- pg_search/sql/pg_search--0.12.2--0.13.0.sql
+++ pg_search/sql/pg_search--0.12.2--0.13.0.sql
@@ -50,19 +50,3 @@ CREATE  FUNCTION "index_fields"(
 STRICT
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'index_fields_wrapper';
-
-DROP FUNCTION IF EXISTS tokenizer(name text, remove_long pg_catalog.int4, lowercase bool, min_gram pg_catalog.int4, max_gram pg_catalog.int4, prefix_only bool, language text, pattern text, stemmer text);
-CREATE OR REPLACE FUNCTION tokenizer(
-    name text,
-    remove_long pg_catalog.int4 DEFAULT '255',
-    lowercase bool DEFAULT '(('t')::pg_catalog.bool)',
-    min_gram pg_catalog.int4 DEFAULT NULL,
-    max_gram pg_catalog.int4 DEFAULT NULL,
-    prefix_only bool DEFAULT NULL,
-    language text DEFAULT NULL,
-    pattern text DEFAULT NULL,
-    stemmer TEXT DEFAULT NULL,
-    stopwords_language text DEFAULT NULL,
-    stopwords text[] DEFAULT NULL
-) RETURNS jsonb
-AS 'MODULE_PATHNAME', 'tokenizer_wrapper' IMMUTABLE LANGUAGE c PARALLEL SAFE;

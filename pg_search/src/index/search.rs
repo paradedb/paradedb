3a55e681134a534273a92bfeb03a0de49a2211ad
Ming Ying <ming.ying.nyc@gmail.com>
2025-01-09 12:24:04 -0500

    debug logs

--- pg_search/src/index/search.rs
+++ pg_search/src/index/search.rs
@@ -68,12 +68,19 @@ impl BlockDirectoryType {
 
 impl WriterResources {
     pub fn resources(&self, indexrel: &PgRelation) -> IndexConfig {
+        let default_parallelism: usize = std::thread::available_parallelism()
+            .expect("your computer should have at least one core")
+            .get()
+            .try_into()
+            .expect("your computer has too many cores");
+        pgrx::info!("default_parallelism: {}", default_parallelism);
         let options = indexrel.rd_options as *mut SearchIndexCreateOptions;
         if options.is_null() {
             panic!("must specify key_field")
         }
         let options = unsafe { &*options };
         let target_segment_count = options.target_segment_count();
+        pgrx::info!("target_segment_count: {}", target_segment_count);
         let merge_on_insert = options.merge_on_insert();
 
         match self {

34923a37f5eec97c4a91ccf5f38239267feae4b6
Eric B. Ridge <eebbrr@paradedb.com>
2025-01-11 08:58:56 -0500

    add to schema upgrade script, per schemabot

--- pg_search/sql/pg_search--0.13.2--0.13.3.sql
+++ pg_search/sql/pg_search--0.13.2--0.13.3.sql
@@ -28,3 +28,16 @@ CREATE  FUNCTION "storage_info"(
 STRICT
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'storage_info_wrapper';
+/* </end connected objects> */
+/* <begin connected objects> */
+-- pg_search/src/api/index.rs:554
+-- pg_search::api::index::regex_phrase
+CREATE  FUNCTION "regex_phrase"(
+    "field" FieldName, /* pg_search::api::index::FieldName */
+    "regexes" TEXT[], /* alloc::vec::Vec<alloc::string::String> */
+    "slop" INT DEFAULT NULL, /* core::option::Option<i32> */
+    "max_expansions" INT DEFAULT NULL /* core::option::Option<i32> */
+) RETURNS SearchQueryInput /* pg_search::query::SearchQueryInput */
+    IMMUTABLE PARALLEL SAFE
+    LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'regex_phrase_wrapper';

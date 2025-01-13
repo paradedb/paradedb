4ac08bdbad67893bbd0dfd2865ae458011bc2a07
Akshat Jaimini <destrex271@gmail.com>
2025-01-13 20:49:08 +0530

    Update install.sh
    Co-authored-by: Philippe Noël <21990816+philippemnoel@users.noreply.github.com>
    Signed-off-by: Akshat Jaimini <destrex271@gmail.com>

--- install.sh
+++ install.sh
@@ -109,13 +109,6 @@ installDocker() {
 
   if docker inspect "paradedb" > /dev/null 2>&1; then
     echo -e "We found a previous paradedb container on your system.\nWe need to remove it to continue this setup."
-    read -r -p "Would you like to remove it? [y/N] " response
-    case "$response" in
-      [yY][eE][sS]|[yY])
-        docker stop paradedb || true
-        docker rm paradedb || true
-        echo "Successfully Removed Container ✅"
-    esac
   fi
 
   # Pull Docker image

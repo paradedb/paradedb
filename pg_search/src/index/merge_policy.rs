6d86d4bd99c51b0c49ae094ebc84d2ddac4f4801
Eric B. Ridge <eebbrr@paradedb.com>
2025-02-19 08:11:24 -0500

    fix: no longer use a Snapshot to acquire the MergeLock
    We can check the "vacuum horizon" (my words) to decide if the last merge
    transaction's effects are visible to the backend trying to obtain a
    MergeLock, instead of looking at active Snapshot.
    
    In fact, looking at the active Snapshot is not even possible when
    operating inside a Logical Replication Worker -- doing so leads to an
    assert failure in Postgres.

--- pg_search/src/index/merge_policy.rs
+++ pg_search/src/index/merge_policy.rs
@@ -71,9 +71,38 @@ impl MergeLock {
             let mut page = merge_lock.page_mut();
             let metadata = page.contents_mut::<MergeLockData>();
             let last_merge = metadata.last_merge;
-            let snapshot = pg_sys::GetActiveSnapshot();
-            let last_merge_visible = pg_sys::TransactionIdIsCurrentTransactionId(last_merge)
-                || !pg_sys::XidInMVCCSnapshot(last_merge, snapshot);
+
+            // in order to return the MergeLock we need to make sure we can see the effects of the
+            // last merge that ran.
+            //
+            // We already know we're the only backend with the Buffer-level lock, because the
+            // `.get_buffer_conditional()` call above gave us the Buffer, so now we need to ensure
+            // we're allowed to touch the segments that may have been modified by the last merge
+            let last_merge_visible =
+                // the last_merge value is zero b/c we've never done a merge
+                last_merge == pg_sys::InvalidTransactionId
+
+                    // or it is from this transaction
+                || pg_sys::TransactionIdIsCurrentTransactionId(last_merge)
+
+                    // or the last_merge transaction's effects are known to be visible by all
+                    // current/future transactions
+                || {
+                    #[cfg(feature = "pg13")]
+                    {
+                        pg_sys::TransactionIdPrecedes(last_merge, pg_sys::RecentGlobalXmin)
+                    }
+
+                    #[cfg(any(
+                        feature = "pg14",
+                        feature = "pg15",
+                        feature = "pg16",
+                        feature = "pg17"
+                    ))]
+                    {
+                        pg_sys::GlobalVisCheckRemovableXid(std::ptr::null_mut(), last_merge)
+                    }
+                };
 
             if last_merge_visible {
                 metadata.last_merge = pg_sys::GetCurrentTransactionId();

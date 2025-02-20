91dfb0dced7d223288ef298c230413fe9e10335c
Eric B. Ridge <eebbrr@paradedb.com>
2025-02-20 14:14:19 -0500

    come on, rust.  you're killing me

--- pg_search/src/postgres/mod.rs
+++ pg_search/src/postgres/mod.rs
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+#![allow(unpredictable_function_pointer_comparisons)]
 use crate::postgres::parallel::Spinlock;
 use crate::query::SearchQueryInput;
 use pgrx::*;

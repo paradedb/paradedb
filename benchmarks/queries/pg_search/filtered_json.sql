f07e4a067916815e98ac5ae6bcaba12b826ac970
Ming Ying <ming.ying.nyc@gmail.com>
2025-03-03 09:22:02 -0500

    pushdown on json query

--- benchmarks/queries/pg_search/filtered_json.sql
+++ benchmarks/queries/pg_search/filtered_json.sql
@@ -1,2 +1,2 @@
-SELECT * FROM benchmark_logs WHERE id @@@ paradedb.term('metadata.label', 'critical system alert') AND message @@@ 'research' AND severity @@@ '<3' LIMIT 10;
+SELECT * FROM benchmark_logs WHERE id @@@ paradedb.term('metadata.label', 'critical system alert') AND message @@@ 'research' AND severity @@@ < 3 LIMIT 10;
 SELECT * FROM benchmark_logs WHERE id @@@ paradedb.term('metadata.label', 'critical system alert') AND id @@@ paradedb.range('metadata.value', int4range(10, NULL, '[)')) AND message @@@ 'research' LIMIT 10;

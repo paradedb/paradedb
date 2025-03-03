f5cddc12199381184cc58956e885c9c0c77b1bf5
Ming Ying <ming.ying.nyc@gmail.com>
2025-03-03 09:00:13 -0500

    clean up queries

--- benchmarks/queries/tuned_postgres/filtering.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM benchmark_logs WHERE to_tsvector('english', message) @@ to_tsquery('english', 'research') AND severity < 3 LIMIT 10;
-SELECT * FROM benchmark_logs WHERE to_tsvector('english', message) @@ to_tsquery('english', 'research') AND timestamp >= '2020-10-02T15:00:00Z' LIMIT 10;

797bf18a6e860853eda201463484c4a95e489779
Eric B. Ridge <eebbrr@paradedb.com>
2025-03-10 14:43:30 -0400

    fix: `NPlusOneMergePolicy` better balances segments by num_docs
    This adjusts things such that when we have more than N segments, we
    re-calcuate our understanding of the num_docs "mean" to be that of the
    segments that we're not going to consider merging.
    
    This allows for uniform grouping of segments by their document count,
    ensuring the resulting segments stay closer together in size, rather
    than having a single segment that's potentially significantly larger
    than the existing largest segment.
    
    It also means that we may not merge down to exactly N segments in the
    first pass, but that's okay as we also want to do as little rewriting as
    possible on any given merge.
    
    ---
    
    While here, also fix the standard deviation calculation to be that of
    the sample rather than the population.  In practice I'm not sure this
    makes much of a difference, but it now matches what Postgres' `stddev()`
    aggregate function would calculate over the same set of integers, and
    that seems reasonable.

--- pg_search/src/lib.rs
+++ pg_search/src/lib.rs
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+#![recursion_limit = "512"]
 #![allow(unexpected_cfgs)]
 
 mod api;

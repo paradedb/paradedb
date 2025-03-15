09f3fc8409b692b1382d765d8dad14e943df7ee7
Akshat Sharma <akshatsharma2602@gmail.com>
2025-03-15 10:06:50 -0400

    fix: fixed snippets not respecting max_num_chars and added tests for the same

--- tests/tests/bm25_search.rs
+++ tests/tests/bm25_search.rs
@@ -377,6 +377,24 @@ fn snippet(mut conn: PgConnection) {
     assert_eq!(row.0, 3);
     assert_eq!(row.1, "Sleek running <h1>shoes</h1>");
     assert_relative_eq!(row.2, 2.484906, epsilon = 1e-6);
+
+    let row: (i32, String, f32) = "
+        SELECT id, paradedb.snippet(description, max_num_chars=>17), paradedb.score(id)
+        FROM paradedb.bm25_search WHERE bm25_search @@@ 'description:shoes' ORDER BY id"
+        .fetch_one(&mut conn);
+
+    assert_eq!(row.0, 3);
+    assert_eq!(row.1, "<b>shoes</b>");
+    assert_relative_eq!(row.2, 2.484906, epsilon = 1e-6);
+
+    let row: (i32, String, f32) = "
+        SELECT id, paradedb.snippet(description, max_num_chars=>17), paradedb.score(id)
+        FROM paradedb.bm25_search WHERE bm25_search @@@ 'description:shoes' ORDER BY score DESC"
+        .fetch_one(&mut conn);
+
+    assert_eq!(row.0, 5);
+    assert_eq!(row.1, "Generic <b>shoes</b>");
+    assert_relative_eq!(row.2, 2.877260, epsilon = 1e-6);
 }
 
 #[rstest]

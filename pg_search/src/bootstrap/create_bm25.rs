fe2adab4243957f93e5d8a9df724b959db1b5f01
Eric B. Ridge <eebbrr@paradedb.com>
2025-03-27 08:11:53 -0400

    feat: enhance the paradedb.index_layer_info view

--- pg_search/src/bootstrap/create_bm25.rs
+++ pg_search/src/bootstrap/create_bm25.rs
@@ -420,17 +420,22 @@ fn version_info() -> TableIterator<
 }
 
 extension_sql!(
-    r#"
-create view paradedb.index_layer_info as
+    r#"create view paradedb.index_layer_info as
 select relname::text,
        layer_size,
+       low,
+       high,
+       byte_size,
        case when segments = ARRAY [NULL] then 0 else count end       as count,
        case when segments = ARRAY [NULL] then NULL else segments end as segments
 from (select relname,
              coalesce(pg_size_pretty(case when low = 0 then null else low end), '') || '..' ||
              coalesce(pg_size_pretty(case when high = 9223372036854775807 then null else high end), '') as layer_size,
              count(*),
-             array_agg(segno) as segments
+             coalesce(sum(byte_size), 0)                                                                as byte_size,
+             min(low)                                                                                   as low,
+             max(high)                                                                                  as high,
+             array_agg(segno)                                                                           as segments
       from (with indexes as (select oid::regclass as relname
                              from pg_class
                              where relam = (select oid from pg_am where amname = 'bm25')),
@@ -441,7 +446,7 @@ from (select relname,
                                  from indexes
                                           inner join lateral (select unnest(0 || paradedb.layer_sizes(indexes.relname) || 9223372036854775807)
                                                               order by 1 desc) x on true)
-            select layer_sizes.relname, layer_sizes.low, layer_sizes.high, segments.segno
+            select layer_sizes.relname, layer_sizes.low, layer_sizes.high, segments.segno, segments.byte_size
             from layer_sizes
                      left join segments on layer_sizes.relname = segments.relname and
                                            (byte_size * 1.33)::bigint between low and high) x

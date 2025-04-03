4d571559ca49f19aaac9f1ea0191206a97fbad42
Stu Hood <stuhood@gmail.com>
2025-04-03 15:31:51 -0700

    Add `LinkedItemList::atomically` with one method and a single test.

--- pg_search/src/postgres/storage/linked_items.rs
+++ pg_search/src/postgres/storage/linked_items.rs
@@ -18,11 +18,12 @@
 use super::block::{
     BM25PageSpecialData, LinkedList, LinkedListData, MVCCEntry, PgItem, SCHEMA_START,
 };
-use super::buffer::{BufferManager, BufferMut};
+use super::buffer::{BufferManager, BufferMut, Page};
 use super::utils::vacuum_get_freeze_limit;
 use anyhow::Result;
 use pgrx::pg_sys;
 use pgrx::pg_sys::BlockNumber;
+use rustc_hash::{FxHashMap, FxHashSet};
 use std::fmt::Debug;
 
 // ---------------------------------------------------------------
@@ -111,6 +112,8 @@ impl<T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> LinkedItemList<
         let mut header_page = header_buffer.init_page();
         start_buffer.init_page();
 
+        // TODO: `LinkedItemsList` currently never modifies this metadata
+        // after creation, but `LinkedBytesList` does. Do we actually need it?
         let metadata = header_page.contents_mut::<LinkedListData>();
         metadata.start_blockno = start_blockno;
         metadata.last_blockno = start_blockno;
@@ -316,6 +319,9 @@ impl<T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> LinkedItemList<
         self.lookup_ex(cmp).map(|(t, _, _)| t)
     }
 
+    ///
+    /// TODO: Rename to `lookup_ex_mut` to parallel `atomically`.
+    ///
     pub unsafe fn lookup_ex<Cmp: Fn(&T) -> bool>(
         &self,
         cmp: Cmp,
@@ -345,6 +351,198 @@ impl<T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> LinkedItemList<
             "transaction {} failed to find the desired entry",
             pg_sys::GetCurrentTransactionIdIfAny()
         )))
+    }
+
+    ///
+    /// Allows for atomically mutating multiple entries.
+    ///
+    /// TODO: currently nothing prevents multiple concurrent `atomically` calls:
+    /// external locking is necessary to ensure that only one `atomically` block
+    /// is running at a time.
+    ///
+    /// The given Accessor allows for adding, removing, and mutating entries.
+    /// Modified pages are copied, and are made visible atomically by replacing
+    /// a single inbound pointer once the Accessor is committed.
+    ///
+    /// If the Accessor is dropped without commit, any copied pages are garbage
+    /// collected instead.
+    ///
+    pub fn atomically<'s>(&'s mut self) -> LinkedItemListAccessor<'s, T> {
+        LinkedItemListAccessor {
+            list: self,
+            writable_mapping: FxHashMap::default(),
+            writable: FxHashSet::default(),
+        }
+    }
+}
+
+pub struct LinkedItemListAccessor<'s, T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> {
+    list: &'s mut LinkedItemList<T>,
+    // A mapping from blocks in the base list to writable copies of those blocks. The writable copies
+    // only exist within this accessor: during `commit` we atomically make all of them visible, and
+    // during `drop` we garbage collect them.
+    writable_mapping: FxHashMap<pg_sys::BlockNumber, pg_sys::BlockNumber>,
+    // Blocks which are writable.
+    writable: FxHashSet<pg_sys::BlockNumber>,
+}
+
+// TODO: Find a way to share common methods with `LinkedItemList`.
+impl<'s, T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> LinkedItemListAccessor<'s, T> {
+    ///
+    /// Identical to `lookup_ex`, except that it returns a page that may be written to atomically
+    /// within this Accessor.
+    ///
+    pub unsafe fn lookup_ex_mut<Cmp: Fn(&T) -> bool>(
+        &mut self,
+        cmp: Cmp,
+    ) -> Result<(T, pg_sys::BlockNumber, pg_sys::OffsetNumber)> {
+        let mut blockno = self.list.get_start_blockno();
+        let mut previous_page = None;
+        while blockno != pg_sys::InvalidBlockNumber {
+            blockno = self.maybe_writable(blockno);
+            let buffer = self.list.bman.get_buffer_mut(blockno);
+            let page = buffer.page();
+            let mut offsetno = pg_sys::FirstOffsetNumber;
+            let max_offset = page.max_offset_number();
+
+            while offsetno <= max_offset {
+                if let Some((deserialized, _)) = page.read_item::<T>(offsetno) {
+                    if cmp(&deserialized) {
+                        blockno = self.ensure_writable(previous_page, page, blockno);
+                        return Ok((deserialized, blockno, offsetno));
+                    }
+                }
+                offsetno += 1;
+            }
+
+            let next_blockno = page.next_blockno();
+            previous_page = Some((blockno, buffer));
+            blockno = next_blockno;
+        }
+
+        // if we get here, we didn't find what we were looking for
+        // but we should have -- how else could we have asked for something from the list?
+        Err(anyhow::anyhow!(format!(
+            "transaction {} failed to find the desired entry",
+            pg_sys::GetCurrentTransactionIdIfAny()
+        )))
+    }
+
+    pub fn commit(mut self) {
+        if self.writable.is_empty() {
+            // Nothing to do.
+            return;
+        }
+
+        // Find the last clean page in the existing list, and adjust its next_blockno pointer
+        // to point into the writable pages, making them visible to other readers.
+        let mut blockno = self.list.get_start_blockno();
+        let mut previous_buffer = None;
+        while blockno != pg_sys::InvalidBlockNumber {
+            let Some(writable_blockno) = self.writable_mapping.get(&blockno).copied() else {
+                // Open this block to get the next pointer, and keep it open until we've
+                // determined that we don't need to modify it.
+                let buffer = self.list.bman.get_buffer_mut(blockno);
+                blockno = buffer.page().next_blockno();
+                previous_buffer = Some(buffer);
+                continue;
+            };
+
+            // We've found the first writable block.
+            if let Some(mut previous_buffer) = previous_buffer {
+                // If we have a previous buffer, then modify it to point to the first
+                // writable block.
+                previous_buffer
+                    .page_mut()
+                    .special_mut::<BM25PageSpecialData>()
+                    .next_blockno = writable_blockno;
+                break;
+            } else {
+                // If we don't have a previous buffer, then the first block of the list
+                // was writable, and so we need to go and modify our sentinel.
+                todo!();
+            }
+        }
+
+        // Clean up any pages that we unlinked.
+        // TODO: Don't leak these pages.
+
+        self.writable_mapping.clear();
+        self.writable.clear();
+    }
+
+    ///
+    /// Get a writable version of the given blockno, if one already exists. Otherwise, the readonly
+    /// version.
+    ///
+    fn maybe_writable(&self, blockno: pg_sys::BlockNumber) -> pg_sys::BlockNumber {
+        self.writable_mapping
+            .get(&blockno)
+            .copied()
+            .unwrap_or(blockno)
+    }
+
+    ///
+    /// If the given blockno is not already writable, create a writable copy (in the context of
+    /// this Accessor), and return the writable blockno.
+    ///
+    fn ensure_writable(
+        &mut self,
+        previous_page: Option<(pg_sys::BlockNumber, BufferMut)>,
+        page: Page,
+        blockno: pg_sys::BlockNumber,
+    ) -> pg_sys::BlockNumber {
+        if self.writable.contains(&blockno) {
+            // Block was already writable.
+            return blockno;
+        }
+
+        // Copy the page, and add it to `writable_mapping`.
+        let mut new_buffer = self.list.bman.new_buffer();
+        let new_blockno = new_buffer.number();
+        new_buffer.init_page();
+        let mut new_page = new_buffer.page_mut();
+
+        // TODO: It looks like this copy should use something similar to
+        // PageGetTempPageCopySpecial: essentially memcopy'ing the page.
+        let mut offsetno = pg_sys::FirstOffsetNumber;
+        let max_offset = page.max_offset_number();
+        while offsetno <= max_offset {
+            if let Some((deserialized, _)) = page.read_item::<T>(offsetno) {
+                let PgItem(item, size) = deserialized.into();
+                let new_offsetno = new_page.append_item(item, size, 0);
+                assert!(new_offsetno != pg_sys::InvalidOffsetNumber);
+            }
+            offsetno += 1;
+        }
+
+        new_page.special_mut::<BM25PageSpecialData>().next_blockno =
+            self.maybe_writable(page.next_blockno());
+
+        // If the previous page was already writable, then we eagerly update it to point
+        // to this new writable page.
+        if let Some((previous_blockno, mut previous_buffer)) = previous_page {
+            if self.writable.contains(&previous_blockno) {
+                previous_buffer
+                    .page_mut()
+                    .special_mut::<BM25PageSpecialData>()
+                    .next_blockno = new_blockno;
+            }
+        }
+
+        self.writable_mapping.insert(blockno, new_blockno);
+        self.writable.insert(new_blockno);
+        new_blockno
+    }
+}
+
+impl<'s, T: From<PgItem> + Into<PgItem> + Debug + Clone + MVCCEntry> Drop
+    for LinkedItemListAccessor<'s, T>
+{
+    fn drop(&mut self) {
+        if !self.writable.is_empty() {
+            // TODO: Don't leak these pages.
+        }
     }
 }
 
@@ -381,12 +579,7 @@ mod tests {
 
     #[pg_test]
     unsafe fn test_linked_items_garbage_collect_single_page() {
-        Spi::run("CREATE TABLE t (id SERIAL, data TEXT);").unwrap();
-        Spi::run("CREATE INDEX t_idx ON t USING bm25(id, data) WITH (key_field = 'id')").unwrap();
-        let relation_oid: pg_sys::Oid =
-            Spi::get_one("SELECT oid FROM pg_class WHERE relname = 't_idx' AND relkind = 'i';")
-                .expect("spi should succeed")
-                .unwrap();
+        let relation_oid = init_bm25_index();
 
         let snapshot = pg_sys::GetActiveSnapshot();
         let delete_xid = (*snapshot).xmin - 1;
@@ -421,12 +614,7 @@ mod tests {
 
     #[pg_test]
     unsafe fn test_linked_items_garbage_collect_multiple_pages() {
-        Spi::run("CREATE TABLE t (id SERIAL, data TEXT);").unwrap();
-        Spi::run("CREATE INDEX t_idx ON t USING bm25(id, data) WITH (key_field = 'id')").unwrap();
-        let relation_oid: pg_sys::Oid =
-            Spi::get_one("SELECT oid FROM pg_class WHERE relname = 't_idx' AND relkind = 'i';")
-                .expect("spi should succeed")
-                .unwrap();
+        let relation_oid = init_bm25_index();
 
         let snapshot = pg_sys::GetActiveSnapshot();
         let deleted_xid = (*snapshot).xmin - 1;
@@ -516,6 +704,106 @@ mod tests {
             assert!(pre_gc_blocks.len() > post_gc_blocks.len());
             assert!(post_gc_blocks.is_subset(&pre_gc_blocks));
         }
+    }
+
+    #[pg_test]
+    unsafe fn test_linked_items_modify_atomically() {
+        let relation_oid = init_bm25_index();
+
+        let snapshot = pg_sys::GetActiveSnapshot();
+
+        // Add 2000 entries.
+        let mut list = LinkedItemList::<SegmentMetaEntry>::create(relation_oid);
+        let entries = (1..2000)
+            .map(|_| SegmentMetaEntry {
+                segment_id: random_segment_id(),
+                xmin: (*snapshot).xmin - 1,
+                xmax: pg_sys::InvalidTransactionId,
+                postings: Some(make_fake_postings(relation_oid)),
+                ..Default::default()
+            })
+            .collect::<Vec<_>>();
+
+        list.add_items(&entries, None).unwrap();
+
+        // Collect the segment ids and positions of every entry.
+        let get_positions = |list: &LinkedItemList<SegmentMetaEntry>| {
+            entries
+                .iter()
+                .map(|entry| {
+                    let (_, blockno, offsetno) = list
+                        .lookup_ex(|el| el.segment_id == entry.segment_id)
+                        .unwrap();
+                    (entry.segment_id, blockno, offsetno)
+                })
+                .collect::<Vec<_>>()
+        };
+        let positions = get_positions(&list);
+
+        // Grab one entry from each block, and validate that we have more four or more blocks.
+        let unique_block_entries = {
+            let mut dedupe = HashSet::new();
+            positions
+                .iter()
+                .filter_map(|(segment_id, blockno, _)| {
+                    if dedupe.insert(blockno) {
+                        Some((segment_id, blockno))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        };
+        assert!(
+            unique_block_entries.len() >= 4,
+            "Got blocks: {unique_block_entries:?}"
+        );
+
+        // Atomically modify blocks 1 and 2.
+        {
+            let mut accessor = list.atomically();
+            accessor
+                .lookup_ex_mut(|el| el.segment_id == *unique_block_entries[1].0)
+                .unwrap();
+            accessor
+                .lookup_ex_mut(|el| el.segment_id == *unique_block_entries[2].0)
+                .unwrap();
+            accessor.commit();
+        }
+
+        // Confirm that blocks 0 and 3 have not changed, but that blocks 1 and 2 have.
+        for unchanged_block in [0, 3] {
+            assert_eq!(
+                list.lookup_ex(|el| el.segment_id == *unique_block_entries[unchanged_block].0)
+                    .unwrap()
+                    .1,
+                *unique_block_entries[unchanged_block].1,
+            );
+        }
+        for changed_block in [1, 2] {
+            assert_ne!(
+                list.lookup_ex(|el| el.segment_id == *unique_block_entries[changed_block].0)
+                    .unwrap()
+                    .1,
+                *unique_block_entries[changed_block].1,
+            );
+        }
+
+        // Confirm that all entries are still present, in order.
+        let segments_before = positions.into_iter().map(|x| x.0).collect::<Vec<_>>();
+        let segments_after = get_positions(&list)
+            .into_iter()
+            .map(|x| x.0)
+            .collect::<Vec<_>>();
+        assert_eq!(segments_before, segments_after);
+    }
+
+    fn init_bm25_index() -> pg_sys::Oid {
+        Spi::run("CREATE TABLE t (id SERIAL, data TEXT);").unwrap();
+        Spi::run("CREATE INDEX t_idx ON t USING bm25(id, data) WITH (key_field = 'id')").unwrap();
+        return Spi::get_one("SELECT oid FROM pg_class WHERE relname = 't_idx' AND relkind = 'i';")
+            .expect("spi should succeed")
+            .unwrap();
     }
 
     fn make_fake_postings(relation_oid: pg_sys::Oid) -> FileEntry {

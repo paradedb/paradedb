40e6dfd4945024a597890ed80edbe3985c6c8f8d
Ming Ying <ming.ying.nyc@gmail.com>
2025-04-10 22:41:13 -0400

    wip

--- tests/tests/layerd_merge.rs
+++ tests/tests/layerd_merge.rs
@@ -81,3 +81,26 @@ fn force_merge(mut conn: PgConnection) {
         .fetch_one::<(i64,)>(&mut conn);
     assert_eq!(nsegments, 4);
 }
+
+#[rstest]
+fn dont_merge_create_index_segments(mut conn: PgConnection) {
+    r#"
+        CREATE TABLE dont_merge_create_index_segments (id bigint);
+        INSERT INTO dont_merge_create_index_segments (id) SELECT x FROM generate_series(1, 1000000) x;
+        CREATE INDEX idxdont_merge_create_index_segments ON dont_merge_create_index_segments USING bm25(id) WITH (key_field='id', layer_sizes = '500kb, 2mb, 5mb, 10mb');
+    "#
+    .execute_result(&mut conn).expect("creating table/index should not fail");
+
+    let (nsegments_before,) = "select count(*) from paradedb.index_info('idxdont_merge_create_index_segments');"
+        .fetch_one::<(i64,)>(&mut conn);
+
+    "INSERT INTO dont_merge_create_index_segments (id) VALUES (1)".execute(&mut conn);
+
+    let (nsegments_after,) = "select count(*) from paradedb.index_info('idxdont_merge_create_index_segments');"
+        .fetch_one::<(i64,)>(&mut conn);
+
+    assert_eq!(nsegments_after, nsegments_before + 1);
+
+    "DELETE FROM dont_merge_create_index_segments WHERE id > 10".execute(&mut conn);
+    "VACUUM dont_merge_create_index_segments".execute(&mut conn);
+}

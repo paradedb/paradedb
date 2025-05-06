a6af613dd55e943e6c9a5b8a65a616df137ea52f
Eric B. Ridge <eebbrr@paradedb.com>
2025-05-06 10:46:53 -0400

    chore: remove some `#[allow(...)]` annotations
    Some of these allow annotations have bothered me for awhile and so now
    they're gone.
    
    In particular, we now lookup the `USING bm25`'s index access method
    handler `pg_sys::Oid` in a new way -- by asking the SysCache.

--- pg_search/src/lib.rs
+++ pg_search/src/lib.rs
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 #![recursion_limit = "512"]
-#![allow(unexpected_cfgs)]
 
 mod api;
 mod bootstrap;

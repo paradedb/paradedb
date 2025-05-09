8dcfb796e89586979adb364d9d05d1da56dbeaad
Stu Hood <stuhood@gmail.com>
2025-05-09 13:47:46 -0700

    Convert `gqen` to proptests using `join` and `where` strategies.

--- tests/tests/fixtures/querygen/mod.rs
+++ tests/tests/fixtures/querygen/mod.rs
@@ -16,3 +16,46 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 pub mod joingen;
 pub mod wheregen;
+
+use std::fmt::Debug;
+
+use proptest::prelude::*;
+
+use joingen::{JoinExpr, JoinType};
+use wheregen::{Expr, SqlValue};
+
+///
+/// Generates arbitrary joins and where clauses for the given tables and columns.
+///
+pub fn arb_joins_and_wheres<V: Clone + Debug + Eq + SqlValue + 'static>(
+    join_types: impl Strategy<Value = JoinType> + Clone,
+    tables: Vec<impl AsRef<str>>,
+    columns: Vec<(impl AsRef<str>, V)>,
+) -> impl Strategy<Value = (JoinExpr, Expr<V>)> {
+    let table_names = tables
+        .into_iter()
+        .map(|tn| tn.as_ref().to_string())
+        .collect::<Vec<_>>();
+    let columns = columns
+        .into_iter()
+        .map(|(cn, v)| (cn.as_ref().to_string(), v))
+        .collect::<Vec<_>>();
+
+    // Choose how many tables will be joined.
+    (2..=table_names.len())
+        .prop_flat_map(move |join_size| {
+            // Then choose tables for that join size.
+            proptest::sample::subsequence(table_names.clone(), join_size)
+        })
+        .prop_flat_map(move |tables| {
+            // Finally, choose the joins and where clauses for those tables.
+            (
+                joingen::arb_joins(
+                    join_types.clone(),
+                    tables.clone(),
+                    columns.iter().map(|(cn, _)| cn.clone()).collect(),
+                ),
+                wheregen::arb_wheres(tables.clone(), columns.clone()),
+            )
+        })
+}

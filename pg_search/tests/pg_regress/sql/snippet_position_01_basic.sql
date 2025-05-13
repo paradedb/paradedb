5be93169e094ecc34b9a7cb10ce97ffdf1d59f99
Ming Ying <ming.ying.nyc@gmail.com>
2025-05-12 20:13:41 -0400

    add tests

--- pg_search/tests/pg_regress/sql/snippet_position_01_basic.sql
+++ pg_search/tests/pg_regress/sql/snippet_position_01_basic.sql
@@ -8,8 +8,16 @@ SELECT id, paradedb.snippet(titles), paradedb.snippet_positions(titles)
 FROM snippet_test
 WHERE titles @@@ 'test' OR titles @@@ 'snippet';
 
-SELECT id, paradedb.snippet(metadata, 'test'), paradedb.snippet_positions(metadata, 'test')
+SELECT id, paradedb.snippet(content) as content_snippet, paradedb.snippet_positions(content) as content_snippet_positions, paradedb.snippet(titles) as titles_snippet, paradedb.snippet_positions(titles) as titles_snippet_positions, paradedb.score(id) as score
 FROM snippet_test
-WHERE metadata @@@ 'test';
+WHERE titles @@@ 'test' OR content @@@ 'ipsum'
+ORDER BY score DESC
+LIMIT 5;
+
+SELECT id, paradedb.snippet(content) as content_snippet, paradedb.snippet_positions(content) as content_snippet_positions, paradedb.snippet(titles) as titles_snippet, paradedb.snippet_positions(titles) as titles_snippet_positions, paradedb.score(id) as score
+FROM snippet_test
+WHERE titles @@@ 'test' OR content @@@ 'ipsum'
+ORDER BY id ASC
+LIMIT 5;
 
 \i common/snippet_position_basic_cleanup.sql

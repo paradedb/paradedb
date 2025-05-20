2141c7b736ca4b4663da2835021914d1f65643b8
Stu Hood <stuhood@gmail.com>
2025-05-20 09:31:02 -0700

    Make the tables wider to match real world use cases, and give query groups semantic names.

--- benchmarks/datasets/join/queries/pg_search/hierarchical_content.sql
+++ benchmarks/datasets/join/queries/pg_search/hierarchical_content.sql
@@ -3,3 +3,9 @@ FROM documents
 JOIN files ON documents.id = files."documentId"
 JOIN pages ON pages."fileId" = files.id
 WHERE documents.parents @@@ 'SFR' AND files.title @@@ 'collab12' AND pages."content" @@@ 'Single Number Reach';
+
+SELECT *
+FROM documents
+JOIN files ON documents.id = files."documentId"
+JOIN pages ON pages."fileId" = files.id
+WHERE documents.parents @@@ 'SFR' AND files.title @@@ 'collab12' AND pages."content" @@@ 'Single Number Reach';

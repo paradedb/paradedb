37ccc165e2731330f2ec7ec7964e117172d0447d
Eric Ridge <eebbrr@gmail.com>
2025-06-05 13:00:29 -0400

    perf: improve INSERT/UPDATE perf with `SerialIndexWriter` (#2658)
    We now use the `SerialIndexWriter` to insert rows during `aminsert()`,
    which seems to essentially double INSERT/UPDATE performance of small,
    single-row statements.
    
    This then led to `SearchIndexWriter` being renamed to
    `SearchIndexDeleter` because now it's only used by `ambulkdelete()`.
    
    We're basically one step away from being able to completely delete the
    `ChannelDirectory` and all the complexity it contains!
    
    As a result of all this, the `paradedb.statement_parallelism` GUC is now
    gone. Internally we keep the concept, but it's hardcoded to 1 --
    Postgres doesn't support parallel worker-based INSERT/UPDATE/COPY
    statements.

--- pg_search/src/postgres/insert.rs
+++ pg_search/src/postgres/insert.rs
@@ -18,8 +18,8 @@
 use crate::api::index::FieldName;
 use crate::index::merge_policy::{LayeredMergePolicy, NumCandidates, NumMerged};
 use crate::index::mvcc::MvccSatisfies;
-use crate::index::writer::index::{Mergeable, SearchIndexMerger, SearchIndexWriter};
-use crate::index::WriterResources;
+use crate::index::writer::index::{Mergeable, SearchIndexMerger, SerialIndexWriter};
+use crate::index::{get_index_schema, WriterResources};
 use crate::postgres::options::SearchIndexCreateOptions;
 use crate::postgres::storage::block::{SegmentMetaEntry, CLEANUP_LOCK, SEGMENT_METAS_START};
 use crate::postgres::storage::buffer::BufferManager;
@@ -28,15 +28,15 @@ use crate::postgres::storage::{LinkedBytesList, LinkedItemList};
 use crate::postgres::utils::{
     categorize_fields, item_pointer_to_u64, row_to_search_document, CategorizedFieldData,
 };
-use crate::schema::SearchField;
+use crate::schema::{SearchDocument, SearchField};
 use pgrx::{check_for_interrupts, pg_guard, pg_sys, PgMemoryContexts, PgRelation, PgTupleDesc};
 use std::panic::{catch_unwind, resume_unwind};
-use tantivy::SegmentMeta;
+use tantivy::{SegmentMeta, TantivyDocument};
 
 pub struct InsertState {
     #[allow(dead_code)] // field is used by pg<16 for the fakeaminsertcleanup stuff
     pub indexrelid: pg_sys::Oid,
-    pub writer: Option<SearchIndexWriter>,
+    pub writer: Option<SerialIndexWriter>,
     categorized_fields: Vec<(SearchField, CategorizedFieldData)>,
     key_field_name: FieldName,
     per_row_context: PgMemoryContexts,
@@ -47,10 +47,14 @@ impl InsertState {
         indexrel: &PgRelation,
         writer_resources: WriterResources,
     ) -> anyhow::Result<Self> {
-        let writer = SearchIndexWriter::open(indexrel, MvccSatisfies::Mergeable, writer_resources)?;
+        let (parallelism, memory_budget) = writer_resources.resources();
+        let memory_budget = memory_budget / parallelism;
+        let writer =
+            SerialIndexWriter::with_mvcc(indexrel, MvccSatisfies::Mergeable, memory_budget)?;
+        let schema = get_index_schema(indexrel)?;
         let tupdesc = unsafe { PgTupleDesc::from_pg_unchecked(indexrel.rd_att) };
-        let categorized_fields = categorize_fields(&tupdesc, &writer.schema);
-        let key_field_name = writer.schema.key_field().name;
+        let categorized_fields = categorize_fields(&tupdesc, &schema);
+        let key_field_name = schema.key_field().name;
 
         let per_row_context = pg_sys::AllocSetContextCreateExtended(
             PgMemoryContexts::CurrentMemoryContext.value(),
@@ -122,21 +126,10 @@ pub unsafe extern "C-unwind" fn aminsert(
     index_relation: pg_sys::Relation,
     values: *mut pg_sys::Datum,
     isnull: *mut bool,
-    heap_tid: pg_sys::ItemPointer,
+    ctid: pg_sys::ItemPointer,
     _heap_relation: pg_sys::Relation,
     _check_unique: pg_sys::IndexUniqueCheck::Type,
     _index_unchanged: bool,
-    index_info: *mut pg_sys::IndexInfo,
-) -> bool {
-    aminsert_internal(index_relation, values, isnull, heap_tid, index_info)
-}
-
-#[inline(always)]
-unsafe fn aminsert_internal(
-    index_relation: pg_sys::Relation,
-    values: *mut pg_sys::Datum,
-    isnull: *mut bool,
-    ctid: pg_sys::ItemPointer,
     index_info: *mut pg_sys::IndexInfo,
 ) -> bool {
     if pg_sys::IsLogicalWorker() {
@@ -157,7 +150,9 @@ unsafe fn aminsert_internal(
             let key_field_name = &state.key_field_name;
             let writer = state.writer.as_mut().expect("writer should not be null");
 
-            let mut search_document = writer.schema.new_document();
+            let mut search_document = SearchDocument {
+                doc: TantivyDocument::new(),
+            };
 
             row_to_search_document(
                 values,
@@ -196,9 +191,9 @@ pub unsafe extern "C-unwind" fn aminsertcleanup(
     paradedb_aminsertcleanup(state.as_mut().and_then(|state| state.writer.take()));
 }
 
-pub fn paradedb_aminsertcleanup(mut writer: Option<SearchIndexWriter>) {
+pub fn paradedb_aminsertcleanup(mut writer: Option<SerialIndexWriter>) {
     if let Some(writer) = writer.take() {
-        let indexrelid = writer.indexrelid;
+        let indexrelid = writer.index_oid();
 
         writer
             .commit()

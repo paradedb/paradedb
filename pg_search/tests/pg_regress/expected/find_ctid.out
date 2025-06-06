paradedb/paradedb

commit a514af04f4ea219baca4ac3409fceb1e32c8288b

chore: Clean up schema code (#2660)
# Ticket(s) Closed

- Closes #

## What

A refactor of our schema-related code. Most of the actual changes are in
`postgres/options.rs` and `schema/`. The rest of the diffs are just the
fallout of that.

As part of the cleanup I caught and fixed some things:

- The range field `fast` option can default to `false`, causing it to
not be sortable -- this should never happen
- We now use `serde` to reject any unknown fields in the `CREATE INDEX`
JSON config options
- Because unknown fields were previously not rejected, some of our unit
tests were asserting that the wrong configs were being deserialized --
this has been fixed
- We were allowing users to alias non-text fields -- this doesn't make
sense and the option has been removed

## Why

## How

- Deleted unnecessary abstractions like `SearchDocument` and
`SearchFieldId`
- Deleted confusing traits like `AsFieldType`
- Refactored `options.rs` and `schema/mod.rs`

## Tests
All existing tests should pass

pg_search/src/postgres/customscan/pdbscan/exec_methods/fast_fields/mod.rs
@@ -223,15 +223,17 @@ fn collect_fast_field_try_for_attno(
 
             // Get attribute info - use if let to handle missing attributes gracefully
             if let Some(att) = tupdesc.get((attno - 1) as usize) {
-                if schema.is_fast_field(&FieldName::from(att.name())) {
-                    let ff_type = if att.type_oid().value() == pg_sys::TEXTOID
-                        || att.type_oid().value() == pg_sys::VARCHAROID
-                    {
-                        FastFieldType::String
-                    } else {
-                        FastFieldType::Numeric
-                    };
-                    matches.push(WhichFastField::Named(att.name().to_string(), ff_type));
+                if let Some(search_field) = schema.search_field(att.name()) {
+                    if search_field.is_fast() {
+                        let ff_type = if att.type_oid().value() == pg_sys::TEXTOID
+                            || att.type_oid().value() == pg_sys::VARCHAROID
+                        {
+                            FastFieldType::String
+                        } else {
+                            FastFieldType::Numeric
+                        };
+                        matches.push(WhichFastField::Named(att.name().to_string(), ff_type));
+                    }
                 }
             }
             // If the attribute doesn't exist in this relation, just continue

pg_search/src/postgres/customscan/pdbscan/mod.rs
@@ -235,7 +235,8 @@ impl CustomScan for PdbScan {
 
             let directory = MVCCDirectory::snapshot(bm25_index.oid());
             let index = Index::open(directory).expect("custom_scan: should be able to open index");
-            let schema = SearchIndexSchema::open(index.schema(), &bm25_index);
+            let schema = SearchIndexSchema::open(bm25_index.oid())
+                .expect("custom_scan: should be able to open schema");
             let pathkey = pullup_orderby_pathkey(&mut builder, rti, &schema, root);
 
             #[cfg(any(feature = "pg14", feature = "pg15"))]
@@ -1133,7 +1134,8 @@ fn compute_exec_which_fast_fields(
         let directory = MVCCDirectory::snapshot(indexrel.oid());
         let index =
             Index::open(directory).expect("create_custom_scan_state: should be able to open index");
-        let schema = SearchIndexSchema::open(index.schema(), &indexrel);
+        let schema = SearchIndexSchema::open(indexrel.oid())
+            .expect("custom_scan: should be able to open schema");
 
         // Calculate the ordered set of fast fields which have actually been requested in
         // the target list.
@@ -1242,8 +1244,10 @@ unsafe fn pullup_orderby_pathkey<P: Into<*mut pg_sys::List> + Default>(
                 let heaprel = PgRelation::with_lock(heaprelid, pg_sys::AccessShareLock as _);
                 let tupdesc = heaprel.tuple_desc();
                 if let Some(att) = tupdesc.get(attno as usize - 1) {
-                    if schema.is_field_lower_sortable(&att.name().into()) {
-                        return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                    if let Some(search_field) = schema.search_field(att.name()) {
+                        if search_field.is_lower_sortable() {
+                            return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                        }
                     }
                 }
             } else if let Some(relabel) = nodecast!(RelabelType, T_RelabelType, expr) {
@@ -1252,8 +1256,10 @@ unsafe fn pullup_orderby_pathkey<P: Into<*mut pg_sys::List> + Default>(
                     let heaprel = PgRelation::with_lock(heaprelid, pg_sys::AccessShareLock as _);
                     let tupdesc = heaprel.tuple_desc();
                     if let Some(att) = tupdesc.get(attno as usize - 1) {
-                        if schema.is_field_raw_sortable(&att.name().into()) {
-                            return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                        if let Some(search_field) = schema.search_field(att.name()) {
+                            if search_field.is_raw_sortable() {
+                                return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                            }
                         }
                     }
                 }
@@ -1265,8 +1271,10 @@ unsafe fn pullup_orderby_pathkey<P: Into<*mut pg_sys::List> + Default>(
                 let heaprel = PgRelation::with_lock(heaprelid, pg_sys::AccessShareLock as _);
                 let tupdesc = heaprel.tuple_desc();
                 if let Some(att) = tupdesc.get(attno as usize - 1) {
-                    if schema.is_field_raw_sortable(&att.name().into()) {
-                        return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                    if let Some(search_field) = schema.search_field(att.name()) {
+                        if search_field.is_raw_sortable() {
+                            return Some(OrderByStyle::Field(first_pathkey, att.name().into()));
+                        }
                     }
                 }
             }

pg_search/src/api/index.rs
@@ -71,18 +71,10 @@ pub fn schema(
 
     let search_reader = SearchIndexReader::open(&index, MvccSatisfies::Snapshot)
         .expect("could not open search index reader");
-    let schema = search_reader.schema().schema.clone();
-    let mut field_entries: Vec<_> = schema.fields().collect();
-
-    // To ensure consistent ordering of outputs, we'll sort the results by field name.
-    field_entries.sort_by_key(|(field, _)| schema.get_field_name(*field).to_string());
+    let schema = search_reader.schema();
 
     let mut field_rows = Vec::new();
-
-    for field in field_entries {
-        let (field, field_entry) = field;
-        let name = schema.get_field_name(field).to_string();
-
+    for (_, field_entry) in schema.fields() {
         let (field_type, tokenizer, record, normalizer, expand_dots) =
             match field_entry.field_type() {
                 FieldType::I64(_) => ("I64".to_string(), None, None, None, None),
@@ -121,7 +113,7 @@ pub fn schema(
             };
 
         let row = (
-            name,
+            field_entry.name().to_string(),
             field_type,
             field_entry.is_stored(),
             field_entry.is_indexed(),
@@ -136,6 +128,8 @@ pub fn schema(
         field_rows.push(row);
     }
 
+    // Sort field rows for consistent ordering
+    field_rows.sort_by_key(|(name, _, _, _, _, _, _, _, _, _)| name.clone());
     TableIterator::new(field_rows)
 }
 
@@ -978,19 +972,23 @@ impl Display for FieldName {
 
 impl AsRef<str> for FieldName {
     fn as_ref(&self) -> &str {
-        self.0.as_str()
+        self
     }
 }
 
-impl From<String> for FieldName {
-    fn from(value: String) -> Self {
-        Self(value)
+impl std::ops::Deref for FieldName {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-impl From<&str> for FieldName {
-    fn from(value: &str) -> Self {
-        Self(value.to_string())
+impl<T> From<T> for FieldName
+where
+    T: Into<String>,
+{
+    fn from(value: T) -> Self {
+        FieldName(value.into())
     }
 }
 

pg_search/src/api/operator/searchqueryinput.rs
@@ -150,9 +150,10 @@ pub fn search_with_query_input(
         };
         let search_reader = SearchIndexReader::open(&index_relation, MvccSatisfies::Snapshot)
             .expect("search_with_query_input: should be able to open a SearchIndexReader");
+        let schema = search_reader.schema();
         let key_field = search_reader.key_field();
-        let key_field_name = key_field.name.root();
-        let key_field_type = key_field.type_.into();
+        let key_field_name = key_field.field_name().root();
+        let key_field_type = schema.search_field(&key_field_name).unwrap().field_type().into();
         let ff_helper =
             FFHelper::with_fields(&search_reader, &[(key_field_name, key_field_type).into()]);
 

pg_search/src/bootstrap/create_bm25.rs
@@ -22,7 +22,7 @@ use crate::index::mvcc::MvccSatisfies;
 use crate::index::reader::index::SearchIndexReader;
 use crate::postgres::index::IndexKind;
 use crate::postgres::insert::merge_index_with_policy;
-use crate::postgres::options::SearchIndexCreateOptions;
+use crate::postgres::options::SearchIndexOptions;
 use crate::postgres::storage::block::{
     LinkedList, MVCCEntry, SegmentMetaEntry, SEGMENT_METAS_START,
 };
@@ -30,7 +30,7 @@ use crate::postgres::storage::metadata::MetaPage;
 use crate::postgres::storage::LinkedItemList;
 use crate::postgres::utils::item_pointer_to_u64;
 use crate::query::SearchQueryInput;
-use crate::schema::SearchFieldConfig;
+use crate::schema::SearchIndexSchema;
 use anyhow::Result;
 use pgrx::prelude::*;
 use pgrx::JsonB;
@@ -39,21 +39,24 @@ use serde_json::Value;
 
 #[pg_extern]
 pub unsafe fn index_fields(index: PgRelation) -> anyhow::Result<JsonB> {
-    let options = SearchIndexCreateOptions::from_relation(&index);
-    let fields = options.get_all_fields(&index).collect::<Vec<_>>();
-    let name_and_config: HashMap<FieldName, SearchFieldConfig> = fields
-        .into_iter()
-        .map(|(field_name, field_config, _)| (field_name, field_config))
-        .collect();
+    let options = SearchIndexOptions::from_relation(&index);
+    let schema = SearchIndexSchema::open(index.oid())?;
+
+    let mut name_and_config = HashMap::default();
+    for (_, field_entry) in schema.fields() {
+        let field_name = field_entry.name();
+        let field_config = options.field_config_or_default(&FieldName::from(field_name));
+        name_and_config.insert(field_name, field_config);
+    }
 
     Ok(JsonB(serde_json::to_value(name_and_config)?))
 }
 
 #[pg_extern]
 pub unsafe fn layer_sizes(index: PgRelation) -> Vec<AnyNumeric> {
-    let options = SearchIndexCreateOptions::from_relation(&index);
+    let options = SearchIndexOptions::from_relation(&index);
     options
-        .layer_sizes(crate::postgres::insert::DEFAULT_LAYER_SIZES)
+        .layer_sizes()
         .into_iter()
         .map(|layer_size| layer_size.into())
         .collect()

pg_search/src/index/fast_fields_helper.rs
@@ -252,7 +252,7 @@ pub enum FastFieldType {
 impl From<SearchFieldType> for FastFieldType {
     fn from(value: SearchFieldType) -> Self {
         match value {
-            SearchFieldType::Text => FastFieldType::String,
+            SearchFieldType::Text(_) => FastFieldType::String,
             _ => FastFieldType::Numeric,
         }
     }

commit 34782fee92756406381f5c6bcfa837e3bfb768b0

fix: should be able to construct a `SearchField` for `ctid` (#2662)
## What

Like the title says, we also need to be able to construct a
`SearchField` instance for the postgres "ctid" system column. This was a
minor regression from #2660, because now we only rely on the index's
TupleDescriptor to lookup attributes.

## Why

Specifically, the `paradedb.find_ctid()` was broken due to this, which
is something used in some stressgres test suites.

## How

## Tests

Yes, yes there's a new one to assert that `paradedb.find_ctid()` works.

pg_search/src/schema/mod.rs
@@ -244,25 +244,32 @@ impl SearchField {
         let index_relation = unsafe { PgRelation::open(relation_oid) };
         let options = unsafe { SearchIndexOptions::from_relation(&index_relation) };
 
-        let field_name = schema.get_field_name(field);
-        let field_config = options.field_config_or_default(&FieldName::from(field_name));
-        let attribute_name = field_config.alias().unwrap_or(field_name);
+        let field_name: FieldName = schema.get_field_name(field).into();
+        let field_config = options.field_config_or_default(&field_name);
+        let attribute_name = field_config.alias().unwrap_or(field_name.as_ref());
         let tuple_desc = index_relation.tuple_desc();
-        let attribute = tuple_desc
-            .iter()
-            .find(|attribute| attribute.name() == attribute_name)
-            .unwrap_or_else(|| {
+
+        let field_type: SearchFieldType = if field_name.is_ctid() {
+            // the "ctid" field isn't an attribute, per se, in the index itself
+            // it's one we add directly, so we need to account for it here
+            SearchFieldType::U64(pg_sys::TIDOID)
+        } else {
+            let attribute = tuple_desc
+                .iter()
+                .find(|attribute| attribute.name() == attribute_name)
+                .unwrap_or_else(|| {
+                    panic!(
+                        "the column {} referenced by the field configuration for {} should exist",
+                        field_name, attribute_name
+                    )
+                });
+            (&attribute.type_oid()).try_into().unwrap_or_else(|_| {
                 panic!(
-                    "the column {} referenced by the field configuration for {} should exist",
-                    field_name, attribute_name
+                    "failed to convert attribute {} to search field type",
+                    attribute_name
                 )
-            });
-        let field_type: SearchFieldType = (&attribute.type_oid()).try_into().unwrap_or_else(|_| {
-            panic!(
-                "failed to convert attribute {} to search field type",
-                attribute_name
-            )
-        });
+            })
+        };
 
         Self {
             field,

pg_search/tests/pg_regress/expected/find_ctid.out
@@ -1,0 +1,10 @@
+DROP TABLE IF EXISTS tblfind_ctid;
+CREATE TABLE tblfind_ctid (id bigint);
+CREATE INDEX idxfind_ctid ON tblfind_ctid USING bm25 (id) WITH (key_field = 'id');
+INSERT INTO tblfind_ctid (id) VALUES (1);
+SELECT count(*) FROM paradedb.find_ctid('idxfind_ctid', '(0, 1)');
+ count 
+-------
+     1
+(1 row)
+

pg_search/tests/pg_regress/sql/find_ctid.sql
@@ -1,0 +1,5 @@
+DROP TABLE IF EXISTS tblfind_ctid;
+CREATE TABLE tblfind_ctid (id bigint);
+CREATE INDEX idxfind_ctid ON tblfind_ctid USING bm25 (id) WITH (key_field = 'id');
+INSERT INTO tblfind_ctid (id) VALUES (1);
+SELECT count(*) FROM paradedb.find_ctid('idxfind_ctid', '(0, 1)');
6104f6812c198756414f4028193f1dcf1027b438
Mohammad Dashti <mdashti@gmail.com>
2025-06-11 09:39:14 -0700

    Fixed the issue

--- pg_search/src/postgres/customscan/pdbscan/privdat.rs
+++ pg_search/src/postgres/customscan/pdbscan/privdat.rs
@@ -50,6 +50,9 @@ pub struct PrivateData {
     // Additional search predicates from join filters that are relevant for snippet/score generation
     // Stores the entire simplified Boolean expression to preserve OR structures like (TRUE OR name:"Rowling")
     join_predicates: Option<SearchQueryInput>,
+    // Store the heap filter expression as a serialized PostgreSQL node string
+    // This will be deserialized at execution time and converted to ExprState for evaluation
+    heap_filter_node_string: Option<String>,
 }
 
 mod var_attname_lookup_serializer {
@@ -223,6 +226,10 @@ impl PrivateData {
     pub fn set_join_predicates(&mut self, predicates: Option<SearchQueryInput>) {
         self.join_predicates = predicates;
     }
+
+    pub fn set_heap_filter_node_string(&mut self, heap_filter_node_string: Option<String>) {
+        self.heap_filter_node_string = heap_filter_node_string;
+    }
 }
 
 //
@@ -297,5 +304,9 @@ impl PrivateData {
 
     pub fn join_predicates(&self) -> &Option<SearchQueryInput> {
         &self.join_predicates
+    }
+
+    pub fn heap_filter_node_string(&self) -> &Option<String> {
+        &self.heap_filter_node_string
     }
 }

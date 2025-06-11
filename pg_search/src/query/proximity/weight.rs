adba8bdab76ed66292c73f5be6807549937f0a87
Eric B. Ridge <eebbrr@paradedb.com>
2025-06-11 14:58:50 -0400

    more work on proximity.  no supports regexes and nested proximity clauses

--- pg_search/src/query/proximity/weight.rs
+++ pg_search/src/query/proximity/weight.rs
@@ -1,9 +1,15 @@
 use crate::query::proximity::query::ProximityQuery;
 use crate::query::proximity::scorer::ProximityScorer;
+use crate::query::proximity::{ProximityClause, ProximityTermStyle, WhichTerms};
+use std::sync::Arc;
 use tantivy::fieldnorm::FieldNormReader;
-use tantivy::query::{does_not_match, Bm25Weight, EmptyScorer, Explanation, Scorer, Weight};
+use tantivy::postings::{LoadedPostings, Postings};
+use tantivy::query::{
+    does_not_match, AutomatonWeight, Bm25Weight, EmptyScorer, Explanation, RegexPhraseWeight,
+    Scorer, Weight,
+};
 use tantivy::schema::IndexRecordOption;
-use tantivy::{DocId, DocSet, Score, SegmentReader};
+use tantivy::{DocId, DocSet, Score, SegmentReader, TantivyError, Term, TERMINATED};
 
 pub struct ProximityWeight {
     query: ProximityQuery,
@@ -39,28 +45,13 @@ impl ProximityWeight {
             .map(|bm25_weight| bm25_weight.boost_by(boost));
         let fieldnorm_reader = self.fieldnorm_reader(reader)?;
 
-        let mut left_postings = Vec::new();
-        for term in self.query.left().terms() {
-            if let Some(postings) = reader
-                .inverted_index(term.field())?
-                .read_postings(term, IndexRecordOption::WithFreqsAndPositions)?
-            {
-                left_postings.push(postings);
-            }
-        }
+        let left_postings =
+            self.read_postings(reader, self.query.left(), WhichTerms::Left, false)?;
         if left_postings.is_empty() {
             return Ok(None);
         }
-
-        let mut right_postings = Vec::new();
-        for term in self.query.right().terms() {
-            if let Some(postings) = reader
-                .inverted_index(term.field())?
-                .read_postings(term, IndexRecordOption::WithFreqsAndPositions)?
-            {
-                right_postings.push(postings);
-            }
-        }
+        let right_postings =
+            self.read_postings(reader, self.query.right(), WhichTerms::Right, false)?;
         if right_postings.is_empty() {
             return Ok(None);
         }
@@ -72,6 +63,125 @@ impl ProximityWeight {
             fieldnorm_reader,
             weight_opt,
         )))
+    }
+
+    fn read_postings(
+        &self,
+        reader: &SegmentReader,
+        clause: &ProximityClause,
+        which_terms: WhichTerms,
+        nested: bool,
+    ) -> tantivy::Result<Vec<Box<dyn Postings>>> {
+        if let ProximityClause::Proximity {
+            left,
+            distance,
+            right,
+        } = clause
+        {
+            let query = ProximityQuery::new(
+                self.query.field(),
+                *left.clone(),
+                distance.clone(),
+                *right.clone(),
+            );
+            let weight = ProximityWeight::new(query, self.weight_opt.clone());
+            let left_postings = weight.read_postings(reader, left, which_terms, true)?;
+            let right_postings = weight.read_postings(reader, right, which_terms, true)?;
+
+            let mut scorer = ProximityScorer::new(
+                left_postings,
+                distance.clone(),
+                right_postings,
+                self.fieldnorm_reader(reader)?,
+                self.weight_opt.clone(),
+            );
+
+            let mut doc_ids = Vec::new();
+            let mut positions = Vec::new();
+            let mut offsets = Vec::new();
+            while scorer.doc() != TERMINATED {
+                offsets.push(positions.len() as u32);
+                doc_ids.push(scorer.doc());
+
+                for (l, r) in scorer.prox_iter() {
+                    if nested {
+                        match which_terms {
+                            // *** NOTICE! ***
+                            //
+                            // these are *purposely* reversed.  If we're nested and the user is asking
+                            // for postings on the left-side of a proximity query what *we* need to
+                            // return are the postings from the right side
+                            WhichTerms::Left => positions.push(r),
+
+                            // same goes for the right-side, just in reverse
+                            WhichTerms::Right => positions.push(l),
+
+                            // gotta collect 'em all
+                            WhichTerms::All => {
+                                positions.push(l);
+                                positions.push(r);
+                            }
+                        }
+                    } else {
+                        positions.push(l);
+                        positions.push(r);
+                    }
+                }
+                scorer.advance();
+            }
+            offsets.push(positions.len() as u32);
+
+            let loaded_postings = LoadedPostings {
+                doc_ids: doc_ids.into_boxed_slice(),
+                position_offsets: offsets.into_boxed_slice(),
+                positions: positions.into_boxed_slice(),
+                cursor: 0,
+            };
+
+            Ok(vec![Box::new(loaded_postings)])
+        } else {
+            let mut postings: Vec<Box<dyn Postings>> = Vec::new();
+            let mut num_regex_terms = 0;
+            let inverted_index = reader.inverted_index(self.query.field())?;
+            for term in clause.terms(which_terms) {
+                match term {
+                    ProximityTermStyle::Term(term) => {
+                        let term = Term::from_field_text(self.query.field(), term);
+                        if let Some(segment_postings) = inverted_index
+                            .read_postings(&term, IndexRecordOption::WithFreqsAndPositions)?
+                        {
+                            postings.push(Box::new(segment_postings));
+                        }
+                    }
+                    ProximityTermStyle::Rexgex(re, max_expansions) => {
+                        let regex =
+                            tantivy_fst::Regex::new(re.as_str()).unwrap_or_else(|e| panic!("{e}"));
+                        let automaton = AutomatonWeight::<tantivy_fst::Regex>::new(
+                            self.query.field(),
+                            Arc::new(regex),
+                        );
+                        let term_infos = automaton.get_match_term_infos(reader)?;
+                        if term_infos.is_empty() {
+                            // if term_infos is empty, that's fine -- we might have other terms
+                            continue;
+                        }
+                        num_regex_terms += term_infos.len();
+                        if num_regex_terms > *max_expansions {
+                            return Err(TantivyError::InvalidArgument(format!(
+                                "Regex ProximityClause(s) exceeded max expansions: {num_regex_terms} > {max_expansions}",
+                            )));
+                        }
+                        let union = RegexPhraseWeight::get_union_from_term_infos(
+                            &term_infos,
+                            reader,
+                            &inverted_index,
+                        )?;
+                        postings.push(Box::new(union))
+                    }
+                }
+            }
+            Ok(postings)
+        }
     }
 }
 

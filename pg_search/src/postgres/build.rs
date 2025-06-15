d5e4f3a22faffaafa787015c8e18c777e42bc9dc
Ming Ying <ming.ying.nyc@gmail.com>
2025-06-15 13:25:46 -0400

    sort by ctid

--- pg_search/src/postgres/build.rs
+++ pg_search/src/postgres/build.rs
@@ -30,7 +30,7 @@ use anyhow::Result;
 use pgrx::pg_sys::panic::ErrorReport;
 use pgrx::*;
 use tantivy::schema::Schema;
-use tantivy::{Index, IndexSettings};
+use tantivy::{Index, IndexSettings, IndexSortByField, Order};
 
 #[pg_guard]
 pub extern "C-unwind" fn ambuild(
@@ -205,6 +205,10 @@ fn create_index(index_relation: &PgRelation) -> Result<()> {
     let directory = MVCCDirectory::snapshot(index_relation.oid());
     let settings = IndexSettings {
         docstore_compress_dedicated_thread: false,
+        sort_by_field: Some(IndexSortByField {
+            field: "ctid".to_string(),
+            order: Order::Asc,
+        }),
         ..IndexSettings::default()
     };
     let _ = Index::create(directory, schema, settings)?;

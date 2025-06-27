29761b3db76f14d914bcf40d12b39b07ab54f447
Ming Ying <ming.ying.nyc@gmail.com>
2025-06-27 14:35:22 -0400

    scaffolding for launching bgworker

--- pg_search/src/postgres/vacuum.rs
+++ pg_search/src/postgres/vacuum.rs
@@ -15,12 +15,34 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+use pgrx::bgworkers::BackgroundWorkerBuilder;
 use pgrx::*;
 
 #[pg_guard]
 pub unsafe extern "C-unwind" fn amvacuumcleanup(
-    _info: *mut pg_sys::IndexVacuumInfo,
+    info: *mut pg_sys::IndexVacuumInfo,
     stats: *mut pg_sys::IndexBulkDeleteResult,
 ) -> *mut pg_sys::IndexBulkDeleteResult {
+    let worker = BackgroundWorkerBuilder::new("background merger")
+        .enable_shmem_access(None)
+        .set_library("pg_search")
+        .set_function("merge_in_background")
+        .set_argument((*(*info).index).rd_id.into_datum())
+        .set_notify_pid(unsafe { pg_sys::MyProcPid })
+        .load_dynamic()
+        .expect("background merger should have loaded");
+    let pid = worker
+        .wait_for_startup()
+        .expect("background merger should have started");
+    assert!(pid > 0, "background mergershould have a valid PID");
+
     stats
+}
+
+#[pg_guard]
+#[no_mangle]
+pub extern "C-unwind" fn merge_in_background(arg: pg_sys::Datum) {
+    use pgrx::bgworkers::*;
+    BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGHUP | SignalWakeFlags::SIGTERM);
+    let index_oid = unsafe { u32::from_datum(arg, false) }.unwrap();
 }

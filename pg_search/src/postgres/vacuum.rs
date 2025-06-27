0e91ff4f3ffbd1baae80639ae1db629526cb0d52
Ming Ying <ming.ying.nyc@gmail.com>
2025-06-27 17:24:15 -0400

    undo vacuum.rs changes

--- pg_search/src/postgres/vacuum.rs
+++ pg_search/src/postgres/vacuum.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use pgrx::{pg_guard, pg_sys};
+use pgrx::*;
 
 #[pg_guard]
 pub unsafe extern "C-unwind" fn amvacuumcleanup(

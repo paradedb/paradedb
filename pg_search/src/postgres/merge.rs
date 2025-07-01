413d7f6e3f8630c6e8d1b4953d4c9f581c4c6dab
Ming Ying <ming.ying.nyc@gmail.com>
2025-07-01 16:22:35 -0400

    dont launch bgworker unless necessary

--- pg_search/src/postgres/merge.rs
+++ pg_search/src/postgres/merge.rs
@@ -16,8 +16,11 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use crate::index::merge_policy::LayeredMergePolicy;
+use crate::index::mvcc::MvccSatisfies;
+use crate::index::writer::index::SearchIndexMerger;
 use crate::postgres::insert::merge_index_with_policy;
 use crate::postgres::options::SearchIndexOptions;
+use crate::postgres::storage::metadata::MetaPage;
 use crate::postgres::PgSearchRelation;
 
 use pgrx::bgworkers::*;
@@ -87,6 +90,25 @@ pub unsafe fn do_merge(index_oid: pg_sys::Oid) {
     unsafe { merge_index_with_policy(&index, foreground_merge_policy, false, false, false) };
 
     // then launch a background process to merge down the background layers
+    // only if we determine that there are enough segments to merge in the background
+    let background_layers = layer_sizes.background();
+    let mut background_merge_policy = LayeredMergePolicy::new(background_layers.clone());
+
+    let metadata = MetaPage::open(&index);
+    let merge_lock = metadata.acquire_merge_lock();
+    let directory = MvccSatisfies::Mergeable.directory(&index);
+    let merger =
+        SearchIndexMerger::open(directory).expect("should be able to open a SearchIndexMerger");
+    let (merge_candidates, _) =
+        background_merge_policy.simulate(&metadata, &merge_lock, &merger, false);
+    pgrx::debug1!(
+        "background layers: {background_layers:?}, merge_candidates: {merge_candidates:?}"
+    );
+
+    if merge_candidates.is_empty() {
+        return;
+    }
+
     try_launch_background_merger(index_oid);
 }
 

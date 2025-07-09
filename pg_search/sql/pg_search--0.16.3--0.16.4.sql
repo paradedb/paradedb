16c3861e930b2f6372a359027985d452a7dfb4a4
Eric B. Ridge <eebbrr@paradedb.com>
2025-07-09 10:34:48 -0400

    fix upgrade script.

--- pg_search/sql/pg_search--0.16.3--0.16.4.sql
+++ pg_search/sql/pg_search--0.16.3--0.16.4.sql
@@ -1,8 +1,11 @@
-/* </end connected objects> */
+--
+-- setup the new ProximityClause type
+--
 /* <begin connected objects> */
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::ProximityClause
 CREATE TYPE ProximityClause;
+
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::proximityclause_in
 CREATE  FUNCTION "proximityclause_in"(
@@ -11,6 +14,7 @@ CREATE  FUNCTION "proximityclause_in"(
     IMMUTABLE PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'proximityclause_in_wrapper';
+
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::proximityclause_out
 CREATE  FUNCTION "proximityclause_out"(
@@ -19,8 +23,21 @@ CREATE  FUNCTION "proximityclause_out"(
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'proximityclause_out_wrapper';
+
+-- pg_search/src/query/proximity/mod.rs:28
+-- pg_search::query::proximity::ProximityClause
+CREATE TYPE ProximityClause (
+    INTERNALLENGTH = variable,
+    INPUT = proximityclause_in, /* pg_search::query::proximity::proximityclause_in */
+    OUTPUT = proximityclause_out, /* pg_search::query::proximity::proximityclause_out */
+    STORAGE = extended
+);
 /* </end connected objects> */
-/* <begin connected objects> */
+
+--
+-- all the various UDFs for proximity searching
+---
+
 -- pg_search/src/api/proximity.rs:23
 -- pg_search::api::proximity::prox_array
 CREATE  FUNCTION "prox_array"(
@@ -101,6 +118,11 @@ CREATE  FUNCTION "proximity_in_order"(
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'proximity_in_order_wrapper';
 /* </end connected objects> */
+
+--
+-- convenience CASTs
+--
+
 /* <begin connected objects> */
 -- pg_search/src/api/proximity.rs:91
 -- pg_search::api::proximity::text_array_to_prox_clause

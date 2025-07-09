93a91b5a608160119ad833d9aeac7ab21df6e860
Ming Ying <ming.ying.nyc@gmail.com>
2025-07-09 10:26:19 -0400

    clean up regression test

--- pg_search/tests/pg_regress/sql/layer_size_config.sql
+++ pg_search/tests/pg_regress/sql/layer_size_config.sql
@@ -16,10 +16,7 @@ DROP INDEX idxlayer_sizes;
 
 -- negative layer ❌
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '-1kb');
-DROP INDEX idxlayer_sizes;
-
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', background_layer_sizes = '-1kb');
-DROP INDEX idxlayer_sizes;
 
 -- zero layer ✅
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '0kb, 10kb');
@@ -34,10 +31,7 @@ DROP INDEX idxlayer_sizes;
 
 -- malformed layer ❌
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '1kb, bob''s your uncle');
-DROP INDEX idxlayer_sizes;
-
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', background_layer_sizes = '1kb, bob''s your uncle');
-DROP INDEX idxlayer_sizes;
 
 -- multiple layers ✅
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '1kb, 10kb, 100MB');

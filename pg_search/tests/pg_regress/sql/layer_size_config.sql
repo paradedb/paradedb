741531f4e1c98287469cd61583a150a605948654
Ming Ying <ming.ying.nyc@gmail.com>
2025-07-09 09:39:14 -0400

    add new layer size UDFs

--- pg_search/tests/pg_regress/sql/layer_size_config.sql
+++ pg_search/tests/pg_regress/sql/layer_size_config.sql
@@ -5,9 +5,13 @@ CREATE TABLE layer_sizes (id serial8 not null primary key);
 
 -- 1 layer ✅
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '1kb');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', background_layer_sizes = '1kb');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 -- negative layer ❌
@@ -19,9 +23,13 @@ DROP INDEX idxlayer_sizes;
 
 -- zero layer ✅
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '0kb, 10kb');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', background_layer_sizes = '0kb, 10kb');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 -- malformed layer ❌
@@ -33,9 +41,13 @@ DROP INDEX idxlayer_sizes;
 
 -- multiple layers ✅
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', foreground_layer_sizes = '1kb, 10kb, 100MB');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 CREATE INDEX idxlayer_sizes ON layer_sizes USING bm25(id) WITH (key_field='id', background_layer_sizes = '1kb, 10kb, 100MB');
+SELECT * FROM paradedb.foreground_layer_sizes('idxlayer_sizes');
+SELECT * FROM paradedb.background_layer_sizes('idxlayer_sizes');
 DROP INDEX idxlayer_sizes;
 
 DROP TABLE layer_sizes;

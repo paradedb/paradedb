8fb2cde6f379e327bb464ed0f2024226126215a6
github-action-benchmark <github@users.noreply.github.com>
2025-07-10 14:00:56 +0000

    add pg_search single-server.toml Performance (customSmallerIsBetter) benchmark result for ce8e33ae49785f0afe220ca985de3d0c7c270503

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752156054303,
+  "lastUpdate": 1752156056092,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search bulk-updates.toml Performance": [
@@ -232,6 +232,166 @@ window.BENCHMARK_DATA = {
             "value": 21.6323500058667,
             "unit": "avg tps",
             "extra": "max tps: 1704.8641479003745, count: 58434"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "Philippe Noël",
+            "username": "philippemnoel",
+            "email": "philippemnoel@gmail.com"
+          },
+          "id": "ce8e33ae49785f0afe220ca985de3d0c7c270503",
+          "message": "chore: more improvements to index/schema configuration and management (#2771)\n\n## What\n\n#2660 brought a much needed round of cleanups to how we manage index\nschemas. Unfortunately, it introduced quite some overhead in\nreading/decoding/validating the schema. This process was happening quite\na bit throughout the execution paths of `aminsert` and other hot-spots.\n\n#2176 brought the ability to essentially keep one heavy-weight\n`PgSearchRelation` instantiated and cheaply clone it when necessary.\nThis PR cleans up things further such that the `SearchIndexSchema` is\nnow a lazily-evaluated property of `PgSearchRelation`. This means\n`SearchIndexSchema` is only evaluated when needed, and then only once\n(at least per statement).\n\nFurthermore, its internal properties are lazily-evaluated, ensuring any\ngiven code path doesn't do more work than it needs.\n\nThis also renames `SearchIndexOptions` to `BM25IndexOptions`, mainly\nbecause I kept getting confused about what `SearchIndexOptions`\nrepresented (it was too similarly named to `SearchIndexSchema` for my\ntastes). And `BM25IndexOptions` is now a property of `PgSearchRelation`\ntoo.\n\nThis seems to have drastically improved the write throughput of the\nINSERT/UPDATE jobs in our `single-server.toml` stressgress test.\nv0.15.26 was 176/s INSERTs and 154/s UPDATEs. This PR clocks in at 275/s\nand 260/s, respectively.\n\n# Other Notable Changes\n\n- Index configuration validation now happens during CREATE INDEX/REINDEX\nin `ambuildempty()` rather than on every instantiation of\n`SearchIndexSchema`.\n\n- The \"raw\" tokenizer deprecation warnings are now gone, unless somehow\nthe \"key_field\" is configured with it -- which is no longer possible\n\n## Why\n\nTrying to rollback performance regressions that were introduced in\n0.16.0\n\n## How\n\n## Tests\n\nAll existing tests pass, and a few were updated due to the \"raw\"\ntokenizer deprecation warning going away and a change in wording for a\nspecific validation error.",
+          "timestamp": "2025-07-05T15:13:47Z",
+          "url": "https://github.com/paradedb/paradedb/commit/ce8e33ae49785f0afe220ca985de3d0c7c270503"
+        },
+        "date": 1752156055217,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - cpu",
+            "value": 6.8215708501977455,
+            "unit": "avg cpu",
+            "extra": "max cpu: 24.691359, count: 58450"
+          },
+          {
+            "name": "Custom Scan - Primary - mem",
+            "value": 95.23434646332335,
+            "unit": "avg mem",
+            "extra": "max mem: 99.59375, count: 58450"
+          },
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 497.52280765767745,
+            "unit": "avg tps",
+            "extra": "max tps: 647.5381478818036, count: 58450"
+          },
+          {
+            "name": "Delete values - Primary - cpu",
+            "value": 4.828020140173067,
+            "unit": "avg cpu",
+            "extra": "max cpu: 9.876543, count: 58450"
+          },
+          {
+            "name": "Delete values - Primary - mem",
+            "value": 79.3191313355432,
+            "unit": "avg mem",
+            "extra": "max mem: 85.2421875, count: 58450"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 2983.380218616359,
+            "unit": "avg tps",
+            "extra": "max tps: 3237.7095675289547, count: 58450"
+          },
+          {
+            "name": "Index Only Scan - Primary - cpu",
+            "value": 6.80343302097104,
+            "unit": "avg cpu",
+            "extra": "max cpu: 24.84472, count: 58450"
+          },
+          {
+            "name": "Index Only Scan - Primary - mem",
+            "value": 95.81746838911462,
+            "unit": "avg mem",
+            "extra": "max mem: 100.55859375, count: 58450"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 497.5174239202883,
+            "unit": "avg tps",
+            "extra": "max tps: 646.152469523439, count: 58450"
+          },
+          {
+            "name": "Index Scan - Primary - cpu",
+            "value": 4.704471546638148,
+            "unit": "avg cpu",
+            "extra": "max cpu: 5.0314465, count: 58450"
+          },
+          {
+            "name": "Index Scan - Primary - mem",
+            "value": 92.215718429213,
+            "unit": "avg mem",
+            "extra": "max mem: 95.41015625, count: 58450"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 458.83755457540644,
+            "unit": "avg tps",
+            "extra": "max tps: 574.4985604215072, count: 58450"
+          },
+          {
+            "name": "Insert value - Primary - cpu",
+            "value": 7.01865730946624,
+            "unit": "avg cpu",
+            "extra": "max cpu: 20.0, count: 116900"
+          },
+          {
+            "name": "Insert value - Primary - mem",
+            "value": 99.28503542023097,
+            "unit": "avg mem",
+            "extra": "max mem: 106.4375, count: 116900"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 317.4829589804477,
+            "unit": "avg tps",
+            "extra": "max tps: 324.68216037620454, count: 116900"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 7190.149392643285,
+            "unit": "avg block_count",
+            "extra": "max block_count: 7411.0, count: 58450"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 117.2043113772455,
+            "unit": "avg segment_count",
+            "extra": "max segment_count: 360.0, count: 58450"
+          },
+          {
+            "name": "Update random values - Primary - cpu",
+            "value": 5.675070685712289,
+            "unit": "avg cpu",
+            "extra": "max cpu: 14.814815, count: 58450"
+          },
+          {
+            "name": "Update random values - Primary - mem",
+            "value": 98.67097779886656,
+            "unit": "avg mem",
+            "extra": "max mem: 108.35546875, count: 58450"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 288.1845053347515,
+            "unit": "avg tps",
+            "extra": "max tps: 299.5747719186087, count: 58450"
+          },
+          {
+            "name": "Vacuum - Primary - cpu",
+            "value": 14.571036148713613,
+            "unit": "avg cpu",
+            "extra": "max cpu: 29.62963, count: 58450"
+          },
+          {
+            "name": "Vacuum - Primary - mem",
+            "value": 82.52467633928572,
+            "unit": "avg mem",
+            "extra": "max mem: 88.31640625, count: 58450"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 20.402791422903903,
+            "unit": "avg tps",
+            "extra": "max tps: 1758.8386036932093, count: 58450"
           }
         ]
       }

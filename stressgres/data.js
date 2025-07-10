7f32ec78b4c4ea1bcedde0434dec02a65acd9d98
github-action-benchmark <github@users.noreply.github.com>
2025-07-10 19:24:43 +0000

    add pg_search bulk-updates.toml Performance - TPS (customBiggerIsBetter) benchmark result for e350be5d171ddb28a700c02d8adc32a1a9f4b084

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752175425066,
+  "lastUpdate": 1752175483168,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search bulk-updates.toml Performance - TPS": [
@@ -238,6 +238,40 @@ window.BENCHMARK_DATA = {
             "value": 8.418583493652815,
             "unit": "median tps",
             "extra": "avg tps: 7.734302646182228, max tps: 9.265874810991182, count: 58845"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "e350be5d171ddb28a700c02d8adc32a1a9f4b084",
+          "message": "feat: custom fsm (#2765)\n\nA custom FSM implementation that allows us to internally track\nfree/reusable blocks without generating full read+write cycles on every\npage that is returned to the FSM.\n\nThere's a new UDF called `paradedb.fsm_info()` that returns a table of\nFSM block numbers in use and the free block numbers they contain.\n\n## Why\n\nTo reduce I/O during segment merging and garbage collection, with an aim of reducing WAL traffic for enterprise.",
+          "timestamp": "2025-07-09T15:42:21Z",
+          "url": "https://github.com/paradedb/paradedb/commit/e350be5d171ddb28a700c02d8adc32a1a9f4b084"
+        },
+        "date": 1752175481889,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 10.364295889170931,
+            "unit": "median tps",
+            "extra": "avg tps: 8.969474012726929, max tps: 13.393801994747202, count: 58851"
+          },
+          {
+            "name": "Count Query - Primary - tps",
+            "value": 8.243601732286322,
+            "unit": "median tps",
+            "extra": "avg tps: 7.574151600991649, max tps: 9.18397307665833, count: 58851"
           }
         ]
       }

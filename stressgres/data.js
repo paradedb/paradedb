e50b017a0a731a823b4d3d3630295931abaa13ea
github-action-benchmark <github@users.noreply.github.com>
2025-07-10 14:01:09 +0000

    add pg_search wide-table.toml Performance (customSmallerIsBetter) benchmark result for ce8e33ae49785f0afe220ca985de3d0c7c270503

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752156067969,
+  "lastUpdate": 1752156069658,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search bulk-updates.toml Performance": [
@@ -1174,6 +1174,76 @@ window.BENCHMARK_DATA = {
             "value": 111.40455185647092,
             "unit": "avg tps",
             "extra": "max tps: 114.90431896051523, count: 59103"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "Philippe Noël",
+            "username": "philippemnoel",
+            "email": "philippemnoel@gmail.com"
+          },
+          "id": "ce8e33ae49785f0afe220ca985de3d0c7c270503",
+          "message": "chore: more improvements to index/schema configuration and management (#2771)\n\n## What\n\n#2660 brought a much needed round of cleanups to how we manage index\nschemas. Unfortunately, it introduced quite some overhead in\nreading/decoding/validating the schema. This process was happening quite\na bit throughout the execution paths of `aminsert` and other hot-spots.\n\n#2176 brought the ability to essentially keep one heavy-weight\n`PgSearchRelation` instantiated and cheaply clone it when necessary.\nThis PR cleans up things further such that the `SearchIndexSchema` is\nnow a lazily-evaluated property of `PgSearchRelation`. This means\n`SearchIndexSchema` is only evaluated when needed, and then only once\n(at least per statement).\n\nFurthermore, its internal properties are lazily-evaluated, ensuring any\ngiven code path doesn't do more work than it needs.\n\nThis also renames `SearchIndexOptions` to `BM25IndexOptions`, mainly\nbecause I kept getting confused about what `SearchIndexOptions`\nrepresented (it was too similarly named to `SearchIndexSchema` for my\ntastes). And `BM25IndexOptions` is now a property of `PgSearchRelation`\ntoo.\n\nThis seems to have drastically improved the write throughput of the\nINSERT/UPDATE jobs in our `single-server.toml` stressgress test.\nv0.15.26 was 176/s INSERTs and 154/s UPDATEs. This PR clocks in at 275/s\nand 260/s, respectively.\n\n# Other Notable Changes\n\n- Index configuration validation now happens during CREATE INDEX/REINDEX\nin `ambuildempty()` rather than on every instantiation of\n`SearchIndexSchema`.\n\n- The \"raw\" tokenizer deprecation warnings are now gone, unless somehow\nthe \"key_field\" is configured with it -- which is no longer possible\n\n## Why\n\nTrying to rollback performance regressions that were introduced in\n0.16.0\n\n## How\n\n## Tests\n\nAll existing tests pass, and a few were updated due to the \"raw\"\ntokenizer deprecation warning going away and a change in wording for a\nspecific validation error.",
+          "timestamp": "2025-07-05T15:13:47Z",
+          "url": "https://github.com/paradedb/paradedb/commit/ce8e33ae49785f0afe220ca985de3d0c7c270503"
+        },
+        "date": 1752156062976,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 19.49478776843815,
+            "unit": "avg cpu",
+            "extra": "max cpu: 49.079754, count: 59121"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 171.72761132888482,
+            "unit": "avg mem",
+            "extra": "max mem: 175.53515625, count: 59121"
+          },
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 38.262295750957584,
+            "unit": "avg tps",
+            "extra": "max tps: 38.981558510785796, count: 59121"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 18419.098273033273,
+            "unit": "avg block_count",
+            "extra": "max block_count: 20544.0, count: 59121"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 53.51628017117437,
+            "unit": "avg segment_count",
+            "extra": "max segment_count: 147.0, count: 59121"
+          },
+          {
+            "name": "Single Update - Primary - cpu",
+            "value": 8.935382735396159,
+            "unit": "avg cpu",
+            "extra": "max cpu: 31.372551, count: 59121"
+          },
+          {
+            "name": "Single Update - Primary - mem",
+            "value": 158.25416697059845,
+            "unit": "avg mem",
+            "extra": "max mem: 174.9375, count: 59121"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 199.65894751537644,
+            "unit": "avg tps",
+            "extra": "max tps: 215.8753897753016, count: 59121"
           }
         ]
       }

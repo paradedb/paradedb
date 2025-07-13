a2fa88da2889c4c5b63a614b741d9b35766b8c6a
github-action-benchmark <github@users.noreply.github.com>
2025-07-13 19:11:44 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for 4fd1b2b6b6664d03946be0f4836732f0f40df0cc

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752433902821,
+  "lastUpdate": 1752433904659,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search wide-table.toml Performance - TPS": [
@@ -484,6 +484,40 @@ window.BENCHMARK_DATA = {
             "value": 138.22114965602546,
             "unit": "median tps",
             "extra": "avg tps: 137.98474001930987, max tps: 147.4443421142904, count: 57220"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stu Hood",
+            "username": "stuhood",
+            "email": "stuhood@paradedb.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4fd1b2b6b6664d03946be0f4836732f0f40df0cc",
+          "message": "chore: Rename datasets and add string paging queries (#2834)\n\n## What\n\nAdd a high-cardinality paging/top-n query to the benchmarks, and rename\ndatasets to match their content. Additionally, improve the generation\nscript for the `docs` dataset to avoid joins and allow for deterministic\nrelative-position queries.\n\n## Why\n\nWe don't currently have a high-cardinality string paging/top-n query in\nthe benchmark. We have top-n on a string column, but only for\nlow-cardinality values (`top_n-string.sql`). The top-n case represented\nan important gap that a user encountered, which #2828 addresses.\n\nThe names of the `benchmark` datasets don't currently describe their\nshape / schema, and for the `join` dataset in particular, that would\ndiscourage using it for other types of queries. We rename it to `docs`\nhere, and then use the `pages` table as the dataset for top-n.\n\n## Tests\n\nTested locally that the new query demonstrates a speedup for #2828.",
+          "timestamp": "2025-07-13T18:04:27Z",
+          "url": "https://github.com/paradedb/paradedb/commit/4fd1b2b6b6664d03946be0f4836732f0f40df0cc"
+        },
+        "date": 1752433903782,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 28.17113660588908,
+            "unit": "median tps",
+            "extra": "avg tps: 28.08175373395458, max tps: 29.887623717333156, count: 57751"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 135.6761365119886,
+            "unit": "median tps",
+            "extra": "avg tps: 135.3196764325807, max tps: 144.8240320945384, count: 57751"
           }
         ]
       }

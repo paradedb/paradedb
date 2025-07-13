bc9a79015f62d28abe960b31d05b52b57c47d13b
github-action-benchmark <github@users.noreply.github.com>
2025-07-13 18:50:50 +0000

    add pg_search single-server.toml Performance - TPS (customBiggerIsBetter) benchmark result for 4fd1b2b6b6664d03946be0f4836732f0f40df0cc

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752432648706,
+  "lastUpdate": 1752432650565,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search wide-table.toml Performance - TPS": [
@@ -1422,6 +1422,70 @@ window.BENCHMARK_DATA = {
             "value": 4.885432987996397,
             "unit": "median tps",
             "extra": "avg tps: 9.287483643011267, max tps: 1119.8358768538883, count: 55139"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stu Hood",
+            "username": "stuhood",
+            "email": "stuhood@paradedb.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4fd1b2b6b6664d03946be0f4836732f0f40df0cc",
+          "message": "chore: Rename datasets and add string paging queries (#2834)\n\n## What\n\nAdd a high-cardinality paging/top-n query to the benchmarks, and rename\ndatasets to match their content. Additionally, improve the generation\nscript for the `docs` dataset to avoid joins and allow for deterministic\nrelative-position queries.\n\n## Why\n\nWe don't currently have a high-cardinality string paging/top-n query in\nthe benchmark. We have top-n on a string column, but only for\nlow-cardinality values (`top_n-string.sql`). The top-n case represented\nan important gap that a user encountered, which #2828 addresses.\n\nThe names of the `benchmark` datasets don't currently describe their\nshape / schema, and for the `join` dataset in particular, that would\ndiscourage using it for other types of queries. We rename it to `docs`\nhere, and then use the `pages` table as the dataset for top-n.\n\n## Tests\n\nTested locally that the new query demonstrates a speedup for #2828.",
+          "timestamp": "2025-07-13T18:04:27Z",
+          "url": "https://github.com/paradedb/paradedb/commit/4fd1b2b6b6664d03946be0f4836732f0f40df0cc"
+        },
+        "date": 1752432648116,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 283.9363723751385,
+            "unit": "median tps",
+            "extra": "avg tps: 286.0383760280728, max tps: 538.2504209849611, count: 55231"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 2189.642691072867,
+            "unit": "median tps",
+            "extra": "avg tps: 2192.3981522800163, max tps: 2686.0042290952615, count: 55231"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 283.15778469653907,
+            "unit": "median tps",
+            "extra": "avg tps: 285.4281484014078, max tps: 532.5478360804331, count: 55231"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 249.14373875405133,
+            "unit": "median tps",
+            "extra": "avg tps: 250.04185566947297, max tps: 416.59435283025454, count: 55231"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 135.94773764134112,
+            "unit": "median tps",
+            "extra": "avg tps: 136.2005346839254, max tps: 153.66913094753642, count: 110462"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 155.86887115486712,
+            "unit": "median tps",
+            "extra": "avg tps: 153.1622252342047, max tps: 156.805432528729, count: 55231"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 4.915880137709071,
+            "unit": "median tps",
+            "extra": "avg tps: 9.102878730811579, max tps: 1121.983127617727, count: 55231"
           }
         ]
       }

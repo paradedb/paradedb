b64a170edf715cd72ffab02a36dd304d926a8b5b
Eric B. Ridge <eebbrr@paradedb.com>
2025-07-21 14:57:12 -0400

    use our existing parse/parse_with_field functions.  other cleanup

--- pg_search/src/api/operator/hashhashhash.rs
+++ pg_search/src/api/operator/hashhashhash.rs
@@ -49,13 +49,14 @@ fn search_with_phrase_support(arg: Internal) -> ReturnedNodePointer {
                 .expect("The left hand side of the `###(field, TEXT)` operator must be a field."),
             match to_tokenize {
                 RHSValue::Text(to_tokenize) => to_tokenize,
-                _ => unreachable!("The right hand side of the `###(key_field, TEXT)` operator must be a text value")
+                _ => unreachable!("The right-hand side of the `###(key_field, TEXT)` operator must be a text value")
             }), |field, rhs| {
-            assert!(get_expr_result_type(rhs) == pg_sys::TEXTOID, "The right hand side of the `###(field, TEXT)` operator must be a text value");
+            let field = field.expect("The left hand side of the `###(field, TEXT)` operator must be a field.");
+            assert!(get_expr_result_type(rhs) == pg_sys::TEXTOID, "The right-hand side of the `###(field, TEXT)` operator must be a text value");
 
             let mut args = PgList::<pg_sys::Node>::new();
 
-            args.push(field.map(|field| field.into()).unwrap_or_else(FieldName::null_const).cast());
+            args.push(field.into_const().cast());
             args.push(rhs.cast());
 
             pg_sys::FuncExpr {

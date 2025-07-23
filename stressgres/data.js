5764bb500ff1cc7aba2fcedc7f0ccb927174c981
github-action-benchmark <github@users.noreply.github.com>
2025-07-23 16:40:14 +0000

    add pg_search bulk-updates.toml Performance - Other Metrics (customSmallerIsBetter) benchmark result for 53fa29a57e7daa2bdec9bf05f86d8db50088fd51

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753288812478,
+  "lastUpdate": 1753288814702,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -6650,6 +6650,66 @@ window.BENCHMARK_DATA = {
             "value": 67,
             "unit": "median segment_count",
             "extra": "avg segment_count: 68.4196116776086, max segment_count: 95.0, count: 57272"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stuhood@paradedb.com",
+            "name": "Stu Hood",
+            "username": "stuhood"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "53fa29a57e7daa2bdec9bf05f86d8db50088fd51",
+          "message": "perf: Avoid a re-query when a top-n query is exhausted (#2888)\n\n## What\n\nSplit the iterators consumed by the `TopN` exec method and by the\n`Normal`/`FF` exec methods into two separate implementations.\n\nAfter splitting them, use the known exact size of the `TopN` iterator to\nexit early (as `exhausted: bool`) if we had fewer matches than were\nrequested. This avoids re-querying an iterator (and re-scanning its\ncolumns) that we know has no more results.\n\n## Why\n\nThe two types of consumers of these iterators have very different\nconsumption patterns and constraints:\n* TopN knows exactly how many results there will be, and already buffers\nthem all in memory. It would like to be able to know the precise count\nof results.\n* `Normal` and `FF` exec methods do not know the total number of results\nto expect, and in some cases would like to be able to consume in a\nsegment-aware fashion in order to be able to late-fetch fast field\ncolumns (see #2623).\n\n## Tests\n\nAdded a test to cement the change in query count.\n\nBenchmarks show a 1.5x speedup for `paging-string-max`, and no change\nfor other queries.",
+          "timestamp": "2025-07-23T09:13:45-07:00",
+          "tree_id": "c162e59fb9a6b4523c0ff39e10d9d54d92761460",
+          "url": "https://github.com/paradedb/paradedb/commit/53fa29a57e7daa2bdec9bf05f86d8db50088fd51"
+        },
+        "date": 1753288813669,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 23.188406,
+            "unit": "median cpu",
+            "extra": "avg cpu: 21.33187967137643, max cpu: 42.772278, count: 57319"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 226.41796875,
+            "unit": "median mem",
+            "extra": "avg mem: 225.80655526799578, max mem: 231.17578125, count: 57319"
+          },
+          {
+            "name": "Count Query - Primary - cpu",
+            "value": 23.233301,
+            "unit": "median cpu",
+            "extra": "avg cpu: 22.203146847996134, max cpu: 33.136093, count: 57319"
+          },
+          {
+            "name": "Count Query - Primary - mem",
+            "value": 159.65234375,
+            "unit": "median mem",
+            "extra": "avg mem: 159.4213017280483, max mem: 160.94921875, count: 57319"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 22195,
+            "unit": "median block_count",
+            "extra": "avg block_count: 20721.82180428828, max block_count: 23553.0, count: 57319"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 67,
+            "unit": "median segment_count",
+            "extra": "avg segment_count: 68.58313997103927, max segment_count: 94.0, count: 57319"
           }
         ]
       }

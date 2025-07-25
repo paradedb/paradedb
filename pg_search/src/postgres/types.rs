d82454c14838bf587fd85bafc5ee88598a73ba7f
Mohammad Dashti <mdashti@gmail.com>
2025-07-25 03:53:56 -0700

    nitz.

--- pg_search/src/postgres/types.rs
+++ pg_search/src/postgres/types.rs
@@ -384,8 +384,6 @@ impl PartialOrd for TantivyValue {
 
 impl Ord for TantivyValue {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        use tantivy::schema::OwnedValue;
-
         // First, try partial_cmp - if it returns Some(ordering), use that
         if let Some(ordering) = self.partial_cmp(other) {
             return ordering;
@@ -393,39 +391,31 @@ impl Ord for TantivyValue {
 
         // If partial_cmp returns None, we need to define a total ordering
         // for different types. We'll order them by type discriminant.
-        let self_type_priority = match self.tantivy_schema_value() {
-            OwnedValue::Null => 0,
-            OwnedValue::Str(_) => 1,
-            OwnedValue::U64(_) => 2,
-            OwnedValue::I64(_) => 3,
-            OwnedValue::F64(_) => 4,
-            OwnedValue::Bool(_) => 5,
-            OwnedValue::Date(_) => 6,
-            OwnedValue::Facet(_) => 7,
-            OwnedValue::Bytes(_) => 8,
-            OwnedValue::IpAddr(_) => 9,
-            OwnedValue::PreTokStr(_) => 10,
-            OwnedValue::Object(_) => 11,
-            OwnedValue::Array(_) => 12,
-        };
-
-        let other_type_priority = match other.tantivy_schema_value() {
-            OwnedValue::Null => 0,
-            OwnedValue::Str(_) => 1,
-            OwnedValue::U64(_) => 2,
-            OwnedValue::I64(_) => 3,
-            OwnedValue::F64(_) => 4,
-            OwnedValue::Bool(_) => 5,
-            OwnedValue::Date(_) => 6,
-            OwnedValue::Facet(_) => 7,
-            OwnedValue::Bytes(_) => 8,
-            OwnedValue::IpAddr(_) => 9,
-            OwnedValue::PreTokStr(_) => 10,
-            OwnedValue::Object(_) => 11,
-            OwnedValue::Array(_) => 12,
-        };
+        let self_type_priority = type_priority(&self.tantivy_schema_value());
+        let other_type_priority = type_priority(&other.tantivy_schema_value());
 
         self_type_priority.cmp(&other_type_priority)
+    }
+}
+
+/// Returns the type priority for ordering different TantivyValue types
+fn type_priority(value: &tantivy::schema::OwnedValue) -> u8 {
+    use tantivy::schema::OwnedValue;
+
+    match value {
+        OwnedValue::Null => 0,
+        OwnedValue::Str(_) => 1,
+        OwnedValue::U64(_) => 2,
+        OwnedValue::I64(_) => 3,
+        OwnedValue::F64(_) => 4,
+        OwnedValue::Bool(_) => 5,
+        OwnedValue::Date(_) => 6,
+        OwnedValue::Facet(_) => 7,
+        OwnedValue::Bytes(_) => 8,
+        OwnedValue::IpAddr(_) => 9,
+        OwnedValue::PreTokStr(_) => 10,
+        OwnedValue::Object(_) => 11,
+        OwnedValue::Array(_) => 12,
     }
 }
 

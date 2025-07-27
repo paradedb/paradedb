114929c20a95f591f8d5e88dc1976d7ee02892ff
Akshat Jaimini <destrex271@gmail.com>
2025-07-27 19:44:36 +0530

    update: moved function to 17.2-18

--- pg_search/sql/pg_search--0.17.1--0.17.2.sql
+++ pg_search/sql/pg_search--0.17.1--0.17.2.sql
@@ -1,6 +1,3 @@
 \echo Use "ALTER EXTENSION pg_search UPDATE TO '0.17.2'" to load this file. \quit
 
-CREATE  FUNCTION "score"() RETURNS void
-STRICT 
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'score_invalid_signature_wrapper';
+

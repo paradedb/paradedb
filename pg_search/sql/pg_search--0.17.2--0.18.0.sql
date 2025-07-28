9787e0c312ecbd25c78c9f21b2d7d327fdc9ad15
Eric B. Ridge <eebbrr@paradedb.com>
2025-07-28 12:56:47 -0400

    move ProximityClause to the pdb schema

--- pg_search/sql/pg_search--0.17.2--0.18.0.sql
+++ pg_search/sql/pg_search--0.17.2--0.18.0.sql
@@ -1916,29 +1916,29 @@ DROP SCHEMA IF EXISTS paradedb_tmp;
 --
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::ProximityClause
-CREATE TYPE ProximityClause;
+CREATE TYPE pdb.ProximityClause;
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::proximityclause_in
-CREATE  FUNCTION "proximityclause_in"(
+CREATE  FUNCTION pdb."proximityclause_in"(
     "input" cstring /* core::option::Option<&core::ffi::c_str::CStr> */
-) RETURNS ProximityClause /* core::option::Option<pg_search::query::proximity::ProximityClause> */
+) RETURNS pdb.ProximityClause /* core::option::Option<pg_search::query::proximity::ProximityClause> */
     IMMUTABLE PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'proximityclause_in_wrapper';
 -- pg_search/src/query/proximity/mod.rs:28
 -- pg_search::query::proximity::proximityclause_out
-CREATE  FUNCTION "proximityclause_out"(
-    "input" ProximityClause /* pg_search::query::proximity::ProximityClause */
+CREATE  FUNCTION pdb."proximityclause_out"(
+    "input" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
 ) RETURNS cstring /* alloc::ffi::c_str::CString */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'proximityclause_out_wrapper';
 
 
-CREATE TYPE ProximityClause (
+CREATE TYPE pdb.ProximityClause (
   INTERNALLENGTH = variable,
-  INPUT = paradedb.proximityclause_in,
-  OUTPUT = paradedb.proximityclause_out,
+  INPUT = pdb..proximityclause_in,
+  OUTPUT = pdb..proximityclause_out,
   STORAGE = extended
 );
 
@@ -1948,9 +1948,9 @@ CREATE TYPE ProximityClause (
 -- pg_search::api::builder_fns::proximity::pdb::_db50eb::proximity_bfn
 CREATE  FUNCTION "proximity"(
     "field" FieldName, /* pg_search::api::FieldName */
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
 ) RETURNS SearchQueryInput /* pg_search::query::SearchQueryInput */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
@@ -1961,9 +1961,9 @@ AS 'MODULE_PATHNAME', 'proximity_bfn_wrapper';
 -- pg_search::api::builder_fns::proximity::pdb::_592d4e::proximity_in_order_bfn
 CREATE  FUNCTION "proximity_in_order"(
     "field" FieldName, /* pg_search::api::FieldName */
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
 ) RETURNS SearchQueryInput /* pg_search::query::SearchQueryInput */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
@@ -1974,7 +1974,7 @@ AS 'MODULE_PATHNAME', 'proximity_in_order_bfn_wrapper';
 -- pg_search::api::builder_fns::proximity::text_array_to_prox_clause
 CREATE  FUNCTION "text_array_to_prox_clause"(
     "t" TEXT[] /* alloc::vec::Vec<alloc::string::String> */
-) RETURNS ProximityClause /* pg_search::query::proximity::ProximityClause */
+) RETURNS pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'text_array_to_prox_clause_wrapper';
@@ -1983,7 +1983,7 @@ AS 'MODULE_PATHNAME', 'text_array_to_prox_clause_wrapper';
 CREATE CAST (
     TEXT[] /* alloc::vec::Vec<alloc::string::String> */
     AS
-    ProximityClause /* pg_search::query::proximity::ProximityClause */
+    pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     )
     WITH FUNCTION text_array_to_prox_clause AS IMPLICIT;
 /* </end connected objects> */
@@ -1992,7 +1992,7 @@ CREATE CAST (
 -- pg_search::api::builder_fns::proximity::text_to_prox_clause
 CREATE  FUNCTION "text_to_prox_clause"(
     "t" TEXT /* alloc::string::String */
-) RETURNS ProximityClause /* pg_search::query::proximity::ProximityClause */
+) RETURNS pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'text_to_prox_clause_wrapper';
@@ -2001,7 +2001,7 @@ AS 'MODULE_PATHNAME', 'text_to_prox_clause_wrapper';
 CREATE CAST (
     TEXT /* alloc::string::String */
     AS
-    ProximityClause /* pg_search::query::proximity::ProximityClause */
+    pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     )
     WITH FUNCTION text_to_prox_clause AS IMPLICIT;
 /* pg_search::api::builder_fns::proximity::pdb */
@@ -2010,10 +2010,10 @@ CREATE CAST (
 -- pg_search/src/api/builder_fns/proximity.rs:35
 -- pg_search::api::builder_fns::proximity::pdb::prox_clause
 CREATE  FUNCTION pdb."prox_clause"(
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
-) RETURNS ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
+) RETURNS pdb.ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'prox_clause_wrapper';
@@ -2022,10 +2022,10 @@ AS 'MODULE_PATHNAME', 'prox_clause_wrapper';
 -- pg_search/src/api/builder_fns/proximity.rs:48
 -- pg_search::api::builder_fns::proximity::pdb::prox_clause_in_order
 CREATE  FUNCTION pdb."prox_clause_in_order"(
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
-) RETURNS ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
+) RETURNS pdb.ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'prox_clause_in_order_wrapper';
@@ -2034,9 +2034,9 @@ AS 'MODULE_PATHNAME', 'prox_clause_in_order_wrapper';
 -- pg_search/src/api/builder_fns/proximity.rs:79
 -- pg_search::api::builder_fns::proximity::pdb::proximity_in_order
 CREATE  FUNCTION pdb."proximity_in_order"(
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
 ) RETURNS pdb.Query /* pg_search::query::pdb_query::pdb::Query */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
@@ -2046,8 +2046,8 @@ AS 'MODULE_PATHNAME', 'proximity_in_order_wrapper';
 -- pg_search/src/api/builder_fns/proximity.rs:30
 -- pg_search::api::builder_fns::proximity::pdb::prox_array
 CREATE  FUNCTION pdb."prox_array"(
-    "clauses" VARIADIC ProximityClause[] /* pgrx::datum::array::VariadicArray<pg_search::query::proximity::ProximityClause> */
-) RETURNS ProximityClause /* pg_search::query::proximity::ProximityClause */
+    "clauses" VARIADIC pdb.ProximityClause[] /* pgrx::datum::array::VariadicArray<pg_search::query::proximity::ProximityClause> */
+) RETURNS pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'prox_array_wrapper';
@@ -2058,7 +2058,7 @@ AS 'MODULE_PATHNAME', 'prox_array_wrapper';
 CREATE  FUNCTION pdb."prox_regex"(
     "regex" TEXT, /* alloc::string::String */
     "max_expansions" INT DEFAULT 50 /* i32 */
-) RETURNS ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
+) RETURNS pdb.ProximityClause /* core::result::Result<pg_search::query::proximity::ProximityClause, anyhow::Error> */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'prox_regex_wrapper';
@@ -2068,7 +2068,7 @@ AS 'MODULE_PATHNAME', 'prox_regex_wrapper';
 -- pg_search::api::builder_fns::proximity::pdb::prox_term
 CREATE  FUNCTION pdb."prox_term"(
     "term" TEXT /* alloc::string::String */
-) RETURNS ProximityClause /* pg_search::query::proximity::ProximityClause */
+) RETURNS pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'prox_term_wrapper';
@@ -2077,9 +2077,9 @@ AS 'MODULE_PATHNAME', 'prox_term_wrapper';
 -- pg_search/src/api/builder_fns/proximity.rs:62
 -- pg_search::api::builder_fns::proximity::pdb::proximity
 CREATE  FUNCTION pdb."proximity"(
-    "left" ProximityClause, /* pg_search::query::proximity::ProximityClause */
+    "left" pdb.ProximityClause, /* pg_search::query::proximity::ProximityClause */
     "distance" INT, /* i32 */
-    "right" ProximityClause /* pg_search::query::proximity::ProximityClause */
+    "right" pdb.ProximityClause /* pg_search::query::proximity::ProximityClause */
 ) RETURNS pdb.Query /* pg_search::query::pdb_query::pdb::Query */
     IMMUTABLE STRICT PARALLEL SAFE
     LANGUAGE c /* Rust */
--- pg_search/src/query/proximity/mod.rs
+++ pg_search/src/query/proximity/mod.rs
@@ -15,173 +15,181 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use crate::api::Regex;
-use pgrx::{InOutFuncs, PostgresType, StringInfo};
-use serde::{Deserialize, Serialize};
-use std::borrow::Cow;
-use std::ffi::CStr;
-use tantivy::schema::Field;
-use tantivy::SegmentReader;
-
 pub mod query;
 mod scorer;
 mod weight;
 
-#[derive(Debug, Clone, Eq, PartialEq, PostgresType, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-#[inoutfuncs]
-pub enum ProximityClause {
-    Term(String),
-    Regex {
-        pattern: Regex,
-        max_expansions: usize,
-    },
-    Clauses(Vec<ProximityClause>),
-    Proximity {
-        left: Box<ProximityClause>,
-        distance: ProximityDistance,
-        right: Box<ProximityClause>,
-    },
-}
+pub use pdb::*;
+#[pgrx::pg_schema]
+mod pdb {
+    use crate::api::Regex;
+    use pgrx::{InOutFuncs, PostgresType, StringInfo};
+    use serde::{Deserialize, Serialize};
+    use std::borrow::Cow;
+    use std::ffi::CStr;
+    use tantivy::schema::Field;
+    use tantivy::SegmentReader;
 
-impl InOutFuncs for ProximityClause {
-    fn input(input: &CStr) -> Self
-    where
-        Self: Sized,
-    {
-        if let Ok(from_json) = serde_json::from_slice::<ProximityClause>(input.to_bytes()) {
-            from_json
-        } else {
-            // assume it's just a string
-            ProximityClause::Term(
-                input
-                    .to_str()
-                    .expect("input should be valid UTF8")
-                    .to_string(),
-            )
-        }
+    #[derive(Debug, Clone, Eq, PartialEq, PostgresType, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    #[inoutfuncs]
+    pub enum ProximityClause {
+        Term(String),
+        Regex {
+            pattern: Regex,
+            max_expansions: usize,
+        },
+        Clauses(Vec<ProximityClause>),
+        Proximity {
+            left: Box<ProximityClause>,
+            distance: ProximityDistance,
+            right: Box<ProximityClause>,
+        },
     }
 
-    fn output(&self, buffer: &mut StringInfo) {
-        if let ProximityClause::Term(s) = self {
-            buffer.push_str(s);
-        } else {
-            serde_json::to_writer(buffer, self).unwrap();
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-pub enum WhichTerms {
-    Left,
-    Right,
-    All,
-}
-
-pub enum ProxTermStyle<'a> {
-    Term(Cow<'a, str>),
-    Regex(&'a Regex, usize),
-}
-
-impl<'a> ProxTermStyle<'a> {
-    pub fn as_str(&self) -> &str {
-        match self {
-            ProxTermStyle::Term(term) => term.as_ref(),
-            ProxTermStyle::Regex(regex, ..) => regex.as_str(),
-        }
-    }
-}
-
-impl ProximityClause {
-    pub fn is_empty(&self) -> bool {
-        match self {
-            ProximityClause::Term(_) => false,
-            ProximityClause::Regex { .. } => false,
-            ProximityClause::Clauses(clauses) => {
-                clauses.is_empty() || clauses.iter().all(|clause| clause.is_empty())
+    impl InOutFuncs for ProximityClause {
+        fn input(input: &CStr) -> Self
+        where
+            Self: Sized,
+        {
+            if let Ok(from_json) = serde_json::from_slice::<ProximityClause>(input.to_bytes()) {
+                from_json
+            } else {
+                // assume it's just a string
+                ProximityClause::Term(
+                    input
+                        .to_str()
+                        .expect("input should be valid UTF8")
+                        .to_string(),
+                )
             }
-            ProximityClause::Proximity { left, right, .. } => left.is_empty() || right.is_empty(),
+        }
+
+        fn output(&self, buffer: &mut StringInfo) {
+            if let ProximityClause::Term(s) = self {
+                buffer.push_str(s);
+            } else {
+                serde_json::to_writer(buffer, self).unwrap();
+            }
         }
     }
 
-    pub fn terms<'a>(
-        &'a self,
-        field: Field,
-        segment_reader: Option<&'a SegmentReader>,
-        which_terms: WhichTerms,
-    ) -> tantivy::Result<impl Iterator<Item = ProxTermStyle<'a>>> {
-        let iter: Box<dyn Iterator<Item = ProxTermStyle>> = match self {
-            ProximityClause::Term(term) => Box::new(std::iter::once(ProxTermStyle::Term(
-                Cow::Borrowed(term.as_str()),
-            ))),
-            ProximityClause::Regex {
-                pattern,
-                max_expansions,
-            } if segment_reader.is_none() => Box::new(std::iter::once(ProxTermStyle::Regex(
-                pattern,
-                *max_expansions,
-            ))),
-            ProximityClause::Regex { pattern, .. } => {
-                let segment_reader = segment_reader.unwrap();
-                let regex =
-                    tantivy_fst::Regex::new(pattern.as_str()).unwrap_or_else(|e| panic!("{e}"));
-                let inverted_index = segment_reader.inverted_index(field)?;
-                let dict = inverted_index.terms();
-                let mut term_stream = dict.search_with_state(regex).into_stream()?;
+    #[derive(Copy, Clone)]
+    pub enum WhichTerms {
+        Left,
+        Right,
+        All,
+    }
 
-                let mut terms = Vec::new();
-                while let Some((bytes, ..)) = term_stream.next() {
-                    terms.push(ProxTermStyle::Term(Cow::Owned(
-                        String::from_utf8_lossy(bytes).to_string(),
-                    )));
+    pub enum ProxTermStyle<'a> {
+        Term(Cow<'a, str>),
+        Regex(&'a Regex, usize),
+    }
+
+    impl<'a> ProxTermStyle<'a> {
+        pub fn as_str(&self) -> &str {
+            match self {
+                ProxTermStyle::Term(term) => term.as_ref(),
+                ProxTermStyle::Regex(regex, ..) => regex.as_str(),
+            }
+        }
+    }
+
+    impl ProximityClause {
+        pub fn is_empty(&self) -> bool {
+            match self {
+                ProximityClause::Term(_) => false,
+                ProximityClause::Regex { .. } => false,
+                ProximityClause::Clauses(clauses) => {
+                    clauses.is_empty() || clauses.iter().all(|clause| clause.is_empty())
                 }
-                Box::new(terms.into_iter())
+                ProximityClause::Proximity { left, right, .. } => {
+                    left.is_empty() || right.is_empty()
+                }
             }
-            ProximityClause::Clauses(clauses) => {
-                let iter = clauses
-                    .iter()
-                    .map(move |clause| clause.terms(field, segment_reader, which_terms))
-                    .collect::<tantivy::Result<Vec<_>>>()?;
+        }
 
-                Box::new(iter.into_iter().flatten())
-            }
-            ProximityClause::Proximity { left, right, .. } => match which_terms {
-                WhichTerms::Left => Box::new(left.terms(field, segment_reader, which_terms)?),
-                WhichTerms::Right => Box::new(right.terms(field, segment_reader, which_terms)?),
-                WhichTerms::All => Box::new(
-                    left.terms(field, segment_reader, which_terms)?
-                        .chain(right.terms(field, segment_reader, which_terms)?),
-                ),
-            },
-        };
-        Ok(iter)
-    }
-}
+        pub fn terms<'a>(
+            &'a self,
+            field: Field,
+            segment_reader: Option<&'a SegmentReader>,
+            which_terms: WhichTerms,
+        ) -> tantivy::Result<impl Iterator<Item = ProxTermStyle<'a>>> {
+            let iter: Box<dyn Iterator<Item = ProxTermStyle>> = match self {
+                ProximityClause::Term(term) => Box::new(std::iter::once(ProxTermStyle::Term(
+                    Cow::Borrowed(term.as_str()),
+                ))),
+                ProximityClause::Regex {
+                    pattern,
+                    max_expansions,
+                } if segment_reader.is_none() => Box::new(std::iter::once(ProxTermStyle::Regex(
+                    pattern,
+                    *max_expansions,
+                ))),
+                ProximityClause::Regex { pattern, .. } => {
+                    let segment_reader = segment_reader.unwrap();
+                    let regex =
+                        tantivy_fst::Regex::new(pattern.as_str()).unwrap_or_else(|e| panic!("{e}"));
+                    let inverted_index = segment_reader.inverted_index(field)?;
+                    let dict = inverted_index.terms();
+                    let mut term_stream = dict.search_with_state(regex).into_stream()?;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum ProximityDistance {
-    InOrder(u32),
-    AnyOrder(u32),
-}
+                    let mut terms = Vec::new();
+                    while let Some((bytes, ..)) = term_stream.next() {
+                        terms.push(ProxTermStyle::Term(Cow::Owned(
+                            String::from_utf8_lossy(bytes).to_string(),
+                        )));
+                    }
+                    Box::new(terms.into_iter())
+                }
+                ProximityClause::Clauses(clauses) => {
+                    let iter = clauses
+                        .iter()
+                        .map(move |clause| clause.terms(field, segment_reader, which_terms))
+                        .collect::<tantivy::Result<Vec<_>>>()?;
 
-impl ProximityDistance {
-    #[inline(always)]
-    pub fn diff(&self, l: u32, r: u32) -> u32 {
-        match self {
-            ProximityDistance::InOrder(_) => r.wrapping_sub(l),
-            ProximityDistance::AnyOrder(_) => r.abs_diff(l),
+                    Box::new(iter.into_iter().flatten())
+                }
+                ProximityClause::Proximity { left, right, .. } => match which_terms {
+                    WhichTerms::Left => Box::new(left.terms(field, segment_reader, which_terms)?),
+                    WhichTerms::Right => {
+                        Box::new(right.terms(field, segment_reader, which_terms)?)
+                    }
+                    WhichTerms::All => Box::new(
+                        left.terms(field, segment_reader, which_terms)?
+                            .chain(right.terms(field, segment_reader, which_terms)?),
+                    ),
+                },
+            };
+            Ok(iter)
         }
     }
 
-    pub fn distance(&self) -> u32 {
-        match self {
-            ProximityDistance::InOrder(distance) => *distance,
-            ProximityDistance::AnyOrder(distance) => *distance,
-        }
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub enum ProximityDistance {
+        InOrder(u32),
+        AnyOrder(u32),
     }
 
-    pub fn in_order(&self) -> bool {
-        matches!(self, ProximityDistance::InOrder(_))
+    impl ProximityDistance {
+        #[inline(always)]
+        pub fn diff(&self, l: u32, r: u32) -> u32 {
+            match self {
+                ProximityDistance::InOrder(_) => r.wrapping_sub(l),
+                ProximityDistance::AnyOrder(_) => r.abs_diff(l),
+            }
+        }
+
+        pub fn distance(&self) -> u32 {
+            match self {
+                ProximityDistance::InOrder(distance) => *distance,
+                ProximityDistance::AnyOrder(distance) => *distance,
+            }
+        }
+
+        pub fn in_order(&self) -> bool {
+            matches!(self, ProximityDistance::InOrder(_))
+        }
     }
 }

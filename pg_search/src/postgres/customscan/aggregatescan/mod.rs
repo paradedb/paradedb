de0dcfa05f9e3e756445ad18ad319de148bdd673
Moe <mdashti@gmail.com>
2025-07-28 11:21:39 -0700

    feat: improved `bool` type support (#2907)
    # Ticket(s) Closed
    
    - Related to #2885
    
    ## What
    
    ## Why
    
    ## How
    
    ## Tests

--- pg_search/src/postgres/customscan/aggregatescan/mod.rs
+++ pg_search/src/postgres/customscan/aggregatescan/mod.rs
@@ -45,6 +45,7 @@ use crate::postgres::customscan::{
 };
 use crate::postgres::rel::PgSearchRelation;
 use crate::postgres::rel_get_bm25_index;
+use crate::postgres::types::TantivyValue;
 use crate::postgres::var::find_var_relation;
 use crate::query::SearchQueryInput;
 use crate::schema::SearchIndexSchema;
@@ -322,9 +323,10 @@ impl CustomScan for AggregateScan {
                         let attr = tupdesc.get(i).expect("missing attribute");
                         let typoid = attr.type_oid().value();
 
-                        // Convert the TantivyValue directly to the appropriate type
+                        // Convert the OwnedValue to TantivyValue and then to datum
                         let oid = pgrx::PgOid::from(typoid);
-                        match group_val.clone().try_into_datum(oid) {
+                        let tantivy_value = TantivyValue(group_val.clone());
+                        match tantivy_value.try_into_datum(oid) {
                             Ok(Some(datum)) => {
                                 datums[i] = datum;
                             }

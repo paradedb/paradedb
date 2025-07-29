ae5a76eb25bd042247c213c286da446a06e08700
Mohammad Dashti <mdashti@gmail.com>
2025-07-29 15:25:06 -0700

    No need for ORDER BY in benchmarks (for equivalent queries).

--- benchmarks/datasets/logs/queries/pg_search/cardinality.sql
+++ benchmarks/datasets/logs/queries/pg_search/cardinality.sql
@@ -11,4 +11,4 @@ SELECT * FROM paradedb.aggregate(index=>'benchmark_logs_idx', query=>paradedb.te
 SELECT * FROM paradedb.aggregate(index=>'benchmark_logs_idx', query=>paradedb.term('message', 'research'), agg=>'{"buckets": { "terms": { "field": "severity" }}}', solve_mvcc=>false);
 
 -- aggregate custom scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT COUNT(*) FROM (SELECT severity FROM benchmark_logs WHERE message @@@ 'research' GROUP BY severity ORDER BY severity);
+SET paradedb.enable_aggregate_custom_scan TO on; SELECT COUNT(*) FROM (SELECT severity FROM benchmark_logs WHERE message @@@ 'research' GROUP BY severity);

664270ae21fc58e6628995cb1eec0df20847d701
github-action-benchmark <github@users.noreply.github.com>
2025-08-15 20:45:19 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for 3f8baf8a4104344bb0f823c01036bc698926415a

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755290075037,
+  "lastUpdate": 1755290719337,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -17478,6 +17478,40 @@ window.BENCHMARK_DATA = {
             "value": 123.15411285013738,
             "unit": "median tps",
             "extra": "avg tps: 122.19416562564916, max tps: 124.51427041655818, count: 57699"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "3f8baf8a4104344bb0f823c01036bc698926415a",
+          "message": "fix: bugs with 0.17.x exposed by the custom FSM (#2971)\n\nThis PR starts off fixing three distinct bugs that we've had for awhile\nbut the new custom FSM brought to light:\n\n- failed to properly identify an orphaned \".deletes\" files, which could\nlead to adding the same block to the FSM multiple times\n- `BufferMut.return_to_fsm()` needs to unlock/release the buffer before\ngiving the block number to the FSM\n- `AtomicGuard::commit()` was returning a block to the FSM prematurely\n\nWe also retool the FSM so that it will only return blocks when they're\nknown to be all visible by all concurrent transactions. Here on\ncommunity that is actually \"immediately\" but is more sophsicated on our\n-enterprise product.\n\nThis also allows us to completely remove the concept of the\n\"segment_meta_garbage\" list. Now that the FSM ensures blocks don't get\nreused until they're allowed, we don't need a separate way of tracking\nthat.\n\nThe FSM is also largely rewritten to be centered around a \"draining\niterator\".\n\nAnother minor change is that if we forget to call\n`AtomicGuard::commit()` its Drop impl now panics\n\n---\n\nAnd one follow-up change is porting a fix we apparently only made to\n-enterprise related to parallel build workers not being able to get the\ncurrent transaction id.\n\nThat's necessary here too.",
+          "timestamp": "2025-08-15T19:51:00Z",
+          "url": "https://github.com/paradedb/paradedb/commit/3f8baf8a4104344bb0f823c01036bc698926415a"
+        },
+        "date": 1755290717856,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 28.48685834089334,
+            "unit": "median tps",
+            "extra": "avg tps: 28.380708019312642, max tps: 28.724920050234356, count: 57328"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 144.99201017585767,
+            "unit": "median tps",
+            "extra": "avg tps: 143.97558271214976, max tps: 147.37549990603725, count: 57328"
           }
         ]
       }

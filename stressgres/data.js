2c65cc8f6e76514236998c5f6ddc21445b636200
github-action-benchmark <github@users.noreply.github.com>
2025-08-20 02:10:20 +0000

    add pg_search background-merge.toml Performance - TPS (customBiggerIsBetter) benchmark result for ccb5dac7a93c1f752202997d2b075820920dc946

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755655190114,
+  "lastUpdate": 1755655819982,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -24888,6 +24888,60 @@ window.BENCHMARK_DATA = {
             "value": 16.128005921603986,
             "unit": "median tps",
             "extra": "avg tps: 16.11556146760594, max tps: 19.549479125633123, count: 55644"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ming.ying.nyc@gmail.com",
+            "name": "Ming",
+            "username": "rebasedming"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ccb5dac7a93c1f752202997d2b075820920dc946",
+          "message": "fix: parallel vacuum process now runs correctly (#2987)\n\n# Ticket(s) Closed\n\n- Closes #2986\n\n## What\n\nThis fixes issue #2986. The primary problem here is that parallel vacuum\nprocesses can't create a transaction id via\n`pg_sys::GetCurrentTransactionId()` so we instead teach the FSM to use\n`pg_sys::GetCurrentTransactionIdIfAny()` and in the case of a parallel\nvacuum process we'll fall back to the\n`pg_sys::FirstNormalTransactionId`.\n\nA subtle difference is here that parallel vacuum processes won't be able\nto reuse blocks from the FSM in the case where `ambulkdelete()` adds a\n\".deletes\" file to a segment. This is better than not being able to\nvacuum at all.\n\nThanks to @rebasedming for getting a test case together.\n\nAdditionally, that test case uncovered a race condition with the\nbackground merge process because it turns out it didn't hold a relation\nlock on the index at all. Now it holds a `pg_sys::AccessShareLock`,\nwhich is sufficient for the work it's doing and to prevent concurrent\nDDL like `DROP TABLE` from changing things out from under it.\n\n## Why\n\nFixing bugs.\n\n## How\n\n## Tests\n\nA new regression test has been added and some local stressgres tests\nlook good.\n\n---------\n\nCo-authored-by: Eric B. Ridge <eebbrr@paradedb.com>",
+          "timestamp": "2025-08-19T21:22:11-04:00",
+          "tree_id": "3522d056ac1edafd6d2478a22b2e019d03595c59",
+          "url": "https://github.com/paradedb/paradedb/commit/ccb5dac7a93c1f752202997d2b075820920dc946"
+        },
+        "date": 1755655818447,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom scan - Primary - tps",
+            "value": 36.07986190767639,
+            "unit": "median tps",
+            "extra": "avg tps: 36.30890103533418, max tps: 37.786937097411055, count: 55648"
+          },
+          {
+            "name": "Delete value - Primary - tps",
+            "value": 257.7888407651141,
+            "unit": "median tps",
+            "extra": "avg tps: 297.7632209402222, max tps: 2590.119087358273, count: 55648"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 122.63419714151932,
+            "unit": "median tps",
+            "extra": "avg tps: 122.03934639658402, max tps: 123.08148613059839, count: 55648"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 68.30277636500082,
+            "unit": "median tps",
+            "extra": "avg tps: 64.42876621545567, max tps: 101.26374044657572, count: 111296"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 16.388674128172735,
+            "unit": "median tps",
+            "extra": "avg tps: 16.512087107614622, max tps: 18.801462069375777, count: 55648"
           }
         ]
       }

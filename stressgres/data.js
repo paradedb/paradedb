f798294f08b4cc276ead10457384afb3c74988a7
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 20:50:29 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for 4b08446356edfe8faed3798c0521304b8789d3e5

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757450359341,
+  "lastUpdate": 1757451029066,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -2976,6 +2976,40 @@ window.BENCHMARK_DATA = {
             "value": 375.5254253247341,
             "unit": "median tps",
             "extra": "avg tps: 373.26010831136927, max tps: 388.0165557199958, count: 57655"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Philippe Noël",
+            "username": "philippemnoel",
+            "email": "21990816+philippemnoel@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4b08446356edfe8faed3798c0521304b8789d3e5",
+          "message": "fix: make IndexLayerSizes calculations more accurate (#3040)\n\n## What\n\nThe `From<&PgSearchRelation> for IndexLayerSizes` `from()` ctor was\nimproperly calculating both the total `index_byte_size` and the\n`target_segment_byte_size`.\n\nFor the former, it needs to only consider SegmentMetaEntries that are\nactually visible. In almost every case, at least after a CREATE INDEX of\na large index, the index could actually have 2x+ total segments as there\nare actually visible segments.\n\nSecondly, we need to adjust the `target_segment_byte_size` down by 1/3\nto account for how `LayeredMergePolicy` does the same thing. Failure to\ndo this can cause the background worker to merge segments together that\ndon't actually need to be.\n\n## Why\n\nThese two miscalculations are what contributed to the recent query\nbenchmark regressions we saw after #3031 was merged.\n\n## How\n\n## Tests\n\nCo-authored-by: Eric Ridge <eebbrr@gmail.com>",
+          "timestamp": "2025-08-24T17:46:13Z",
+          "url": "https://github.com/paradedb/paradedb/commit/4b08446356edfe8faed3798c0521304b8789d3e5"
+        },
+        "date": 1757451027538,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 30.35858072536879,
+            "unit": "median tps",
+            "extra": "avg tps: 30.14262953371806, max tps: 30.56367442416706, count: 57738"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 154.91712976755116,
+            "unit": "median tps",
+            "extra": "avg tps: 154.66043276460658, max tps: 157.41341693390456, count: 57738"
           }
         ]
       }

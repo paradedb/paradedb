fa862e6aa09bdbd944df0927587d1019fc4e17e0
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 20:09:19 +0000

    add pg_search bulk-updates.toml Performance - TPS (customBiggerIsBetter) benchmark result for 0544c54d64a963065cefc3a922582cc501a4c90e

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757448553388,
+  "lastUpdate": 1757448559175,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -1280,6 +1280,40 @@ window.BENCHMARK_DATA = {
             "value": 5.830694573507872,
             "unit": "median tps",
             "extra": "avg tps: 5.224625625042285, max tps: 6.576885478904082, count: 57753"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0544c54d64a963065cefc3a922582cc501a4c90e",
+          "message": "fix: zero worker threads (#2959) (#3139)\n\nWe don't use any of Tantivy's threading features, and as of\nhttps://github.com/paradedb/tantivy/pull/59 it's now possible to set the\nnumber of merge and worker threads to zero.\n\nDoing so saves overhead of making threads that we never use, and joining\non them, for every segment merge operation.\n\n\n🍒 This is a cherry pick of 98d7dcdc33169d31d80e13ef39aa7242e1a09710 from\n`main/0.18.x`",
+          "timestamp": "2025-09-09T18:06:14Z",
+          "url": "https://github.com/paradedb/paradedb/commit/0544c54d64a963065cefc3a922582cc501a4c90e"
+        },
+        "date": 1757448557645,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 8.38988845607191,
+            "unit": "median tps",
+            "extra": "avg tps: 7.172048072172516, max tps: 11.125643270347044, count: 57677"
+          },
+          {
+            "name": "Count Query - Primary - tps",
+            "value": 5.3205133159313505,
+            "unit": "median tps",
+            "extra": "avg tps: 4.806557445337188, max tps: 5.930422337847166, count: 57677"
           }
         ]
       }

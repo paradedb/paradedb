7063099813809c299cf57c1e02fe276fd77d1b55
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 19:57:38 +0000

    add pg_search single-server.toml Performance - TPS (customBiggerIsBetter) benchmark result for 1c48d3c6427e9fe7bdb10861a16e04d0cf1a1f48

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757446938986,
+  "lastUpdate": 1757447858049,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -148,6 +148,72 @@ window.BENCHMARK_DATA = {
             "value": 200.5234375,
             "unit": "median mem",
             "extra": "avg mem: 197.4519551783023, max mem: 231.1328125, count: 55741"
+          }
+        ]
+      }
+    ],
+    "pg_search single-server.toml Performance - TPS": [
+      {
+        "commit": {
+          "author": {
+            "name": "Eric Ridge",
+            "username": "eeeebbbbrrrr",
+            "email": "eebbrr@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "1c48d3c6427e9fe7bdb10861a16e04d0cf1a1f48",
+          "message": "ci: fix benchmark links in slack messages (#2875)",
+          "timestamp": "2025-07-17T12:13:25Z",
+          "url": "https://github.com/paradedb/paradedb/commit/1c48d3c6427e9fe7bdb10861a16e04d0cf1a1f48"
+        },
+        "date": 1757447856470,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 1259.5320384725492,
+            "unit": "median tps",
+            "extra": "avg tps: 1247.5969043336243, max tps: 1263.4199707943944, count: 55039"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 2586.096064044068,
+            "unit": "median tps",
+            "extra": "avg tps: 2593.38679646088, max tps: 2641.4324289534175, count: 55039"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 1168.8658852724543,
+            "unit": "median tps",
+            "extra": "avg tps: 1168.454479301618, max tps: 1209.8411521199764, count: 55039"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 983.8142784805156,
+            "unit": "median tps",
+            "extra": "avg tps: 980.9535466043926, max tps: 995.1930174342793, count: 55039"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 157.70939915678613,
+            "unit": "median tps",
+            "extra": "avg tps: 157.52784547634755, max tps: 165.27334275816813, count: 110078"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 172.17911064606312,
+            "unit": "median tps",
+            "extra": "avg tps: 170.1576082088954, max tps: 173.21682646991584, count: 55039"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 27.577039448947644,
+            "unit": "median tps",
+            "extra": "avg tps: 33.67427968993795, max tps: 775.373516807384, count: 55039"
           }
         ]
       }

9fca76f1fcbcb1d8217f638987963d2990db4adf
github-action-benchmark <github@users.noreply.github.com>
2025-09-09 19:57:53 +0000

    add pg_search single-server.toml Performance - TPS (customBiggerIsBetter) benchmark result for 4b08446356edfe8faed3798c0521304b8789d3e5

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757447870875,
+  "lastUpdate": 1757447873326,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -342,6 +342,70 @@ window.BENCHMARK_DATA = {
             "value": 116.40060577585945,
             "unit": "median tps",
             "extra": "avg tps: 124.73597312560425, max tps: 836.0686713363888, count: 55174"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Philippe Noël",
+            "username": "philippemnoel",
+            "email": "21990816+philippemnoel@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4b08446356edfe8faed3798c0521304b8789d3e5",
+          "message": "fix: make IndexLayerSizes calculations more accurate (#3040)\n\n## What\n\nThe `From<&PgSearchRelation> for IndexLayerSizes` `from()` ctor was\nimproperly calculating both the total `index_byte_size` and the\n`target_segment_byte_size`.\n\nFor the former, it needs to only consider SegmentMetaEntries that are\nactually visible. In almost every case, at least after a CREATE INDEX of\na large index, the index could actually have 2x+ total segments as there\nare actually visible segments.\n\nSecondly, we need to adjust the `target_segment_byte_size` down by 1/3\nto account for how `LayeredMergePolicy` does the same thing. Failure to\ndo this can cause the background worker to merge segments together that\ndon't actually need to be.\n\n## Why\n\nThese two miscalculations are what contributed to the recent query\nbenchmark regressions we saw after #3031 was merged.\n\n## How\n\n## Tests\n\nCo-authored-by: Eric Ridge <eebbrr@gmail.com>",
+          "timestamp": "2025-08-24T17:46:13Z",
+          "url": "https://github.com/paradedb/paradedb/commit/4b08446356edfe8faed3798c0521304b8789d3e5"
+        },
+        "date": 1757447862254,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 1185.6494261280939,
+            "unit": "median tps",
+            "extra": "avg tps: 1183.9243876960215, max tps: 1206.6087860385494, count: 55127"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 2812.1825349610904,
+            "unit": "median tps",
+            "extra": "avg tps: 2784.0981197118576, max tps: 2884.5545710750007, count: 55127"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 1184.210346139706,
+            "unit": "median tps",
+            "extra": "avg tps: 1183.4388010876341, max tps: 1205.3021870368218, count: 55127"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 996.3600115439618,
+            "unit": "median tps",
+            "extra": "avg tps: 987.3605297187564, max tps: 1000.1905489843774, count: 55127"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 172.14953885805912,
+            "unit": "median tps",
+            "extra": "avg tps: 174.98282719293996, max tps: 187.42583013987147, count: 110254"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 152.9723659432332,
+            "unit": "median tps",
+            "extra": "avg tps: 152.51985906052138, max tps: 156.86970706263682, count: 55127"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 44.671402617838226,
+            "unit": "median tps",
+            "extra": "avg tps: 46.87365147697865, max tps: 836.6653531020623, count: 55127"
           }
         ]
       }

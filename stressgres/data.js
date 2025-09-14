99a336119e07dd7b0a7f484dbd6f8eac1c1cc4a2
github-action-benchmark <github@users.noreply.github.com>
2025-09-14 22:35:31 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for 1cfaa7b311ca8b7ee91491411c8dfecd2ce5619c

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757888654657,
+  "lastUpdate": 1757889331521,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -4770,6 +4770,42 @@ window.BENCHMARK_DATA = {
             "value": 390.97428051202667,
             "unit": "median tps",
             "extra": "avg tps: 390.9001937610643, max tps: 423.327598604861, count: 57400"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ming.ying.nyc@gmail.com",
+            "name": "Ming",
+            "username": "rebasedming"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1cfaa7b311ca8b7ee91491411c8dfecd2ce5619c",
+          "message": "fix: `GROUP BY` doesn't panic when Postgres eliminates group pathkeys (#3152)\n\n# Ticket(s) Closed\n\n- Closes #3050 \n\n## What\n\nIt's possible for Postgres to eliminate group pathkeys if it realizes\nthat one of the pathkeys is unique, making the other ones unnecessary.\n\nWe need to handle this case/not panic.\n\n## Why\n\nSee issue.\n\n## How\n\nInject the dropped group pathkeys back into our list of grouping\ncolumns.\n\n## Tests\n\nAdded regression test",
+          "timestamp": "2025-09-14T17:56:19-04:00",
+          "tree_id": "a41824569d62cfd5dbe40884e6ead540d3b1bd88",
+          "url": "https://github.com/paradedb/paradedb/commit/1cfaa7b311ca8b7ee91491411c8dfecd2ce5619c"
+        },
+        "date": 1757889329985,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 34.8655053725746,
+            "unit": "median tps",
+            "extra": "avg tps: 34.791768462646964, max tps: 35.15468458924473, count: 57464"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 406.1184868155923,
+            "unit": "median tps",
+            "extra": "avg tps: 405.3093430096764, max tps: 441.4728913484981, count: 57464"
           }
         ]
       }

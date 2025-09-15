c6c248db932193fa439d44fd80fb742e08f590b0
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 21:06:34 +0000

    add pg_search wide-table.toml Performance - Other Metrics (customSmallerIsBetter) benchmark result for af5bea23effe976b411147e259e53afad947a393

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757970391588,
+  "lastUpdate": 1757970394102,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search background-merge.toml Performance - TPS": [
@@ -8258,6 +8258,66 @@ window.BENCHMARK_DATA = {
             "value": 163.09765625,
             "unit": "median mem",
             "extra": "avg mem: 152.91570495471456, max mem: 165.46484375, count: 57386"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "developers@paradedb.com",
+            "name": "paradedb[bot]",
+            "username": "paradedb-bot"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af5bea23effe976b411147e259e53afad947a393",
+          "message": "perf: remove `ExactBuffer` in favor of a regular rust `BufWriter` (#3159)\n\n# Ticket(s) Closed\n\n- Closes #2981  (/cc @yjhjstz)\n\nWhile #2981 wasn't the impetus for this, it addresses the complaint made\nthere just the same.\n\n## What\n\nIn profiling, our `ExactBuffer` was a large percentage of certain\nprofiles. This replaces it, and its complexity, with a standard Rust\n`BufWriter`.\n\n## Why\n\nImproves performance of (at least) our `wide-table.toml` test's \"Single\nUpdate\" job by quite a bit.\n\n<img width=\"720\" height=\"141\" alt=\"screenshot_2025-09-15_at_3 28\n33___pm_720\"\nsrc=\"https://github.com/user-attachments/assets/a373a7ae-df38-4691-980a-d6843f073d26\"\n/>\n\n\n## How\n\n## Tests\n\nExisting tests pass\n\nCo-authored-by: Eric Ridge <eebbrr@gmail.com>",
+          "timestamp": "2025-09-15T16:26:19-04:00",
+          "tree_id": "cbc00b9a93c129255360f60e5a70904e87f1e8c1",
+          "url": "https://github.com/paradedb/paradedb/commit/af5bea23effe976b411147e259e53afad947a393"
+        },
+        "date": 1757970392551,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 18.640776,
+            "unit": "median cpu",
+            "extra": "avg cpu: 19.676743279234536, max cpu: 51.014496, count: 57694"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 164.9140625,
+            "unit": "median mem",
+            "extra": "avg mem: 163.86741214966028, max mem: 168.125, count: 57694"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 22429,
+            "unit": "median block_count",
+            "extra": "avg block_count: 20239.711633792074, max block_count: 24713.0, count: 57694"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 49,
+            "unit": "median segment_count",
+            "extra": "avg segment_count: 54.10843415259819, max segment_count: 136.0, count: 57694"
+          },
+          {
+            "name": "Single Update - Primary - cpu",
+            "value": 4.6647234,
+            "unit": "median cpu",
+            "extra": "avg cpu: 5.46851229381257, max cpu: 28.09756, count: 57694"
+          },
+          {
+            "name": "Single Update - Primary - mem",
+            "value": 165.1171875,
+            "unit": "median mem",
+            "extra": "avg mem: 154.93150934239262, max mem: 165.8671875, count: 57694"
           }
         ]
       }

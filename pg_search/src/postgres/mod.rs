43d7a9424bb08565760a6a57c187bcd61eafeb54
Ming Ying <ming.ying.nyc@gmail.com>
2025-09-18 18:49:53 -0400

    remove debug

--- pg_search/src/postgres/mod.rs
+++ pg_search/src/postgres/mod.rs
@@ -486,13 +486,13 @@ pub struct ParallelExplainData {
     workers: BTreeMap<i32, ParallelExplainWorkerData>,
 }
 
-#[derive(Default, serde::Deserialize, serde::Serialize, Debug)]
+#[derive(Default, serde::Deserialize, serde::Serialize)]
 pub struct ParallelExplainWorkerData {
     query_count: Option<u16>,
     claimed_segments: Vec<ClaimedSegmentData>,
 }
 
-#[derive(Default, serde::Deserialize, serde::Serialize, Debug)]
+#[derive(Default, serde::Deserialize, serde::Serialize)]
 pub struct ClaimedSegmentData {
     id: String,
     deleted_docs: u32,

2c2711c15692b2b82830a2ea40a51fd38494ce02
github-action-benchmark <github@users.noreply.github.com>
2025-10-10 18:55:22 +0000

    add pg_search bulk-updates.toml Performance - Other Metrics (customSmallerIsBetter) benchmark result for b6f91a97a4055077ce0f6eacb06290cb8699379c

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760122519763,
+  "lastUpdate": 1760122522609,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -11752,6 +11752,66 @@ window.BENCHMARK_DATA = {
             "value": 71,
             "unit": "median segment_count",
             "extra": "avg segment_count: 72.958497783187, max segment_count: 108.0, count: 57515"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stuhood@paradedb.com",
+            "name": "Stu Hood",
+            "username": "stuhood"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b6f91a97a4055077ce0f6eacb06290cb8699379c",
+          "message": "perf: Introduce a mutable segment type (#3203)\n\n## What\n\nIntroduce an optional mutable segment type containing only a list of\nctids, which are then indexed at read time.\n\n## Why\n\nParadeDB excels at batch inserts, but for single-row INSERT/UPDATE\nstatements, we currently create single-document Tantivy segments.\n\nThese tiny segments have a few problems:\n\n- They consist of up to 8 \"files\" which are represented as\n`LinkedBytesList`s: the minimum overhead of each file is two Postgres\nblocks (one for the content, and one for a random-access index). Many of\nthose blocks will be mostly-empty for a single document segment.\n- They increase write amplification. For example: starting from a 1kb\nsegment means that it takes two merges/compactions to get to 100kb,\nrepresenting a 3x write amplification to write 100kb of data.\n- They require very rapid updates to the `SegmentMetaEntry`s list: all\ninserts insert a new entry, and frequent inserts mean more frequent\nmerges which also mutate that list.\n- Tantivy indexing is relatively fast, but is especially optimized to\noperate as a batch process: a single document index technically doesn't\nactually need many of the structures that it contains (there is no need\nfor a DocId because all storage could be boolean, etc).\n\n## How\n\nA mutable segment is read-time indexed when it is loaded (in `mvcc.rs`),\nand up to `mutable_segment_rows` rows are inserted into each mutable\nsegment. Mutable segments are disabled by default, but\n`mutable_segment_rows=1000` results in no change to Top-N performance on\nthe `wide-table` benchmark, and three times faster insert / update\nperformance.\n\n`SegmentMetaEntry` is bincoded to be stored in the `LinkedItemList` of\nentries: to introduce a second variant, we convert a field which became\n`SegmentMetaEntry::_unused` in version `0.15.18` into a tag. The`\n_unused` field [was previously used to store an xmin\nvalue](http://github.com/paradedb/paradedb/pull/2487). We treat previous\nlegal values of that tag as representing the legacy encoding.\n\nThe insert path defaults to allowing up to `mutable_segment_rows`\ninserts to be made into a mutable segment. If more than\n`mutable_segment_rows` inserts arrive in the same `aminsert`, we switch\nto creating immutable segments for the remainder of that `aminsert`.",
+          "timestamp": "2025-10-10T11:24:19-07:00",
+          "tree_id": "3f09dacf09a775b76929cda9047d4abf4c7c8e78",
+          "url": "https://github.com/paradedb/paradedb/commit/b6f91a97a4055077ce0f6eacb06290cb8699379c"
+        },
+        "date": 1760122520749,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 18.991098,
+            "unit": "median cpu",
+            "extra": "avg cpu: 19.925381734007665, max cpu: 42.857143, count: 57323"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 232.3203125,
+            "unit": "median mem",
+            "extra": "avg mem: 230.74547704182876, max mem: 234.6328125, count: 57323"
+          },
+          {
+            "name": "Count Query - Primary - cpu",
+            "value": 23.323614,
+            "unit": "median cpu",
+            "extra": "avg cpu: 22.428880108947563, max cpu: 33.23442, count: 57323"
+          },
+          {
+            "name": "Count Query - Primary - mem",
+            "value": 162.1015625,
+            "unit": "median mem",
+            "extra": "avg mem: 162.1511713911737, max mem: 163.57421875, count: 57323"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 24304,
+            "unit": "median block_count",
+            "extra": "avg block_count: 23218.265164070268, max block_count: 25870.0, count: 57323"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 71,
+            "unit": "median segment_count",
+            "extra": "avg segment_count: 72.3404392652164, max segment_count: 106.0, count: 57323"
           }
         ]
       }

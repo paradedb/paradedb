61de542dfe4e3514631bf6162ac384516ee23842
Ming Ying <ming.ying.nyc@gmail.com>
2025-10-11 13:54:09 -0400

    tests

--- pg_search/tests/pg_regress/sql/issue_3298.sql
+++ pg_search/tests/pg_regress/sql/issue_3298.sql
@@ -7,7 +7,7 @@ CALL paradedb.create_bm25_test_table(
   table_name => 'mock_items'
 );
 
-CREATE INDEX search_idx ON mock_items USING bm25 (id, description, rating, (category::pdb.exact), metadata) WITH (key_field='id');
+CREATE INDEX search_idx ON mock_items USING bm25 (id, description, rating, (category::pdb.literal), metadata) WITH (key_field='id');
 
 CREATE TABLE allowed_categories (
     category TEXT PRIMARY KEY
@@ -19,6 +19,27 @@ INSERT INTO allowed_categories (category) VALUES
 
 EXPLAIN SELECT COUNT(*) FROM mock_items WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 5));
 SELECT COUNT(*) FROM mock_items WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 5));
+
+EXPLAIN SELECT
+  COUNT(*) AS total,
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2))),
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2 OFFSET 2)))
+FROM mock_items;
+SELECT
+  COUNT(*) AS total,
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2))),
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2 OFFSET 2)))
+FROM mock_items;
+
+-- Make sure the results are correct
+SET paradedb.enable_aggregate_custom_scan TO off;
+
+SELECT COUNT(*) FROM mock_items WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 5));
+SELECT
+  COUNT(*) AS total,
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2))),
+  COUNT(*) FILTER (WHERE category @@@ paradedb.term_set(terms => ARRAY(SELECT paradedb.term('category', category) FROM allowed_categories LIMIT 2 OFFSET 2)))
+FROM mock_items;
 
 DROP TABLE allowed_categories;
 DROP TABLE mock_items;

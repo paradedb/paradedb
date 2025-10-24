06e9dd8ab625b9bfa247c34435f8ac29d6176300
Ming <ming.ying.nyc@gmail.com>
2025-10-23 23:52:46 -0400

    chore: Fix compilation errors against latest Tantivy `main` (#3403)
    # Ticket(s) Closed
    
    - Closes #
    
    ## What
    
    ## Why
    
    ## How
    
    ## Tests

--- pg_search/src/postgres/customscan/aggregatescan/mod.rs
+++ pg_search/src/postgres/customscan/aggregatescan/mod.rs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+pub mod filterquery;
 pub mod privdat;
 pub mod scan_state;
 

e550396307104bf5afec410d249df508d6a3885e
github-action-benchmark <github@users.noreply.github.com>
2025-10-27 16:14:37 +0000

    add pg_search bulk-updates.toml Performance - TPS (customBiggerIsBetter) benchmark result for 0475e6e577d5c62aec3e88a4eb250a617b3d62dd

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761580936464,
+  "lastUpdate": 1761581677792,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -21190,6 +21190,42 @@ window.BENCHMARK_DATA = {
             "value": 5.302374265402674,
             "unit": "median tps",
             "extra": "avg tps: 4.801094511687653, max tps: 5.884093552951306, count: 57763"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stuhood@paradedb.com",
+            "name": "Stu Hood",
+            "username": "stuhood"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0475e6e577d5c62aec3e88a4eb250a617b3d62dd",
+          "message": "perf: Optimize `TermSet` for very large sets of terms. (#3412) (#3419)\n\n## What\n\nFurther optimizes `pdb.term_set` and `paradedb.term_set`, and deprecates\nusing `pdb.term_set` in aggregate position.\n\n## Why\n\n#3351 optimized `term_set` queries for very large input term sets by\nswitching to using fast fields when more than `1024` terms were used in\nthe set. But there was more that could be done.\n\nFor a `paradedb.aggregate` query using a `pdb.term_set` constructed from\nan `array_agg` containing 10mm distinct inputs and 8 segments, this PR\nfurther optimizes the fast field execution path:\n\n| version | runtime |\n| ------- | -------- |\n| pre-#3351 - 0 workers | 35.851 s |\n| pre-#3351 - 8 workers | swapping - did not complete |\n| #3351 - 0 workers | 12.573 s |\n| #3351 - 8 workers | 13.708 s |\n| #3412 - 0 workers | 5.532 s |\n| #3412 - 8 workers | 8.538 s |\n\nBefore #3351, the posting-list based execution mode for term sets was\nnot able to complete on my machine with multiple workers, because it\nrequired enough memory to trigger swapping.\n\nCritical to note: in the case of a massive `pdb.term_set` like this,\nadditional workers might be a pessimization. That's because the cost of\npropagating and creating the query is expensive enough that it can dwarf\nthe actual aggregate time. For larger segment counts or larger\naggregates, the results might be different.\n\nAdditionally, this change deprecates using `pdb.term_set` in aggregate\nposition (as added in #3336): using `pdb.term_set` in function position\nwith an `array_agg` is equivalent, and sometimes slightly faster.\n\n## How\n\n* Incorporates https://github.com/paradedb/tantivy/pull/75\n* Removes some allocation from `pdb.term_set` and `paradedb.term_set`\ncreation.\n* Skips allocation `FieldName::path` arrays if they contain a single\ncomponent.",
+          "timestamp": "2025-10-27T08:45:43-07:00",
+          "tree_id": "93f61bee15d3984a53cdbf14ba422bb5a4e82ae1",
+          "url": "https://github.com/paradedb/paradedb/commit/0475e6e577d5c62aec3e88a4eb250a617b3d62dd"
+        },
+        "date": 1761581675654,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 8.367800925789494,
+            "unit": "median tps",
+            "extra": "avg tps: 7.159617955444664, max tps: 11.132203014670015, count: 57818"
+          },
+          {
+            "name": "Count Query - Primary - tps",
+            "value": 5.37419537811976,
+            "unit": "median tps",
+            "extra": "avg tps: 4.8538283965089075, max tps: 5.959376998297523, count: 57818"
           }
         ]
       }

6e38378d40c761f2123b82b31cd3b0b3b12d4a72
Mohammad Dashti <mdashti@gmail.com>
2025-10-29 10:28:35 -0700

    Fixed the upgrade script

--- pg_search/sql/pg_search--0.19.2--0.19.3.sql
+++ pg_search/sql/pg_search--0.19.2--0.19.3.sql
@@ -14,22 +14,11 @@ CREATE  FUNCTION pdb."snippets"(
 STABLE PARALLEL SAFE 
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'snippets_from_relation_wrapper';
-
-/* </end connected objects> */
-/* <begin connected objects> */
--- pg_search/src/api/aggregate.rs:92
--- pg_search::api::aggregate::agg_placeholder_agg_placeholder_state
-CREATE OR REPLACE FUNCTION paradedb."agg_placeholder_agg_placeholder_state"(
-	"this" internal, /* pgrx::datum::internal::Internal */
-	"arg_one" jsonb /* pgrx::datum::json::JsonB */
-) RETURNS internal /* pgrx::datum::internal::Internal */
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_state_wrapper';
 /* </end connected objects> */
 /* <begin connected objects> */
 -- pg_search/src/api/aggregate.rs:92
 -- pg_search::api::aggregate::agg_placeholder_agg_placeholder_finalize
-CREATE OR REPLACE FUNCTION paradedb."agg_placeholder_agg_placeholder_finalize"(
+CREATE  FUNCTION "agg_placeholder_agg_placeholder_finalize"(
 	"this" internal /* pgrx::datum::internal::Internal */
 ) RETURNS jsonb /* pgrx::datum::json::JsonB */
 LANGUAGE c /* Rust */
@@ -37,23 +26,32 @@ AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_finalize_wrapper';
 /* </end connected objects> */
 /* <begin connected objects> */
 -- pg_search/src/api/aggregate.rs:92
--- pg_search::api::aggregate::AggPlaceholder
-CREATE AGGREGATE paradedb.agg (
-	jsonb /* pgrx::datum::json::JsonB */
-)
-(
-	SFUNC = paradedb."agg_placeholder_agg_placeholder_state", /* pg_search::api::aggregate::AggPlaceholder::state */
-	STYPE = internal, /* pgrx::datum::internal::Internal */
-	FINALFUNC = paradedb."agg_placeholder_agg_placeholder_finalize" /* pg_search::api::aggregate::AggPlaceholder::final */
-);
-
+-- pg_search::api::aggregate::agg_placeholder_agg_placeholder_state
+CREATE  FUNCTION "agg_placeholder_agg_placeholder_state"(
+	"this" internal, /* pgrx::datum::internal::Internal */
+	"arg_one" jsonb /* pgrx::datum::json::JsonB */
+) RETURNS internal /* pgrx::datum::internal::Internal */
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_state_wrapper';
+/* </end connected objects> */
 /* <begin connected objects> */
 -- pg_search/src/api/window_aggregate.rs:52
 -- pg_search::api::window_aggregate::window_agg
-CREATE OR REPLACE FUNCTION paradedb."window_agg"(
+CREATE  FUNCTION "window_agg"(
 	"window_aggregate_json" TEXT /* &str */
 ) RETURNS bigint /* i64 */
 STRICT VOLATILE PARALLEL SAFE 
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'window_agg_placeholder_wrapper';
 /* </end connected objects> */
+/* <begin connected objects> */
+-- pg_search/src/api/aggregate.rs:92
+-- pg_search::api::aggregate::AggPlaceholder
+CREATE AGGREGATE agg (
+	jsonb /* pgrx::datum::json::JsonB */
+)
+(
+	SFUNC = "agg_placeholder_agg_placeholder_state", /* pg_search::api::aggregate::AggPlaceholder::state */
+	STYPE = internal, /* pgrx::datum::internal::Internal */
+	FINALFUNC = "agg_placeholder_agg_placeholder_finalize" /* pg_search::api::aggregate::AggPlaceholder::final */
+);

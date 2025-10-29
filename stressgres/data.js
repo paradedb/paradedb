1814f3154e1067dafb7fbe692e6e217144548feb
github-action-benchmark <github@users.noreply.github.com>
2025-10-29 03:43:20 +0000

    add pg_search bulk-updates.toml Performance - TPS (customBiggerIsBetter) benchmark result for 57a4340e4213e9d2a3c1dfdfb33baac3c37c87df

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761708649123,
+  "lastUpdate": 1761709400040,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -22966,6 +22966,42 @@ window.BENCHMARK_DATA = {
             "value": 5.332364893587305,
             "unit": "median tps",
             "extra": "avg tps: 4.820960064019299, max tps: 5.897134413600715, count: 57890"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "developers@paradedb.com",
+            "name": "paradedb[bot]",
+            "username": "paradedb-bot"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "57a4340e4213e9d2a3c1dfdfb33baac3c37c87df",
+          "message": "feat: Add `paradedb.snippets` to support rendering the top-n snippets for a document. (#3435)\n\n## What\n\nAdds the `paradedb.snippets` function, and deprecates passing an\n`offset` and `limit` to `paradedb.snippet`.\n\n## Why\n\nAs explained in the new docs, `paradedb.snippets` allows for rendering\nthe top-n snippets (by score or position), regardless of their distance\nfrom one another in the document.\n\n## How\n\nSee https://github.com/paradedb/tantivy/pull/76.\n\n---------\n\nCo-authored-by: Stu Hood <stuhood@paradedb.com>\nCo-authored-by: Stu Hood <stuhood@gmail.com>",
+          "timestamp": "2025-10-28T20:14:22-07:00",
+          "tree_id": "6305fc100e2ae240448bb5226fabdb6adcf7d00b",
+          "url": "https://github.com/paradedb/paradedb/commit/57a4340e4213e9d2a3c1dfdfb33baac3c37c87df"
+        },
+        "date": 1761709397985,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 8.497113355576236,
+            "unit": "median tps",
+            "extra": "avg tps: 7.233399504017963, max tps: 11.203938914124574, count: 57490"
+          },
+          {
+            "name": "Count Query - Primary - tps",
+            "value": 5.37078809541366,
+            "unit": "median tps",
+            "extra": "avg tps: 4.867684726134113, max tps: 5.947817677483281, count: 57490"
           }
         ]
       }

41fa8baacfe552e6b3f55e6b2028a9da991d1623
Mohammad Dashti <mdashti@gmail.com>
2025-10-30 15:54:03 -0700

    Renamed benchmark files.

--- benchmarks/datasets/logs/queries/pg_search/top_n-agg-bucket-string.sql
+++ benchmarks/datasets/logs/queries/pg_search/top_n-agg-bucket-string.sql
@@ -1,2 +1,1 @@
 SET paradedb.enable_aggregate_custom_scan TO on; SELECT id, message, country, severity, timestamp, paradedb.agg('{"terms": {"field": "country"}}'::jsonb) OVER () FROM benchmark_logs WHERE message @@@ 'research' ORDER BY timestamp DESC LIMIT 10;
-

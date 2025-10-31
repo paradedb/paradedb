646807506fa1ce4948703b2791898a9a757fe17b
Mohammad Dashti <mdashti@gmail.com>
2025-10-30 19:26:56 -0700

    removed GUC

--- benchmarks/datasets/logs/queries/pg_search/top_n-agg-avg.sql
+++ benchmarks/datasets/logs/queries/pg_search/top_n-agg-avg.sql
@@ -1,1 +1,1 @@
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT id, message, country, severity, timestamp, paradedb.agg('{"avg": {"field": "severity"}}'::jsonb) OVER () FROM benchmark_logs WHERE message @@@ 'research' ORDER BY timestamp DESC LIMIT 10;
+SELECT id, message, country, severity, timestamp, paradedb.agg('{"avg": {"field": "severity"}}'::jsonb) OVER () FROM benchmark_logs WHERE message @@@ 'research' ORDER BY timestamp DESC LIMIT 10;

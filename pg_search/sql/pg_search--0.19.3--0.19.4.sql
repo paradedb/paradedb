4a3067b885cc6398a925615898e59db033f1886f
Mohammad Dashti <mdashti@gmail.com>
2025-10-30 21:08:26 -0700

    updated schema diff.

--- pg_search/sql/pg_search--0.19.3--0.19.4.sql
+++ pg_search/sql/pg_search--0.19.3--0.19.4.sql
@@ -1,16 +1,8 @@
+/* pg_search::api::aggregate::pdb */
 /* </end connected objects> */
 /* <begin connected objects> */
--- pg_search/src/api/aggregate.rs:92
--- pg_search::api::aggregate::agg_placeholder_agg_placeholder_finalize
-CREATE  FUNCTION pdb."agg_placeholder_agg_placeholder_finalize"(
-	"this" internal /* pgrx::datum::internal::Internal */
-) RETURNS jsonb /* pgrx::datum::json::JsonB */
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_finalize_wrapper';
-/* </end connected objects> */
-/* <begin connected objects> */
--- pg_search/src/api/aggregate.rs:92
--- pg_search::api::aggregate::agg_placeholder_agg_placeholder_state
+-- pg_search/src/api/aggregate.rs:94
+-- pg_search::api::aggregate::pdb::agg_placeholder_agg_placeholder_state
 CREATE  FUNCTION pdb."agg_placeholder_agg_placeholder_state"(
 	"this" internal, /* pgrx::datum::internal::Internal */
 	"arg_one" jsonb /* pgrx::datum::json::JsonB */
@@ -19,23 +11,33 @@ LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_state_wrapper';
 /* </end connected objects> */
 /* <begin connected objects> */
--- pg_search/src/api/window_aggregate.rs:52
--- pg_search::api::window_aggregate::window_agg
+-- pg_search/src/api/aggregate.rs:94
+-- pg_search::api::aggregate::pdb::agg_placeholder_agg_placeholder_finalize
+CREATE  FUNCTION pdb."agg_placeholder_agg_placeholder_finalize"(
+	"this" internal /* pgrx::datum::internal::Internal */
+) RETURNS jsonb /* pgrx::datum::json::JsonB */
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'agg_placeholder_agg_placeholder_finalize_wrapper';
+/* </end connected objects> */
+/* <begin connected objects> */
+-- pg_search/src/api/aggregate.rs:94
+-- pg_search::api::aggregate::pdb::AggPlaceholder
+CREATE AGGREGATE pdb.agg (
+	jsonb /* pgrx::datum::json::JsonB */
+)
+(
+	SFUNC = pdb."agg_placeholder_agg_placeholder_state", /* pg_search::api::aggregate::pdb::AggPlaceholder::state */
+	STYPE = internal, /* pgrx::datum::internal::Internal */
+	FINALFUNC = pdb."agg_placeholder_agg_placeholder_finalize" /* pg_search::api::aggregate::pdb::AggPlaceholder::final */
+);
+/* pg_search::api::window_aggregate::pdb */
+/* </end connected objects> */
+/* <begin connected objects> */
+-- pg_search/src/api/window_aggregate.rs:57
+-- pg_search::api::window_aggregate::pdb::window_agg
 CREATE  FUNCTION pdb."window_agg"(
 	"window_aggregate_json" TEXT /* &str */
 ) RETURNS bigint /* i64 */
 STRICT VOLATILE PARALLEL SAFE 
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'window_agg_placeholder_wrapper';
-/* </end connected objects> */
-/* <begin connected objects> */
--- pg_search/src/api/aggregate.rs:92
--- pg_search::api::aggregate::AggPlaceholder
-CREATE AGGREGATE pdb.agg (
-	jsonb /* pgrx::datum::json::JsonB */
-)
-(
-	SFUNC = "agg_placeholder_agg_placeholder_state", /* pg_search::api::aggregate::AggPlaceholder::state */
-	STYPE = internal, /* pgrx::datum::internal::Internal */
-	FINALFUNC = "agg_placeholder_agg_placeholder_finalize" /* pg_search::api::aggregate::AggPlaceholder::final */
-);

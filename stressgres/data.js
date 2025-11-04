7dc2ff9c9b631b11df0277085da8d023772bb04e
github-action-benchmark <github@users.noreply.github.com>
2025-11-04 21:37:40 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for f5a68fe2205fa78de879c4d248f0070a7293e6bd

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762291656773,
+  "lastUpdate": 1762292260305,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -40252,6 +40252,54 @@ window.BENCHMARK_DATA = {
             "value": 5.984911361177262,
             "unit": "median tps",
             "extra": "avg tps: 5.993926570178633, max tps: 7.47240828186737, count: 56244"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ming.ying.nyc@gmail.com",
+            "name": "Ming",
+            "username": "rebasedming"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5a68fe2205fa78de879c4d248f0070a7293e6bd",
+          "message": "feat: Add a validator for tokenizer typmods (#3488)\n\n# Ticket(s) Closed\n\n- Closes #\n\n## What\n\nAdds validation for tokenizer typmod properties:\n\n- Checks for invalid configuration keys/values\n- Checks for missing required properties\n\nDepends on https://github.com/paradedb/tantivy/pull/79\n\n## Why\n\n## How\n\n## Tests\n\nSee regression tests.",
+          "timestamp": "2025-11-04T15:52:10-05:00",
+          "tree_id": "65bf87fd8513df68530d0fda4ae41516d239b328",
+          "url": "https://github.com/paradedb/paradedb/commit/f5a68fe2205fa78de879c4d248f0070a7293e6bd"
+        },
+        "date": 1762292257807,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 1140.59230633316,
+            "unit": "median tps",
+            "extra": "avg tps: 1131.4604564543176, max tps: 1200.915266423593, count: 56497"
+          },
+          {
+            "name": "Single Insert - Primary - tps",
+            "value": 1256.6694983415512,
+            "unit": "median tps",
+            "extra": "avg tps: 1219.9083254029797, max tps: 1273.1158429851607, count: 56497"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 1821.246780526178,
+            "unit": "median tps",
+            "extra": "avg tps: 1742.8319398125811, max tps: 2051.3162564733857, count: 56497"
+          },
+          {
+            "name": "Top N - Primary - tps",
+            "value": 6.164780276480171,
+            "unit": "median tps",
+            "extra": "avg tps: 6.115719993919406, max tps: 7.518347361391634, count: 56497"
           }
         ]
       }

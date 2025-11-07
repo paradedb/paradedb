d8c89fde506ca2ccd99ce59ad989a4e87a1f2ade
github-action-benchmark <github@users.noreply.github.com>
2025-11-07 04:41:19 +0000

    add pg_search logical-replication.toml Performance - TPS (customBiggerIsBetter) benchmark result for bad99101a1317fe3a20680105c3af6e8b38538a8

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762490156971,
+  "lastUpdate": 1762490479638,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -76308,6 +76308,54 @@ window.BENCHMARK_DATA = {
             "value": 113.22080658779689,
             "unit": "median tps",
             "extra": "avg tps: 112.55148974643566, max tps: 126.83021355645118, count: 107360"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ming.ying.nyc@gmail.com",
+            "name": "Ming",
+            "username": "rebasedming"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bad99101a1317fe3a20680105c3af6e8b38538a8",
+          "message": "fix: FSM freelist sparsity causes too many page reads/lock acquisitions (#3509)\n\n# Ticket(s) Closed\n\n- Closes #\n\n## What\n\nThe `extend_freelist` function in V2 FSM had a \"fast path splice\" that\ncould inject tons of tiny, partially-full blocks into the freelist. If\nit saw that a freelist block was full, it would blindly just assume the\nnext block was also full and inject a new block between the current and\nnext block.\n\nHowever, this fails if a lot of `extend_freelist` calls come in\nconcurrently -- each call would inject a partially-filled block in the\nmiddle of the list, leading to a very sparse freelist.\n\nThe solution is to peek at the next block in the freelist, and only do\nthe fast path if both blocks are full.\n\nAdditionally, we have seen situations in prod (likely related to the\nabove) where freelists contain entirely empty blocks. The existing\nfreelist isn't good at self-healing -- we have modified `drain` to now\nremove these empty blocks from the freelist.\n\nFinally, `drain`s call to `extend_freelist` has been moved to the end\nafter all blocks have been drained. This prevents multiple calls to\n`extend_freelist`, which is expensive (it can do tree mutations).\n\n## Why\n\n## How\n\n## Tests\n\nSee two added regression tests, both of which fail on `main`",
+          "timestamp": "2025-11-06T22:28:22-05:00",
+          "tree_id": "5c3da39a34818529df9f6c15ca1e0fa4e965c023",
+          "url": "https://github.com/paradedb/paradedb/commit/bad99101a1317fe3a20680105c3af6e8b38538a8"
+        },
+        "date": 1762490476790,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Subscriber - tps",
+            "value": 631.9054353932262,
+            "unit": "median tps",
+            "extra": "avg tps: 632.5637267136653, max tps: 876.3420996685152, count: 53691"
+          },
+          {
+            "name": "Index Only Scan - Subscriber - tps",
+            "value": 729.4200222814985,
+            "unit": "median tps",
+            "extra": "avg tps: 730.7090348813196, max tps: 1178.7335308653157, count: 53691"
+          },
+          {
+            "name": "Parallel Custom Scan - Subscriber - tps",
+            "value": 110.01950284138246,
+            "unit": "median tps",
+            "extra": "avg tps: 110.21241773055941, max tps: 120.53595059873238, count: 53691"
+          },
+          {
+            "name": "Top N - Subscriber - tps",
+            "value": 111.90837359821438,
+            "unit": "median tps",
+            "extra": "avg tps: 110.39892021649813, max tps: 124.87183012273897, count: 107382"
           }
         ]
       }

6a8afc555ec987df5818c71dfaa2a109d2bfb545
Kitsu <mail@kitsu.me>
2025-11-25 00:50:12 -0300

    fix: check for MIN/MAX dates supported by tantivy
    `tantivy::DateTime` internally represented as a i64 for nanoseconds and
    current behavior is to silently truncate the underlying value if
    it's out of range for MIN/MAX consts.

--- pg_search/tests/pg_regress/sql/issue_3544.sql
+++ pg_search/tests/pg_regress/sql/issue_3544.sql
@@ -1,8 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS pg_search;
 DROP TABLE IF EXISTS issue_3544_test;
 
-CREATE TABLE issue_3544_test (id SERIAL PRIMARY KEY, dates DATE);
-INSERT INTO issue_3544_test (dates) VALUES ('57439-03-01');
-CREATE INDEX ON issue_3544_test using bm25 (id, dates) WITH (key_field = 'id');
+CREATE TABLE issue_3544_test (id SERIAL PRIMARY KEY, dates DATE, info TEXT);
+
+INSERT INTO issue_3544_test (dates, info) VALUES ('57439-03-01', 'test');
+CREATE INDEX ON issue_3544_test USING bm25 (id, dates, info) WITH (key_field = 'id');
+DELETE FROM issue_3544_test;
+
+INSERT INTO issue_3544_test (dates, info) VALUES ('2020-01-01', 'test');
+CREATE INDEX ON issue_3544_test USING bm25 (id, dates, info) WITH (key_field = 'id');
+
+EXPLAIN SELECT count(*) FROM issue_3544_test WHERE info ||| 'xyz' AND dates <@ '[1677-09-22, 2262-04-10]'::daterange;
 
 DROP TABLE IF EXISTS issue_3544_test;

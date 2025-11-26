fc93e1731273aac7ccf48d6545dc781d1c84a76c
Kitsu <mail@kitsu.me>
2025-11-25 21:11:47 -0300

    test: use explain params for consistency

--- pg_search/tests/pg_regress/sql/issue_3544.sql
+++ pg_search/tests/pg_regress/sql/issue_3544.sql
@@ -10,6 +10,6 @@ DELETE FROM issue_3544_test;
 INSERT INTO issue_3544_test (dates, info) VALUES ('2020-01-01', 'test');
 CREATE INDEX ON issue_3544_test USING bm25 (id, dates, info) WITH (key_field = 'id');
 
-EXPLAIN SELECT count(*) FROM issue_3544_test WHERE info ||| 'xyz' AND dates <@ '[1677-09-22, 2262-04-10]'::daterange;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE) SELECT count(*) FROM issue_3544_test WHERE info ||| 'xyz' AND dates <@ '[1677-09-22, 2262-04-10]'::daterange;
 
 DROP TABLE IF EXISTS issue_3544_test;

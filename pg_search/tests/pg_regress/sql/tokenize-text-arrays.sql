8a5abab5597ab916dca64b1f883b42c633bdcb04
Ming Ying <ming.ying.nyc@gmail.com>
2025-12-01 16:24:47 -0500

    regression test

--- pg_search/tests/pg_regress/sql/tokenize-text-arrays.sql
+++ pg_search/tests/pg_regress/sql/tokenize-text-arrays.sql
@@ -24,3 +24,18 @@ SELECT * FROM index_text_array WHERE arr === 'blue';
 SELECT * FROM index_text_array WHERE arr === 'blue green';
 
 DROP TABLE index_text_array;
+
+CREATE TABLE index_varchar_array(
+    id serial8 not null primary key,
+    arr varchar[]
+);
+INSERT INTO index_varchar_array (arr) VALUES (ARRAY['red', 'blue', 'blue green']), (ARRAY['blue green']);
+CREATE INDEX idxindex_varchar_array ON index_varchar_array USING bm25 (id, (arr::pdb.literal)) WITH (key_field = 'id');
+
+SELECT * FROM paradedb.schema('idxindex_varchar_array') ORDER BY name;
+
+SELECT * FROM index_varchar_array WHERE arr === 'red';
+SELECT * FROM index_varchar_array WHERE arr === 'blue';
+SELECT * FROM index_varchar_array WHERE arr === 'blue green';
+
+DROP TABLE index_varchar_array;

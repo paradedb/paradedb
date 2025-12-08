9d1c076f99d703dafc1e4231a5bea8143e00b08f
github-action-benchmark <github@users.noreply.github.com>
2025-12-08 21:19:11 +0000

    add pg_search single-server.toml Performance - TPS (customBiggerIsBetter) benchmark result for 384e98944239f72eb0565bd709f410d6a803dc4c

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765227991901,
+  "lastUpdate": 1765228751243,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -3356,6 +3356,72 @@ window.BENCHMARK_DATA = {
             "value": 145.71366280027223,
             "unit": "median tps",
             "extra": "avg tps: 169.70356627098633, max tps: 656.7976255452242, count: 55298"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adria@prealfa.com",
+            "name": "Adria Lopez",
+            "username": "adlpz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "384e98944239f72eb0565bd709f410d6a803dc4c",
+          "message": "fix: Wrap the Jieba tokenizer to fix the token indices to be sequential (#3665)\n\n# Ticket(s) Closed\n\n- Closes #3664\n\n## What\n\nWrapped the upstream `tantivy-jieba` JiebaTokenizer in order to set the\ntoken positions to be sequential instead of set to the token start\noffset in the original text.\n\nThis makes it behave like the other tokenizers.\n\n## Why\n\nThe phrase search would return no matches for very obvious tests of\nverbatim token-aligned pieces of text when using the Jieba tokenizer.\nAfter some digging around, turns out the upstream `tantivy-jieba` sets\ntoken positions to character offsets instead of sequential ordinals,\nlike the rest of the tokenizers do in ParadeDB.\n\nAs far as I can tell, then doing a phrase query, tokens are assumed to\nbe consecutively indexed and that's what results in a match with no\nslop. For all other tokenizers this works fine as I see that they have\nthe `position` set with something like this:\n\n```rust\nself.token.position = self.token.position.wrapping_add(1);\n```\n\nBut in\nhttps://github.com/jiegec/tantivy-jieba/blob/master/src/lib.rs#L163 the\ntoken stream generated has `position` set to `token.start`. I guess this\nfits fine their use upstream, but fails here.\n\nThe fix was to just wrap `tantivy_jieba::JiebaTokenizer` to set the\npositions to be sequential during tokenization.\n\n## How\n\nWrapping JiebaTokenizer, intercepting the token stream and re-setting\nthe position to a simple incrementing integer.\n\n## Tests\n\nBuilt with the new code, tokenization returns the correct indices,\nsequential.\n\n---------\n\nCo-authored-by: Lei Li <1715734693@qq.com>\nCo-authored-by: Stu Hood <stuhood@gmail.com>",
+          "timestamp": "2025-12-08T13:02:22-08:00",
+          "tree_id": "2b345628b296d50b5ee2179687e12f2302657080",
+          "url": "https://github.com/paradedb/paradedb/commit/384e98944239f72eb0565bd709f410d6a803dc4c"
+        },
+        "date": 1765228748357,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 578.8505722039633,
+            "unit": "median tps",
+            "extra": "avg tps: 575.6503328410599, max tps: 705.7431495528882, count: 55238"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 3210.7370983753462,
+            "unit": "median tps",
+            "extra": "avg tps: 3138.5139230180907, max tps: 3226.1533159026794, count: 55238"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 573.2956059687604,
+            "unit": "median tps",
+            "extra": "avg tps: 569.9969536298277, max tps: 689.0077317691628, count: 55238"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 467.82786484903744,
+            "unit": "median tps",
+            "extra": "avg tps: 463.13491416423614, max tps: 485.9397261275876, count: 55238"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 3175.5905270240432,
+            "unit": "median tps",
+            "extra": "avg tps: 3126.606863645091, max tps: 3469.2804611136303, count: 110476"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 2157.4073947105844,
+            "unit": "median tps",
+            "extra": "avg tps: 2112.8860898788903, max tps: 2166.824396873039, count: 55238"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 225.0832416656254,
+            "unit": "median tps",
+            "extra": "avg tps: 264.75271377243234, max tps: 458.79716361106665, count: 55238"
           }
         ]
       }

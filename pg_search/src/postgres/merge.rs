9e176aaa9e272e615a4fdbc171c64134abc5226d
Ming Ying <ming.ying.nyc@gmail.com>
2025-12-15 16:19:30 -0500

    only bgmerge if index is valid

--- pg_search/src/postgres/merge.rs
+++ pg_search/src/postgres/merge.rs
@@ -220,6 +220,10 @@ pub unsafe fn do_merge(
     current_xid: Option<pg_sys::FullTransactionId>,
     next_xid: Option<pg_sys::FullTransactionId>,
 ) -> anyhow::Result<()> {
+    if !index.is_valid() {
+        return Ok(());
+    }
+
     let layer_sizes = IndexLayerSizes::from(index);
     let metadata = MetaPage::open(index);
     let cleanup_lock = metadata.cleanup_lock_shared();

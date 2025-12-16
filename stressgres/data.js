449b8baa8f10043391ec6abf956a9efed0e89d9b
github-action-benchmark <github@users.noreply.github.com>
2025-12-16 21:49:08 +0000

    add pg_search bulk-updates.toml Performance - TPS (customBiggerIsBetter) benchmark result for 1a592128d17a6ede4a65237164ea471803dbacb6

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765920880907,
+  "lastUpdate": 1765921748202,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -14854,6 +14854,42 @@ window.BENCHMARK_DATA = {
             "value": 5.34808766442578,
             "unit": "median tps",
             "extra": "avg tps: 4.804518134937062, max tps: 5.975192952874321, count: 57351"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "james.sewell@gmail.com",
+            "name": "James Sewell",
+            "username": "jamessewell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a592128d17a6ede4a65237164ea471803dbacb6",
+          "message": "feat: Add support for the json/b subscript [] operator (#3785)\n\nThis can be used in place of `->`, the following two cases are treated\nthe same:\n\n```\nWHERE metadata->'color' @@@ 'white'\nWHERE metadata['color'] @@@ 'white'\n```\n\nBut the second shows up as a `T_SubscriptingRef` node and not a\n`T_OpExpr` node.\n\nNote that if you create an index in these ways:\n\n```\nUSING BM25(id, (metadata->'color'))\nUSING BM25(id, (metadata['color']))\n```\nThen you will have to access it with that operator as well, you can't\nswap. This is the same as standard Postgres expression indexes.",
+          "timestamp": "2025-12-17T10:17:41+13:00",
+          "tree_id": "220f15018597bade02d1530722690ccd44f215e7",
+          "url": "https://github.com/paradedb/paradedb/commit/1a592128d17a6ede4a65237164ea471803dbacb6"
+        },
+        "date": 1765921745413,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 8.063214253341398,
+            "unit": "median tps",
+            "extra": "avg tps: 6.877770379467112, max tps: 10.425071015940237, count: 57759"
+          },
+          {
+            "name": "Count Query - Primary - tps",
+            "value": 5.329607516455054,
+            "unit": "median tps",
+            "extra": "avg tps: 4.789387718639856, max tps: 5.978265329113915, count: 57759"
           }
         ]
       }

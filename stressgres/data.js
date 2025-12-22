f9b49a22b6e0ca294840ab1fb7352d8f6ead8a01
github-action-benchmark <github@users.noreply.github.com>
2025-12-22 20:21:31 +0000

    add pg_search logical-replication.toml Performance - TPS (customBiggerIsBetter) benchmark result for ae38d61a8474a66e32e14b3c79e35368c0c9a026

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766434040781,
+  "lastUpdate": 1766434891490,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -45152,6 +45152,54 @@ window.BENCHMARK_DATA = {
             "value": 114.21516076785812,
             "unit": "median tps",
             "extra": "avg tps: 114.01655515332934, max tps: 167.98625364753374, count: 107418"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "43472713+lilei1128@users.noreply.github.com",
+            "name": "sanshi",
+            "username": "lilei1128"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae38d61a8474a66e32e14b3c79e35368c0c9a026",
+          "message": "feat: JIEBA supports conversion between simplified and traditional Chinese characters (#3764)\n\n# Ticket(s) Closed\n\nClose #3758 \n\n## What\nEnhance the capabilities of the JIEBA tokenizer, which supports the\nconversion between traditional and simplified Chinese characters.\n\n## Why\nIn the languages used in Chinese mainland, many user scenarios encounter\nsituations where both traditional and simplified characters coexist.\nUser need query traditional document by simplified characters .\n\n## How\nI have already implemented this function in the local environment, the\ndetail is:\n\nConfig when creating bm25 index, and the usage method is as follows:\nCREATE INDEX search_idx ON mock_items\nUSING bm25 (id, (description::pdb.jieba('chinese_convert=t2s')))\nWITH (key_field='id');\n\nExplanation：\n\ns2t：simple -> traditional\nt2s：traditional -> simple\ns2tw：simple-> TaiWai Standard\ntw2s：TaiWai Standard -> simple\n...\n\n## Tests\n<img width=\"1386\" height=\"402\" alt=\"image\"\nsrc=\"https://github.com/user-attachments/assets/4496c944-5f2b-4f1a-8a1f-0957c73998de\"\n/>",
+          "timestamp": "2025-12-22T11:04:21-08:00",
+          "tree_id": "060f808f49f5f5470b936923fd0c42f4d362ed71",
+          "url": "https://github.com/paradedb/paradedb/commit/ae38d61a8474a66e32e14b3c79e35368c0c9a026"
+        },
+        "date": 1766434888508,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Subscriber - tps",
+            "value": 526.8949522817817,
+            "unit": "median tps",
+            "extra": "avg tps: 527.4048434719564, max tps: 703.3634092237586, count: 53707"
+          },
+          {
+            "name": "Index Only Scan - Subscriber - tps",
+            "value": 656.3446548619341,
+            "unit": "median tps",
+            "extra": "avg tps: 653.4125410286057, max tps: 790.525504820507, count: 53707"
+          },
+          {
+            "name": "Parallel Custom Scan - Subscriber - tps",
+            "value": 87.9099654802703,
+            "unit": "median tps",
+            "extra": "avg tps: 87.86848459083348, max tps: 92.74053903655636, count: 53707"
+          },
+          {
+            "name": "Top N - Subscriber - tps",
+            "value": 107.12108169929671,
+            "unit": "median tps",
+            "extra": "avg tps: 107.11801782949173, max tps: 136.96198106354518, count: 107414"
           }
         ]
       }

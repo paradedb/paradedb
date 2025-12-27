b94ab15aedb154e175b88bd251b36a4cbdadf977
Mohammad Dashti <mdashti@gmail.com>
2025-12-26 19:47:03 -0800

    Updated upgrade script

--- pg_search/sql/pg_search--0.20.5--0.20.6.sql
+++ pg_search/sql/pg_search--0.20.5--0.20.6.sql
@@ -1,5 +1,16 @@
 \echo Use "ALTER EXTENSION pg_search UPDATE TO '0.20.6'" to load this file. \quit
 
+/* </end connected objects> */
+/* <begin connected objects> */
+-- pg_search/src/api/tokenizers/definitions.rs:326
+-- pg_search::api::tokenizers::definitions::pdb::alias_out_safe
+CREATE OR REPLACE FUNCTION pdb."alias_out_safe"(
+	"input" pdb.alias /* pg_search::api::tokenizers::definitions::pdb::Alias */
+) RETURNS cstring /* alloc::ffi::c_str::CString */
+IMMUTABLE STRICT PARALLEL SAFE
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'alias_out_safe_wrapper';
+
 -- Override the output function for pdb.alias
 CREATE OR REPLACE FUNCTION pdb.alias_out(pdb.alias) RETURNS cstring
 AS 'MODULE_PATHNAME', 'alias_out_safe_wrapper'

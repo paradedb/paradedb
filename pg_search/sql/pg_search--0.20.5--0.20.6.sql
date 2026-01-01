be0cc836e9d2d3d17d704b7e93f96b09e3a46c2d
Mohammad Dashti <mdashti@gmail.com>
2025-12-31 16:10:36 -0800

    Reapply "all changes for snippet"
    This reverts commit 3c4435a457bb38fe8fb2c78f5ace0bd0a1af5e84.

--- pg_search/sql/pg_search--0.20.5--0.20.6.sql
+++ pg_search/sql/pg_search--0.20.5--0.20.6.sql
@@ -15,3 +15,21 @@ AS 'MODULE_PATHNAME', 'alias_out_safe_wrapper';
 CREATE OR REPLACE FUNCTION pdb.alias_out(pdb.alias) RETURNS cstring
 AS 'MODULE_PATHNAME', 'alias_out_safe_wrapper'
 LANGUAGE c IMMUTABLE STRICT PARALLEL SAFE;
+
+DROP FUNCTION IF EXISTS snippet_positions(field anyelement, "limit" pg_catalog.int4, "offset" pg_catalog.int4);
+/* </end connected objects> */
+
+/* <begin connected objects> */
+-- pg_search/src/postgres/customscan/pdbscan/projections/snippet.rs:341
+-- pg_search::postgres::customscan::pdbscan::projections::snippet::paradedb_snippet_positions_from_relation
+
+CREATE OR REPLACE FUNCTION "paradedb"."snippet_positions"(
+    "field" anyelement,
+    "limit" INT DEFAULT NULL,
+    "offset" INT DEFAULT NULL
+) RETURNS integer[]
+STABLE PARALLEL SAFE
+LANGUAGE c
+AS 'MODULE_PATHNAME', 'paradedb_snippet_positions_from_relation_wrapper';
+DROP FUNCTION IF EXISTS pdb.snippet_positions(field anyelement, "limit" pg_catalog.int4, "offset" pg_catalog.int4);
+CREATE OR REPLACE FUNCTION pdb.snippet_positions(field anyelement, "limit" pg_catalog.int4 DEFAULT NULL, "offset" pg_catalog.int4 DEFAULT NULL) RETURNS pg_catalog.int4[] AS 'MODULE_PATHNAME', 'snippet_positions_from_relation_wrapper' LANGUAGE c PARALLEL SAFE STABLE;

448b3077929e8a13ba03d02fb19191926039cd50
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 03:51:11 +0000

    add pg_search single-server.toml Performance - TPS (customBiggerIsBetter) benchmark result for f3b1b69d7a0df731bd552bbf078b1ca2b19c397d

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768534429689,
+  "lastUpdate": 1768535471789,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -7118,6 +7118,72 @@ window.BENCHMARK_DATA = {
             "value": 121.30137367544769,
             "unit": "median tps",
             "extra": "avg tps: 129.92658250083096, max tps: 472.66032254104084, count: 55274"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ming.ying.nyc@gmail.com",
+            "name": "Ming",
+            "username": "rebasedming"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3b1b69d7a0df731bd552bbf078b1ca2b19c397d",
+          "message": "fix: use advisory locks to coordinate background merging (#3929)\n\n# Ticket(s) Closed\n\n- Closes #\n\n## What\n\nPR #3519 coordinated the number of background mergers using buffer-level\nlocking: specifically cleanup locks + pins to determine whether a merge\nwas running. This led to WAL replay issues where cleanup locks were\nreplayed on hot standbys, which would hang if that buffer was already\npinned.\n\nThis fix gets rid of these buffer-level locks altogether, and instead\nuses Postgres advisory locks\nhttps://www.postgresql.org/docs/current/explicit-locking.html#ADVISORY-LOCKS\n\nThe way it works is `aminsert` first does a quick advisory lock\nacquisition/release to determine if a merge is already happening. If\nnot, it launches a background worker, which then re-acquires the\nadvisory lock (signaling to concurrent inserts not to launch more\nbackground workers).\n\n## Why\n\n## How\n\n## Tests",
+          "timestamp": "2026-01-15T17:32:32-10:00",
+          "tree_id": "5a8ecf3aa4b769aae4698244b1730a6172f47a3b",
+          "url": "https://github.com/paradedb/paradedb/commit/f3b1b69d7a0df731bd552bbf078b1ca2b19c397d"
+        },
+        "date": 1768535468205,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Primary - tps",
+            "value": 569.4760379125651,
+            "unit": "median tps",
+            "extra": "avg tps: 570.8050260383438, max tps: 732.5498047982114, count: 55444"
+          },
+          {
+            "name": "Delete values - Primary - tps",
+            "value": 2864.951144677515,
+            "unit": "median tps",
+            "extra": "avg tps: 2880.0660526615916, max tps: 3007.1850160935464, count: 55444"
+          },
+          {
+            "name": "Index Only Scan - Primary - tps",
+            "value": 550.8357185458258,
+            "unit": "median tps",
+            "extra": "avg tps: 550.8450145533708, max tps: 709.1667390348514, count: 55444"
+          },
+          {
+            "name": "Index Scan - Primary - tps",
+            "value": 476.9888078153662,
+            "unit": "median tps",
+            "extra": "avg tps: 477.94177547681664, max tps: 504.8687483782548, count: 55444"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 3012.711688064693,
+            "unit": "median tps",
+            "extra": "avg tps: 3016.067164619028, max tps: 3242.7099489178604, count: 110888"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 2082.34823259482,
+            "unit": "median tps",
+            "extra": "avg tps: 2072.9697799427213, max tps: 2088.514473350471, count: 55444"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 125.55714013066267,
+            "unit": "median tps",
+            "extra": "avg tps: 144.46927596194848, max tps: 776.5548764151742, count: 55444"
           }
         ]
       }

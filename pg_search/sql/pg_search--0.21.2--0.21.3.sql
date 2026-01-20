e59e55c1796a5b452ce5961800bb13b263821255
Ming Ying <ming.ying.nyc@gmail.com>
2026-01-20 18:14:58 -0500

    fix upgrade script

--- pg_search/sql/pg_search--0.21.2--0.21.3.sql
+++ pg_search/sql/pg_search--0.21.2--0.21.3.sql
@@ -302,32 +302,6 @@ CREATE CAST (uuid AS pdb.whitespace) WITH FUNCTION pdb.uuid_to_whitespace AS ASS
 -- creates:
 --   Type(pg_search::api::tokenizers::definitions::pdb::Icu)
 
-
-CREATE TYPE pdb.icu;
-CREATE OR REPLACE FUNCTION pdb.icu_in(cstring) RETURNS pdb.icu AS 'textin' LANGUAGE internal IMMUTABLE STRICT;
-CREATE OR REPLACE FUNCTION pdb.icu_out(pdb.icu) RETURNS cstring AS 'textout' LANGUAGE internal IMMUTABLE STRICT;
-CREATE OR REPLACE FUNCTION pdb.icu_send(pdb.icu) RETURNS bytea AS 'textsend' LANGUAGE internal IMMUTABLE STRICT;
-CREATE OR REPLACE FUNCTION pdb.icu_recv(internal) RETURNS pdb.icu AS 'textrecv' LANGUAGE internal IMMUTABLE STRICT;
-CREATE TYPE pdb.icu (
-                          INPUT = pdb.icu_in,
-                          OUTPUT = pdb.icu_out,
-                          SEND = pdb.icu_send,
-                          RECEIVE = pdb.icu_recv,
-                          COLLATABLE = true,
-                          CATEGORY = 't', -- 't' is for tokenizer
-                          PREFERRED = false,
-                          LIKE = text
-                      );
-/* </end connected objects> */
-
-/* <begin connected objects> */
--- pg_search/src/api/tokenizers/definitions.rs:529
--- requires:
---   generic_typmod_in
---   generic_typmod_out
---   icu_definition
-
-ALTER TYPE pdb.icu SET (TYPMOD_IN = generic_typmod_in, TYPMOD_OUT = generic_typmod_out);
 /* </end connected objects> */
 
 /* <begin connected objects> */

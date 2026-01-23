68960528cab42f901f56774a4743f33631f3e95b
Ming Ying <ming.ying.nyc@gmail.com>
2026-01-23 14:36:16 -0500

    fix test

--- pg_search/tests/pg_regress/sql/tokenizer_literal_normalized.sql
+++ pg_search/tests/pg_regress/sql/tokenizer_literal_normalized.sql
@@ -10,17 +10,23 @@ INSERT INTO test_table (text) VALUES
     ('Hello, world!');
 
 CREATE INDEX idx_test_table ON test_table USING bm25 (id, (text::pdb.unicode_words('ascii_folding=true'))) WITH (key_field='id');
-EXPLAIN SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
-EXPLAIN SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
 DROP INDEX idx_test_table;
 
 CREATE INDEX idx_test_table ON test_table USING bm25 (id, (text::pdb.literal_normalized('ascii_folding=true'))) WITH (key_field='id');
-EXPLAIN SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
-EXPLAIN SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
 DROP INDEX idx_test_table;
 
 CREATE INDEX idx_test_table ON test_table USING bm25 (id, (text::pdb.literal)) WITH (key_field='id');
-EXPLAIN SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
-EXPLAIN SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT text, pdb.agg('{"value_count": {"field": "id"}}') FROM test_table WHERE id @@@ pdb.all() GROUP BY text ORDER BY text LIMIT 5;
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT * FROM test_table WHERE id @@@ pdb.all() ORDER BY text LIMIT 5;
 
 DROP TABLE test_table;

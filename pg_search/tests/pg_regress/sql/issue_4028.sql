320621fa5133b6d6decee6ca74a4c6240201f395
Ming <ming.ying.nyc@gmail.com>
2026-01-30 12:15:34 -0500

    fix: `ORDER BY lower()` gets Top N if literal normalized (#4029)
    # Ticket(s) Closed
    
    - Closes #4028
    
    ## What
    
    See GH issue/test
    
    ## Why
    
    ## How
    
    ## Tests
    
    ---------
    
    Signed-off-by: Ming <ming.ying.nyc@gmail.com>
    Co-authored-by: Stu Hood <stuhood@paradedb.com>
    (cherry picked from commit 198039ded4e94756d9c142200aa40ce88f518bb1)

---
+++ pg_search/tests/pg_regress/sql/issue_4028.sql
@@ -0,0 +1,26 @@
+\i common/common_setup.sql
+
+CREATE TABLE test (id serial PRIMARY KEY, description text);
+
+INSERT INTO test (description) VALUES ('Cloud Engagement Manager'),('cloud engineer'), ('Clōüd engineer'),('cloud Engineer'), ('Cloud engineer');
+
+CREATE INDEX test_bm25 ON test
+USING bm25 (
+	id,
+	(lower(description)::pdb.literal_normalized('ascii_folding=true'))
+) WITH (key_field = id);
+
+SELECT * FROM paradedb.schema('test_bm25');
+
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF)
+SELECT * FROM test
+WHERE lower(description) === 'cloud engineer'
+ORDER BY lower(description)
+LIMIT 10;
+
+SELECT * FROM test
+WHERE lower(description) === 'cloud engineer'
+ORDER BY lower(description)
+LIMIT 10;
+
+DROP TABLE test;

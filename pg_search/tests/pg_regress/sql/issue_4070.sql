5c8f4512285724b4a3593d0992042e7bb4eec52f
Ming Ying <ming.ying.nyc@gmail.com>
2026-02-03 14:57:07 -0500

    fix test output

--- pg_search/tests/pg_regress/sql/issue_4070.sql
+++ pg_search/tests/pg_regress/sql/issue_4070.sql
@@ -6,7 +6,7 @@ CALL paradedb.create_bm25_test_table(
 );
 
 CREATE INDEX search_idx ON mock_items USING bm25 (id, (description::pdb.literal), rating) WITH (key_field = id);
-EXPLAIN SELECT * from mock_items where rating @@@ '4';
+EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF) SELECT * from mock_items where rating @@@ '4';
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF) SELECT * FROM mock_items WHERE rating @@@ 'IN [1 2]';
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF) SELECT * FROM mock_items WHERE id @@@ pdb.all() AND rating = 4;
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF) SELECT * FROM mock_items WHERE id @@@ pdb.all() AND rating IN (1, 2);

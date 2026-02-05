a5c20eb44b54ffb634cd71ccab794d804207e97a
dask-58 <140686560+dask-58@users.noreply.github.com>
2026-02-05 23:04:40 +0530

    Add 0.21.6 → 0.21.7 migration for implicit casts

---
+++ pg_search/sql/pg_search--0.21.6--0.21.7.sql
@@ -0,0 +1,28 @@
+\echo Use "ALTER EXTENSION pg_search UPDATE TO '0.21.7'" to load this file. \quit
+
+CREATE CAST (varchar AS paradedb.fieldname) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.simple) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.simple) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.whitespace) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.whitespace) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.literal) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.literal) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.literal_normalized) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.literal_normalized) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.chinese_compatible) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.chinese_compatible) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.lindera) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.lindera) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.jieba) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.jieba) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.source_code) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.source_code) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.icu) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.icu) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.ngram) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.ngram) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.regex_pattern) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.regex_pattern) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.unicode_words) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.unicode_words) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS paradedb.fieldname) WITH INOUT AS IMPLICIT;

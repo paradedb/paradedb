9470be451a4b5538c6b9a102bd67b372a321d9ca
Ming <ming.ying.nyc@gmail.com>
2026-02-10 11:19:38 -0500

    fix: move `0.21.7` upgrade script content to `0.22.0` (#4144)
    # Ticket(s) Closed
    
    - Closes #
    
    ## What
    
    Because this wasn't cherry-picked into `0.21.x`, it should be in the
    `0.22.0` upgrade script. Otherwise if we release `0.21.8` someone who
    installs it on `0.21.8` and upgrades to `0.22.x` will never have these
    upgrades applied.
    
    ## Why
    
    ## How
    
    ## Tests

--- pg_search/sql/pg_search--0.21.7--0.22.0.sql
+++ pg_search/sql/pg_search--0.21.7--0.22.0.sql
@@ -273,3 +273,34 @@ AS 'MODULE_PATHNAME', 'uuid_to_icu_wrapper';
 --   uuid_to_icu
 
 CREATE CAST (uuid AS pdb.icu) WITH FUNCTION pdb.uuid_to_icu AS ASSIGNMENT;
+
+CREATE CAST (text AS pdb.simple) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.simple) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.whitespace) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.whitespace) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.literal) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.literal) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.literal_normalized) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.literal_normalized) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.chinese_compatible) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.chinese_compatible) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.lindera) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.lindera) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.jieba) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.jieba) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.source_code) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.source_code) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.icu) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.icu) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.ngram) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.ngram) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.regex_pattern) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.regex_pattern) WITH INOUT AS IMPLICIT;
+CREATE CAST (text AS pdb.unicode_words) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS pdb.unicode_words) WITH INOUT AS IMPLICIT;
+CREATE CAST (varchar AS paradedb.fieldname) WITH INOUT AS IMPLICIT;
+
+DROP FUNCTION IF EXISTS score(_relation_reference anyelement);
+CREATE OR REPLACE FUNCTION score(relation_reference anyelement) RETURNS pg_catalog.float4 AS 'MODULE_PATHNAME', 'paradedb_score_from_relation_wrapper' COST 1 LANGUAGE c PARALLEL SAFE STABLE STRICT;
+DROP FUNCTION IF EXISTS pdb.score(_relation_reference anyelement);
+CREATE OR REPLACE FUNCTION pdb.score(relation_reference anyelement) RETURNS pg_catalog.float4 AS 'MODULE_PATHNAME', 'score_from_relation_wrapper' COST 1 LANGUAGE c PARALLEL SAFE STABLE STRICT;

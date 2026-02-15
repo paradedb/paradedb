2f76c2681a3d4f8a727bad5cfac9e604098126ed
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2026-02-15 15:08:34 +0530

    feat: Add auto-tuning for Docker Resolves: #3857

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -41,4 +41,7 @@ for DB in template1 paradedb "$POSTGRES_DB"; do
   psql -d "$DB" -c "ALTER DATABASE \"$DB\" SET search_path TO public,paradedb;"
 done
 
+echo "ParadeDB auto-tune: Running initial configuration..."
+/usr/local/bin/tune-postgresql.sh
+
 echo "ParadeDB bootstrap completed!"

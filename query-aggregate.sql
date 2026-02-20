261ff8b16cd941d01d3c1319f4aba986bf98c60b
Stu Hood <stuhood@gmail.com>
2026-02-20 08:55:15 -0800

    Still aggregates, but as a subquery

--- query-aggregate.sql
+++ query-aggregate.sql
@@ -1,55 +1,35 @@
 SET max_parallel_workers to 0;
 
-SELECT *
-FROM paradedb.aggregate(
-    'contacts_companies_combined_full_idx',
-    paradedb.boolean(
-      must => ARRAY[
-          paradedb.boolean(
-              must => ARRAY[
-                  paradedb.boolean(
-                      should => ARRAY[paradedb.parse((
-                          SELECT concat('contact_id:IN [', string_agg(ldf_id::TEXT, ' '), ']')
-                          FROM contact_list
-                          WHERE list_id IN ('tjy3slfS5wk')
-                      ))]
-                  )
-              ]
-          )
---           paradedb.boolean(
---               must => ARRAY[
---                   paradedb.boolean(
---                       must_not => ARRAY[
---                           paradedb.parse((
---                               SELECT concat('contact_id:IN [', string_agg(ldf_id::TEXT, ' '), ']')
---                               FROM contact_list
---                               WHERE list_id IN ('loxSXiPQRww','SZWEZLQhwbE')
---                       ))],
---                       must => ARRAY[paradedb.all()]
---                   )
---               ]
---           ),
---           paradedb.boolean(
---               should => ARRAY[
---                   paradedb.boolean(
---                       must => ARRAY[
---                           paradedb.boolean(must => ARRAY[paradedb.parse('contact_job_title:"Senior Programmer"')]),
---                           paradedb.boolean(must => ARRAY[paradedb.parse('contact_job_details.job_function:"product management, research, & innovation"')]),
---                           paradedb.boolean(must => ARRAY[paradedb.parse('contact_job_details.job_area:"software development & engineering"')])
---                       ])])
---           paradedb.boolean(must => ARRAY[paradedb.range(field => 'contact_id', range => '(0,)'::int8range)])
-      ]
-    ),
-    '{
-        "company_revenue_range_enum": {
-            "terms": {
-                "field": "contact_last_name",
-                "size": 10,
-                "segment_size": 10
-            },
-            "aggs": { "company_count": { "cardinality": { "field": "company_id" } } }
-        }
-    }',
-    FALSE,
-    1200000000
-);
+SELECT
+    contact_last_name,
+    count(*) AS doc_count,
+    count(DISTINCT company_id) AS company_count
+FROM contacts_companies_combined_full
+WHERE contact_id IN (
+    SELECT ldf_id
+    FROM contact_list
+    WHERE list_id IN ('tjy3slfS5wk')
+)
+AND contact_id NOT IN (
+    SELECT ldf_id
+    FROM contact_list
+    WHERE list_id IN ('loxSXiPQRww','SZWEZLQhwbE')
+)
+AND contact_id @@@ paradedb.boolean(
+    must => ARRAY[
+        paradedb.boolean(
+            should => ARRAY[
+                paradedb.boolean(
+                    must => ARRAY[
+                        paradedb.parse('contact_job_title:"Senior Programmer"'),
+                        paradedb.parse('contact_job_details.job_function:"product management, research, & innovation"'),
+                        paradedb.parse('contact_job_details.job_area:"software development & engineering"')
+                    ])])
+        ),
+        paradedb.range(field => 'contact_id', range => '(0,)'::int8range)
+    ]
+)
+AND contact_last_name IS NOT NULL
+GROUP BY contact_last_name
+ORDER BY doc_count DESC
+LIMIT 10;

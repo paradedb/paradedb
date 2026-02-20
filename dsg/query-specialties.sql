3a019912125fcfef5a7f9e3ff300633435e0ef92
Stu Hood <stuhood@gmail.com>
2026-02-20 10:53:31 -0800

    Simplify the `specialities` query.

--- dsg/query-specialties.sql
+++ dsg/query-specialties.sql
@@ -2,14 +2,18 @@ PREPARE query_stmt AS
 SELECT *
 FROM contacts_companies_combined_full
 WHERE
-    company_id IN (
-        SELECT company_id
-        FROM company_specialties_autocomplete
-        WHERE company_id NOT IN (
-            SELECT company_id
-            FROM company_specialties_autocomplete
-            WHERE unique_id @@@ paradedb.parse('speciality:salesforce')
-        )
+    -- Must have at least one specialty
+    EXISTS (
+        SELECT 1
+        FROM company_specialties_autocomplete csa_exists
+        WHERE csa_exists.company_id = contacts_companies_combined_full.company_id
+    )
+    -- Must NOT have the 'salesforce' specialty
+    AND NOT EXISTS (
+        SELECT 1
+        FROM company_specialties_autocomplete csa_not_exists
+        WHERE csa_not_exists.company_id = contacts_companies_combined_full.company_id
+          AND csa_not_exists.unique_id @@@ paradedb.parse('speciality:salesforce')
     )
     AND contact_id @@@ paradedb.range(field => 'contact_id', range => '(0,)'::int8range)
 ORDER BY revenue_rank DESC NULLS LAST, contact_id ASC

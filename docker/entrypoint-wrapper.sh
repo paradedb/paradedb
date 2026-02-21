ba8ff2c2b2679e9708b807058493af2c55c7155b
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2026-02-21 15:47:03 +0530

    refactor: apply maintainer feedback (move function, remove log,improve comment)

--- docker/entrypoint-wrapper.sh
+++ docker/entrypoint-wrapper.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 export PGDATA=${PGDATA:-/var/lib/postgresql/data}
 # We only tune from `entrypoint-wrapper.sh` on pre-existing deployments, as it is otherwise handled by `bootstrap.sh` as part of the initial deploy.
 if [ -s "$PGDATA/PG_VERSION" ]; then
-	/usr/local/bin/tune-postgresql.sh
-else
-	echo "ParadeDB auto-tune: Detected fresh install.Deferring tuning to bootstrap."
+  /usr/local/bin/tune-postgresql.sh
 fi
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"

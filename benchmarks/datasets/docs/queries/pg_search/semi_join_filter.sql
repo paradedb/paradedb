373e8691207df3e9344c3113c729189be0d6073d
Stu Hood <stuhood@gmail.com>
2026-02-21 11:16:01 -0800

    _Correctly_ add some other alternatives.

--- benchmarks/datasets/docs/queries/pg_search/semi_join_filter.sql
+++ benchmarks/datasets/docs/queries/pg_search/semi_join_filter.sql
@@ -53,3 +53,37 @@ WHERE
 ORDER BY
     f.title ASC                       -- Single Feature Sort (Local Fast Field)
 LIMIT 25;
+
+SET work_mem TO '4GB'; SET paradedb.enable_mixed_fast_field_sort TO on; SET paradedb.enable_join_custom_scan TO on; SET paradedb.dynamic_filter_batch_size = 8192; SELECT
+    f.id,
+    f.title,
+    f."createdAt"
+FROM files f
+WHERE
+    -- The "Join" is a filter against a list of IDs (Semi-Join)
+    f."documentId" IN (
+        SELECT id
+        FROM documents
+        WHERE parents @@@ 'PROJECT_ALPHA'
+        AND title @@@ 'Document Title 1'
+    )
+ORDER BY
+    f.title ASC                       -- Single Feature Sort (Local Fast Field)
+LIMIT 25;
+
+SET work_mem TO '4GB'; SET paradedb.enable_mixed_fast_field_sort TO on; SET paradedb.enable_join_custom_scan TO on; SET paradedb.dynamic_filter_batch_size = 65536; SELECT
+    f.id,
+    f.title,
+    f."createdAt"
+FROM files f
+WHERE
+    -- The "Join" is a filter against a list of IDs (Semi-Join)
+    f."documentId" IN (
+        SELECT id
+        FROM documents
+        WHERE parents @@@ 'PROJECT_ALPHA'
+        AND title @@@ 'Document Title 1'
+    )
+ORDER BY
+    f.title ASC                       -- Single Feature Sort (Local Fast Field)
+LIMIT 25;

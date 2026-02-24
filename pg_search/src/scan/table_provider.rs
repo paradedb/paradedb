f8540844ec91d0c4ee4e2521ebd1a162bf54e98d
Stu Hood <stuhood@gmail.com>
2026-02-23 21:00:35 -0800

    Implement PreFilters directly using DataFusion expressions.

--- pg_search/src/scan/table_provider.rs
+++ pg_search/src/scan/table_provider.rs
@@ -234,7 +234,12 @@ impl PgSearchTableProvider {
                 heap_relid,
             );
             // Do real work between checkouts to avoid one worker claiming all segments.
-            partition.0.prefetch_next(&ffhelper, &mut partition.2, &[]);
+            partition.0.prefetch_next(
+                &ffhelper,
+                &mut partition.2,
+                &[],
+                &std::sync::Arc::new(arrow_schema::Schema::empty()),
+            );
 
             segments.push(partition);
         }

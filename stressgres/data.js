4f340b369c8ab0e2ac86df8f0b0ea2316d8ada3f
github-action-benchmark <github@users.noreply.github.com>
2026-02-27 21:08:01 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for a1223f87c786cfc9d5d74f15459ec8eb94e8850c

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772225967001,
+  "lastUpdate": 1772226481518,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -13842,6 +13842,54 @@ window.BENCHMARK_DATA = {
             "value": 5.373236329725549,
             "unit": "median tps",
             "extra": "avg tps: 5.402311830978523, max tps: 7.238776638158137, count: 56357"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stuhood@paradedb.com",
+            "name": "Stu Hood",
+            "username": "stuhood"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1223f87c786cfc9d5d74f15459ec8eb94e8850c",
+          "message": "perf: Only prefetch when producing sorted outputs (#4247)\n\n## What\n\nOnly call `prefetch_next` while creating a scan which needs to be\nsorted.\n\n## Why\n\nWe were calling `prefetch_next` for all scans, rather than only for\nthose which needed ahead-of-time load balancing.\n\nEagerly assigning work and prefetching prevents use of dynamic filtering\nfor the first batch of a segment, and can lead to lopsided work\ndistributions in the case of multiple segments per worker.\n\n## How\n\nDifferentiated between the three types of plans that we can create with\nour scan. For the `create_lazy_scan` case, directly adopted/shared the\nstrategy used in `MixedFF` and `Normal` base scans.\n\nBecause scans always need estimates, additionally produced a planning\ntime estimate for these lazy scans (which is also effectively what\nhappens for parallel workers in general).\n\n## Tests\n\n`semi_join_filter` shows a 40% speedup for warm runs: currently 20%\nfaster than a Postgres join for warm.",
+          "timestamp": "2026-02-27T12:11:27-08:00",
+          "tree_id": "4d4cc931868f3014e476a89bf274367ca9e77537",
+          "url": "https://github.com/paradedb/paradedb/commit/a1223f87c786cfc9d5d74f15459ec8eb94e8850c"
+        },
+        "date": 1772226476687,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 1102.3293962333557,
+            "unit": "median tps",
+            "extra": "avg tps: 1104.1400477547631, max tps: 1150.6150035254452, count: 56200"
+          },
+          {
+            "name": "Single Insert - Primary - tps",
+            "value": 1269.2071435517546,
+            "unit": "median tps",
+            "extra": "avg tps: 1254.1192526151133, max tps: 1281.4487124284983, count: 56200"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 1875.2416235852888,
+            "unit": "median tps",
+            "extra": "avg tps: 1839.9555945245072, max tps: 2009.319093859214, count: 56200"
+          },
+          {
+            "name": "Top N - Primary - tps",
+            "value": 5.3320000097903915,
+            "unit": "median tps",
+            "extra": "avg tps: 5.3751793510799954, max tps: 8.008247020914089, count: 56200"
           }
         ]
       }

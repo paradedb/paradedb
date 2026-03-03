649c6ef4d8441dad778dcbdd7609e840e2b66abf
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 01:00:38 +0000

    add pg_search background-merge.toml Performance - TPS (customBiggerIsBetter) benchmark result for 51af7b52f08823ef61ccf312531a288da3a67e15

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772498625961,
+  "lastUpdate": 1772499638616,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -21584,6 +21584,60 @@ window.BENCHMARK_DATA = {
             "value": 17.073395189017347,
             "unit": "median tps",
             "extra": "avg tps: 17.01760987868789, max tps: 22.174814603951493, count: 55519"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stuhood@paradedb.com",
+            "name": "Stu Hood",
+            "username": "stuhood"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51af7b52f08823ef61ccf312531a288da3a67e15",
+          "message": "chore: Add support for segmented top-k on multiple columns (#4262)\n\n# Ticket(s) Closed\n\n- Closes #4255\n- Closes #4256\n\n## What\n\nAdd support for deferring multiple columns for `SegmentedTopKExec`, and\nfor building `PhysicalExpr`s for pushdown.\n\n## Why\n\n#4234 added support for pushing down `SegmentedThresholds` to the\n`Scanner`, but their implementation was tied to one column at a time.\nThis change adds support for multiple deferred columns per index, and\nfor non-deferred tiebreakers on those columns.\n\n## How\n\nConverts the filters stored in `SegmentedThresholds` to `PhysicalExpr`s\n(which are also used in dynamic filters), and then produces the\nthreshold expressions in a manner similar to upstream `TopK`. This\nallows us to avoid custom logic for null handling during the application\nof filters, and the support for compound columns means we can use an\nexclusive comparison in the case of tiebreaker columns and duplicates.\n\n## Tests\n\nExpanded the tests to test multiple deferred columns, and to confirm\nthat we do the right thing when potentially-deferred columns from\n_different_ indexes are in the same `ORDER BY`.",
+          "timestamp": "2026-03-02T15:50:28-08:00",
+          "tree_id": "3ec7f7e684ad355d46cb027a63d538132a9b800f",
+          "url": "https://github.com/paradedb/paradedb/commit/51af7b52f08823ef61ccf312531a288da3a67e15"
+        },
+        "date": 1772499633380,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom scan - Primary - tps",
+            "value": 31.9886878392296,
+            "unit": "median tps",
+            "extra": "avg tps: 31.912400854469364, max tps: 37.525462726832686, count: 55517"
+          },
+          {
+            "name": "Delete value - Primary - tps",
+            "value": 242.1605004266296,
+            "unit": "median tps",
+            "extra": "avg tps: 270.79628621752124, max tps: 2944.2416708337814, count: 55517"
+          },
+          {
+            "name": "Insert value - Primary - tps",
+            "value": 713.8248802776551,
+            "unit": "median tps",
+            "extra": "avg tps: 702.2746651675669, max tps: 1347.5867584777525, count: 55517"
+          },
+          {
+            "name": "Update random values - Primary - tps",
+            "value": 169.44310567635125,
+            "unit": "median tps",
+            "extra": "avg tps: 185.51506808631478, max tps: 1316.190543786262, count: 111034"
+          },
+          {
+            "name": "Vacuum - Primary - tps",
+            "value": 16.634853335881598,
+            "unit": "median tps",
+            "extra": "avg tps: 16.558513387778362, max tps: 21.51069202929266, count: 55517"
           }
         ]
       }

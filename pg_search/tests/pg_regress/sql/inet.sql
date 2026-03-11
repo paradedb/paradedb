41996e953ac0b5e459eb581dd994019f681bbe21
howenyap <wenhyap02@gmail.com>
2026-03-11 00:46:10 -0700

    feat: support inet operators

--- pg_search/tests/pg_regress/sql/inet.sql
+++ pg_search/tests/pg_regress/sql/inet.sql
@@ -1,13 +1,33 @@
 CREATE EXTENSION IF NOT EXISTS pg_search;
-
 SET paradedb.enable_aggregate_custom_scan TO on;
 
 DROP TABLE IF EXISTS tbl_inet;
 CREATE TABLE tbl_inet (ip inet);
 CREATE INDEX idx_inet ON tbl_inet USING bm25 (ip) WITH (key_field = 'ip');
-INSERT INTO tbl_inet (ip) VALUES ('192.168.0.1');
+
+INSERT INTO tbl_inet (ip) VALUES
+    ('10.0.0.1'),
+    ('172.16.0.1'),
+    ('192.168.0.1'),
+    ('198.51.100.1'),
+    ('203.0.113.1');
+
 SELECT count(*) FROM tbl_inet WHERE ip @@@ '192.168.0.1';
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
 SELECT count(*) FROM tbl_inet WHERE ip @@@ '192.168.0.1';
+
+SELECT ip::text FROM tbl_inet WHERE ip = '192.168.0.1'::inet;
+
+SELECT ip::text FROM tbl_inet WHERE ip > '192.168.0.1'::inet ORDER BY ip;
+
+SELECT ip::text FROM tbl_inet WHERE ip < '192.168.0.1'::inet ORDER BY ip;
+
+SELECT ip::text FROM tbl_inet WHERE ip <= '192.168.0.1'::inet ORDER BY ip;
+
+SELECT ip::text FROM tbl_inet WHERE ip >= '192.168.0.1'::inet ORDER BY ip;
+
+SELECT ip::text FROM tbl_inet WHERE ip <> '192.168.0.1'::inet ORDER BY ip;
+
+SELECT ip::text FROM tbl_inet WHERE ip = ANY(ARRAY['10.0.0.1'::inet, '192.168.0.1'::inet]) ORDER BY ip;
 
 RESET paradedb.enable_aggregate_custom_scan;

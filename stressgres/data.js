8e4722be711083d922f1c1f557e7b7429381792c
github-action-benchmark <github@users.noreply.github.com>
2026-03-16 17:43:13 +0000

    add pg_search wide-table.toml Performance - TPS (customBiggerIsBetter) benchmark result for 4f9e401abb9e1a353ce54c0695f66ff48f83619a

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773682070602,
+  "lastUpdate": 1773682993471,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -24978,6 +24978,54 @@ window.BENCHMARK_DATA = {
             "value": 5.118415866101409,
             "unit": "median tps",
             "extra": "avg tps: 5.166794674729126, max tps: 6.825298572515422, count: 56499"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "121796315+kiran-4444@users.noreply.github.com",
+            "name": "Chandra Kiran G",
+            "username": "kiran-4444"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4f9e401abb9e1a353ce54c0695f66ff48f83619a",
+          "message": "feat: Implement mlt for datetime objects (#4286)\n\n# Ticket(s) Closed\n\n- Closes #2549 \n\n## What\nThis extends `more_like_this` support for datetime objects.\n\n## Why\nUse more_like_this on datetime fields.\n\n\n## How\nModified `try_coerce` function to convert string (datetime object is\nsent a string to the function) to tantivity datetime object using pgOid.\n\n## Tests\nIncluded in the commit.\n\n```sql\npsql (18.3)\nType \"help\" for help.\n\npg_search=# DROP EXTENSION pg_search cascade;\nCREATE EXTENSION pg_search;\n\ndrop table if exists more_like_this_dt cascade;\n\nCREATE TABLE more_like_this_dt (\n    id INT PRIMARY KEY,\n    created_at TIMESTAMP\n);\n\nINSERT INTO more_like_this_dt (id, created_at) VALUES\n    (1, '2012-01-01 00:00:00'),\n    (2, '2013-01-03 00:00:00'),\n    (3, '2014-01-02 00:00:00'),\n    (4, '2015-01-01 00:00:00');\n\nNOTICE:  drop cascades to 2 other objects\nDETAIL:  drop cascades to index test_more_like_this_index\ndrop cascades to index search_idx\nDROP EXTENSION\nCREATE EXTENSION\nDROP TABLE\nCREATE TABLE\nINSERT 0 4\npg_search=# drop index if exists test_more_like_this_index;\n\nCREATE INDEX test_more_like_this_index on more_like_this_dt USING bm25 (id, created_at)\nWITH (key_field='id');\nNOTICE:  index \"test_more_like_this_index\" does not exist, skipping\nDROP INDEX\nCREATE INDEX\npg_search=# select * from more_like_this_dt\nWHERE id @@@ pdb.more_like_this(\n    document => '{\"created_at\": \"2013-01-03 00:00:00\"}'\n);\n id |     created_at      \n----+---------------------\n  2 | 2013-01-03 00:00:00\n(1 row)\n```",
+          "timestamp": "2026-03-16T09:52:53-07:00",
+          "tree_id": "ef87a1061d97a0f7f00eac834fcbf6b1e79cc1d8",
+          "url": "https://github.com/paradedb/paradedb/commit/4f9e401abb9e1a353ce54c0695f66ff48f83619a"
+        },
+        "date": 1773682987251,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - tps",
+            "value": 1075.8755239459397,
+            "unit": "median tps",
+            "extra": "avg tps: 1084.5927936529497, max tps: 1151.3362004201404, count: 56651"
+          },
+          {
+            "name": "Single Insert - Primary - tps",
+            "value": 1174.560985339215,
+            "unit": "median tps",
+            "extra": "avg tps: 1158.5695927346592, max tps: 1194.0877418977345, count: 56651"
+          },
+          {
+            "name": "Single Update - Primary - tps",
+            "value": 1835.1582164382983,
+            "unit": "median tps",
+            "extra": "avg tps: 1796.5897060045158, max tps: 2017.4467694212278, count: 56651"
+          },
+          {
+            "name": "Top K - Primary - tps",
+            "value": 5.282277330482828,
+            "unit": "median tps",
+            "extra": "avg tps: 5.29433775171129, max tps: 6.30800926138135, count: 56651"
           }
         ]
       }

84e57b1226313dd39fe951ba62564931b63420f2
Philippe Noël <philippemnoel@gmail.com>
2026-03-18 15:27:28 -0700

    Fix set work mem following ShreyPant's PR

--- benchmarks/datasets/docs/queries/pg_search/hierarchical_content-no-scores-large.sql
+++ benchmarks/datasets/docs/queries/pg_search/hierarchical_content-no-scores-large.sql
@@ -8,7 +8,7 @@ WHERE
   documents.parents @@@ 'parent group' AND files.title @@@ 'collab12' AND pages."content" @@@ 'Single Number Reach'
 LIMIT 5;
 
-SET work_mem TO '4GB'; SET paradedb.enable_join_custom_scan TO on; SELECT
+SET work_mem TO '64MB'; SET paradedb.enable_join_custom_scan TO on; SELECT
   *
 FROM
   documents JOIN files ON documents.id = files."documentId" JOIN pages ON pages."fileId" = files.id

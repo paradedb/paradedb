8652dd6c940e7a0c1d6d18cdab51d0f0b13c830a
Stu Hood <stuhood@paradedb.com>
2026-03-19 19:49:10 -0700

    chore: Prepare `0.22.2`. (#4435)

--- pg_search/sql/pg_search--0.22.1--0.22.2.sql
+++ pg_search/sql/pg_search--0.22.1--0.22.2.sql
@@ -1,4 +1,24 @@
 -- pg_search/src/api/aggregate.rs:195
+-- pg_search::api::aggregate::pdb::agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_state
+DROP FUNCTION IF EXISTS pdb."agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_state"(internal, jsonb, bool) CASCADE;
+CREATE  FUNCTION pdb."agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_state"(
+	"this" internal, /* pgrx::datum::internal::Internal */
+	"arg_one" jsonb, /* pgrx::datum::json::JsonB */
+	"arg_two" bool /* bool */
+) RETURNS internal /* pgrx::datum::internal::Internal */
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_state_wrapper';
+
+-- pg_search/src/api/aggregate.rs:195
+-- pg_search::api::aggregate::pdb::agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_finalize
+DROP FUNCTION IF EXISTS pdb."agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_finalize"(internal) CASCADE;
+CREATE  FUNCTION pdb."agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_finalize"(
+	"this" internal /* pgrx::datum::internal::Internal */
+) RETURNS jsonb /* pgrx::datum::json::JsonB */
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'agg_placeholder_with_mvcc_agg_placeholder_with_mvcc_finalize_wrapper';
+
+-- pg_search/src/api/aggregate.rs:195
 -- pg_search::api::aggregate::pdb::AggPlaceholderWithMvcc
 DROP AGGREGATE IF EXISTS pdb.agg(jsonb, bool);
 CREATE AGGREGATE pdb.agg (

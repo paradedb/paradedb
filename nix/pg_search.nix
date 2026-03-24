0f0eebf37d058631c6fd8cefe9cf7b4805421649
Moe <mdashti@gmail.com>
2026-03-24 13:05:52 -0700

    chore: Prepare `0.22.3`. (#4469) (#4471)
    ## Summary
    
    - Bump workspace version from `0.22.2` to `0.22.3`
    - Add `pg_search--0.22.2--0.22.3.sql` upgrade script
    - Add changelog entry for `0.22.3`
    - Update version references in docs (upgrading, extension, DigitalOcean,
    docs.json)
    - Refresh `Cargo.lock`
    
    **Note:** The `nix/pg_search.nix` `cargoHash` will need to be updated
    once CI determines the new hash.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -64,7 +64,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-kZfWYk3bx3bKV3GcDikrhg3N7gg31bi86V7dvotLtTE=";
+  cargoHash = "sha256-rEJ4uWvsUUSnYxjpY1+rJ29zbs+Bwv6ZCNVSPcy4NwI=";
 
   inherit cargo-pgrx postgresql;
 

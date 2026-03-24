05aec83db10dc88d045df952bcf35b9beef05a03
Mohammad Dashti <mdashti@gmail.com>
2026-03-24 15:01:15 -0700

    Revert "chore: Bump version to `0.22.4` for development. (#4472)"
    This reverts commit 9aae0ac996fc2f0321eed00c052ba0ef67d32f5d.

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -64,7 +64,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-eigvneca61cWW0hV0NTAJPa+MApZgA/YjPDsTq05754=";
+  cargoHash = "sha256-TbFKEU5Hv6+4D7h1tvd03dlsGYq3YYGWte7SH0KovpA=";
 
   inherit cargo-pgrx postgresql;
 

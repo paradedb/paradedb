51b405fc021477880ad6f95668e9058369ae6fe3
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-03-25 17:06:46 -0500

    add support for rails

---
+++ docs/scripts/rails_snippet_harness.rb
@@ -0,0 +1,68 @@
+require "active_record"
+require "parade_db"
+
+ParadeDB::Arel::Visitor.install!
+ParadeDB::Arel::Predications.install!
+
+module RailsSnippetHarness
+  class ApplicationRecord < ActiveRecord::Base
+    self.abstract_class = true
+  end
+
+  class MockItem < ApplicationRecord
+    include ParadeDB::Model
+
+    self.table_name = "mock_items"
+
+    has_many :orders,
+      class_name: "RailsSnippetHarness::Order",
+      foreign_key: :product_id,
+      inverse_of: :mock_item
+  end
+
+  class Order < ApplicationRecord
+    include ParadeDB::Model
+
+    self.table_name = "orders"
+    self.primary_key = "order_id"
+
+    belongs_to :mock_item,
+      class_name: "RailsSnippetHarness::MockItem",
+      foreign_key: :product_id,
+      inverse_of: :orders
+  end
+
+  module_function
+
+  def establish_connection!
+    ActiveRecord::Base.establish_connection(normalize_database_url(database_url))
+  end
+
+  def database_url
+    ENV.fetch("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+  end
+
+  def normalize_database_url(url)
+    return "postgresql://#{url.delete_prefix('postgres://')}" if url.start_with?("postgres://")
+
+    url
+  end
+
+  def execute!(value)
+    case value
+    when ActiveRecord::Relation
+      value.load
+    when Array
+      value.each { |item| execute!(item) }
+    when Hash
+      value.each_value { |item| execute!(item) }
+    end
+
+    value
+  end
+end
+
+RailsSnippetHarness.establish_connection!
+
+MockItem = RailsSnippetHarness::MockItem
+Order = RailsSnippetHarness::Order

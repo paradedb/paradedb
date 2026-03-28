7cab3e41d27c62eef691af837fb151fa1af87947
Mohammad Dashti <mdashti@gmail.com>
2026-03-28 16:24:12 -0700

    fix: handle T_List in JoinExpr.quals for equi-key extraction

--- pg_search/src/postgres/customscan/aggregatescan/datafusion_build.rs
+++ pg_search/src/postgres/customscan/aggregatescan/datafusion_build.rs
@@ -360,6 +360,12 @@ unsafe fn extract_equi_keys_from_expr(
                 keys.extend(extract_equi_keys_from_expr(arg, sources)?);
             }
         }
+    } else if tag == pg_sys::NodeTag::T_List {
+        // Postgres may wrap ON clause quals in a List node
+        let list = PgList::<pg_sys::Node>::from_pg(node as *mut pg_sys::List);
+        for item in list.iter_ptr() {
+            keys.extend(extract_equi_keys_from_expr(_root, item, sources)?);
+        }
     }
 
     Ok(keys)

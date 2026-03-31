28005315de868a8e985d013429a20b47fe666008
Mohammad Dashti <mdashti@gmail.com>
2026-03-31 02:47:31 -0700

    fix: use aggregate-specific session context and physical plan imports

--- pg_search/src/postgres/customscan/aggregatescan/mod.rs
+++ pg_search/src/postgres/customscan/aggregatescan/mod.rs
@@ -45,7 +45,7 @@ use crate::postgres::customscan::aggregatescan::datafusion_build::{
     all_have_bm25_index, collect_join_agg_sources, extract_join_tree_from_parse, has_any_bm25_index,
 };
 use crate::postgres::customscan::aggregatescan::datafusion_exec::{
-    build_join_aggregate_plan, create_aggregate_session_context,
+    build_aggregate_physical_plan, build_join_aggregate_plan, create_aggregate_session_context,
 };
 use crate::postgres::customscan::aggregatescan::datafusion_project::project_aggregate_row_to_slot;
 use crate::postgres::customscan::aggregatescan::exec::aggregation_results_iter;
@@ -60,7 +60,6 @@ use crate::postgres::customscan::builders::custom_state::{
 };
 use crate::postgres::customscan::explainer::Explainer;
 use crate::postgres::customscan::joinscan::memory::create_memory_pool;
-use crate::postgres::customscan::joinscan::scan_state::build_physical_plan;
 use crate::postgres::customscan::limit_offset::LimitOffset;
 use crate::postgres::customscan::projections::{create_placeholder_targetlist, placeholder_procid};
 use crate::postgres::customscan::solve_expr::SolvePostgresExpressions;

f0b070cc1fe6fc56ae528896cc76c71b795f16a7
Ming <ming.ying.nyc@gmail.com>
2026-04-01 15:42:47 -0700

    chore: Upgrade to `0.22.5` (#4638)
    # Ticket(s) Closed
    
    - Closes #
    
    ## What
    
    ## Why
    
    ## How
    
    ## Tests
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -64,7 +64,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-P2vgULDzVhP/fwI7x6rNj/wY8ObUoHwTehOyv217+GE=";
+  cargoHash = "sha256-MILIABZESFfY6KfO0YREAazvTFC4cgJYaSXzGrKb61k=";
 
   inherit cargo-pgrx postgresql;
 

9e94298e081e556a945468ed753f17f11598ea20
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-04-09 08:31:52 -0500

    fix: Allow unaliased, tokenized index fields to be selected by name when the same column is indexed multiple times (#4698)
    # Ticket(s) Closed
    
    - Closes #
    
    ## What
    When a column is indexed multiple times with different tokenizers, if
    one copy doesn't have an alias, it should be chosen by name instead of
    requiring it to be disambiguated with `pdb::alias`.
    
    ## Why
    
    ## How
    
    ## Tests
    I updated a regression test to include a query for this scenario

--- pg_search/tests/pg_regress/sql/aliased_text_expression_resolution.sql
+++ pg_search/tests/pg_regress/sql/aliased_text_expression_resolution.sql
@@ -36,6 +36,23 @@ WHERE description ||| 'sleek running shoes';
 
 DROP INDEX search_idx;
 
+-- A tokenized column without an alias should be selected over the aliased version
+CREATE INDEX search_idx ON mock_items
+USING bm25 (
+  id,
+  (description::pdb.simple),
+  (description::pdb.literal('alias=literal_description')),
+  rating
+)
+WITH (key_field='id');
+
+-- description is not ambiguous here
+SELECT description, rating
+FROM mock_items
+WHERE description ||| 'sleek running shoes';
+
+DROP INDEX search_idx;
+
 CREATE INDEX search_idx ON mock_items
 USING bm25 (
   id,
@@ -60,10 +77,13 @@ WHERE description::pdb.alias('simple_description') ||| 'sleek running shoes';
 
 DROP INDEX search_idx;
 
+
+-- Composite index variations
 CREATE TYPE aliased_description_fields AS (
   simple_description pdb.simple('alias=simple_description'),
   literal_description pdb.literal('alias=literal_description')
 );
+
 
 CREATE INDEX search_idx ON mock_items
 USING bm25 (
@@ -91,5 +111,33 @@ SELECT description, rating
 FROM mock_items
 WHERE description::pdb.alias('simple_description') ||| 'sleek running shoes';
 
-DROP TABLE mock_items;
+DROP INDEX search_idx;
 DROP TYPE aliased_description_fields;
+
+-- A tokenized column without an alias should be selected over the aliased version
+CREATE TYPE partially_aliased_description_fields AS (
+  description pdb.simple,
+  literal_description pdb.literal('alias=literal_description')
+);
+
+CREATE INDEX search_idx ON mock_items
+USING bm25 (
+  id,
+  (
+    ROW(
+      description::pdb.simple,
+      description::pdb.literal('alias=literal_description')
+    )::partially_aliased_description_fields
+  ),
+  rating
+)
+WITH (key_field='id');
+
+-- description is not ambiguous here
+SELECT description, rating
+FROM mock_items
+WHERE description ||| 'sleek running shoes';
+
+DROP INDEX search_idx;
+DROP TYPE partially_aliased_description_fields;
+DROP TABLE mock_items;

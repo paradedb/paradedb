4009da0a6cae8e7c9df1bfa79ab90831afa53a26
rjhallsted <rjhallsted@gmail.com>
2026-04-13 16:55:10 -0600

    Try one more, updated from main, no limit

--- benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-nofilter.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-nofilter.sql
@@ -11,4 +11,4 @@ SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>parade
 SET paradedb.enable_aggregate_custom_scan TO on; SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY owner_display_name;
 
 -- pdb.agg with GROUP BY
-SELECT owner_display_name, pdb.agg('{"terms": {"field": "owner_display_name"}}'::jsonb) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY owner_display_name ORDER BY owner_display_name LIMIT 65000;
+SELECT owner_display_name, pdb.agg('{"terms": {"field": "owner_display_name"}}'::jsonb) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY owner_display_name;

866a9db35a369227f78c4f9804d38159efff5ea6
rjhallsted <rjhallsted@gmail.com>
2026-04-13 13:00:57 -0600

    try owner display name instead of tags

--- benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-nofilter.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-nofilter.sql
@@ -2,13 +2,13 @@
 SELECT tags, COUNT(*) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY tags ORDER BY tags;
 
 -- aggregate with mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.all(), agg=>'{"buckets": { "terms": { "field": "tags" }}}', solve_mvcc=>true);
+SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.all(), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>true);
 
 -- aggregate without mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.all(), agg=>'{"buckets": { "terms": { "field": "tags" }}}', solve_mvcc=>false);
+SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.all(), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>false);
 
 -- aggregate custom scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT tags, COUNT(*) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY tags;
+SET paradedb.enable_aggregate_custom_scan TO on; SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY owner_display_name;
 
 -- pdb.agg with GROUP BY
-SELECT tags, pdb.agg('{"terms": {"field": "tags"}}'::jsonb) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY tags;
+SELECT owner_display_name, pdb.agg('{"terms": {"field": "owner_display_name"}}'::jsonb) FROM stackoverflow_posts WHERE id @@@ paradedb.all() GROUP BY owner_display_name;

3efbbff1790e17dc719e74404b06e9f1dd794a41
rjhallsted <rjhallsted@gmail.com>
2026-04-13 13:29:02 -0600

    update other query

--- benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
@@ -1,14 +1,14 @@
 -- string ff
-SELECT tags, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY tags ORDER BY tags;
+SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name ORDER BY owner_display_name;
 
 -- aggregate with mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "tags" }}}', solve_mvcc=>true);
+SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>true);
 
 -- aggregate without mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "tags" }}}', solve_mvcc=>false);
+SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>false);
 
 -- aggregate custom scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT tags, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY tags;
+SET paradedb.enable_aggregate_custom_scan TO on; SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name;
 
 -- pdb.agg with GROUP BY
-SELECT tags, pdb.agg('{"terms": {"field": "tags"}}'::jsonb) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY tags;
+SELECT owner_display_name, pdb.agg('{"terms": {"field": "owner_display_name"}}'::jsonb) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name;

fb71bf154a9b0d6883b2af05095a3e6fc474e50e
rjhallsted <rjhallsted@gmail.com>
2026-04-13 19:23:38 -0600

    just use users table

--- benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
@@ -1,14 +1,14 @@
 -- string ff
-SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name ORDER BY owner_display_name;
+SELECT location, COUNT(*) FROM users WHERE about_me @@@ 'javascript' GROUP BY location ORDER BY location;
 
 -- aggregate with mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>true);
+SELECT * FROM paradedb.aggregate(index=>'users_idx', query=>paradedb.term('about_me', 'javascript'), agg=>'{"buckets": { "terms": { "field": "location" }}}', solve_mvcc=>true);
 
 -- aggregate without mvcc
-SELECT * FROM paradedb.aggregate(index=>'stackoverflow_posts_idx', query=>paradedb.term('body', 'javascript'), agg=>'{"buckets": { "terms": { "field": "owner_display_name" }}}', solve_mvcc=>false);
+SELECT * FROM paradedb.aggregate(index=>'users_idx', query=>paradedb.term('about_me', 'javascript'), agg=>'{"buckets": { "terms": { "field": "location" }}}', solve_mvcc=>false);
 
 -- aggregate custom scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT owner_display_name, COUNT(*) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name;
+SET paradedb.enable_aggregate_custom_scan TO on; SELECT location, COUNT(*) FROM users WHERE about_me @@@ 'javascript' GROUP BY location;
 
 -- pdb.agg with GROUP BY
-SELECT owner_display_name, pdb.agg('{"terms": {"field": "owner_display_name"}}'::jsonb) FROM stackoverflow_posts WHERE body @@@ 'javascript' GROUP BY owner_display_name;
+SELECT location, pdb.agg('{"terms": {"field": "location"}}'::jsonb) FROM users WHERE about_me @@@ 'javascript' GROUP BY location;

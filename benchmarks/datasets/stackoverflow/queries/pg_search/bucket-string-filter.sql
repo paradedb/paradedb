e3ceb3ebc95e2b117f69255edbca1d6e02898085
rjhallsted <rjhallsted@gmail.com>
2026-04-14 16:05:59 -0600

    Change to ||| to match logs

--- benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/bucket-string-filter.sql
@@ -1,11 +1,11 @@
 -- string ff
-SELECT name, COUNT(*) FROM badges WHERE name === 'Question' GROUP BY name ORDER BY name;
+SELECT name, COUNT(*) FROM badges WHERE name ||| 'Question' GROUP BY name ORDER BY name;
 
 -- aggregate custom scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT name, COUNT(*) FROM badges WHERE tag_based GROUP BY name;
+SET paradedb.enable_aggregate_custom_scan TO on; SELECT name, COUNT(*) FROM badges WHERE name ||| 'Question' GROUP BY name;
 
 -- pdb.agg with GROUP BY
-SELECT name, pdb.agg('{"value_count": {"field": "name"}}') FROM badges WHERE tag_based GROUP BY name;
+SELECT name, pdb.agg('{"value_count": {"field": "name"}}') FROM badges WHERE name ||| 'Question' GROUP BY name;
 
 -- pdb.agg with GROUP BY (mvcc disabled)
-SELECT name, pdb.agg('{"value_count": {"field": "name"}}', false) FROM badges WHERE tag_based GROUP BY name;
+SELECT name, pdb.agg('{"value_count": {"field": "name"}}', false) FROM badges WHERE name ||| 'Question' GROUP BY name;

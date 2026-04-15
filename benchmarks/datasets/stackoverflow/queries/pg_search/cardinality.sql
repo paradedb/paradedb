8c66efe33579253054693ab8f780db366baa48b5
rjhallsted <rjhallsted@gmail.com>
2026-04-15 12:06:05 -0600

    Make tags a columnar field in the stack overflow index

--- benchmarks/datasets/stackoverflow/queries/pg_search/cardinality.sql
+++ benchmarks/datasets/stackoverflow/queries/pg_search/cardinality.sql
@@ -12,3 +12,14 @@ SELECT pdb.agg('{"value_count": {"field": "post_type_id"}}') FROM stackoverflow_
 
 -- pdb.agg without GROUP BY (mvcc disabled)
 SELECT pdb.agg('{"value_count": {"field": "post_type_id"}}', false) FROM stackoverflow_posts WHERE body ||| 'javascript';
+
+-- high-cardinality aggregate scan
+SELECT tags, COUNT(*), MIN(score), MAX(score), SUM(score) FROM stackoverflow_posts WHERE body ||| 'javascript' GROUP BY tags;
+
+-- high-cardinality aggregate scan using pdb.agg
+-- SELECT pdb.agg('{"value_count": {"field": "tags"}}'), MIN(score), MAX(score), SUM(score) FROM stackoverflow_posts WHERE body ||| 'javascript' AND id @@@ pdb.all() GROUP BY tags;
+SELECT tags, pdb.agg('{"value_count": {"field": "tags"}}') as count, pdb.agg('{"min": {"field": "score"}}') as min, pdb.agg('{"max": {"field": "score"}}') as max, pdb.agg('{"sum": {"field": "score"}}') as sum FROM stackoverflow_posts WHERE body ||| 'javascript' AND id @@@ pdb.all() GROUP BY tags;
+
+-- high-cardinality aggregate scan using pdb.agg (mvcc disabled)
+SELECT pdb.agg('{"value_count": {"field": "tags"}}', false), MIN(score), MAX(score), SUM(score) FROM stackoverflow_posts WHERE body ||| 'javascript' AND id @@@ pdb.all() GROUP BY tags;
+

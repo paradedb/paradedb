e53c2705436e6a902535e369afc28a2aab98c0fb
James Blackwood-Sewell <james.sewell@gmail.com>
2026-04-28 12:12:43 +1200

    Add warning when TOPK is used with ASC

--- pg_search/src/postgres/customscan/joinscan/planning.rs
+++ pg_search/src/postgres/customscan/joinscan/planning.rs
@@ -1815,6 +1815,12 @@ impl JoinSortExprKind {
                 if !output_rtis.contains(&source.scan_info.heap_rti) {
                     continue;
                 }
+                if direction.is_asc() {
+                    pgrx::warning!(
+                        "ORDER BY pdb.score(...) ASC returns the least relevant results first. \
+                         Did you mean ORDER BY pdb.score(...) DESC?"
+                    );
+                }
                 return Self::Resolved(OrderByInfo {
                     feature: OrderByFeature::Score {
                         rti: source.scan_info.heap_rti,

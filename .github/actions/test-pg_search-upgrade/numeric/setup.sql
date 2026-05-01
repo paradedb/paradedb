316ad88c6fdc134f689843c4974d98426f0fa1fe
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-05-01 09:35:42 -0500

    Include bounded numeric in test case

--- .github/actions/test-pg_search-upgrade/numeric/setup.sql
+++ .github/actions/test-pg_search-upgrade/numeric/setup.sql
@@ -1,9 +1,10 @@
 create table items (
     id bigserial,
-    number numeric
+    numeric64 numeric(15, 9),
+    numeric_bytes numeric
 );
 
 create index search_idx on items
-using bm25 (id, number) with (key_field='id');
+using bm25 (id, numeric64, numeric_bytes) with (key_field='id');
 
-insert into items (id, number) values (1, 0.09809809809809809);
+insert into items (id, numeric64, numeric_bytes) values (1, 0.098098098, 0.09809809809809809);

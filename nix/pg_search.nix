6e0eb26fa43e189745da16f1533f97439fd9e7f1
Stu Hood <stuhood@paradedb.com>
2026-05-05 12:13:24 -0700

    Backport of #4994 to `0.23.x`.

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -64,7 +64,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-0V/OHGj/ZkjdkXm6dW9L5cROTxRv9FxR1G1leEaIMwg=";
+  cargoHash = "sha256-yI3dTNWGiW/GSXdl9QA+2TT2Uo5c+Js1wvW9PaXvcBY=";
 
   inherit cargo-pgrx postgresql;
 

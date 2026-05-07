ca7b8e2400b5b9513d732c66bbd7daf6fb1c2fbf
Stu Hood <stuhood@gmail.com>
2026-05-06 20:51:42 -0700

    Increase memory limits to allow for HashJoins.

--- benchmarks/datasets/stackoverflow/queries/aggregate_join_count.sql
+++ benchmarks/datasets/stackoverflow/queries/aggregate_join_count.sql
@@ -14,7 +14,7 @@ JOIN comments c ON p.id = c.post_id
 WHERE p.body ||| 'code';
 
 -- DataFusion aggregate scan
-SET paradedb.enable_aggregate_custom_scan TO on; SELECT COUNT(*)
+SET work_mem TO '4GB'; SET paradedb.enable_aggregate_custom_scan TO on; SELECT COUNT(*)
 FROM stackoverflow_posts p
 JOIN comments c ON p.id = c.post_id
 WHERE p.body ||| 'code';

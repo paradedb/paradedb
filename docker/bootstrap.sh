5a235d557cb223de326e22133dc79c3c981803f4
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-06-08 11:49:33 -0500

    Reorganize bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -8,38 +8,7 @@ set -Eeuo pipefail
 
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
-
-# The `pg_cron` extension can only be installed in the `postgres` database, as per
-# our configuration in our Dockerfile. Therefore, we install it separately here.
-psql -d postgres -c "CREATE EXTENSION IF NOT EXISTS pg_cron;"
-
-# Always create a `paradedb` database, regardless of what $POSTGRES_DB is set to
-if [ "$POSTGRES_DB" != "paradedb" ]; then
-  echo "Creating default 'paradedb' database"
-  psql -d postgres -c "CREATE DATABASE paradedb;"
-fi
-
-# Load ParadeDB and third-party extensions into template1, paradedb, and $POSTGRES_DB
-# Creating extensions in template1 ensures that they are available in all new databases.
-for DB in template1 paradedb "$POSTGRES_DB"; do
-  echo "Loading ParadeDB extensions into $DB"
-  psql -d "$DB" <<-'EOSQL'
-    CREATE EXTENSION IF NOT EXISTS pg_search;
-    CREATE EXTENSION IF NOT EXISTS pg_ivm;
-    CREATE EXTENSION IF NOT EXISTS vector;
-    CREATE EXTENSION IF NOT EXISTS postgis;
-    CREATE EXTENSION IF NOT EXISTS postgis_topology;
-    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
-    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-EOSQL
-done
-
-# Add the `paradedb` schema to template1, paradedb, and $POSTGRES_DB
-for DB in template1 paradedb "$POSTGRES_DB"; do
-  echo "Adding 'paradedb' search_path to $DB"
-  psql -d "$DB" -c "ALTER DATABASE \"$DB\" SET search_path TO public,paradedb;"
-done
+PDB_TUNE=${PDB_TUNE:-true}
 
 container_memory_mb() {
   local bytes=""
@@ -89,8 +58,6 @@ container_cpu_count() {
   awk -v cpus="$cpus" -v visible_cpus="$(nproc)" 'BEGIN {cpus = cpus > visible_cpus ? visible_cpus : cpus; print (cpus < 1 ? 1 : cpus)}'
 }
 
-PDB_TUNE=${PDB_TUNE:-true}
-
 tune() {
   if [[ "$PDB_TUNE" == "false" ]]; then
     echo "Auto-tuning is disabled, skipping."
@@ -126,6 +93,38 @@ tune() {
     echo "# End ParadeDB tuning recommendations"
   } | tee -a "$PGDATA/postgresql.conf"
 }
+
+# The `pg_cron` extension can only be installed in the `postgres` database, as per
+# our configuration in our Dockerfile. Therefore, we install it separately here.
+psql -d postgres -c "CREATE EXTENSION IF NOT EXISTS pg_cron;"
+
+# Always create a `paradedb` database, regardless of what $POSTGRES_DB is set to
+if [ "$POSTGRES_DB" != "paradedb" ]; then
+  echo "Creating default 'paradedb' database"
+  psql -d postgres -c "CREATE DATABASE paradedb;"
+fi
+
+# Load ParadeDB and third-party extensions into template1, paradedb, and $POSTGRES_DB
+# Creating extensions in template1 ensures that they are available in all new databases.
+for DB in template1 paradedb "$POSTGRES_DB"; do
+  echo "Loading ParadeDB extensions into $DB"
+  psql -d "$DB" <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS pg_search;
+    CREATE EXTENSION IF NOT EXISTS pg_ivm;
+    CREATE EXTENSION IF NOT EXISTS vector;
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL
+done
+
+# Add the `paradedb` schema to template1, paradedb, and $POSTGRES_DB
+for DB in template1 paradedb "$POSTGRES_DB"; do
+  echo "Adding 'paradedb' search_path to $DB"
+  psql -d "$DB" -c "ALTER DATABASE \"$DB\" SET search_path TO public,paradedb;"
+done
 
 tune
 

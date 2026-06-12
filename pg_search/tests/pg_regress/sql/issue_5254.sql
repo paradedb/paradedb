ccefcc7f401a37970311b63700b0dd30459cdeca
yash <yasha4658@gmail.com>
2026-06-12 22:16:35 +0530

    add alias_typmod_in handler

---
+++ pg_search/tests/pg_regress/sql/issue_5254.sql
@@ -0,0 +1,22 @@
+\i common/common_setup.sql
+
+DROP TABLE IF EXISTS issue_5254;
+
+CREATE TABLE issue_5254 (
+    id SERIAL PRIMARY KEY,
+    bar TEXT NOT NULL
+);
+
+INSERT INTO issue_5254 (bar) VALUES ('alpha'), ('beta'), ('gamma');
+
+CREATE INDEX issue_5254_idx ON issue_5254
+    USING bm25 (id, (bar::pdb.literal), (bar::pdb.literal_normalized('alias=bar_lower')))
+    WITH (key_field = id);
+
+-- Before the fix, this failed with:
+-- ERROR: cannot execute INSERT in a read-only transaction
+BEGIN TRANSACTION READ ONLY;
+SELECT id, bar FROM issue_5254 WHERE bar::pdb.alias('bar_lower') IS NOT NULL ORDER BY id;
+COMMIT;
+
+DROP TABLE issue_5254;

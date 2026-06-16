afee3686f803137dfef5b7d3ce61570e792f356d
github-action-benchmark <github@users.noreply.github.com>
2026-06-16 21:18:31 +0000

    add pg_search wide-table.toml Performance - Other Metrics (customSmallerIsBetter) benchmark result for 660a874856091997f2f271887bf7cba371544032

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781644678417,
+  "lastUpdate": 1781644710916,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -35998,6 +35998,108 @@ window.BENCHMARK_DATA = {
             "value": 25.375,
             "unit": "median mem",
             "extra": "avg mem: 48.09497690035379, max mem: 82.9921875, count: 58226"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "660a874856091997f2f271887bf7cba371544032",
+          "message": "fix: Don't use shared threshold on top-k query over-reads (#5346)\n\n## What\nFixes an issue surfaced by\n[antithesis](https://paradedb.antithesis.com/report/qM4XfIcVDJoPGAFSZREiDm9U/LgjIW-gYHYB7ySu0KCJW4yvuptlwQ0xzzqLu6jnwa8Q.html?auth=v2.public.eyJzY29wZSI6eyJSZXBvcnRTY29wZVYxIjp7ImFzc2V0IjoiTGdqSVctZ1lIWUI3eVN1MEtDSlc0eXZ1cHRsd1EweHp6cUx1Nmpud2E4US5odG1sIiwicmVwb3J0X2lkIjoicU00WGZJY1ZESm9QR0FGU1pSRWlEbTlVIn19LCJuYmYiOiIyMDI2LTA2LTE1VDEyOjQ5OjAxLjk3Nzg0MzIwNloifTNl3Fuh9tFkyoFE9WoW_5_Vb01awmv9XdIbEEypN6sC7c-hUqnFhYs9F2UhY0czHmOhQjW-nnRu6koE3ZWiCAk#/run/13adf2ad41f558e789ff486f7f44f9d6-55-14/finding/342f29e442a6534057f60e56fb353695fc319247).\n\n## Why\nDuring parallelized top_k queries that encounter deleted tuples, we need\nto issue additional queries to fill the remaining slots. The use of a\nshared threshold was causing all tuples encountered by these additional\nqueries to be skipped as they would always be below the threshold.\n\n## How\nOn retries (any case where we are on a query number >1), don't use the\nshared threshold.\n\n## Tests\n- Added a test that repros the fast-field case of this.\n- All existing tests pass",
+          "timestamp": "2026-06-16T14:35:47-06:00",
+          "tree_id": "5afde2a7de640db244aa35a8a7801a026f0336f1",
+          "url": "https://github.com/paradedb/paradedb/commit/660a874856091997f2f271887bf7cba371544032"
+        },
+        "date": 1781644681135,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Background Merger - Primary - background_merging",
+            "value": 0,
+            "unit": "median background_merging",
+            "extra": "avg background_merging: 0.051473113110363296, max background_merging: 2.0, count: 58244"
+          },
+          {
+            "name": "Background Merger - Primary - cpu",
+            "value": 4.7524753,
+            "unit": "median cpu",
+            "extra": "avg cpu: 4.788564666460111, max cpu: 9.638554, count: 58244"
+          },
+          {
+            "name": "Background Merger - Primary - mem",
+            "value": 19.79296875,
+            "unit": "median mem",
+            "extra": "avg mem: 19.839067516611152, max mem: 19.90234375, count: 58244"
+          },
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 4.7524753,
+            "unit": "median cpu",
+            "extra": "avg cpu: 5.009381444542236, max cpu: 18.972332, count: 58244"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 45.1953125,
+            "unit": "median mem",
+            "extra": "avg mem: 42.41373990775874, max mem: 47.41015625, count: 58244"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 63576,
+            "unit": "median block_count",
+            "extra": "avg block_count: 63310.55705308701, max block_count: 63576.0, count: 58244"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 74,
+            "unit": "median segment_count",
+            "extra": "avg segment_count: 69.96605658951995, max segment_count: 105.0, count: 58244"
+          },
+          {
+            "name": "Single Insert - Primary - cpu",
+            "value": 4.745428,
+            "unit": "median cpu",
+            "extra": "avg cpu: 4.755159563322134, max cpu: 28.543112, count: 58244"
+          },
+          {
+            "name": "Single Insert - Primary - mem",
+            "value": 47.4609375,
+            "unit": "median mem",
+            "extra": "avg mem: 44.02213023015246, max mem: 49.328125, count: 58244"
+          },
+          {
+            "name": "Single Update - Primary - cpu",
+            "value": 4.7524753,
+            "unit": "median cpu",
+            "extra": "avg cpu: 5.26901012598716, max cpu: 33.15244, count: 58244"
+          },
+          {
+            "name": "Single Update - Primary - mem",
+            "value": 61.4453125,
+            "unit": "median mem",
+            "extra": "avg mem: 62.00769499862647, max mem: 116.265625, count: 58244"
+          },
+          {
+            "name": "Top K - Primary - cpu",
+            "value": 23.727139,
+            "unit": "median cpu",
+            "extra": "avg cpu: 22.49363353154047, max cpu: 33.51621, count: 58244"
+          },
+          {
+            "name": "Top K - Primary - mem",
+            "value": 25.46875,
+            "unit": "median mem",
+            "extra": "avg mem: 48.783264021830576, max mem: 83.0625, count: 58244"
           }
         ]
       }

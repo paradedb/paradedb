eee18a73ad55b0e3289806c177dafc81cae538de
github-action-benchmark <github@users.noreply.github.com>
2026-06-16 21:06:37 +0000

    add pg_search bulk-updates.toml Performance - Other Metrics (customSmallerIsBetter) benchmark result for 660a874856091997f2f271887bf7cba371544032

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781643967560,
+  "lastUpdate": 1781643997783,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -24110,6 +24110,66 @@ window.BENCHMARK_DATA = {
             "value": 131,
             "unit": "median segment_count",
             "extra": "avg segment_count: 134.0720820108963, max segment_count: 191.0, count: 58919"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "660a874856091997f2f271887bf7cba371544032",
+          "message": "fix: Don't use shared threshold on top-k query over-reads (#5346)\n\n## What\nFixes an issue surfaced by\n[antithesis](https://paradedb.antithesis.com/report/qM4XfIcVDJoPGAFSZREiDm9U/LgjIW-gYHYB7ySu0KCJW4yvuptlwQ0xzzqLu6jnwa8Q.html?auth=v2.public.eyJzY29wZSI6eyJSZXBvcnRTY29wZVYxIjp7ImFzc2V0IjoiTGdqSVctZ1lIWUI3eVN1MEtDSlc0eXZ1cHRsd1EweHp6cUx1Nmpud2E4US5odG1sIiwicmVwb3J0X2lkIjoicU00WGZJY1ZESm9QR0FGU1pSRWlEbTlVIn19LCJuYmYiOiIyMDI2LTA2LTE1VDEyOjQ5OjAxLjk3Nzg0MzIwNloifTNl3Fuh9tFkyoFE9WoW_5_Vb01awmv9XdIbEEypN6sC7c-hUqnFhYs9F2UhY0czHmOhQjW-nnRu6koE3ZWiCAk#/run/13adf2ad41f558e789ff486f7f44f9d6-55-14/finding/342f29e442a6534057f60e56fb353695fc319247).\n\n## Why\nDuring parallelized top_k queries that encounter deleted tuples, we need\nto issue additional queries to fill the remaining slots. The use of a\nshared threshold was causing all tuples encountered by these additional\nqueries to be skipped as they would always be below the threshold.\n\n## How\nOn retries (any case where we are on a query number >1), don't use the\nshared threshold.\n\n## Tests\n- Added a test that repros the fast-field case of this.\n- All existing tests pass",
+          "timestamp": "2026-06-16T14:35:47-06:00",
+          "tree_id": "5afde2a7de640db244aa35a8a7801a026f0336f1",
+          "url": "https://github.com/paradedb/paradedb/commit/660a874856091997f2f271887bf7cba371544032"
+        },
+        "date": 1781643969593,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Bulk Update - Primary - cpu",
+            "value": 18.944252,
+            "unit": "median cpu",
+            "extra": "avg cpu: 19.209153651842776, max cpu: 43.680485, count: 58923"
+          },
+          {
+            "name": "Bulk Update - Primary - mem",
+            "value": 96.60546875,
+            "unit": "median mem",
+            "extra": "avg mem: 96.54939596751268, max mem: 98.0703125, count: 58923"
+          },
+          {
+            "name": "Count Query - Primary - cpu",
+            "value": 23.656975,
+            "unit": "median cpu",
+            "extra": "avg cpu: 20.907171199613714, max cpu: 33.549675, count: 58923"
+          },
+          {
+            "name": "Count Query - Primary - mem",
+            "value": 41.09765625,
+            "unit": "median mem",
+            "extra": "avg mem: 40.53755052939854, max mem: 41.1796875, count: 58923"
+          },
+          {
+            "name": "Monitor Index Size - Primary - block_count",
+            "value": 37295,
+            "unit": "median block_count",
+            "extra": "avg block_count: 34104.27756563651, max block_count: 39313.0, count: 58923"
+          },
+          {
+            "name": "Monitor Index Size - Primary - segment_count",
+            "value": 130,
+            "unit": "median segment_count",
+            "extra": "avg segment_count: 134.11345315072214, max segment_count: 190.0, count: 58923"
           }
         ]
       }

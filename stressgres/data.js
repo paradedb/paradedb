1272cab30ae11f6bdc63bdd6edba7b3af5e4e8a6
github-action-benchmark <github@users.noreply.github.com>
2026-06-16 23:55:07 +0000

    add pg_search logical-replication.toml Performance - TPS (customBiggerIsBetter) benchmark result for 07b1f2f7d18a303c4fce95b5aa9f57301f9c8a95

--- stressgres/data.js
+++ stressgres/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781653461418,
+  "lastUpdate": 1781654106829,
   "repoUrl": "https://github.com/paradedb/paradedb",
   "entries": {
     "pg_search single-server.toml Performance - TPS": [
@@ -54298,6 +54298,54 @@ window.BENCHMARK_DATA = {
             "value": 430.39934661897144,
             "unit": "median tps",
             "extra": "avg tps: 418.74319786917914, max tps: 819.3707740196763, count: 113066"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wwoodal@paradedb.com",
+            "name": "Walter Woodall",
+            "username": "walter-woodall"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07b1f2f7d18a303c4fce95b5aa9f57301f9c8a95",
+          "message": "perf: apply ctid sorting by default (#5354)\n\nRefs: 5265\n\n# Ticket(s) Closed\n\n- Closes #\n\n## What\n\nApplied `ctid` sorting by default to give best performance when\nperforming visibility checks.\n\n## Why\n\nFor aggregate / scan queries of shuffled / non ctid sorted indexes the\nvisibility checking algorithm ends up accessing / releasing the same\nheap buffer multiple times.\n\nBy applying `ctid` sorting by default it minimizes the number of buffer\naccess for a given block number across batches. Only at the boundary of\nthe batch size could we access a buffer at the end of a batch and then\nbeginning of the next after this change.\n\n## How\n\n## Tests",
+          "timestamp": "2026-06-16T15:50:36-07:00",
+          "tree_id": "e5474984f21280323d39ee46cbc5febcec53033f",
+          "url": "https://github.com/paradedb/paradedb/commit/07b1f2f7d18a303c4fce95b5aa9f57301f9c8a95"
+        },
+        "date": 1781654076711,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Custom Scan - Subscriber - tps",
+            "value": 228.39957910755484,
+            "unit": "median tps",
+            "extra": "avg tps: 228.56569531914008, max tps: 249.11084349608439, count: 56524"
+          },
+          {
+            "name": "Index Only Scan - Subscriber - tps",
+            "value": 796.4237284579701,
+            "unit": "median tps",
+            "extra": "avg tps: 789.9939538921194, max tps: 1034.6991075779324, count: 56524"
+          },
+          {
+            "name": "Parallel Custom Scan - Subscriber - tps",
+            "value": 242.30137014658524,
+            "unit": "median tps",
+            "extra": "avg tps: 242.1228303353886, max tps: 267.2595169047658, count: 56524"
+          },
+          {
+            "name": "Top K - Subscriber - tps",
+            "value": 428.8446856045384,
+            "unit": "median tps",
+            "extra": "avg tps: 422.6526159312066, max tps: 823.6720637859399, count: 113048"
           }
         ]
       }

c86eb7e50c30c5d6e27dafb063324fa683a8d007
Stu Hood <stuhood@gmail.com>
2026-07-08 14:13:21 -0700

    Add comments.

--- benchmarks/datasets/stackoverflow/queries/top_k-score-desc-tiebreaker.sql
+++ benchmarks/datasets/stackoverflow/queries/top_k-score-desc-tiebreaker.sql
@@ -1,3 +1,10 @@
+-- Shape: TopK Score with Tiebreaker (Single Table, BM25)
+-- Description: BM25 score ordered DESC with a primary key tiebreaker and LIMIT 10.
+-- This tests that the Block-Max WAND optimization is able to trigger when a score
+-- is used as a prefix in the ORDER BY clause along with a tiebreaker.
+--
+-- Query Info (statistics from 1M dataset; larger datasets may have different values):
+-- - 'javascript' selectivity on stackoverflow_posts.body: ~4%
 SELECT *, pdb.score(id) FROM stackoverflow_posts WHERE body ||| 'javascript' ORDER BY pdb.score(id) DESC, id LIMIT 10;
 
 -- force single worker

b0209eaebf6c1ad4ed39aceef1a620ec6e147498
Stu Hood <stuhood@paradedb.com>
2026-07-09 14:39:15 -0700

    fix: Allow BMW to be used for compound `ORDER BY` with a score prefix (#5496)
    ## What
    
    Incorporate https://github.com/paradedb/tantivy/pull/167 to allow
    Block-Max WAND to be used for a compound `ORDER BY` with a score prefix.
    
    ## Why
    
    While Tantivy and Lucene generally lean on `Score` ordering itself being
    deterministic (by implicitly including the `DocAddress` during Top-K),
    this is insufficient for `pg_search` for a few reasons:
    1. when parallel workers or deeper query paths are used, Postgres will
    introduce additional sorts on the `Score`, which will be oblivious to
    the `DocAddress`
    2. the `DocAddress` for a tuple will change due to compaction and
    merging of segments
    
    ## Tests
    
    Benchmarks show [a ~2x speedup for
    `top_k-score-desc-tiebreaker`](https://github.com/paradedb/paradedb/pull/5496#pullrequestreview-4657963818),
    demonstrating the use of BMW.
    
    ---------
    
    Co-authored-by: paradedb-github-app[bot] <282009505+paradedb-github-app[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -64,7 +64,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-tQoddWgHHdbqX88ImexER7r5qKraoKHP/73wvVkMhUQ=";
+  cargoHash = "sha256-98nsCm43nVa67DI382QbOISZmDdtVeH6Djgspi9FQ6U=";
 
   inherit cargo-pgrx postgresql;
 

d60c1f83e25c107bcb1c3ac8922549189b336820
jj <71215065+junnjiee@users.noreply.github.com>
2026-07-25 23:45:26 +0800

    update regression test

--- pg_search/tests/pg_regress/sql/inet.sql
+++ pg_search/tests/pg_regress/sql/inet.sql
@@ -3,19 +3,18 @@ CREATE EXTENSION IF NOT EXISTS pg_search;
 SET paradedb.enable_aggregate_custom_scan TO on;
 
 DROP TABLE IF EXISTS tbl_inet;
-CREATE TABLE tbl_inet (id int, ip inet);
+CREATE TABLE tbl_inet (ip inet);
 CREATE INDEX idx_inet
     ON tbl_inet
-    USING bm25 (id, ip)
-    WITH (key_field = 'id');
-INSERT INTO tbl_inet (id, ip) VALUES
-    (1, '192.168.1.5/24'),
-    (2, '192.168.1.5/32'),
-    (3, '1.2.3.4'),
-    (4, '::ffff:1.2.3.4'),
-    (5, '255.255.255.255'),
-    (6, '::1'),
-    (7, NULL);
+    USING bm25 (ip)
+    WITH (key_field = 'ip');
+INSERT INTO tbl_inet (ip) VALUES
+    ('192.168.1.5/24'),
+    ('192.168.1.5/32'),
+    ('1.2.3.4'),
+    ('::ffff:1.2.3.4'),
+    ('255.255.255.255'),
+    ('::1');
 
 SELECT count(*) FROM tbl_inet WHERE ip @@@ '1.2.3.4';
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
@@ -25,36 +24,36 @@ SET paradedb.enable_custom_scan_without_operator TO on;
 SET enable_seqscan TO off;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT ip FROM tbl_inet WHERE id @@@ paradedb.all() AND ip = '192.168.1.5/24'::inet;
-SELECT ip FROM tbl_inet WHERE id @@@ paradedb.all() AND ip = '192.168.1.5/24'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip = '192.168.1.5/24'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip = '192.168.1.5/24'::inet;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip <> '1.2.3.4'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip <> '1.2.3.4'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip <> '1.2.3.4'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip <> '1.2.3.4'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip < '::1'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip < '::1'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip < '::1'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip < '::1'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip <= '::1'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip <= '::1'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip <= '::1'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip <= '::1'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip > '::1'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip > '::1'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip > '::1'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip > '::1'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip >= '::1'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip >= '::1'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip >= '::1'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip >= '::1'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip BETWEEN '192.168.1.5/24'::inet AND '::1'::inet;
-SELECT id FROM tbl_inet WHERE id @@@ paradedb.all() AND ip BETWEEN '192.168.1.5/24'::inet AND '::1'::inet ORDER BY id;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip BETWEEN '192.168.1.5/24'::inet AND '::1'::inet;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() AND ip BETWEEN '192.168.1.5/24'::inet AND '::1'::inet ORDER BY ip;
 
 EXPLAIN (FORMAT TEXT, COSTS OFF, TIMING OFF, VERBOSE)
-SELECT id, ip FROM tbl_inet WHERE id @@@ paradedb.all() ORDER BY ip ASC NULLS LAST LIMIT 6;
-SELECT id, ip FROM tbl_inet WHERE id @@@ paradedb.all() ORDER BY ip ASC NULLS LAST LIMIT 6;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() ORDER BY ip ASC LIMIT 4;
+SELECT ip FROM tbl_inet WHERE ip @@@ paradedb.all() ORDER BY ip ASC LIMIT 4;
 
 RESET paradedb.enable_custom_scan_without_operator;
 RESET enable_seqscan;

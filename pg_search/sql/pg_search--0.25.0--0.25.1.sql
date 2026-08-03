5431a8a1a6cd85d5b805ac8724bc24260c368206
Devdatta Talele <devtalele0@gmail.com>
2026-08-04 02:57:15 +0530

    chore: add term_search_query_input to 0.25.0 to 0.25.1 migration

--- pg_search/sql/pg_search--0.25.0--0.25.1.sql
+++ pg_search/sql/pg_search--0.25.0--0.25.1.sql
@@ -1,4 +1,16 @@
+<<<<<<< HEAD
 \echo Use "ALTER EXTENSION pg_search UPDATE TO '0.25.1'" to load this file. \quit
 
 -- 0.25.0 -> 0.25.1: no schema changes (epsilon GUC removal and the
 -- bounds_scope reloption are not schema objects).
+=======
+-- pg_search/src/api/operator/eqeqeq.rs:38
+-- pg_search::api::operator::eqeqeq::term_search_query_input
+CREATE  FUNCTION "term_search_query_input"(
+	"field" FieldName, /* FieldName */
+	"query" pdb.Query /* pdb :: Query */
+) RETURNS SearchQueryInput /* SearchQueryInput */
+IMMUTABLE STRICT PARALLEL SAFE
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'term_search_query_input_wrapper';
+>>>>>>> 7fd3046b2 (chore: add term_search_query_input to 0.25.0 to 0.25.1 migration)

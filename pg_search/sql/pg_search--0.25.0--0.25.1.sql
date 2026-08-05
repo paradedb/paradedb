18fbad52475c9e2f312fe40408425ae443f8257c
Devdatta Talele <devtalele0@gmail.com>
2026-08-05 12:10:47 +0530

    chore: clean up merge marker in 0.25.0 to 0.25.1 migration from rebase

--- pg_search/sql/pg_search--0.25.0--0.25.1.sql
+++ pg_search/sql/pg_search--0.25.0--0.25.1.sql
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 \echo Use "ALTER EXTENSION pg_search UPDATE TO '0.25.1'" to load this file. \quit
 
--- 0.25.0 -> 0.25.1: no schema changes (epsilon GUC removal and the
--- bounds_scope reloption are not schema objects).
-=======
 -- pg_search/src/api/operator/eqeqeq.rs:38
 -- pg_search::api::operator::eqeqeq::term_search_query_input
 CREATE  FUNCTION "term_search_query_input"(
@@ -13,4 +9,3 @@ CREATE  FUNCTION "term_search_query_input"(
 IMMUTABLE STRICT PARALLEL SAFE
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'term_search_query_input_wrapper';
->>>>>>> 7fd3046b2 (chore: add term_search_query_input to 0.25.0 to 0.25.1 migration)

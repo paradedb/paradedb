2a4d771acda7258fa10d8b480d725dc395855ed2
Devdatta Talele <devtalele0@gmail.com>
2026-08-06 12:47:11 +0530

    chore: restore 0.25.0 to 0.25.1 migration to upstream after v0.25.1 tag

--- pg_search/sql/pg_search--0.25.0--0.25.1.sql
+++ pg_search/sql/pg_search--0.25.0--0.25.1.sql
@@ -1,11 +1,4 @@
 \echo Use "ALTER EXTENSION pg_search UPDATE TO '0.25.1'" to load this file. \quit
 
--- pg_search/src/api/operator/eqeqeq.rs:38
--- pg_search::api::operator::eqeqeq::term_search_query_input
-CREATE  FUNCTION "term_search_query_input"(
-	"field" FieldName, /* FieldName */
-	"query" pdb.Query /* pdb :: Query */
-) RETURNS SearchQueryInput /* SearchQueryInput */
-IMMUTABLE STRICT PARALLEL SAFE
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'term_search_query_input_wrapper';
+-- 0.25.0 -> 0.25.1: no schema changes (epsilon GUC removal and the
+-- bounds_scope reloption are not schema objects).

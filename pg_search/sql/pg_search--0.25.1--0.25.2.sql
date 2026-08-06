12c184ffe336a1ef4c1b7f23b221c73e7418937e
Devdatta Talele <devtalele0@gmail.com>
2026-08-06 12:48:41 +0530

    chore: add sibling operator pg_externs to 0.25.1 to 0.25.2 migration

--- pg_search/sql/pg_search--0.25.1--0.25.2.sql
+++ pg_search/sql/pg_search--0.25.1--0.25.2.sql
@@ -9,3 +9,33 @@ CREATE  FUNCTION "term_search_query_input"(
 IMMUTABLE STRICT PARALLEL SAFE
 LANGUAGE c /* Rust */
 AS 'MODULE_PATHNAME', 'term_search_query_input_wrapper';
+
+-- pg_search/src/api/operator/ororor.rs:38
+-- pg_search::api::operator::ororor::match_disjunction_search_query_input
+CREATE  FUNCTION "match_disjunction_search_query_input"(
+	"field" FieldName, /* FieldName */
+	"query" pdb.Query /* pdb :: Query */
+) RETURNS SearchQueryInput /* SearchQueryInput */
+IMMUTABLE STRICT PARALLEL SAFE
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'match_disjunction_search_query_input_wrapper';
+
+-- pg_search/src/api/operator/andandand.rs:38
+-- pg_search::api::operator::andandand::match_conjunction_search_query_input
+CREATE  FUNCTION "match_conjunction_search_query_input"(
+	"field" FieldName, /* FieldName */
+	"query" pdb.Query /* pdb :: Query */
+) RETURNS SearchQueryInput /* SearchQueryInput */
+IMMUTABLE STRICT PARALLEL SAFE
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'match_conjunction_search_query_input_wrapper';
+
+-- pg_search/src/api/operator/hashhashhash.rs:38
+-- pg_search::api::operator::hashhashhash::phrase_search_query_input
+CREATE  FUNCTION "phrase_search_query_input"(
+	"field" FieldName, /* FieldName */
+	"query" pdb.Query /* pdb :: Query */
+) RETURNS SearchQueryInput /* SearchQueryInput */
+IMMUTABLE STRICT PARALLEL SAFE
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'phrase_search_query_input_wrapper';

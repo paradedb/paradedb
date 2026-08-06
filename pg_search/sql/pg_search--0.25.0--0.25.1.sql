4fef36a281a49279479536dbfae11b72874cc0ed
Devdatta Talele <devtalele0@gmail.com>
2026-08-06 12:47:11 +0530

    chore: restore 0.25.0 to 0.25.1 migration to upstream after v0.25.1 tag

--- pg_search/sql/pg_search--0.25.0--0.25.1.sql
+++ pg_search/sql/pg_search--0.25.0--0.25.1.sql
@@ -1,39 +1,4 @@
--- pg_search/src/api/operator/eqeqeq.rs:38
--- pg_search::api::operator::eqeqeq::term_search_query_input
-CREATE  FUNCTION "term_search_query_input"(
-	"field" FieldName, /* FieldName */
-	"query" pdb.Query /* pdb :: Query */
-) RETURNS SearchQueryInput /* SearchQueryInput */
-IMMUTABLE STRICT PARALLEL SAFE
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'term_search_query_input_wrapper';
+\echo Use "ALTER EXTENSION pg_search UPDATE TO '0.25.1'" to load this file. \quit
 
--- pg_search/src/api/operator/ororor.rs:38
--- pg_search::api::operator::ororor::match_disjunction_search_query_input
-CREATE  FUNCTION "match_disjunction_search_query_input"(
-	"field" FieldName, /* FieldName */
-	"query" pdb.Query /* pdb :: Query */
-) RETURNS SearchQueryInput /* SearchQueryInput */
-IMMUTABLE STRICT PARALLEL SAFE
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'match_disjunction_search_query_input_wrapper';
-
--- pg_search/src/api/operator/andandand.rs:38
--- pg_search::api::operator::andandand::match_conjunction_search_query_input
-CREATE  FUNCTION "match_conjunction_search_query_input"(
-	"field" FieldName, /* FieldName */
-	"query" pdb.Query /* pdb :: Query */
-) RETURNS SearchQueryInput /* SearchQueryInput */
-IMMUTABLE STRICT PARALLEL SAFE
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'match_conjunction_search_query_input_wrapper';
-
--- pg_search/src/api/operator/hashhashhash.rs:38
--- pg_search::api::operator::hashhashhash::phrase_search_query_input
-CREATE  FUNCTION "phrase_search_query_input"(
-	"field" FieldName, /* FieldName */
-	"query" pdb.Query /* pdb :: Query */
-) RETURNS SearchQueryInput /* SearchQueryInput */
-IMMUTABLE STRICT PARALLEL SAFE
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'phrase_search_query_input_wrapper';
+-- 0.25.0 -> 0.25.1: no schema changes (epsilon GUC removal and the
+-- bounds_scope reloption are not schema objects).

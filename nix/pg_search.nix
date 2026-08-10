edfe77d8d53eb3239e9fd835e388205850774bff
RJ Barman <rjhallsted@gmail.com>
2026-08-10 16:47:13 -0400

    chore: Bump df-d rev to use version with correctness fixes (#5919)
    ## What
    Bump the DatafusionDistributed rev. This rev includes plan-shape
    correctness fixes that we don't currently have.
    
    ---------
    
    Co-authored-by: paradedb-github-app[bot] <282009505+paradedb-github-app[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -65,7 +65,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-4uFOckvmexPeat1PaBh7dYNCcn4PBfT9Pq1bWjn+vbU=";
+  cargoHash = "sha256-cngqrxJDNdlQ0+01t9ZfsqtsrwYo4zjLjSoBuh6IO5Q=";
 
   inherit cargo-pgrx postgresql;
 

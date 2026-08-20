802585d3398c5f6f0384021feb29b3813c7e75b5
Stu Hood <stuhood@paradedb.com>
2026-08-19 19:03:30 -0700

    perf: Use an in-memory channel for self-loops in MPP (#6002)
    # Ticket(s) Closed
    
    - Closes #5332
    
    ## What
    
    Incorporates a new tag of `datafusion-distributed` which picks up
    https://github.com/datafusion-contrib/datafusion-distributed/pull/656,
    and switches to using it for self-loops.
    
    ## Why
    
    To avoid serialization costs when sending data in-process.
    
    ## Tests
    
    Causes some benchmark movement on small datasets, but no change on
    larger datasets.
    
    ---------
    
    Co-authored-by: paradedb-github-app[bot] <282009505+paradedb-github-app[bot]@users.noreply.github.com>

--- nix/pg_search.nix
+++ nix/pg_search.nix
@@ -65,7 +65,7 @@ buildPgrxExtension (finalAttrs: {
   # If maintainers forget to do so, Nix will throw an error message that begins
   # like this and then provides the correct new hash:
   # error: hash mismatch in fixed-output derivation '...'
-  cargoHash = "sha256-YilptQU9A3DE3n949pY0nxL/qEFaDPu/zumr6LFVuaw=";
+  cargoHash = "sha256-B4RShh7F26o+VRub7IMz8aHNTg1wpGxqnW1Wv7m1VTs=";
 
   inherit cargo-pgrx postgresql;
 

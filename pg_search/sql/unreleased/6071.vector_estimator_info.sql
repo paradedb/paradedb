803156202c033e42b77d9eaa20566889eb9c09c7
Ruchir Raj <ruchirraj24@gmail.com>
2026-08-31 21:49:42 +0530

    fix: normalize vector schema fragment

--- pg_search/sql/unreleased/6071.vector_estimator_info.sql
+++ pg_search/sql/unreleased/6071.vector_estimator_info.sql
@@ -16,26 +16,6 @@ STABLE PARALLEL UNSAFE
 LANGUAGE c
 AS 'MODULE_PATHNAME', 'vector_estimator_info_internal_wrapper';
 
-DROP FUNCTION IF EXISTS vector_info(regclass, text);
-CREATE  FUNCTION "vector_info"(
-	"index" regclass, /* PgRelation */
-	"field" TEXT /* String */
-) RETURNS TABLE (
-	"segno" TEXT,  /* String */
-	"vector_field" TEXT,  /* String */
-	"vector_format" TEXT,  /* String */
-	"vector_num_vectors" NUMERIC,  /* AnyNumeric */
-	"vector_num_centroids" NUMERIC,  /* Option < AnyNumeric > */
-	"vector_min_cluster_size" NUMERIC,  /* Option < AnyNumeric > */
-	"vector_max_cluster_size" NUMERIC,  /* Option < AnyNumeric > */
-	"vector_avg_cluster_size" double precision,  /* Option < f64 > */
-	"vector_empty_clusters" NUMERIC,  /* Option < AnyNumeric > */
-	"vector_total_memberships" NUMERIC,  /* Option < AnyNumeric > */
-	"quantized" bool,  /* bool */
-	"layers" INT[],  /* :: std :: option :: Option < Vec < i32 > > */
-	"bytes_per_row" INT,  /* Option < i32 > */
-	"format" INT  /* Option < i32 > */
-)
-STRICT
-LANGUAGE c /* Rust */
-AS 'MODULE_PATHNAME', 'vector_info_wrapper';
+DROP FUNCTION IF EXISTS vector_info(index regclass, field text);
+
+CREATE OR REPLACE FUNCTION vector_info(index regclass, field text) RETURNS TABLE(segno text, vector_field text, vector_format text, vector_num_vectors pg_catalog."numeric", vector_num_centroids pg_catalog."numeric", vector_min_cluster_size pg_catalog."numeric", vector_max_cluster_size pg_catalog."numeric", vector_avg_cluster_size pg_catalog.float8, vector_empty_clusters pg_catalog."numeric", vector_total_memberships pg_catalog."numeric", quantized bool, layers pg_catalog.int4[], bytes_per_row pg_catalog.int4, format pg_catalog.int4) AS 'MODULE_PATHNAME', 'vector_info_wrapper' LANGUAGE c STRICT;

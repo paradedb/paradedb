9516d82b99ca1b40330955f6100263a144c11609
Ming Ying <ming.ying.nyc@gmail.com>
2026-09-06 16:17:24 -0700

    refactor: expose search predicate containment on node pointers

--- pg_search/src/api/operator.rs
+++ pg_search/src/api/operator.rs
@@ -377,24 +377,6 @@ impl SearchPredicate {
         })
     }
 
-    pub(crate) unsafe fn contained_by(node: *mut pg_sys::Node) -> bool {
-        #[pg_guard]
-        unsafe extern "C-unwind" fn walker(
-            node: *mut pg_sys::Node,
-            context: *mut core::ffi::c_void,
-        ) -> bool {
-            if node.is_null() {
-                return false;
-            }
-            if SearchPredicate::from_node(node).is_some() {
-                return true;
-            }
-            pg_sys::expression_tree_walker(node, Some(walker), context)
-        }
-
-        walker(node, std::ptr::null_mut())
-    }
-
     pub(crate) unsafe fn into_opexpr(self) -> *mut pg_sys::OpExpr {
         let operator = pg_sys::make_opclause(
             anyelement_query_input_opoid(),

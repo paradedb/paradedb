fc6762507826a2af9a2f621898105637814a4ff4
rjhallsted <rjhallsted@gmail.com>
2026-09-12 10:16:00 +0200

    add window functions to generated_join_small prop test

--- tests/src/fixtures/querygen/mod.rs
+++ tests/src/fixtures/querygen/mod.rs
@@ -25,6 +25,7 @@ pub mod orderbygen;
 pub mod pagegen;
 pub mod pdbagggen;
 pub mod wheregen;
+pub mod windowgen;
 
 use std::fmt::{Debug, Write};
 use std::num::NonZeroUsize;
@@ -948,6 +949,7 @@ pub fn compare_plan_retrying(
     pool: &MutexObjectPool<PgConnection>,
     setup: &SetupScript,
     expected_any: &[&str],
+    forbidden_any: &[&str],
 ) -> CaseOutcome {
     use crate::fixtures::fault_grace::{RetryError, retry_transient, sql_attempt};
 
@@ -987,6 +989,15 @@ pub fn compare_plan_retrying(
         let expected_desc = expected_any.join(" or ");
         return fail(format!(
             "Query should use {expected_desc} but got plan: {plan_str}\nQuery: {bm25_query}"
+        ));
+    }
+
+    if let Some(forbidden) = forbidden_any
+        .iter()
+        .find(|forbidden| plan_str.contains(**forbidden))
+    {
+        return fail(format!(
+            "Query should not use {forbidden} but got plan: {plan_str}\nQuery: {bm25_query}"
         ));
     }
 

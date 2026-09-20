e9c2fdf3f29a02c0a3ee7fa037da6a25a320216b
Stu Hood <stuhood@gmail.com>
2026-09-19 20:18:07 -0700

    Fix query names, apply review feedback.

--- benchmarks/datasets/stackoverflow/queries/join_aggregate_date_histogram/hash_partitioned.sql
+++ benchmarks/datasets/stackoverflow/queries/join_aggregate_date_histogram/hash_partitioned.sql
@@ -1,4 +1,4 @@
--- TODO(https://github.com/paradedb/paradedb/issues/4082): Aggregate scan currently falls back to Postgres for date_trunc.
+-- FIXME: Aggregate scan currently falls back to Postgres for date_trunc. DATE(..) is supported, but switching to it would require adding a LIMIT due to higher cardinality.
 SET work_mem TO '4GB'; SET paradedb.enable_aggregate_custom_scan TO on; SELECT
     date_trunc('month', p.creation_date) AS month,
     COUNT(*)
--- benchmarks/datasets/stackoverflow/queries/join_aggregate_date_histogram/range_partitioned.sql
+++ benchmarks/datasets/stackoverflow/queries/join_aggregate_date_histogram/range_partitioned.sql
@@ -1,3 +1,4 @@
+-- FIXME: Aggregate scan currently falls back to Postgres for date_trunc. DATE(..) is supported, but switching to it would require adding a LIMIT due to higher cardinality.
 SET work_mem TO '4GB'; SET paradedb.enable_aggregate_custom_scan TO on; SET paradedb.enable_range_partitioned_join TO on; SELECT
     date_trunc('month', p.creation_date) AS month,
     COUNT(*)
